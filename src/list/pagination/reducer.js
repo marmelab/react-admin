e323fa9f1f46a54d38b02f5bd2a823e98203bec0
Francois Zaninotto <fzaninotto@gmail.com>
2016-07-07 12:10:30 +0200

    Changing sort order resets paginatino

--- src/list/pagination/reducer.js
+++ src/list/pagination/reducer.js
@@ -1,9 +1,6 @@
-import {
-    CRUD_NEXT_PAGE,
-    CRUD_PREV_PAGE,
-    CRUD_GOTO_PAGE,
-} from './actions';
+import { CRUD_NEXT_PAGE, CRUD_PREV_PAGE, CRUD_GOTO_PAGE } from './actions';
 import { CRUD_FETCH_LIST_SUCCESS } from '../data/actions';
+import { CRUD_SET_SORT } from '../sort/actions';
 
 const initialState = {
     page: 1,
@@ -19,6 +16,8 @@ export default (resource) => (previousState = initialState, { type, payload, met
     switch (type) {
     case CRUD_FETCH_LIST_SUCCESS:
         return { ...previousState, total: parseInt(payload.headers['content-range'].split('/').pop(), 10) };
+    case CRUD_SET_SORT:
+        return { ...previousState, page: 1 };
     case CRUD_NEXT_PAGE:
         if (previousState.page === nbPages) {
             throw new Error('Cannot got to next page: pagination already at last page');
