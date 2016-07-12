e45648e7bd24abacd7b9f4ad0b93ac0aef3f02ab
Francois Zaninotto <fzaninotto@gmail.com>
2016-07-12 23:22:56 +0200

    ToolbarGroup injects styles according to children type

--- src/button/SaveButton.js
+++ src/button/SaveButton.js
@@ -2,6 +2,14 @@ import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import ContentSave from 'material-ui/svg-icons/content/save';
 
-const SaveButton = () => <FlatButton type="submit" label="Save" icon={<ContentSave />} />;
+const SaveButton = () => <FlatButton
+    type="submit"
+    label="Save"
+    icon={<ContentSave />}
+    style={{
+        margin: '10px 24px',
+        position: 'relative',
+    }}
+/>;
 
 export default SaveButton;
