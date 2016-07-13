f70fe4075969c67675c04c204d3643368df0ebc1
Francois Zaninotto <fzaninotto@gmail.com>
2016-07-13 13:43:33 +0200

    Fix CSS glitch in datagrid buttons

--- src/components/material-ui/button/CreateButton.js
+++ src/components/material-ui/button/CreateButton.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 import FlatButton from 'material-ui/FlatButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
-const CreateButton = ({ basePath }) => <FlatButton label="Create" icon={<ContentAdd />} containerElement={<Link to={`${basePath}/create`} />} />;
+const CreateButton = ({ basePath }) => <FlatButton label="Create" icon={<ContentAdd />} containerElement={<Link to={`${basePath}/create`} />} style={{ overflow: 'inherit' }}/>;
 
 CreateButton.propTypes = {
     basePath: PropTypes.string.isRequired,
