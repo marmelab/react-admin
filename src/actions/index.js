b43bd8b73ae439b1af0f10f432a4008ba35f84d5
Francois Zaninotto <fzaninotto@gmail.com>
2016-07-27 12:24:40 +0200

    Simplify export of actions

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export dataActions from './dataActions';
+export fetchActions from './fetchActions';
+export notificationActions from './notificationActions';
+export paginationActions from './paginationActions';
+export referenceActions from './referenceActions';
+export sortActions from './sortActions';
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-/* actions */
-export dataActions from './actions/dataActions';
-export fetchActions from './actions/fetchActions';
-export notificationActions from './actions/notificationActions';
-export paginationActions from './actions/paginationActions';
-export referenceActions from './actions/referenceActions';
-export sortActions from './actions/sortActions';
-
-/* reducers */
+export * from './actions';
 export adminReducer from './reducer';
-
-/* rest */
 export simpleRest from './rest/simple';
-
-/* side effects */
 export crudSaga from './sideEffect/saga';
-
-/* utils */
 export fetchUtils from './util/fetch';
-
-/* routes */
 export CrudRoute from './CrudRoute';
