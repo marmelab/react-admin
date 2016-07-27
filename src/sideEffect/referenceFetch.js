fc1555c01efc079176ee911763c00700feb8ff1f
Francois Zaninotto <fzaninotto@gmail.com>
2016-07-27 10:43:56 +0200

    Fix issue in reference fetcher

--- src/sideEffect/referenceFetch.js
+++ src/sideEffect/referenceFetch.js
@@ -3,26 +3,39 @@ import { call, cancel, fork, put, take } from 'redux-saga/effects';
 import { CRUD_GET_ONE_REFERENCE } from '../actions/referenceActions';
 import { crudGetMany } from '../actions/dataActions';
 
-function *referenceFetch(resource, ids) {
+/**
+ * Example
+ *
+ * let id = {
+ *   posts: { 4: true, 7: true, 345: true },
+ *   authors: { 23: true, 47: true, 78: true },
+ * }
+ */
+let ids = {};
+
+// see http://yelouafi.github.io/redux-saga/docs/recipes/index.html#debouncing
+function *referenceFetch(resource) {
+    // combined with cancel(), this debounces the calls
     yield call(delay, 50);
     yield Object.keys(ids).map(reference =>
+        // FIXME fails when there is more than one relationship to fetch, because the fetch saga uses takeLatest()
         put(crudGetMany(resource, Object.keys(ids[reference])))
     );
+    ids = {};
 }
 
 export default function *watchReferenceFetch() {
     let task;
-    const ids = {};
     while (true) {
         const { payload } = yield take(CRUD_GET_ONE_REFERENCE);
         const { id, resource } = payload;
         if (!ids[resource]) {
             ids[resource] = {};
         }
-        ids[resource][id] = true; // poor man's UNIQUE
+        ids[resource][id] = true; // fast UNIQUE
         if (task) {
             yield cancel(task);
         }
-        task = yield fork(referenceFetch, resource, ids);
+        task = yield fork(referenceFetch, resource);
     }
 }
