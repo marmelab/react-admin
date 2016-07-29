7800995e8e3a69b2e098191c8938f27d0d083d66
Francois Zaninotto <fzaninotto@gmail.com>
2016-07-29 19:38:47 +0200

    small refactoring on data reducer

--- src/reducer/resource/data.js
+++ src/reducer/resource/data.js
@@ -8,32 +8,27 @@ import {
     CRUD_GET_MATCHING_SUCCESS,
 } from '../../actions/dataActions';
 
+const addRecords = (newRecords = [], oldRecords) => {
+    const newRecordsById = newRecords.reduce((prev, record) => {
+        prev[record.id] = record;
+        return prev;
+    }, {});
+    return {
+        ...oldRecords,
+        ...newRecordsById,
+    };
+};
+
 export default (resource) => (previousState = {}, { type, payload, meta }) => {
     if (!meta || meta.resource !== resource) {
         return previousState;
     }
     switch (type) {
-    case CRUD_GET_LIST_SUCCESS: {
-        const newRecords = {};
-        payload.data.forEach(record => {
-            newRecords[record.id] = record;
-        });
-        return {
-            ...previousState,
-            ...newRecords,
-        };
-    }
+    case CRUD_GET_LIST_SUCCESS:
+        return addRecords(payload.data, previousState);
     case CRUD_GET_MANY_SUCCESS:
-    case CRUD_GET_MATCHING_SUCCESS: {
-        const newRecords = {};
-        payload.forEach(record => {
-            newRecords[record.id] = record;
-        });
-        return {
-            ...previousState,
-            ...newRecords,
-        };
-    }
+    case CRUD_GET_MATCHING_SUCCESS:
+        return addRecords(payload, previousState);
     case CRUD_UPDATE: // replace record in edit form with edited one to avoid displaying previous record version
         return { ...previousState, [payload.id]: payload.data };
     case CRUD_GET_ONE_SUCCESS:
