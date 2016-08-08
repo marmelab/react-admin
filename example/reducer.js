0bc636f03a80e9e37e48627262b2efdb63aabfe7
Francois Zaninotto <fzaninotto@gmail.com>
2016-08-08 16:59:51 +0200

    remove old (useless) file

--- example/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { adminReducer } from 'admin-on-rest';
-import { routerReducer } from 'react-router-redux';
-
-export default combineReducers({
-    admin: adminReducer(['comments', 'posts']),
-    routing: routerReducer,
-});
