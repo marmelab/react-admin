c3f8bc9dea822c191879f6a5203a39606aee5892
Francois Zaninotto <fzaninotto@gmail.com>
2016-09-06 19:27:20 +0200

    Fix fetchUtil export
    Closes #14

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export * from './actions';
 export adminReducer from './reducer';
 export * from './rest';
 export crudSaga from './sideEffect/saga';
-export fetchUtils from './util/fetch';
+export * as fetchUtils from './util/fetch';
 export Admin from './Admin';
 export CrudRoute from './CrudRoute';
 export Resource from './Resource';
