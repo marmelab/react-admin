7c8e01d283b3ef73fadcb769a4a6b58f6beec197
Fabio Berta <fnberta@gmail.com>
2016-09-17 10:42:58 +0200

    Rename to DateField

--- src/mui/field/DateField.js
+++ src/mui/field/DateField.js
@@ -1,15 +1,15 @@
 import React, { PropTypes } from 'react';
 
-const TextField = ({ source, record, showTime = false }) => {
+const DateField = ({ source, record, showTime = false }) => {
     const date = record[source] instanceof Date ? record[source] : new Date(record[source]);
     return <span>{showTime ? date.toLocaleString() : date.toLocaleDateString()}</span>;
 };
 
-TextField.propTypes = {
+DateField.propTypes = {
     source: PropTypes.string.isRequired,
     label: PropTypes.string,
     record: PropTypes.object,
     showTime: PropTypes.bool,
 };
 
-export default TextField;
+export default DateField;
