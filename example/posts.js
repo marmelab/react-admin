73d575dd4783b7923c7e09c8ebca803d3d88e911
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-09-26 10:38:22 +0200

    Allow to specify field type

--- example/posts.js
+++ example/posts.js
@@ -63,6 +63,7 @@ export const PostCreate = (props) => (
         }}
     >
         <TextInput label="Title" source="title" />
+        <TextInput label="Password (if protected post)" source="password" type="password" />
         <TextInput label="Teaser" source="teaser" options={{ multiLine: true }} />
         <RichTextInput label="Body" source="body" />
         <DateInput label="Publication date" source="published_at" />
@@ -74,6 +75,7 @@ export const PostEdit = (props) => (
     <Edit title={PostTitle} {...props}>
         <DisabledInput label="Id" source="id" />
         <TextInput label="Title" source="title" validation={{ required: true }} />
+        <TextInput label="Password (if protected post)" source="password" type="password" />
         <TextInput label="Teaser" source="teaser" options={{ multiLine: true }} validation={{ required: true }} />
         <RichTextInput label="Body" source="body" validation={{ required: true }} />
         <DateInput label="Publication date" source="published_at" />
