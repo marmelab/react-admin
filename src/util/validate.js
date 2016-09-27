2b37b0b9ee8ecbdada7e40b9ea8ec09039c01efc
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-09-27 11:09:52 +0200

    Add core constraints tests

--- src/util/validate.js
+++ src/util/validate.js
@@ -1,16 +1,19 @@
+/* eslint-disable no-underscore-dangle */
 /* @link http://stackoverflow.com/questions/46155/validate-email-address-in-javascript */
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const isEmpty = (value) => (typeof value === 'undefined' || value === null || value === '');
+
 /* eslint-disable no-confusing-arrow */
 export const coreConstraints = {
-    required: (value) => (typeof value === 'undefined' || value === null || value === '') ? 'Required field' : null,
-    min: (value, _, min) => parseInt(value, 10) < min ? `Minimum value: ${min}` : null,
-    max: (value, _, max) => parseInt(value, 10) > max ? `Maximum value: ${max}` : null,
-    minLength: (value, _, min) => value.length < min ? `Minimum length: ${min}` : null,
-    maxLength: (value, _, max) => value.length > max ? `Maximum length: ${max}` : null,
-    email: (value) => EMAIL_REGEX.test(value) ? 'Must be a valid email' : null,
-    regex: (value, _, { pattern, message }) => pattern.test(value) ? message : null,
-    choices: (value, _, { list, message }) => list.indexOf(value) === -1 ? message : null,
+    required: (value) => isEmpty(value) ? 'Required field' : null,
+    min: (value, _, min) => !isEmpty(value) && (isNaN(parseInt(value, 10)) || parseInt(value, 10) < min) ? `Minimum value: ${min}` : null,
+    max: (value, _, max) => !isEmpty(value) && (isNaN(parseInt(value, 10)) || parseInt(value, 10) > max) ? `Maximum value: ${max}` : null,
+    minLength: (value, _, min) => isEmpty(value) || `${value}`.length < min ? `Minimum length: ${min}` : null,
+    maxLength: (value, _, max) => !isEmpty(value) && `${value}`.length > max ? `Maximum length: ${max}` : null,
+    email: (value) => !isEmpty(value) && !EMAIL_REGEX.test(value) ? 'Must be a valid email' : null,
+    regex: (value, _, { pattern, message }) => !isEmpty(value) && !pattern.test(value) ? message : null,
+    choices: (value, _, { list, message }) => !isEmpty(value) && list.indexOf(value) === -1 ? message : null,
     custom: (value, values, func) => func(value, values),
 };
 
@@ -25,13 +28,13 @@ export const getConstraintsFunction = (constraints) => (value, values) => Object
     .filter(constraintName => coreConstraints[constraintName])
     .map(constraintName => {
         const constraint = coreConstraints[constraintName];
-        constraint.name = constraintName;
+        constraint._name = constraintName; // .name does not exist on IE
         return constraint;
     })
-    .reduce((errors, constraint) => {
-        errors = [...errors, ...constraint(value, values, constraints[constraint.name])]
-        return errors;
-    }, [])
+    .reduce((errors, constraint) => [
+        ...errors,
+        ...constraint(value, values, constraints[constraint._name]),
+    ], [])
     .filter(error => error !== null);
 
 export const getConstraintsFunctionFromFunctionOrObject = (constraints) => {
--- src/util/validate.spec.js
+++ src/util/validate.spec.js
@@ -1,67 +1,120 @@
 import assert from 'assert';
-import validate from './validate';
+import { coreConstraints } from './validate';
 
-describe('Validator', () => {
-    it('should not return any error message if no constraint is given', () => {
-        assert.equal(validate({}, 'name', []), '');
-    });
+describe.only('Validator', () => {
+    describe('Core Constraints', () => {
+        it('.required should return error message if field is empty', () => {
+            assert.equal(coreConstraints.required(), 'Required field');
+            assert.equal(coreConstraints.required(''), 'Required field');
+            assert.equal(coreConstraints.required(null), 'Required field');
 
-    it('should apply all given constraints if several are given', () => {
-        const constraints = [
-            { required: true },
-            { min: 1 },
-        ];
-
-        assert.equal(validate({ rate: null }, 'rate', constraints), 'Required field');
-        assert.equal(validate({ rate: 0 }, 'rate', constraints), 'Minimum value: 1');
-    });
-
-    describe('Required Validator', () => {
-        it('should return error message if field is either null or empty', () => {
-            assert.equal(validate({}, 'name', [{ required: true }]), 'Required field');
-            assert.equal(validate({ name: null }, 'name', [{ required: true }]), 'Required field');
-            assert.equal(validate({ name: '' }, 'name', [{ required: true }]), 'Required field');
+            assert.equal(coreConstraints.required('foo'), null);
+            assert.equal(coreConstraints.required(12), null);
         });
 
-        it('should return empty string if field is not null', () => {
-            assert.equal(validate({ name: 0 }, 'name', [{ required: true }]), '');
-            assert.equal(validate({ name: 'Raphael' }, 'name', [{ required: true }]), '');
-        });
-    });
+        // coreConstraints.min(value, _, minimumValue)
+        it('.min should return an error message including min value if value is lower than given minimum', () => {
+            assert.equal(coreConstraints.min(undefined, {}, 1), null);
+            assert.equal(coreConstraints.min(null, {}, 1), null);
+            assert.equal(coreConstraints.min('', {}, 1), null);
+            assert.equal(coreConstraints.min(1, {}, 1), null);
+            assert.equal(coreConstraints.min(2, {}, 1), null);
+            assert.equal(coreConstraints.min(1.2, {}, 1), null);
+            assert.equal(coreConstraints.min('12', {}, 1), null);
+            assert.equal(coreConstraints.min('12aqd', {}, 1), null);
 
-    describe('Extrema', () => {
-        it('should return error message if field is lower than min', () => {
-            assert.equal(validate({ note: -1 }, 'note', [{ min: 0 }]), 'Minimum value: 0');
-        });
-
-        it('should return empty string if field is greater or equal to min', () => {
-            assert.equal(validate({ note: 0 }, 'name', [{ min: 0 }]), '');
-            assert.equal(validate({ note: 1 }, 'name', [{ min: 0 }]), '');
-        });
-
-        it('should return error message if field is greater than max', () => {
-            assert.equal(validate({ note: 15 }, 'note', [{ max: 10 }]), 'Maximum value: 10');
+            assert.equal(coreConstraints.min('foobar', {}, 1), 'Minimum value: 1');
+            assert.equal(coreConstraints.min(0, {}, 1), 'Minimum value: 1');
         });
 
-        it('should return empty string if field is lower or equal to max', () => {
-            assert.equal(validate({ note: 8 }, 'note', [{ max: 10 }]), '');
-            assert.equal(validate({ note: 10 }, 'note', [{ max: 10 }]), '');
+        // coreConstraints.max(value, _, minimumValue)
+        it('.min should return an error message including max value if value is greater than given maximum', () => {
+            assert.equal(coreConstraints.max(undefined, {}, 10), null);
+            assert.equal(coreConstraints.max(null, {}, 10), null);
+            assert.equal(coreConstraints.max('', {}, 10), null);
+            assert.equal(coreConstraints.max(0, {}, 10), null);
+            assert.equal(coreConstraints.max(1, {}, 10), null);
+            assert.equal(coreConstraints.max(1.2, {}, 10), null);
+            assert.equal(coreConstraints.max('2', {}, 10), null);
+            assert.equal(coreConstraints.max('2aqd', {}, 10), null);
+
+            assert.equal(coreConstraints.max('foobar', {}, 10), 'Maximum value: 10');
+            assert.equal(coreConstraints.max(12, {}, 10), 'Maximum value: 10');
         });
-    });
 
-    describe('Custom Function', () => {
-        it('should return custom function result', () => {
-            assert.equal(validate({ note: -1 }, 'note', [{ custom: () => 'Error' }]), 'Error');
-            assert.equal(validate({ note: -1 }, 'note', [{ custom: () => '' }]), '');
+        it('.minLength should return an error message including length if string is too short', () => {
+            assert.equal(coreConstraints.minLength(undefined, {}, 5), 'Minimum length: 5');
+            assert.equal(coreConstraints.minLength(null, {}, 5), 'Minimum length: 5');
+            assert.equal(coreConstraints.minLength(10, {}, 5), 'Minimum length: 5');
+            assert.equal(coreConstraints.minLength('', {}, 5), 'Minimum length: 5');
+            assert.equal(coreConstraints.minLength('tiny', {}, 5), 'Minimum length: 5');
+            assert.equal(coreConstraints.minLength('small', {}, 5), null);
+            assert.equal(coreConstraints.minLength('quite long', {}, 5), null);
         });
 
-        it('should pass field value and whole record as parameters', () => {
-            let passedArguments;
+        it('.maxLength should return an error message including length if string is too long', () => {
+            assert.equal(coreConstraints.maxLength(undefined, {}, 5), null);
+            assert.equal(coreConstraints.maxLength(null, {}, 5), null);
+            assert.equal(coreConstraints.maxLength(10, {}, 5), null);
+            assert.equal(coreConstraints.maxLength('', {}, 5), null);
+            assert.equal(coreConstraints.maxLength('tiny', {}, 5), null);
+            assert.equal(coreConstraints.maxLength('small', {}, 5), null);
+            assert.equal(coreConstraints.maxLength('quite long', {}, 5), 'Maximum length: 5');
+        });
 
-            const record = { react: 15, angular: 2 };
-            validate(record, 'react', [{ custom: (...args) => { passedArguments = args; } }]);
+        it('.email should test entered email address is a valid one', () => {
+            assert.equal(coreConstraints.email(undefined), null);
+            assert.equal(coreConstraints.email(null), null);
+            assert.equal(coreConstraints.email(10), 'Must be a valid email');
+            assert.equal(coreConstraints.email(''), null);
+            assert.equal(coreConstraints.email('foobar'), 'Must be a valid email');
+            assert.equal(coreConstraints.email('john.doe@mycompany.com'), null);
+        });
 
-            assert.deepEqual(passedArguments, [15, record]);
+        it('.regex should return given error message if given value does not match the passed regex', () => {
+            const regex = { pattern: /a{2,}$/, message: 'Must finish by (at least) two a' };
+
+            assert.equal(coreConstraints.regex(undefined, {}, regex), null);
+            assert.equal(coreConstraints.regex(null, {}, regex), null);
+            assert.equal(coreConstraints.regex(10, {}, regex), 'Must finish by (at least) two a');
+            assert.equal(coreConstraints.regex('hello', {}, regex), 'Must finish by (at least) two a');
+            assert.equal(coreConstraints.regex('Time for teaaa', {}, regex), null);
+            assert.equal(coreConstraints.regex('Time for tea!', {}, {
+                ...regex,
+                message: 'Another error message',
+            }), 'Another error message');
+        });
+
+        it('.choices should return given error message if given value is not in given list', () => {
+            const params = {
+                list: ['Koa', 'Express'],
+                message: 'Should be a Node.js framework',
+            };
+
+            assert.equal(coreConstraints.choices(undefined, {}, params), null);
+            assert.equal(coreConstraints.choices(null, {}, params), null);
+            assert.equal(coreConstraints.choices(10, {}, params), 'Should be a Node.js framework');
+            assert.equal(coreConstraints.choices('Symfony', {}, params), 'Should be a Node.js framework');
+            assert.equal(coreConstraints.choices('Express', {}, params), null);
+            assert.equal(coreConstraints.choices('Spip', {}, {
+                ...params,
+                message: 'Should be either Koa or Express',
+            }), 'Should be either Koa or Express');
+        });
+
+        describe('.custom', () => {
+            it('should return result of given function', () => {
+                assert.equal(coreConstraints.custom(1, {}, () => 'Error'), 'Error');
+                assert.equal(coreConstraints.custom(1, {}, () => ''), '');
+            });
+
+            it('should pass edited value and whole record values as arguments', () => {
+                let passedArguments;
+                const record = { react: 15, angular: 2 };
+
+                coreConstraints.custom('foo', record, (...args) => { passedArguments = args; });
+                assert.deepEqual(passedArguments, ['foo', record]);
+            });
         });
     });
 });
