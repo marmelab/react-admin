1dd9f0bc886a501ce015dfcf3bcbc947566641b4
Francois Zaninotto <fzaninotto@gmail.com>
2017-01-04 23:06:45 +0100

    Add double submission protection in Save button

--- src/mui/button/SaveButton.js
+++ src/mui/button/SaveButton.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import ContentSave from 'material-ui/svg-icons/content/save';
+import CircularProgress from 'material-ui/CircularProgress';
 
-const SaveButton = () => <RaisedButton
-    type="submit"
-    label="Save"
-    icon={<ContentSave />}
-    primary
-    style={{
-        margin: '10px 24px',
-        position: 'relative',
-    }}
-/>;
+class SaveButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            submitting: false,
+        };
+    }
+
+    handleClick = (e) => {
+        if (this.state.submitting) {
+            // prevent double submission
+            e.preventDefault();
+        }
+        this.setState({ submitting: true });
+    }
+
+    render() {
+        return <RaisedButton
+            type="submit"
+            label="Save"
+            icon={this.state.submitting ? <CircularProgress size={25} thickness={2} /> : <ContentSave />}
+            onClick={this.handleClick}
+            primary={!this.state.submitting}
+            style={{
+                margin: '10px 24px',
+                position: 'relative',
+            }}
+        />;
+    }
+}
 
 export default SaveButton;
