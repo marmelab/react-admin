e50c44a7519a3a2408823c1117cdcb8633053b32
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-01-11 12:18:45 +0100

    Introduce FileField

--- src/mui/input/FileInput.js
+++ src/mui/input/FileInput.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import Dropzone from 'react-dropzone';
-import CircularProgress from 'material-ui/CircularProgress';
+import FileField from '../field/FileField';
 
 const defaultStyle = {
     dropZone: {
@@ -14,72 +14,53 @@ const defaultStyle = {
         position: 'relative',
         display: 'inline-block',
     },
-    preview: {
-        margin: '.5rem',
-        maxHeight: '10rem',
-    },
-    loader: {
-        margin: 0,
-        display: 'block',
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)',
-    },
-    uploading: {
-        opacity: .3,
-    }
 };
 
 class FileInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            files: [],
+            files: props.record[props.source] || [],
         };
     }
 
-    onDrop = files => {
-        const loadingFiles = files.map(f => ({
-            data: f, // destructuring a File object remove almost all its properties
-            uploading: true,
-        }));
-        this.setState({ files: loadingFiles });
+    onDrop = (files) => {
+        const updatedFiles = [
+            ...this.state.files.map(this.props.pictureFormatter),
+            ...files.map(f => ({
+                alt: f.name,
+                src: f.preview,
+                uploading: true,
+            })),
+        ];
+console.table(updatedFiles, files);
+        this.setState({ files: updatedFiles });
 
-        this.props.onUpload(files, () => {
-            const loadedFiles = files.map(f => ({
-                data: f,
-                uploading: false,
-            }));
-            this.setState({ files: loadedFiles });
-        });
+        // this.props.onUpload(files, (uploadedFiles) => {
+        //     const allFiles = {
+        //         ...this.state.files.map(this.props.pictureF),
+        //         ...uploadedFiles,
+        //     };
+        //
+        //     this.setState({ files: uploadedFiles });
+        //
+        //     this.props.input.onChange(uploadedFiles.map((f) => {
+        //         const cleanedFile = { ...f };
+        //         delete cleanedFile.uploading;
+        //         return cleanedFile;
+        //     }));
+        // });
     }
 
     label() {
         if (this.props.multiple) {
-            return <p>Drop some files to upload, or click to select one.</p>;
+            return (
+                <p>Drop some files to upload, or click to select one.</p>
+            );
         }
 
-        return <p>Drop a file to upload, or click to select it.</p>;
-    }
-
-    renderFile = file => {
-        const uploadingStyle = file.uploading ? defaultStyle.uploading : {};
-        const style = {
-            ...defaultStyle.preview,
-            ...uploadingStyle,
-            ...this.props.style.preview,
-        };
-
         return (
-            <div style={defaultStyle.previewContainer} key={file.data.name}>
-                {file.uploading ? <CircularProgress style={defaultStyle.loader}/> : null}
-                <img
-                    alt={file.data.alt}
-                    src={file.data.preview}
-                    style={style}
-                />
-            </div>
+            <p>Drop a file to upload, or click to select it.</p>
         );
     }
 
@@ -90,6 +71,7 @@ class FileInput extends Component {
             maxSize,
             minSize,
             multiple,
+            pictureFormatter,
             style,
         } = this.props;
 
@@ -112,7 +94,16 @@ class FileInput extends Component {
                     {this.label()}
                 </Dropzone>
                 <div className="previews">
-                    {this.state.files.map(this.renderFile)}
+                    {this.state.files.map((f) => {
+                        const preparedFile = pictureFormatter(f);
+                        return (
+                            <FileField
+                                key={preparedFile.src}
+                                src={preparedFile.src}
+                                alt={preparedFile.alt}
+                            />
+                        );
+                    })}
                 </div>
             </div>
         );
@@ -123,17 +114,20 @@ FileInput.propTypes = {
     accept: PropTypes.string,
     disableClick: PropTypes.bool,
     includesLabel: PropTypes.bool.isRequired,
+    input: PropTypes.object,
     maxSize: PropTypes.number,
     minSize: PropTypes.number,
     multiple: PropTypes.bool,
     onUpload: PropTypes.func,
     style: PropTypes.object,
+    pictureFormatter: PropTypes.func,
 };
 
 FileInput.defaultProps = {
     includesLabel: false,
     multiple: false,
     onUpload: () => {},
+    pictureFormatter: p => p,
 };
 
 export default FileInput;
