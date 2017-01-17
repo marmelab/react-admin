de40d2d72672417b190617aae6e20ee5c5885633
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-01-17 16:53:02 +0100

    Decorate simple rest client for upload capabilities

--- example/app.js
+++ example/app.js
@@ -3,19 +3,20 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { Admin, Resource } from 'admin-on-rest';
-import RestClient from './RestClient';
 import { Delete } from 'admin-on-rest/mui';
+
+import UploadCapableRestClient from './uploadCapableRestClient';
 
 import { PostList, PostCreate, PostEdit, PostShow, PostIcon } from './posts';
 import { CommentList, CommentEdit, CommentCreate, CommentIcon } from './comments';
 
 import data from './data';
 
-const restClient = RestClient(data, true);
+const restClient = UploadCapableRestClient(data, true);
 const delayedRestClient = (type, resource, params) => new Promise(resolve => setTimeout(() => resolve(restClient(type, resource, params)), 1000));
 
 render(
-    <Admin restClient={restClient} title="Example Admin">
+    <Admin restClient={delayedRestClient} title="Example Admin">
         <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} show={PostShow} remove={Delete} icon={PostIcon} />
         <Resource name="comments" list={CommentList} create={CommentCreate} edit={CommentEdit} remove={Delete} icon={CommentIcon} />
     </Admin>,
---
+++ example/uploadCapableRestClient.js
@@ -0,0 +1,31 @@
+import jsonRestClient from 'aor-json-rest-client';
+
+const convertFileToBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+});
+
+const addUploadCapabilities = requestHandler => (type, resource, params) => {
+    if (type === 'UPDATE' && resource === 'posts') {
+        if (params.data.pictures && params.data.pictures.length) {
+            return Promise.all(params.data.pictures.map(convertFileToBase64))
+                .then(base64Pictures => requestHandler(type, resource, {
+                    ...params,
+                    data: {
+                        ...params.data,
+                        pictures: base64Pictures.map((base64, index) => ({
+                            src: base64,
+                            title: `${params.data.title} - Photo ${index}`,
+                        })),
+                    },
+                }));
+        }
+    }
+
+    return requestHandler(type, resource, params);
+};
+
+export default (data, loggingEnabled = false) => addUploadCapabilities(jsonRestClient(data, loggingEnabled));
