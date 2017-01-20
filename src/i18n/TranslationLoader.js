7923f94cdbccedf037b1dec461ff63c6128f9629
Julien <julien@marmelab.com>
2017-01-20 15:53:01 +0100

    Add TranslationLoader test

--- src/i18n/TranslationLoader.js
+++ src/i18n/TranslationLoader.js
@@ -6,7 +6,7 @@ export const resolveBrowserLocale = () => {
     // from http://blog.ksol.fr/user-locale-detection-browser-javascript/
     // Rely on the window.navigator object to determine user locale
     const { language, browserLanguage, userLanguage } = window.navigator;
-    return (language || browserLanguage || userLanguage).split('-')[0];
+    return (language || browserLanguage || userLanguage || DEFAULT_LOCALE).split('-')[0];
 };
 
 export const loadTranslations = locale => (
