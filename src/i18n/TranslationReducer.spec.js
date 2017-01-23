67177c0fe64a853834aab4bd87446e4b3f4001ed
Julien <julien@marmelab.com>
2017-01-23 18:25:06 +0100

    Typo fix

--- src/i18n/TranslationReducer.spec.js
+++ src/i18n/TranslationReducer.spec.js
@@ -4,7 +4,7 @@ import reducer from './TranslationReducer';
 import { DEFAULT_LOCALE } from './index';
 
 describe('TranslationReducer', () => {
-    it('should return DEFAULT_LOCALE by DEFAULT_LOCALE', () => {
+    it('should return DEFAULT_LOCALE by default', () => {
         assert.equal(DEFAULT_LOCALE, reducer()(undefined, {}));
     });
 });
