09ca0a448cb4343f152095a5b862b3ff59764eb3
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-01-23 10:27:27 +0100

    Add documentation about decorating REST client

--- example/app.js
+++ example/app.js
@@ -7,7 +7,7 @@ import jsonRestClient from 'aor-json-rest-client';
 import { Delete } from 'admin-on-rest/mui';
 import frenchMessages from 'aor-language-french';
 
-import UploadCapableRestClient from './uploadCapableRestClient';
+import addUploadFeature from './addUploadFeature';
 
 import { PostList, PostCreate, PostEdit, PostShow, PostIcon } from './posts';
 import { CommentList, CommentEdit, CommentCreate, CommentIcon } from './comments';
@@ -20,8 +20,9 @@ const messages = {
     en: { ...englishMessages, ...customMessages.en },
 };
 
-const restClient = UploadCapableRestClient(data);
-const delayedRestClient = (type, resource, params) => new Promise(resolve => setTimeout(() => resolve(restClient(type, resource, params)), 1000));
+const restClient = jsonRestClient(data, true);
+const uploadCapableClient = addUploadFeature(restClient);
+const delayedRestClient = (type, resource, params) => new Promise(resolve => setTimeout(() => resolve(uploadCapableClient(type, resource, params)), 1000));
 
 render(
     <Admin restClient={delayedRestClient} title="Example Admin" locale={resolveBrowserLocale()} messages={messages}>
