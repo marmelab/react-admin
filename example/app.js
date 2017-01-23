7188c8245b8da87b8d6dfa7f63b79a8315fbf33c
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-01-23 10:27:27 +0100

    Add documentation about decorating REST client

--- example/app.js
+++ example/app.js
@@ -4,21 +4,23 @@ import { render } from 'react-dom';
 
 import { Admin, Resource } from 'admin-on-rest';
 import { Delete } from 'admin-on-rest/mui';
+import jsonRestClient from 'aor-json-rest-client';
 
-import UploadCapableRestClient from './uploadCapableRestClient';
+import addUploadFeature from './addUploadFeature';
 
 import { PostList, PostCreate, PostEdit, PostShow, PostIcon } from './posts';
 import { CommentList, CommentEdit, CommentCreate, CommentIcon } from './comments';
 
 import data from './data';
 
-const restClient = UploadCapableRestClient(data);
-const delayedRestClient = (type, resource, params) => new Promise(resolve => setTimeout(() => resolve(restClient(type, resource, params)), 1000));
+const restClient = jsonRestClient(data, true);
+const uploadCapableClient = addUploadFeature(restClient);
+const delayedRestClient = (type, resource, params) => new Promise(resolve => setTimeout(() => resolve(uploadCapableClient(type, resource, params)), 1000));
 
 render(
     <Admin restClient={delayedRestClient} title="Example Admin">
         <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} show={PostShow} remove={Delete} icon={PostIcon} />
         <Resource name="comments" list={CommentList} create={CommentCreate} edit={CommentEdit} remove={Delete} icon={CommentIcon} />
     </Admin>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
