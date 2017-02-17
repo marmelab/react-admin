18f68a27190965dc1e1495c957e27e4537514e75
Francois Zaninotto <fzaninotto@gmail.com>
2017-02-17 15:18:55 +0100

    Add ability to toggle sidebar

--- src/mui/layout/AppBar.js
+++ src/mui/layout/AppBar.js
@@ -4,10 +4,10 @@ import pure from 'recompose/pure';
 import MuiAppBar from 'material-ui/AppBar';
 import CircularProgress from 'material-ui/CircularProgress';
 
-const AppBar = ({ title, isLoading }) => {
+const AppBar = ({ title, isLoading, onLeftIconButtonTouchTap }) => {
     const Title = <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>{title}</Link>;
     const RightElement = isLoading ? <CircularProgress color="#fff" size={30} thickness={2} style={{ margin: 8 }} /> : <span />;
-    return <MuiAppBar title={Title} iconElementRight={RightElement} />;
+    return <MuiAppBar title={Title} iconElementRight={RightElement} onLeftIconButtonTouchTap={onLeftIconButtonTouchTap} />;
 };
 
 AppBar.propTypes = {
--- src/mui/layout/Layout.js
+++ src/mui/layout/Layout.js
@@ -1,9 +1,11 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import withWidth, { SMALL } from 'material-ui/utils/withWidth';
 import autoprefixer from 'material-ui/utils/autoprefixer';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import compose from 'recompose/compose';
 import AppBar from './AppBar';
 import Notification from './Notification';
 import Menu from './Menu';
@@ -11,22 +13,69 @@ import defaultTheme from '../defaultTheme';
 
 injectTapEventPlugin();
 
-const Layout = ({ isLoading, children, route, title, theme, logout }) => {
-    const muiTheme = getMuiTheme(theme);
-    const prefix = autoprefixer(muiTheme);
-    return (
-        <MuiThemeProvider muiTheme={muiTheme}>
-            <div style={prefix({ display: 'flex', flexDirection: 'column', minHeight: '100vh' })}>
-                <AppBar title={title} isLoading={isLoading} />
-                <div className="body" style={prefix({ display: 'flex', flex: '1', backgroundColor: '#edecec' })}>
-                    <div style={prefix({ flex: 1 })}>{children}</div>
-                    <Menu resources={route.resources} logout={logout} />
-                </div>
-                <Notification />
-            </div>
-        </MuiThemeProvider>
-    );
+const styles = {
+    mainWithMargin: {
+        marginLeft: 256,
+        transition: 'margin-left 0.2s',
+    },
+    main: {
+        transition: 'margin-left 0.2s',
+    },
 };
+
+class Layout extends Component {
+    state = {
+        navDrawerOpen: true,
+    };
+
+    componentWillMount() {
+        this.hideSidebarIfNeeded(this.props.width);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.hideSidebarIfNeeded(nextProps.width);
+    }
+
+    toggleSidebar = () => {
+        this.setState({
+            navDrawerOpen: !this.state.navDrawerOpen,
+        });
+    };
+
+    navHiddenBySmallScreen = false;
+
+    hideSidebarIfNeeded(width) {
+        if (width === SMALL) {
+            if (!this.navHiddenBySmallScreen) {
+                this.setState({
+                    navDrawerOpen: false,
+                });
+                this.navHiddenBySmallScreen = true;
+            }
+        } else {
+            this.navHiddenBySmallScreen = false;
+        }
+    }
+
+    render() {
+        const { isLoading, children, route, title, theme, logout, width } = this.props;
+        const muiTheme = getMuiTheme(theme);
+        const prefix = autoprefixer(muiTheme);
+        const { navDrawerOpen } = this.state;
+        return (
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <div style={prefix({ display: 'flex', flexDirection: 'column', minHeight: '100vh' })}>
+                    <AppBar title={title} isLoading={isLoading} onLeftIconButtonTouchTap={this.toggleSidebar} />
+                    <div className="body" style={prefix({ flex: '1', backgroundColor: '#edecec' })}>
+                        <div style={navDrawerOpen && width !== SMALL ? styles.mainWithMargin : styles.main}>{children}</div>
+                        <Menu resources={route.resources} logout={logout} open={navDrawerOpen} width={width} toggleSidebar={this.toggleSidebar} />
+                    </div>
+                    <Notification />
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
 
 Layout.propTypes = {
     isLoading: PropTypes.bool.isRequired,
@@ -35,6 +84,7 @@ Layout.propTypes = {
     route: PropTypes.object.isRequired,
     title: PropTypes.string.isRequired,
     theme: PropTypes.object.isRequired,
+    width: PropTypes.number.isRequired,
 };
 
 Layout.defaultProps = {
@@ -45,6 +95,9 @@ function mapStateToProps(state) {
     return { isLoading: state.admin.loading > 0 };
 }
 
-export default connect(
-  mapStateToProps,
-)(Layout);
+const enhance = compose(
+    connect(mapStateToProps),
+    withWidth(),
+);
+
+export default enhance(Layout);
