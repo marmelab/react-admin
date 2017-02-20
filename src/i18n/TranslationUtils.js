cd6958099e7ceaf908baa1d8ef5e5f3029f49e0e
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-02-20 15:55:33 +0100

    Allow to specify default locale

--- src/i18n/TranslationUtils.js
+++ src/i18n/TranslationUtils.js
@@ -1,8 +1,8 @@
 import { DEFAULT_LOCALE } from './index';
 
-export const resolveBrowserLocale = () => {
+export const resolveBrowserLocale = (defaultLocale = DEFAULT_LOCALE) => {
     // from http://blog.ksol.fr/user-locale-detection-browser-javascript/
     // Rely on the window.navigator object to determine user locale
     const { language, browserLanguage, userLanguage } = window.navigator;
-    return (language || browserLanguage || userLanguage || DEFAULT_LOCALE).split('-')[0];
+    return (language || browserLanguage || userLanguage || defaultLocale).split('-')[0];
 };
