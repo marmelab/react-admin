d53bc54b01c88535096c938993fe35ab8b377156
thedersen <thedersen@gmail.com>
2017-03-06 15:10:19 +0100

    Revert change to crudGetList

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -13,10 +13,10 @@ export const CRUD_GET_LIST_LOADING = 'CRUD_GET_LIST_LOADING';
 export const CRUD_GET_LIST_FAILURE = 'CRUD_GET_LIST_FAILURE';
 export const CRUD_GET_LIST_SUCCESS = 'CRUD_GET_LIST_SUCCESS';
 
-export const crudGetList = (resource, pagination, sort, filter, cancelPrevious = true) => ({
+export const crudGetList = (resource, pagination, sort, filter) => ({
     type: CRUD_GET_LIST,
     payload: { pagination, sort, filter },
-    meta: { resource, fetch: GET_LIST, cancelPrevious },
+    meta: { resource, fetch: GET_LIST, cancelPrevious: true },
 });
 
 export const CRUD_GET_ONE = 'CRUD_GET_ONE';
