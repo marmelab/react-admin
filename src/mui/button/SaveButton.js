664e30c81fdd8e92a1e7428e943ea8003d652d7d
Francois Zaninotto <fzaninotto@gmail.com>
2017-03-08 09:57:27 +0100

    Make edition toolbar responsive

--- src/mui/button/SaveButton.js
+++ src/mui/button/SaveButton.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import ContentSave from 'material-ui/svg-icons/content/save';
 import CircularProgress from 'material-ui/CircularProgress';
 import translate from '../../i18n/translate';
@@ -15,23 +16,37 @@ class SaveButton extends Component {
     }
 
     render() {
-        const { saving, label = 'aor.action.save', translate } = this.props;
-        return <RaisedButton
-            type="submit"
-            label={label && translate(label)}
-            icon={saving ? <CircularProgress size={25} thickness={2} /> : <ContentSave />}
-            onClick={this.handleClick}
-            primary={!saving}
-            style={{
-                margin: '10px 24px',
-                position: 'relative',
-            }}
-        />;
+        const { saving, label = 'aor.action.save', raised = true, translate } = this.props;
+        return raised
+            ? <RaisedButton
+                type="submit"
+                label={label && translate(label)}
+                icon={saving ? <CircularProgress size={25} thickness={2} /> : <ContentSave />}
+                onClick={this.handleClick}
+                primary={!saving}
+                style={{
+                    margin: '10px 24px',
+                    position: 'relative',
+                }}
+            />
+            : <FlatButton
+                type="submit"
+                label={label && translate(label)}
+                icon={saving ? <CircularProgress size={25} thickness={2} /> : <ContentSave />}
+                onClick={this.handleClick}
+                primary={!saving}
+                style={{
+                    margin: '10px 24px',
+                    position: 'relative',
+                }}
+            />
+        ;
     }
 }
 
 SaveButton.propTypes = {
     label: PropTypes.string,
+    raised: PropTypes.bool,
     saving: PropTypes.bool,
     translate: PropTypes.func.isRequired,
 };
