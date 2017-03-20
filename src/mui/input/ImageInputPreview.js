26ff7b3522980e5e49367f5d39c28910588ff3e2
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-03-20 11:06:08 +0100

    Improve design of image input preview delete button

--- src/mui/input/ImageInputPreview.js
+++ src/mui/input/ImageInputPreview.js
@@ -1,8 +1,7 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import FlatButton from 'material-ui/FlatButton';
-import { red600 } from 'material-ui/styles/colors';
+import { pinkA200 } from 'material-ui/styles/colors';
 import RemoveCircle from 'material-ui/svg-icons/content/remove-circle';
-
 
 const styles = {
     container: {
@@ -14,19 +13,50 @@ const styles = {
         top: '0.5rem',
         right: '0.5rem',
         minWidth: '2rem',
+        opacity: 0,
+    },
+    removeButtonHovered: {
+        opacity: 1,
     },
 };
 
-export const ImageInputPreview = ({ children, onRemove }) => (
-    <div style={styles.container}>
-        <FlatButton
-            style={styles.removeButton}
-            icon={<RemoveCircle color={red600} />}
-            onClick={onRemove}
-        />
-        {children}
-    </div>
-);
+export class ImageInputPreview extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hovered: false,
+        };
+    }
+
+    onMouseOut = () => this.setState({ hovered: false });
+    onMouseOver = () => this.setState({ hovered: true });
+
+    render() {
+        const { children, onRemove } = this.props;
+
+        const removeButtonStyle = this.state.hovered ? {
+            ...styles.removeButton,
+            ...styles.removeButtonHovered,
+        } : styles.removeButton;
+
+        return (
+            <div
+                onMouseOver={this.onMouseOver}
+                onMouseOut={this.onMouseOut}
+                style={styles.container}
+            >
+                <FlatButton
+                    style={removeButtonStyle}
+                    icon={<RemoveCircle
+                        color={pinkA200}
+                    />}
+                    onClick={onRemove}
+                />
+                {children}
+            </div>
+        );
+    }
+}
 
 ImageInputPreview.propTypes = {
     children: PropTypes.element.isRequired,
