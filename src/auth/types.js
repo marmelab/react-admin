7055e8b9d3e88a05d97aa7e1d3043f7128ad2058
Anton Kulakov <kulakowka@gmail.com>
2017-04-05 23:35:15 +0300

    Added prefixes to “auth” and “rest” action types

--- src/auth/types.js
+++ src/auth/types.js
@@ -1,4 +1,4 @@
-export const AUTH_LOGIN = 'AUTH_LOGIN';
-export const AUTH_CHECK = 'AUTH_CHECK';
-export const AUTH_ERROR = 'AUTH_ERROR';
-export const AUTH_LOGOUT = 'AUTH_LOGOUT';
+export const AUTH_LOGIN = 'admin-on-rest/AUTH_LOGIN';
+export const AUTH_CHECK = 'admin-on-rest/AUTH_CHECK';
+export const AUTH_ERROR = 'admin-on-rest/AUTH_ERROR';
+export const AUTH_LOGOUT = 'admin-on-rest/AUTH_LOGOUT';
--- src/rest/types.js
+++ src/rest/types.js
@@ -1,7 +1,7 @@
-export const GET_LIST = 'GET_LIST';
-export const GET_ONE = 'GET_ONE';
-export const GET_MANY = 'GET_MANY';
-export const GET_MANY_REFERENCE = 'GET_MANY_REFERENCE';
-export const CREATE = 'CREATE';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
+export const GET_LIST = 'admin-on-rest/GET_LIST';
+export const GET_ONE = 'admin-on-rest/GET_ONE';
+export const GET_MANY = 'admin-on-rest/GET_MANY';
+export const GET_MANY_REFERENCE = 'admin-on-rest/GET_MANY_REFERENCE';
+export const CREATE = 'admin-on-rest/CREATE';
+export const UPDATE = 'admin-on-rest/UPDATE';
+export const DELETE = 'admin-on-rest/DELETE';
