edd2d5eb7715dbfecb6f54bb25e4f25231d97da3
Dylan Demougin <dylandemougin@Mac-mini-de-Gildas.local>
2017-04-14 10:57:40 +0200

    Add one test into create page

---
+++ e2e/pages/ShowPage.js
@@ -0,0 +1,29 @@
+import { By, until } from 'selenium-webdriver';
+
+module.exports = (url) => (driver) => ({
+    elements: {
+        appLoader: By.css('.app-loader'),
+        field: (name) => By.css(`.aor-field-${name} > div > span`),
+    },
+
+    navigate() {
+        driver.navigate().to(url);
+        return this.waitUntilVisible();
+    },
+
+    waitUntilVisible() {
+        return driver.wait(until.elementLocated(this.elements.field('title')));
+    },
+
+    waitUntilDataLoaded() {
+        let continued = true;
+        return driver.wait(until.elementLocated(this.elements.appLoader), 400)
+            .catch(() => continued = false) // no loader - we're on the same page !
+            .then(() => continued ? driver.wait(until.stalenessOf(driver.findElement(this.elements.appLoader))) : true)
+            .then(() => driver.sleep(100)); // let some time to redraw
+    },
+
+    getValue(name) {
+        return driver.findElement(this.elements.field(name)).getText();
+    },
+});
--- e2e/tests/create.js
+++ e2e/tests/create.js
@@ -3,10 +3,12 @@ import { By, until } from 'selenium-webdriver';
 import driver from '../chromeDriver';
 import createPageFactory from '../pages/CreatePage';
 import deletePageFactory from '../pages/DeletePage';
+import showPageFactory from '../pages/ShowPage';
 
 describe('Create Page', () => {
     const CreatePage = createPageFactory('http://localhost:8083/#posts/create')(driver);
     const DeletePage = deletePageFactory('http://localhost:8083/#posts/14/delete')(driver);
+    const ShowPage = showPageFactory('http://localhost:8083/#posts/14/show')(driver);
 
     beforeEach(async () => await CreatePage.navigate());
     afterEach(async () => {
@@ -30,5 +32,24 @@ describe('Create Page', () => {
         await CreatePage.setValues(values, 'Lorem Ipsum');
         await CreatePage.submit();
         assert.equal(await driver.getCurrentUrl(), 'http://localhost:8083/#/posts/14');
+    });
+
+    it('should give good values to show page', async() => {
+        const values = [
+            {
+                type: 'input',
+                name: 'title',
+                value: 'Test title',
+            },
+            {
+                type: 'textarea',
+                name: 'teaser',
+                value: 'Test teaser',
+            }
+        ];
+        await CreatePage.setValues(values, 'Lorem Ipsum');
+        await CreatePage.submit();
+        await ShowPage.navigate();
+        assert.equal(await ShowPage.getValue('title'), 'Test title');
     });
 });
