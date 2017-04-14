a31afde1edaa3869870cde480cd501a722d8d442
Dylan Demougin <dylandemougin@Mac-mini-de-Gildas.local>
2017-04-14 10:05:24 +0200

    Add class into SimpleShowLayout, FormTab and SimpleForm

--- src/mui/detail/SimpleShowLayout.js
+++ src/mui/detail/SimpleShowLayout.js
@@ -5,7 +5,7 @@ import Labeled from '../input/Labeled';
 export const SimpleShowLayout = ({ basePath, children, record, resource }) => (
     <div style={{ padding: '0 1em 1em 1em' }}>
         {Children.map(children, field => (
-            <div key={field.props.source} style={field.props.style}>
+            <div key={field.props.source} style={field.props.style} className={`aor-field-${field.props.source}`}>
                 {field.props.addLabel ?
                     <Labeled record={record} resource={resource} basePath={basePath} label={field.props.label} source={field.props.source} disabled={false}>{field}</Labeled> :
                     (typeof field.type === 'string' ?
--- src/mui/form/FormTab.js
+++ src/mui/form/FormTab.js
@@ -3,7 +3,7 @@ import FormField from './FormField';
 
 const FormTab = ({ label, icon, children, ...rest }) => <span>
     {React.Children.map(children, input => input && (
-        <div key={input.props.source} style={input.props.style}>
+        <div key={input.props.source} style={input.props.style} className={`aor-input-${input.props.source}`}>
             <FormField input={input} {...rest} />
         </div>
     ))}
