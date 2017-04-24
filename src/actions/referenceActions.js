ec8f68ceb44e1fc10c6fc3ae9885216d13f84647
leesei <leesei@gmail.com>
2017-04-24 18:07:43 +0800

    feat: ReferenceArrayField, take 2
     Changes to be committed:
    	modified:   docs/Fields.md
    	modified:   src/actions/referenceActions.js
    	new file:   src/mui/field/ReferenceArrayField.js
    	new file:   src/mui/field/ReferenceArrayField.spec.js
    	modified:   src/mui/field/index.js
    	modified:   src/reducer/references/oneToMany.js
    	modified:   src/sideEffect/saga/referenceFetch.js

--- src/actions/referenceActions.js
+++ src/actions/referenceActions.js
@@ -4,3 +4,10 @@ export const crudGetOneReference = (resource, id) => ({
     type: CRUD_GET_ONE_REFERENCE,
     payload: { resource, id },
 });
+
+export const CRUD_DEBOUNCED_GET_MANY = 'CRUD_DEBOUNCED_GET_MANY';
+
+export const crudDebouncedGetMany = (resource, ids) => ({
+    type: CRUD_DEBOUNCED_GET_MANY,
+    payload: { resource, ids },
+});
