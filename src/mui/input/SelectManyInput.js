bf6168e7d4a1ff24c092643ada44fd8199e0e472
Francois Zaninotto <fzaninotto@gmail.com>
2017-04-25 13:56:47 +0200

    Using prop-types package for proptypes

--- src/mui/input/SelectManyInput.js
+++ src/mui/input/SelectManyInput.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ChipInput from 'material-ui-chip-input';
 import FieldTitle from '../../util/FieldTitle';
 
