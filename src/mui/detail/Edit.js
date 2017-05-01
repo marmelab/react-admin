b8a5d7857ea1e7d52c3dc04b639238679d149ca2
Andrea <a.barani@vubai.com>
2017-05-01 18:39:35 +0200

    decode id uri component matched from react router

--- src/mui/detail/Edit.js
+++ src/mui/detail/Edit.js
@@ -141,8 +141,8 @@ Edit.propTypes = {
 
 function mapStateToProps(state, props) {
     return {
-        id: props.match.params.id,
-        data: state.admin[props.resource].data[props.match.params.id],
+        id: decodeURIComponent(props.match.params.id),
+        data: state.admin[props.resource].data[decodeURIComponent(props.match.params.id)],
         isLoading: state.admin.loading > 0,
     };
 }
