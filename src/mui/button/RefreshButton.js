cbd47f4a1ac670b6e68e482cd54ea23b5d0653f0
Stanislav Pavlovichev <bash@wisebits.com>
2017-05-07 17:20:38 +0300

    * move onClick handler into the compose

--- src/mui/button/RefreshButton.js
+++ src/mui/button/RefreshButton.js
@@ -2,26 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
+import withHandlers from 'recompose/withHandlers';
 import FlatButton from 'material-ui/FlatButton';
 import NavigationRefresh from 'material-ui/svg-icons/navigation/refresh';
 import translate from '../../i18n/translate';
 import { refreshList as refreshListAction } from '../../actions/listActions';
 
-const RefreshButton = ({ label = 'aor.action.refresh', translate, refresh, resource }) => <FlatButton
+const RefreshButton = ({ label, translate, onClick }) => <FlatButton
     primary
     label={label && translate(label)}
-    onClick={() => refresh(resource)}
+    onClick={onClick}
     icon={<NavigationRefresh />}
 />;
 
+RefreshButton.defaultProps = {
+    label: 'aor.action.refresh',
+};
+
 RefreshButton.propTypes = {
     label: PropTypes.string,
-    refresh: PropTypes.func.isRequired,
+    onClick: PropTypes.func.isRequired,
     translate: PropTypes.func.isRequired,
     resource: PropTypes.string.isRequired,
 };
 const enhance = compose(
     connect(null, { refresh: refreshListAction }),
+    withHandlers({
+        onClick: props => (event) => {
+            event.preventDefault();
+            props.refresh(props.resource);
+        },
+    }),
     translate,
 );
 
