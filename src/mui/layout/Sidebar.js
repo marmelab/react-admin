179c17e54298b890949cff3a0a799bab7641ec31
gildas <gildas.garcia@gmail.com>
2017-05-10 14:00:43 +0200

    [RFR] Make sidebar themable
    Fixes #634
    Superces #638

--- src/mui/layout/Sidebar.js
+++ src/mui/layout/Sidebar.js
@@ -1,25 +1,28 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import compose from 'recompose/compose';
 import Drawer from 'material-ui/Drawer';
 import Paper from 'material-ui/Paper';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+
 import Responsive from './Responsive';
 import { setSidebarVisibility as setSidebarVisibilityAction } from '../../actions';
 
-const styles = {
+const getStyles = ({ sideBar }) => ({
     sidebarOpen: {
-        flex: '0 0 16em',
+        flex: (sideBar && sideBar.width) ? `0 0 ${sideBar.width}` : '0 0 16em',
         marginLeft: 0,
         order: -1,
         transition: 'margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
     },
     sidebarClosed: {
-        flex: '0 0 16em',
-        marginLeft: '-16em',
+        flex: (sideBar && sideBar.width) ? `0 0 ${sideBar.width}` : '0 0 16em',
+        marginLeft: (sideBar && sideBar.width) ? `-${sideBar.width}` : '-16em',
         order: -1,
         transition: 'margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
     },
-};
+});
 
 // We shouldn't need PureComponent here as it's connected
 // but for some reason it keeps rendering even though mapStateToProps returns the same object
@@ -29,7 +32,9 @@ class Sidebar extends PureComponent {
     }
 
     render() {
-        const { open, setSidebarVisibility, children } = this.props;
+        const { open, setSidebarVisibility, children, muiTheme } = this.props;
+        const styles = getStyles(muiTheme);
+
         return (
             <Responsive
                 small={
@@ -43,13 +48,15 @@ class Sidebar extends PureComponent {
                     </Paper>
                 }
             />
-        )
+        );
     }
 }
 
 Sidebar.propTypes = {
+    children: PropTypes.node.isRequired,
+    muiTheme: PropTypes.object.isRequired,
+    open: PropTypes.bool.isRequired,
     setSidebarVisibility: PropTypes.func.isRequired,
-    open: PropTypes.bool,
 };
 
 const mapStateToProps = (state, props) => ({
@@ -58,6 +65,7 @@ const mapStateToProps = (state, props) => ({
     theme: props.theme, // force redraw on theme changes
 });
 
-export default connect(mapStateToProps, {
-    setSidebarVisibility: setSidebarVisibilityAction,
-})(Sidebar);
+export default compose(
+    muiThemeable(),
+    connect(mapStateToProps, { setSidebarVisibility: setSidebarVisibilityAction }),
+)(Sidebar);
