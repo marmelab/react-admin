80fbb0aa4571788d1f5f2fa38b1b55d7c4d68ade
Francois Zaninotto <fzaninotto@gmail.com>
2017-05-19 20:27:51 +0200

    Fix fetching of simple references

--- src/sideEffect/saga/referenceFetch.js
+++ src/sideEffect/saga/referenceFetch.js
@@ -6,37 +6,62 @@ import { crudGetMany } from '../../actions/dataActions';
 /**
  * Example
  *
- * let id = {
+ * let debouncedIds = {
  *   posts: { 4: true, 7: true, 345: true },
  *   authors: { 23: true, 47: true, 78: true },
  * }
  */
-const ids = {};
+const debouncedIds = {};
+const addIds = (resource, ids) => {
+    if (!debouncedIds[resource]) {
+        debouncedIds[resource] = {};
+    }
+    ids.forEach((id) => {
+        debouncedIds[resource][id] = true;
+    }); // fast UNIQUE
+};
+const getIds = (resource) => {
+    const ids = Object.keys(debouncedIds[resource]);
+    delete debouncedIds[resource];
+    return ids;
+};
+
 const tasks = {};
 
-// see http://yelouafi.github.io/redux-saga/docs/recipes/index.html#debouncing
+/**
+ * Fetch the list of accumulated ids after a delay
+ *
+ * As this gets canceled by subsequent calls to accumulate(), only the last
+ * call to fetchReference() will not be canceled. The delay acts as a
+ * debounce.
+ *
+ * @see http://yelouafi.github.io/redux-saga/docs/recipes/index.html#debouncing
+ */
 function* fetchReference(resource) {
     // combined with cancel(), this debounces the calls
     yield call(delay, 50);
-    yield put(crudGetMany(resource, Object.keys(ids[resource])));
-    delete ids[resource];
+    yield put(crudGetMany(resource, getIds(resource)));
     delete tasks[resource];
 }
 
+/**
+ * Cancel call to fetchReference, accumulate ids, and call fetchReference
+ *
+ * @example
+ * accumulate({ type: CRUD_DEBOUNCED_GET_MANY, payload: { ids: [1, 3, 5], resource: 'posts' } })
+ */
 function* accumulate({ payload }) {
-    const { ids: idArray, resource } = payload;
-    if (!ids[resource]) {
-        ids[resource] = {};
-    }
-    idArray.map(id => ids[resource][id] = true); // fast UNIQUE
+    const { ids, resource } = payload;
     if (tasks[resource]) {
         yield cancel(tasks[resource]);
     }
+    addIds(resource, ids);
     tasks[resource] = yield fork(fetchReference, resource);
 }
 
 export default function* () {
     yield takeEvery(CRUD_GET_ONE_REFERENCE,
-        ({ payload }) => accumulate({ ids: [payload.id], resource: payload.resource }));
+        ({ payload }) => accumulate({ payload: { ids: [payload.id], resource: payload.resource } })
+    );
     yield takeEvery(CRUD_DEBOUNCED_GET_MANY, accumulate);
 }
