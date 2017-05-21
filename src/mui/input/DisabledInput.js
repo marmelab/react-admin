1d6fc35b95c70a864b2b4cc594b4e01b4ffaa5f7
Weijie JIN <wesley6j@gmail.com>
2017-05-21 16:46:02 -0300

    add elStyle to DisabledInput

--- src/mui/input/DisabledInput.js
+++ src/mui/input/DisabledInput.js
@@ -4,9 +4,10 @@ import TextField from 'material-ui/TextField';
 import get from 'lodash.get';
 import FieldTitle from '../../util/FieldTitle';
 
-const DisabledInput = ({ label, record, resource, source }) => <TextField
+const DisabledInput = ({ label, record, resource, source, elStyle }) => <TextField
     value={get(record, source)}
     floatingLabelText={<FieldTitle label={label} source={source} resource={resource} />}
+    style={elStyle}
     disabled
 />;
 
@@ -15,6 +16,7 @@ DisabledInput.propTypes = {
     record: PropTypes.object,
     resource: PropTypes.string,
     source: PropTypes.string,
+    elStyle: PropTypes.object,
 };
 
 export default DisabledInput;
