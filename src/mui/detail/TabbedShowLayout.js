e3556eeb20bd425fd3ae946ceb32ad4b98243b38
Francois Zaninotto <fzaninotto@gmail.com>
2017-06-08 17:12:31 +0200

    Fix warning in TabbedShowLayout

--- src/mui/detail/TabbedShowLayout.js
+++ src/mui/detail/TabbedShowLayout.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import { Tabs, Tab } from 'material-ui/Tabs';
