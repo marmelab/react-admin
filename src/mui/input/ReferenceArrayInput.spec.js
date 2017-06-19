77d32b3d40693de5923b05d69764128908d2359e
Amr Noman <amr.noman1990@gmail.com>
2017-06-19 14:49:01 +0300

    add tests

--- src/mui/input/ReferenceArrayInput.spec.js
+++ src/mui/input/ReferenceArrayInput.spec.js
@@ -5,6 +5,7 @@ import sinon from 'sinon';
 import { ReferenceArrayInput } from './ReferenceArrayInput';
 
 describe('<ReferenceArrayInput />', () => {
+    const InlineForm = () => <span id="inlineform" />;
     const defaultProps = {
         crudGetMatching: () => true,
         crudGetMany: () => true,
@@ -12,12 +13,20 @@ describe('<ReferenceArrayInput />', () => {
         reference: 'tags',
         resource: 'posts',
         source: 'tag_ids',
+        openedFormsCount: 1,
+        inlineFormsData: {},
+        allowEmpty: true,
+        incrementOpenedForms: () => true,
+        decrementOpenedForms: () => true,
+        crudCreateInline: () => true,
+        inlineForm: <InlineForm />,
+        record: {},
     };
     const MyComponent = () => <span id="mycomponent" />;
 
     it('should not render anything if there is no referenceRecord and allowEmpty is false', () => {
         const wrapper = shallow((
-            <ReferenceArrayInput {...defaultProps}>
+            <ReferenceArrayInput {...defaultProps} allowEmpty={false}>
                 <MyComponent />
             </ReferenceArrayInput>
         ));
@@ -195,5 +204,178 @@ describe('<ReferenceArrayInput />', () => {
 
         const myComponent = wrapper.find('MyComponent');
         assert.notEqual(myComponent.prop('meta', undefined));
+    });
+
+
+    describe('inline forms behavior', () => {
+        const getWrapper = props => shallow((
+            <ReferenceArrayInput
+                {...defaultProps}
+                {...props}
+            >
+                <MyComponent />
+            </ReferenceArrayInput>
+            ));
+
+        it('should not render Dialog if there is no inlineForm', () => {
+            const wrapper = getWrapper({ inlineForm: null });
+            const Dialog = wrapper.find('Dialog');
+            assert.equal(Dialog.length, 0);
+        });
+
+        it('should render Dialog if there is an inlineForm', () => {
+            const wrapper = getWrapper();
+            const Dialog = wrapper.find('Dialog');
+            assert.equal(Dialog.length, 1);
+        });
+
+        it('should render inlineForm inside Dialog', () => {
+            const wrapper = getWrapper();
+            const Form = wrapper.find('Dialog InlineForm');
+            assert.equal(Form.length, 1);
+        });
+
+        it('should pass onCreateInline handler to child component if there is an inlineForm', () => {
+            const wrapper = getWrapper();
+            const child = wrapper.find(MyComponent);
+            assert.equal(typeof child.prop('onCreateInline'), 'function');
+        });
+
+        it('should not pass onCreateInline handler to child component if there is no inlineForm', () => {
+            const wrapper = getWrapper({ inlineForm: null });
+            const child = wrapper.find(MyComponent);
+            assert.equal(child.prop('onCreateInline'), null);
+        });
+
+        it('should show Dialog when child calls onCreateInline handler', () => {
+            const wrapper = getWrapper();
+            wrapper.find(MyComponent).simulate('createInline');
+            assert.equal(wrapper.state('showCreateDialog'), true);
+        });
+
+        it('should pass onSubmit handler to inlineForm', () => {
+            const wrapper = getWrapper();
+            const form = wrapper.find(InlineForm);
+            assert.equal(typeof form.prop('onSubmit'), 'function');
+        });
+
+        it('should have a formId that depends on openedFormsCount', () => {
+            const wrapper = getWrapper({ openedFormsCount: 2 });
+            const formId = wrapper.instance().formId;
+            assert.equal(formId, 'posts@tag_ids@2');
+        });
+
+        describe('.handleCreateInline', () => {
+            it('should call incrementOpenedForms', () => {
+                const incrementOpenedForms = sinon.spy();
+                const wrapper = getWrapper({ incrementOpenedForms });
+                wrapper.instance().handleCreateInline();
+                assert(incrementOpenedForms.calledOnce);
+            });
+
+            it('should show Dialog', () => {
+                const wrapper = getWrapper();
+                wrapper.setState({ showCreateDialog: false });
+                wrapper.instance().handleCreateInline();
+                assert(wrapper.state('showCreateDialog'));
+            });
+
+            it('should store a reference to the callback received', () => {
+                const wrapper = getWrapper();
+                const instance = wrapper.instance();
+                const callback = () => true;
+                instance.handleCreateInline(null, callback);
+                assert.equal(instance.recordCreatedCB, callback);
+            });
+
+            it('should store the partial record received in state', () => {
+                const wrapper = getWrapper();
+                const instance = wrapper.instance();
+                instance.handleCreateInline({ id: 1, name: 'some tag' });
+                assert.deepEqual(wrapper.state('inlineRecord'), { id: 1, name: 'some tag' });
+            });
+        });
+
+        describe('.handleInlineFormSubmit', () => {
+            const crudCreateInline = sinon.spy();
+            const wrapper = getWrapper({ crudCreateInline });
+
+            it('should call crudCreateInline with record', () => {
+                const instance = wrapper.instance();
+                instance.handleInlineFormSubmit({ id: 1, name: 'some tag' });
+                assert.deepEqual(crudCreateInline.args[0], [
+                    'tags',
+                    {
+                        id: 1,
+                        name: 'some tag',
+                    },
+                    'posts@tag_ids@1',
+                ]);
+            });
+        });
+
+        describe('.handleRequestCloseDialog', () => {
+            it('should call closeDialog if this is the top-level form', () => {
+                const wrapper = getWrapper({ openedFormsCount: 0 });
+                const closeDialog = sinon.spy();
+                const instance = wrapper.instance();
+
+                wrapper.setProps({ openedFormsCount: 1 });
+                instance.closeDialog = closeDialog;
+                instance.handleRequestCloseDialog();
+                assert(closeDialog.calledOnce);
+            });
+
+            it('should not call closeDialog if this is not the top-level form', () => {
+                const wrapper = getWrapper({ openedFormsCount: 0 });
+                const closeDialog = sinon.spy();
+                const instance = wrapper.instance();
+
+                wrapper.setProps({ openedFormsCount: 2 });
+                instance.closeDialog = closeDialog;
+                instance.handleRequestCloseDialog();
+                assert(closeDialog.notCalled);
+            });
+        });
+
+        describe('.closeDialog', () => {
+            it('should call decrementOpenedForms', () => {
+                const decrementOpenedForms = sinon.spy();
+                const instance = getWrapper({ decrementOpenedForms }).instance();
+                instance.closeDialog();
+                assert(decrementOpenedForms.calledOnce);
+            });
+
+            it('should close dialog', () => {
+                const wrapper = getWrapper();
+                const instance = wrapper.instance();
+                wrapper.setState({
+                    showCreateDialog: true,
+                });
+                instance.closeDialog();
+                assert.equal(wrapper.state('showCreateDialog'), false);
+            });
+        });
+
+        describe('when there is a new created record', () => {
+            it('should call the stored callback with the new record', () => {
+                const wrapper = getWrapper();
+                const instance = wrapper.instance();
+                const callback = sinon.spy();
+                const inlineFormsData = { [instance.formId]: { id: 1, name: 'some tag' } };
+                instance.recordCreatedCB = callback;
+                wrapper.setProps({ inlineFormsData });
+                assert(callback.calledWith({ id: 1, name: 'some tag' }));
+            });
+
+            it('should close Dialog', () => {
+                const wrapper = getWrapper();
+                const instance = wrapper.instance();
+                const inlineFormsData = { [instance.formId]: { id: 1, name: 'some tag' } };
+                wrapper.setState({ showCreateDialog: true });
+                wrapper.setProps({ inlineFormsData });
+                assert.equal(wrapper.state('showCreateDialog'), false);
+            });
+        });
     });
 });
--- src/mui/input/SelectArrayInput.spec.js
+++ src/mui/input/SelectArrayInput.spec.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import assert from 'assert';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { isElement } from 'react-dom/test-utils';
 import { SelectArrayInput } from './SelectArrayInput';
 
 describe('<SelectArrayInput />', () => {
     const defaultProps = {
         source: 'foo',
         meta: {},
-        input: {},
+        input: {
+            onChange: () => true,
+        },
         translate: x => x,
+        onCreateInline: () => true,
+        setFilter: () => true,
+        choices: [
+            {
+                id: 1,
+                name: 'tag 1',
+            },
+            {
+                id: 2,
+                name: 'tag 2',
+            },
+        ],
     };
 
     it('should use a ChipInput', () => {
@@ -18,7 +34,7 @@ describe('<SelectArrayInput />', () => {
     });
 
     it('should use the input parameter value as the initial input value', () => {
-        const wrapper = shallow(<SelectArrayInput {...defaultProps} input={{ value: [1, 2] }} />);
+        const wrapper = shallow(<SelectArrayInput {...defaultProps} choices={[]} input={{ value: [1, 2] }} />);
         const ChipInputElement = wrapper.find('ChipInput').first();
         assert.deepEqual(ChipInputElement.prop('value'), [{ value: 1, text: 1 }, { value: 2, text: 2 }]);
     });
@@ -175,6 +191,90 @@ describe('<SelectArrayInput />', () => {
             const wrapper = shallow(<SelectArrayInput {...defaultProps} meta={{ touched: true, error: 'Required field.' }} />);
             const ChipInputElement = wrapper.find('ChipInput');
             assert.equal(ChipInputElement.prop('errorText'), 'Required field.');
+        });
+    });
+
+    describe('create inline behavior', () => {
+        const getWrapper = props => shallow((
+            <SelectArrayInput
+                {...defaultProps}
+                {...props}
+            />
+        ));
+
+        describe('.getChoices', () => {
+            describe('when there is an onCreateInline handler', () => {
+                it('should add the create button as the first choice if current text does not match any of the choices', () => {
+                    const wrapper = getWrapper();
+                    const instance = wrapper.instance();
+                    wrapper.setState({ currentValue: 'some tag' });
+                    const choices = instance.getChoices();
+                    assert.equal(choices.length, 3);
+                    assert.equal(choices[0].text, 'some tag');
+                    assert(isElement(choices[0].value));
+                });
+
+                it('should not add the create button choice if current text matches any of the choices', () => {
+                    const wrapper = getWrapper();
+                    const instance = wrapper.instance();
+                    wrapper.setState({ currentValue: 'tag 1' });
+                    const choices = instance.getChoices();
+                    assert.equal(isElement(choices[0].value), false);
+                });
+            });
+
+            describe('when there is no onCreateInline handler', () => {
+                it('should never add the create button choice', () => {
+                    const wrapper = getWrapper({ onCreateInline: null });
+                    const instance = wrapper.instance();
+                    wrapper.setState({ currentValue: 'some tag' });
+                    const choices = instance.getChoices();
+                    assert.equal(choices.length, 2);
+                    assert.equal(isElement(choices[0].value), false);
+                });
+            });
+        });
+
+        describe('.handleAdd', () => {
+            describe('when there is an onCreateInline handler', () => {
+                describe('when value does not exist in choices', () => {
+                    it('should call onCreateInline with correct arguments', () => {
+                        const onCreateInline = sinon.spy();
+                        const wrapper = getWrapper({ onCreateInline });
+                        const instance = wrapper.instance();
+                        wrapper.setState({ currentValue: 'some tag' });
+                        instance.handleAdd({ text: 'some tag' });
+                        assert(onCreateInline.calledWith({
+                            name: 'some tag',
+                        }, instance.handleCreatedRecord));
+                    });
+
+                    it('should reset current text input', () => {
+                        const wrapper = getWrapper();
+                        const instance = wrapper.instance();
+                        instance.handleAdd({ text: 'some tag' });
+                        assert.equal(wrapper.state('currentValue'), '');
+                    });
+                });
+
+                describe('when value exists in choices', () => {
+                    it('should add new value to values in state', () => {
+                        const wrapper = getWrapper();
+                        const instance = wrapper.instance();
+                        instance.handleAdd({ text: 'tag 1' });
+                        assert.deepEqual(wrapper.state('values'), [{ text: 'tag 1' }]);
+                    });
+                });
+            });
+        });
+
+        describe('.handleCreatedRecord', () => {
+            it('should add the new record to values in state', () => {
+                const wrapper = getWrapper();
+                const instance = wrapper.instance();
+                instance.handleCreatedRecord({ id: 1, name: 'some tag' });
+                assert.deepEqual(wrapper.state('values'), [{ text: 'some tag', value: 1 }]);
+            });
         });
     });
 });
