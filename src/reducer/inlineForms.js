ddbfc62400d5cb1f97324f22c39fb92ff5472f4d
Amr Noman <amr.noman1990@gmail.com>
2017-06-19 14:25:47 +0300

    fix linting problems

--- src/reducer/inlineForms.js
+++ src/reducer/inlineForms.js
@@ -8,33 +8,33 @@ const initialState = {
 
 export default (previousState = initialState, { type, meta, payload }) => {
     switch (type) {
-        case CRUD_CREATE_SUCCESS: {
-            const identifier = meta.inlineFormId;
-            if (identifier) {
-                return {
-                    ...previousState,
-                    data: {
-                        ...previousState.data,
-                        [identifier]: payload.data,
-                    },
-                };
-            }
-            return previousState;
+    case CRUD_CREATE_SUCCESS: {
+        const identifier = meta.inlineFormId;
+        if (identifier) {
+            return {
+                ...previousState,
+                data: {
+                    ...previousState.data,
+                    [identifier]: payload.data,
+                },
+            };
         }
+        return previousState;
+    }
 
-        case INCREMENT_OPENED_FORMS:
-            return {
-                ...previousState,
-                openedFormsCount: previousState.openedFormsCount + 1,
-            };
+    case INCREMENT_OPENED_FORMS:
+        return {
+            ...previousState,
+            openedFormsCount: previousState.openedFormsCount + 1,
+        };
 
-        case DECREMENT_OPENED_FORMS:
-            return {
-                ...previousState,
-                openedFormsCount: previousState.openedFormsCount - 1,
-            };
+    case DECREMENT_OPENED_FORMS:
+        return {
+            ...previousState,
+            openedFormsCount: previousState.openedFormsCount - 1,
+        };
 
-        default:
-            return previousState;
+    default:
+        return previousState;
     }
 };
