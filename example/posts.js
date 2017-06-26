15a7ae3e2272158398e2ddc0b936cf98bb77a8d8
Sébastien Boulle <sebastien@cleany.fr>
2017-06-26 09:43:45 +0200

    exmaple: add a responsive table in a show layout

--- example/posts.js
+++ example/posts.js
@@ -190,12 +190,22 @@ export const PostShow = ({ ...props }) => (
                 ]}
             />
             <ReferenceManyField label="resources.posts.fields.comments" reference="comments" target="post_id" sort={{ field: 'created_at', order: 'DESC' }}>
-                <Datagrid selectable={false}>
-                    <DateField source="created_at" />
-                    <TextField source="author.name" />
-                    <TextField source="body" />
-                    <EditButton />
-                </Datagrid>
+	      <Responsive
+	        small={
+		  <SimpleList
+		    primaryText={(record) => record.author.name}
+		    primarySecondary={(record) => record.created_at}
+		  />
+		}
+		medium={
+                  <Datagrid selectable={false}>
+                      <DateField source="created_at" />
+                      <TextField source="author.name" />
+                      <TextField source="body" />
+                      <EditButton />
+                  </Datagrid>
+		}
+	      />
             </ReferenceManyField>
             <ReferenceArrayField reference="tags" source="tags">
                 <SingleFieldList>
