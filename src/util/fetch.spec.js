61b43189d78ceec4ea7d4dcaae9ab2dc9ce6005d
Takayuki Maeda <mtakayuki1209@gmail.com>
2017-06-29 21:26:12 +0900

    Use stringify to generate a query string

---
+++ src/util/fetch.spec.js
@@ -0,0 +1,25 @@
+import assert from 'assert';
+import { queryParameters } from './fetch';
+
+describe('queryParameters', () => {
+    it('should generate a query parameter', () => {
+        const data = { foo: 'bar' };
+        assert.equal(queryParameters(data), 'foo=bar');
+    });
+
+    it('should generate multiple query parameters', () => {
+        const data = { foo: 'fooval', bar: 'barval' };
+        const actual = queryParameters(data);
+        assert(['foo=fooval&bar=barval', 'bar=barval&foo=fooval'].includes(actual));
+    });
+
+    it('should generate multiple query parameters with a same name', () => {
+        const data = { foo: ['bar', 'baz'] };
+        assert.equal(queryParameters(data), 'foo=bar&foo=baz');
+    });
+
+    it('should generate an encoded query parameter', () => {
+        const data = ['foo=bar', 'foo?bar&baz'];
+        assert.equal(queryParameters({ [data[0]]: data[1] }), data.map(encodeURIComponent).join('='));
+    });
+});
