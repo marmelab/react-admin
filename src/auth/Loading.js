8c54bd4bbac440857797ce2c9b4a12a5d3f67ec7
gildas <gildas.garcia@gmail.com>
2017-08-23 17:52:14 +0200

    Documentation and removal of the default Loading component

--- src/auth/Loading.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import LinearProgress from 'material-ui/LinearProgress';
-
-const LoadingComponent = () => <LinearProgress mode="indeterminate" />;
-
-export default LoadingComponent;
