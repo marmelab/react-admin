319080b28d29fb484112fc951b99bf62a6fdfe24
Silvio Ginter <silvio.ginter@googlemail.com>
2017-08-25 10:52:00 +0200

    check for "Content-Type" already set, before setting automatically
    ..., this way it is possible to use fetchJson in own rest clients. In our case we had to fork this, because we use 'application/merge-patch+json' and 'application/json-patch+json' in the server, to differentiate between the both formats.

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -4,7 +4,7 @@ export const fetchJson = (url, options = {}) => {
     const requestHeaders = options.headers || new Headers({
         Accept: 'application/json',
     });
-    if (!(options && options.body && options.body instanceof FormData)) {
+    if (!requestHeaders.has('Content-Type') && !(options && options.body && options.body instanceof FormData)) {
         requestHeaders.set('Content-Type', 'application/json');
     }
     if (options.user && options.user.authenticated && options.user.token) {
