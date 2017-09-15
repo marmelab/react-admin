a713168da5ef4b1f7cea21bc12e13a791e42596f
Philippe Saint Jacques <philippe@cleany.fr>
2017-09-15 17:45:53 +0200

    code review: minor change

--- src/mui/layout/Sidebar.js
+++ src/mui/layout/Sidebar.js
@@ -21,14 +21,14 @@ const getStyles = ({ drawer }) => {
             marginLeft: 0,
             order: -1,
             transition: 'margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-            backgroundColor: `${color}`,
+            backgroundColor: color,
         },
         sidebarClosed: {
             flex: `0 0 ${width}`,
             marginLeft: `-${width}`,
             order: -1,
             transition: 'margin 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-            backgroundColor: `${color}`,
+            backgroundColor: color,
         },
     });
 };
