43da0e71808cb98b9daf46c121b062516953093b
Francois Zaninotto <fzaninotto@gmail.com>
2017-10-17 11:25:08 +0200

    Upgrade to prettier 1.7

--- packages/react-admin/src/mui/input/SelectInput.js
+++ packages/react-admin/src/mui/input/SelectInput.js
@@ -113,11 +113,9 @@ export class SelectInput extends Component {
             <MenuItem
                 key={get(choice, optionValue)}
                 primaryText={
-                    translateChoice ? (
-                        translate(choiceName, { _: choiceName })
-                    ) : (
-                        choiceName
-                    )
+                    translateChoice
+                        ? translate(choiceName, { _: choiceName })
+                        : choiceName
                 }
                 value={get(choice, optionValue)}
             />
