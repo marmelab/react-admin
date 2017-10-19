fbbdb239e4451cf4048aee168aa3ce47e6b2356a
Francois Zaninotto <fzaninotto@gmail.com>
2017-10-19 17:07:44 +0200

    Fix ViewTitle in desktop mode

--- packages/react-admin/src/mui/layout/MenuItemLink.js
+++ packages/react-admin/src/mui/layout/MenuItemLink.js
@@ -29,6 +29,7 @@ export class MenuItemLinkComponent extends Component {
         return (
             <MenuItem {...props} onClick={this.handleMenuTap}>
                 {leftIcon}
+                &nbsp;
                 {primaryText}
             </MenuItem>
         );
--- packages/react-admin/src/mui/layout/ViewTitle.js
+++ packages/react-admin/src/mui/layout/ViewTitle.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { CardContent } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
 import Hidden from 'material-ui/Hidden';
 
 import AppBarMobile from './AppBarMobile';
 
 const ViewTitle = ({ title }) => [
-    <Hidden xsDowm key="desktop">
-        <CardContent title={title} className="title" />
+    <Hidden xsDown key="desktop">
+        <CardContent className="title">
+            <Typography type="headline">{title}</Typography>
+        </CardContent>
     </Hidden>,
     <Hidden smUp key="mobile">
         <AppBarMobile title={title} />
