994ca6871db44b430f0fa07246f9cfd3262188e8
Francois Zaninotto <fzaninotto@gmail.com>
2017-11-28 00:03:33 +0700

    defaultTheme is now empty

--- packages/react-admin/src/mui/defaultTheme.js
+++ packages/react-admin/src/mui/defaultTheme.js
@@ -1,10 +1,1 @@
-export default {
-    tabs: {
-        backgroundColor: 'white',
-        selectedTextColor: '#00bcd4',
-        textColor: '#757575',
-    },
-    inkBar: {
-        backgroundColor: '#00bcd4',
-    },
-};
+export default {};
