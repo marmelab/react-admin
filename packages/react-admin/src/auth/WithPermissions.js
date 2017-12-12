b40e0d522260ec4d66424acb6afe8cdb5aac53da
gildas <gildas.garcia@gmail.com>
2017-12-12 11:00:48 +0100

    Fix lint errors

--- packages/react-admin/src/auth/WithPermissions.js
+++ packages/react-admin/src/auth/WithPermissions.js
@@ -1,4 +1,4 @@
-import React, { Children, Component } from 'react';
+import { Children, Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
