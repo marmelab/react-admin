1f3d5c85aac16f82801306ee85248a2428981e64
Wilhelm Herbrich <herbrich@sensape.com>
2017-12-13 17:58:57 +0100

    Fix conflicts

--- packages/react-admin/src/mui/layout/DashboardMenuItem.js
+++ packages/react-admin/src/mui/layout/DashboardMenuItem.js
@@ -17,21 +17,13 @@ const styles = {
     icon: { paddingRight: '0.5em' },
 };
 
-<<<<<<< a284b0a08f189ea178d849bbe8272fafcc1af639
 const DashboardMenuItem = ({ classes, className, onClick, translate, ...props }) => (
-=======
-const DashboardMenuItem = ({ classes, onClick, translate, props }) => (
->>>>>>> Fix remaining props name
     <MenuItemLink
         onClick={onClick}
         to="/"
         className={classnames(classes.link, className)}
         primaryText={translate('ra.page.dashboard')}
-<<<<<<< a284b0a08f189ea178d849bbe8272fafcc1af639
         leftIcon={<DashboardIcon className={classes.icon} />}
-=======
-        leftIcon={<DashboardIcon style={iconPaddingStyle} />}
->>>>>>> Fix remaining props name
         {...props}
     />
 );
