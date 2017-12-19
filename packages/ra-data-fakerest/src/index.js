0e89d6d7556113250a51459aa2c2c66faa972482
gildas <gildas.garcia@gmail.com>
2017-12-19 11:07:58 +0100

    [RFR] Upgrade guide about addons renaming
    - [x] Updated ra-data-fakerest to use constants from react-admin as do all other data packages
    - [x] Upgrade guide

--- packages/ra-data-fakerest/src/index.js
+++ packages/ra-data-fakerest/src/index.js
@@ -1,12 +1,13 @@
 import FakeRest from 'fakerest';
-
-const GET_LIST = 'GET_LIST';
-const GET_ONE = 'GET_ONE';
-const GET_MANY = 'GET_MANY';
-const GET_MANY_REFERENCE = 'GET_MANY_REFERENCE';
-const CREATE = 'CREATE';
-const UPDATE = 'UPDATE';
-const DELETE = 'DELETE';
+import {
+    GET_LIST,
+    GET_ONE,
+    GET_MANY,
+    GET_MANY_REFERENCE,
+    CREATE,
+    UPDATE,
+    DELETE,
+} from 'react-admin';
 
 /* eslint-disable no-console */
 function log(type, resource, params, response) {
