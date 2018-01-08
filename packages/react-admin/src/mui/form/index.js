43c474b13508ab5a7aab3a7b493ffb224382f82e
Dennie de Lange <dennie@tkvw.nl>
2018-01-08 10:10:14 +0100

    Exported SimpleForm

--- packages/react-admin/src/mui/form/index.js
+++ packages/react-admin/src/mui/form/index.js
@@ -1,5 +1,6 @@
 export addField from './addField';
 export FormTab from './FormTab';
+export SanitizedForm from './SanitizedForm';
 export SimpleForm from './SimpleForm';
 export TabbedForm from './TabbedForm';
 export Toolbar from './Toolbar';
