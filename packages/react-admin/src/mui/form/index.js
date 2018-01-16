c0cdc7e0d4ab9db55a1da922eeb64184f2d4aaa3
gildas <gildas.garcia@gmail.com>
2018-01-16 12:01:32 +0100

    Fix tests by fixing exports

--- packages/react-admin/src/mui/form/index.js
+++ packages/react-admin/src/mui/form/index.js
@@ -1,5 +1,6 @@
 export addField from './addField';
 export FormTab from './FormTab';
+export FormField from './FormField';
 export SimpleForm from './SimpleForm';
 export TabbedForm from './TabbedForm';
 export Toolbar from './Toolbar';
