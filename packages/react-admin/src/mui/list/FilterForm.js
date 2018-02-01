743fe1475bc5d4a6686937472ac92c644d2374ef
gildas <gildas.garcia@gmail.com>
2018-02-01 15:23:56 +0100

    [RFR] Fix default value for filter when source is a path with dot
    Fix #1141

--- packages/react-admin/src/mui/list/FilterForm.js
+++ packages/react-admin/src/mui/list/FilterForm.js
@@ -8,7 +8,7 @@ import compose from 'recompose/compose';
 import withProps from 'recompose/withProps';
 import { withStyles } from 'material-ui/styles';
 import classnames from 'classnames';
-
+import lodashSet from 'lodash.set';
 import translate from '../../i18n/translate';
 
 const styles = ({ palette: { primary1Color } }) => ({
@@ -161,11 +161,12 @@ export const mergeInitialValuesWithDefaultValues = ({
                     filterElement.props.defaultValue
             )
             .reduce(
-                (acc, filterElement) => ({
-                    ...acc,
-                    [filterElement.props.source]:
-                        filterElement.props.defaultValue,
-                }),
+                (acc, filterElement) =>
+                    lodashSet(
+                        { ...acc },
+                        filterElement.props.source,
+                        filterElement.props.defaultValue
+                    ),
                 {}
             ),
         ...initialValues,
