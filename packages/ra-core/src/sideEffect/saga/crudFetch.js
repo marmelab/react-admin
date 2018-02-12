025b8d75a1da23df46a417dc86d40f36c5aad1cc
Dennie de Lange <dennie@tkvw.nl>
2018-02-13 00:42:33 +0100

    Added test

--- packages/ra-core/src/sideEffect/saga/crudFetch.js
+++ packages/ra-core/src/sideEffect/saga/crudFetch.js
@@ -14,70 +14,73 @@ import {
     FETCH_CANCEL,
 } from '../../actions/fetchActions';
 
-const crudFetch = dataProvider => {
-    function* handleFetch(action) {
-        const { type, payload, meta: { fetch: fetchMeta, ...meta } } = action;
-        const restType = fetchMeta;
+export const takeFetchAction = action => action.meta && action.meta.fetch;
+export function* handleFetch(dataProvider, action) {
+    const { type, payload, meta: { fetch: fetchMeta, ...meta } } = action;
+    const restType = fetchMeta;
 
-        try {
-            yield all([
-                put({ type: `${type}_LOADING`, payload, meta }),
-                put({ type: FETCH_START }),
-            ]);
-            let response = yield call(
-                dataProvider,
-                restType,
-                meta.resource,
-                payload
-            );
-            if (!response.data) {
-                throw new Error('REST response must contain a data key');
-            }
-            yield put({
-                type: `${type}_SUCCESS`,
-                payload: response,
-                requestPayload: payload,
-                meta: {
-                    ...meta,
-                    fetchResponse: restType,
-                    fetchStatus: FETCH_END,
-                },
-            });
-            yield put({ type: FETCH_END });
-        } catch (error) {
-            yield put({
-                type: `${type}_FAILURE`,
-                error: error.message ? error.message : error,
-                payload: error.body ? error.body : null,
-                requestPayload: payload,
-                meta: {
-                    ...meta,
-                    fetchResponse: restType,
-                    fetchStatus: FETCH_ERROR,
-                },
-            });
-            yield put({ type: FETCH_ERROR, error });
-        } finally {
-            if (yield cancelled()) {
-                yield put({ type: FETCH_CANCEL });
-                return; /* eslint no-unsafe-finally:0 */
-            }
+    try {
+        yield all([
+            put({ type: `${type}_LOADING`, payload, meta }),
+            put({ type: FETCH_START }),
+        ]);
+        let response = yield call(
+            dataProvider,
+            restType,
+            meta.resource,
+            payload
+        );
+        if (!response.data) {
+            throw new Error('REST response must contain a data key');
+        }
+        yield put({
+            type: `${type}_SUCCESS`,
+            payload: response,
+            requestPayload: payload,
+            meta: {
+                ...meta,
+                fetchResponse: restType,
+                fetchStatus: FETCH_END,
+            },
+        });
+        yield put({ type: FETCH_END });
+    } catch (error) {
+        yield put({
+            type: `${type}_FAILURE`,
+            error: error.message ? error.message : error,
+            payload: error.body ? error.body : null,
+            requestPayload: payload,
+            meta: {
+                ...meta,
+                fetchResponse: restType,
+                fetchStatus: FETCH_ERROR,
+            },
+        });
+        yield put({ type: FETCH_ERROR, error });
+    } finally {
+        if (yield cancelled()) {
+            yield put({ type: FETCH_CANCEL });
+            return; /* eslint no-unsafe-finally:0 */
         }
     }
+}
 
+const crudFetch = dataProvider => {
     return function* watchCrudFetch() {
         const runningTasks = {};
 
         while (true) {
-            const action = yield take(
-                action => action.meta && action.meta.fetch
-            );
+            const action = yield take(takeFetchAction);
             const { cancelPrevious, resource } = action.meta;
 
             if (cancelPrevious && runningTasks[resource]) {
                 runningTasks[resource] = yield cancel(runningTasks[resource]);
             }
-            runningTasks[resource] = yield fork(handleFetch, action);
+            runningTasks[resource] = yield fork(
+                handleFetch,
+                dataProvider,
+                action
+            );
         }
     };
 };
---
+++ packages/react-admin/src/sideEffect/saga/crudFetch.spec.js
@@ -0,0 +1,32 @@
+import { cancel, fork, take } from 'redux-saga/effects';
+import { createMockTask } from 'redux-saga/utils';
+
+import crudFetch, { handleFetch, takeFetchAction } from './crudFetch';
+
+describe('crudFetch', () => {
+    const dataProvider = jest.fn();
+    const action = {
+        meta: { fetch: 'FETCH_ONE', resource: 'test', cancelPrevious: true },
+    };
+
+    const saga = crudFetch(dataProvider);
+    const generator = saga();
+
+    it('waits for a fetch action', () => {
+        expect(generator.next().value).toEqual(take(takeFetchAction));
+    });
+
+    it('should fork a handleFetch', () => {
+        expect(generator.next(action).value).toEqual(
+            fork(handleFetch, dataProvider, action)
+        );
+    });
+    const task = createMockTask();
+    it('waits for another fetch action', () => {
+        expect(generator.next(task).value).toEqual(take(takeFetchAction));
+    });
+
+    it('should cancel previous task of same resource', () => {
+        expect(generator.next(action).value).toEqual(cancel(task));
+    });
+});
