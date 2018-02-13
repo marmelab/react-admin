0339332fdf145a454d948c77089f2b7c5a1eca26
Dennie de Lange <dennie@tkvw.nl>
2018-02-13 03:08:10 +0100

    Save

--- packages/react-admin/src/mui/modals/index.js
+++ packages/react-admin/src/mui/modals/index.js
@@ -1,5 +1,4 @@
 export Modal from './Modal';
-export Modals from './Modals';
 export YesNoModal from './YesNoModal';
 export YesNoCancelModal from './YesNoCancelModal';
 export { pushModalWindow as pushModal } from 'redux-promising-modals';
