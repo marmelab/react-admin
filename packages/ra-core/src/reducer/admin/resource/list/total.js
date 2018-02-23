cf5f65b01b40cfc65911f71d867198cb9b858ea1
Francois Zaninotto <fzaninotto@gmail.com>
2018-02-23 13:50:44 +0100

    Fix total after delete one

--- packages/ra-core/src/reducer/admin/resource/list/total.js
+++ packages/ra-core/src/reducer/admin/resource/list/total.js
@@ -1,5 +1,6 @@
 import {
     CRUD_GET_LIST_SUCCESS,
+    CRUD_DELETE,
     CRUD_DELETE_MANY,
 } from '../../../../actions/dataActions';
 
@@ -9,6 +10,9 @@ export default resource => (previousState = 0, { type, payload, meta }) => {
     }
     if (type === CRUD_GET_LIST_SUCCESS) {
         return payload.total;
+    }
+    if (type === CRUD_DELETE) {
+        return previousState - 1;
     }
     if (type === CRUD_DELETE_MANY) {
         return previousState - payload.ids.length;
