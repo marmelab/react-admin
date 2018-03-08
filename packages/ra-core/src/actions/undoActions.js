f8f6cd976af3b81d9b06f7b1ec87f31aa0f70427
Francois Zaninotto <fzaninotto@gmail.com>
2018-03-08 10:15:08 +0100

    Synchronize undoable with default snackbar delay

--- packages/ra-core/src/actions/undoActions.js
+++ packages/ra-core/src/actions/undoActions.js
@@ -3,7 +3,7 @@ export const UNDO = 'RA/UNDO';
 export const START_OPTIMISTIC_MODE = 'RA/START_OPTIMISTIC_MODE';
 export const STOP_OPTIMISTIC_MODE = 'RA/STOP_OPTIMISTIC_MODE';
 
-export const startUndoable = (action, delay = 3000) => ({
+export const startUndoable = (action, delay = 4000) => ({
     type: UNDOABLE,
     payload: { action, delay },
 });
