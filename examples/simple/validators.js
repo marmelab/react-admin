e50d4af34a883b7c3ba63d02a8b225e70cf9ea42
Dennie de Lange <dennie@tkvw.nl>
2018-03-14 01:13:05 +0100

    Add validators to simple project

---
+++ examples/simple/validators.js
@@ -0,0 +1,7 @@
+import {
+    required as createRequiredValidator,
+    number as createNumberValidator,
+} from 'react-admin';
+
+export const required = createRequiredValidator();
+export const number = createNumberValidator();
