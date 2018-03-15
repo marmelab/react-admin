af2191989984fc224df2524f40596e913a1dc844
Luwangel <adrien.amoros@gmail.com>
2018-03-15 14:50:57 +0100

    Migrate simple example to webpack 4

--- examples/simple/webpack.config.js
+++ examples/simple/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         publicPath: '/static/',
     },
     module: {
-        loaders: [
+        rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
             { test: /\.css$/, loader: 'style-loader!css-loader' },
         ],
