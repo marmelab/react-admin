863a53865a5ba3abc70a2703fe92217b245f37e8
Francois Zaninotto <fzaninotto@gmail.com>
2018-04-03 14:14:45 +0200

    Remove unused dependency

--- packages/ra-ui-materialui/src/field/ArrayField.spec.js
+++ packages/ra-ui-materialui/src/field/ArrayField.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { reduxForm, reducer as formReducer } from 'redux-form';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { TranslationProvider } from 'ra-core';
