494a61044f7fbaa4cfead286188c26b69d5472da
Francois Zaninotto <fzaninotto@gmail.com>
2018-04-26 19:42:11 +0200

    Fix tests

--- packages/ra-ui-materialui/src/list/Filter.spec.js
+++ packages/ra-ui-materialui/src/list/Filter.spec.js
@@ -13,9 +13,7 @@ describe('<Filter />', () => {
 
         it('should render a redux <FilterForm /> component', () => {
             const wrapper = shallow(<Filter {...defaultProps} />);
-            const form = wrapper.find(
-                'WithStyles(TranslatedComponent(withProps(ReduxForm)))'
-            ); // FilterForm is an muiThemable component
+            const form = wrapper.find('WithStyles(withProps(ReduxForm))'); // FilterForm is an muiThemable component
             assert.equal(form.length, 1);
         });
 
@@ -24,7 +22,7 @@ describe('<Filter />', () => {
                 <Filter {...defaultProps} filterValues={{ q: 'Lorem' }} />
             );
             const form = wrapper
-                .find('WithStyles(TranslatedComponent(withProps(ReduxForm)))')
+                .find('WithStyles(withProps(ReduxForm))')
                 .first(); // FilterForm is an muiThemable component
             assert.deepEqual(form.prop('initialValues'), { q: 'Lorem' });
         });
