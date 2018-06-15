f0463b68810a18ede24d7e65d0f687ba796db111
Francois Zaninotto <fzaninotto@gmail.com>
2018-06-15 11:50:17 +0200

    Allow redirection using a function
    Closes #1426

--- packages/ra-core/src/util/resolveRedirectTo.spec.js
+++ packages/ra-core/src/util/resolveRedirectTo.spec.js
@@ -1,12 +1,24 @@
 import assert from 'assert';
 import resolveRedirectTo from './resolveRedirectTo';
 
-describe('resolve redirect to', () => {
-    it('should generate expected links', () => {
+describe('resolveRedirectTo', () => {
+    it('should accept a view name', () => {
         assert.equal(resolveRedirectTo('list', 'books', 1), 'books');
         assert.equal(resolveRedirectTo('create', 'books', 1), 'books/create');
         assert.equal(resolveRedirectTo('edit', 'books', 1), 'books/1');
         assert.equal(resolveRedirectTo('show', 'books', 1), 'books/1/show');
+    });
+
+    it('should accept a custom route name', () => {
         assert.equal(resolveRedirectTo('home', 'books', 1), 'home');
+    });
+
+    it('should accept a function as parameter', () => {
+        const redirect = (basePath, id, data) =>
+            `related/${data.related_id}/show`;
+        assert.equal(
+            resolveRedirectTo(redirect, 'books', 1, { related_id: 3 }),
+            'related/3/show'
+        );
     });
 });
