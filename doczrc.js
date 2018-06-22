64dfda97f9a2485f04127266988f76bb96b895b6
Francois Zaninotto <fzaninotto@gmail.com>
2018-06-22 18:01:20 +0200

    Reduce sidebar

--- doczrc.js
+++ doczrc.js
@@ -1,10 +1,14 @@
 import { babel } from 'docz-plugin-babel6';
 
 export default {
+    title: 'react-admin',
     plugins: [babel()],
     wrapper: 'docs/components/DocWrapper',
     themeConfig: {
         styles: {
+            sidebar: {
+                width: 250,
+            },
             playground: {
                 padding: '1em',
             },
