b7f6cf5de12da35f51747a0e0f9a208726b76849
Gildas Garcia <gildas.garcia@gmail.com>
2018-06-28 08:47:38 +0200

    Tests

---
+++ cypress/integration/tabs-with-routing.js
@@ -0,0 +1,86 @@
+import editPageFactory from '../support/EditPage';
+import loginPageFactory from '../support/LoginPage';
+import showPageFactory from '../support/ShowPage';
+
+describe('Tabs with routing', () => {
+    const EditPage = editPageFactory('#/users/1');
+    const ShowPage = showPageFactory('#/users/1/show', 'name');
+    const LoginPage = loginPageFactory('#/login');
+
+    beforeEach(() => {
+        LoginPage.navigate();
+        LoginPage.login('admin', 'password');
+        cy.url().then(url => expect(url).to.contain('#/posts'));
+    });
+
+    describe('in TabbedLayout component', () => {
+        beforeEach(() => ShowPage.navigate());
+
+        it('allows to switch tabs using the buttons', () => {
+            cy.contains('Summary');
+            cy.contains('Security');
+            cy.contains('Id');
+            cy.contains('Name');
+            ShowPage.gotoTab(2);
+            cy
+                .url()
+                .then(url =>
+                    expect(url).to.match(/.*#\/users\/1\/show\/security/)
+                );
+            cy.contains('Role');
+            ShowPage.gotoTab(1);
+            cy.contains('Id');
+            cy.contains('Name');
+            cy.url().then(url => expect(url).to.match(/.*#\/users\/1\/show/));
+        });
+
+        it('allows to switch tabs using the browser history', () => {
+            cy.contains('Id');
+            cy.contains('Name');
+            ShowPage.gotoTab(2);
+            cy
+                .url()
+                .then(url =>
+                    expect(url).to.match(/.*#\/users\/1\/show\/security/)
+                );
+            cy.contains('Role');
+            cy.go('back');
+            cy.contains('Id');
+            cy.contains('Name');
+            cy.url().then(url => expect(url).to.match(/.*#\/users\/1\/show/));
+        });
+    });
+
+    describe('in TabbedForm component', () => {
+        beforeEach(() => EditPage.navigate());
+        it('allows to switch tabs using the buttons', () => {
+            cy.contains('Summary');
+            cy.contains('Security');
+            cy.contains('Id');
+            cy.contains('Name');
+            EditPage.gotoTab(2);
+            cy
+                .url()
+                .then(url => expect(url).to.match(/.*#\/users\/1\/security/));
+            cy.contains('Role');
+            EditPage.gotoTab(1);
+            cy.contains('Id');
+            cy.contains('Name');
+            cy.url().then(url => expect(url).to.match(/.*#\/users\/1/));
+        });
+
+        it('allows to switch tabs using the browser history', () => {
+            cy.contains('Id');
+            cy.contains('Name');
+            EditPage.gotoTab(2);
+            cy
+                .url()
+                .then(url => expect(url).to.match(/.*#\/users\/1\/security/));
+            cy.contains('Role');
+            cy.go('back');
+            cy.contains('Id');
+            cy.contains('Name');
+            cy.url().then(url => expect(url).to.match(/.*#\/users\/1/));
+        });
+    });
+});
--- examples/simple/src/users/UserShow.js
+++ examples/simple/src/users/UserShow.js
@@ -1,22 +1,50 @@
 /* eslint react/jsx-key: off */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Show, Tab, TabbedShowLayout, TextField } from 'react-admin'; // eslint-disable-line import/no-unresolved
+import { Link } from 'react-router-dom';
 import UserTitle from './UserTitle';
 
-const UserShow = ({ permissions, ...props }) => (
-    <Show title={<UserTitle />} {...props}>
-        <TabbedShowLayout>
-            <Tab label="user.form.summary">
-                <TextField source="id" />
-                <TextField source="name" />
-            </Tab>
-            {permissions === 'admin' && (
-                <Tab label="user.form.security">
-                    <TextField source="role" />
+// The last part of the pathname matching our tabs
+const tabs = [
+    'show', // First tab's pathname will be the show route root
+    'security',
+];
+
+const UserShow = ({ permissions, ...props }) => {
+    const lastPartOfPathname = props.location.pathname.substr(
+        props.location.pathname.lastIndexOf('/') + 1
+    );
+    const tab = tabs.indexOf(lastPartOfPathname);
+    return (
+        <Show title={<UserTitle />} {...props}>
+            <TabbedShowLayout value={tab}>
+                <Tab
+                    label="user.form.summary"
+                    component={Link}
+                    to={props.match.url}
+                >
+                    <TextField source="id" />
+                    <TextField source="name" />
                 </Tab>
-            )}
-        </TabbedShowLayout>
-    </Show>
-);
+                {permissions === 'admin' && (
+                    <Tab
+                        label="user.form.security"
+                        component={Link}
+                        to={`${props.match.url}/security`}
+                    >
+                        <TextField source="role" />
+                    </Tab>
+                )}
+            </TabbedShowLayout>
+        </Show>
+    );
+};
+
+UserShow.propTypes = {
+    location: PropTypes.object.isRequired,
+    match: PropTypes.object.isRequired,
+    permissions: PropTypes.string,
+};
 
 export default UserShow;
