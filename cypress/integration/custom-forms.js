17709a040fa1827fb86a11f6a7834d8e8b8fce82
Gildas Garcia <gildas.garcia@gmail.com>
2018-06-30 08:02:59 +0200

    Migrate custom form e2e tests

---
+++ cypress/integration/custom-forms.js
@@ -0,0 +1,34 @@
+import createPageFactory from '../support/CustomFormPage';
+import showPageFactory from '../support/ShowPage';
+
+describe('Custom Forms', () => {
+    const CreatePage = createPageFactory('#/comments/create');
+    const ShowPage = showPageFactory('#/posts/14/show');
+
+    beforeEach(() => CreatePage.navigate());
+
+    it('should allows to preview the selected post', () => {
+        cy.get(CreatePage.elements.postSelect).click();
+        cy.get(CreatePage.elements.postItem(12)).click();
+
+        cy.get(CreatePage.elements.showPostPreviewModalButton).click();
+
+        cy.contains('Qui tempore rerum et voluptates');
+        cy.contains(
+            'Occaecati rem perferendis dolor aut numquam cupiditate. At tenetur dolores pariatur et libero asperiores porro voluptas. Officiis corporis sed eos repellendus perferendis distinctio hic consequatur.'
+        );
+
+        cy.get(CreatePage.elements.modalCloseButton).click();
+    });
+
+    it('should allows to create a new post', () => {
+        cy.get(CreatePage.elements.showPostCreateModalButton).click();
+
+        CreatePage.setInputValue('input', 'title', 'Bazinga!');
+        CreatePage.setInputValue('textarea', 'teaser', 'Bazingaaaaaaaa!');
+        cy.get(CreatePage.elements.modalSubmitButton).click();
+        cy.contains('Bazinga!');
+        ShowPage.navigate();
+        ShowPage.delete();
+    });
+});
