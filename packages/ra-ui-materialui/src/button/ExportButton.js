b5250a1b6aee21ba5a027cd7060b6e76626e6436
Francois Zaninotto <fzaninotto@gmail.com>
2018-07-18 18:17:39 +0200

    Add CSV conversion and download

--- packages/ra-ui-materialui/src/button/ExportButton.js
+++ packages/ra-ui-materialui/src/button/ExportButton.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import GetApp from '@material-ui/icons/GetApp';
 import { crudGetAll } from 'ra-core';
+import Papa from 'papaparse/papaparse.min';
 
 import Button from './Button';
 
 const sanitizeRestProps = ({
     basePath,
+    config,
     crudGetAll,
     exporter,
     filter,
@@ -20,6 +22,7 @@ const sanitizeRestProps = ({
 class ExportButton extends Component {
     handleClick = () => {
         const {
+            config,
             crudGetAll,
             resource,
             sort,
@@ -32,7 +35,32 @@ class ExportButton extends Component {
             sort,
             filter,
             maxResults,
-            ({ payload: { data } }) => exporter(data).then(console.log(data))
+            ({ payload: { data } }) =>
+                exporter(data)
+                    .then(data => Papa.unparse(data, config))
+                    .then(csv => {
+                        const fakeLink = document.createElement('a');
+                        document.body.appendChild(fakeLink);
+
+                        const blobName = `${resource}.csv`;
+
+                        if (
+                            window.navigator &&
+                            window.navigator.msSaveOrOpenBlob
+                        ) {
+                            // Manage IE11+ & Edge
+                            var blob = new Blob([csv], { type: 'text/csv' });
+                            window.navigator.msSaveOrOpenBlob(blob, blobName);
+                        } else {
+                            fakeLink.setAttribute(
+                                'href',
+                                'data:application/octet-stream;charset=utf-8,' +
+                                    encodeURIComponent(csv)
+                            );
+                            fakeLink.setAttribute('download', blobName);
+                            fakeLink.click();
+                        }
+                    })
         );
     };
 
@@ -53,6 +81,7 @@ class ExportButton extends Component {
 
 ExportButton.propTypes = {
     basePath: PropTypes.string,
+    config: PropTypes.object,
     crudGetAll: PropTypes.func.isRequired,
     exporter: PropTypes.func.isRequired, // must return a Promise
     filter: PropTypes.object,
