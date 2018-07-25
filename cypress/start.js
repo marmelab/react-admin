497ed155d6a58408fadb3d8892fcfcf3c2ee67ab
gildas <gildas.garcia@gmail.com>
2018-07-25 17:17:30 +0200

    [RFR] Fix cypress failing on chrome 68

--- cypress/start.js
+++ cypress/start.js
@@ -5,7 +5,8 @@ return server.start().then(listeningServer => {
     // kick off a cypress run
     return cypress
         .run({
-            browser: 'chrome',
+            // TODO: Revert this to "chrome" when https://github.com/cypress-io/cypress/issues/2037 is fixed
+            browser: 'electron',
             config: {
                 baseUrl: 'http://localhost:8080',
                 video: false,
