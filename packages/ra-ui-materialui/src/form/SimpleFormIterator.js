ee36690ccb77499869975828d5f8a7928b0549a1
gildas <gildas.garcia@gmail.com>
2018-07-26 09:44:27 +0200

    Review

--- packages/ra-ui-materialui/src/form/SimpleFormIterator.js
+++ packages/ra-ui-materialui/src/form/SimpleFormIterator.js
@@ -63,17 +63,17 @@ export class SimpleFormIterator extends Component {
         // we need a unique id for each field for a proper enter/exit animation
         // but redux-form doesn't provide one (cf https://github.com/erikras/redux-form/issues/2735)
         // so we keep an internal map between the field position and an autoincrement id
-        this.nextId = 0;
+        this.nextId = props.fields.length
+            ? props.fields.length
+            : props.defaultValue
+                ? props.defaultValue.length
+                : 0;
 
         // We check whether we have a defaultValue (which must be an array) before checking
         // the fields prop which will always be empty for a new record.
         // Without it, our ids wouldn't match the default value and we would get key warnings
         // on the CssTransition element inside our render method
-        this.ids = props.defaultValue
-            ? props.defaultValue.map(() => this.nextId++)
-            : props.fields
-                ? props.fields.map(() => this.nextId++)
-                : [];
+        this.ids = this.nextId > 0 ? Array.from(Array(this.nextId).keys()) : [];
     }
 
     removeField = index => () => {
