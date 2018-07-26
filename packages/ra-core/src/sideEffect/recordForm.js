6a73ed9d64c51f7129a974f9071dc2fb0fe95dd2
gildas <gildas.garcia@gmail.com>
2018-07-26 10:36:20 +0200

    Fix tests

--- packages/ra-core/src/sideEffect/recordForm.js
+++ packages/ra-core/src/sideEffect/recordForm.js
@@ -14,6 +14,7 @@ export function* handleLocationChange({ payload: { pathname } }) {
         !lastLocationPathName.startsWith(pathname) &&
         !pathname.startsWith(lastLocationPathName)
     ) {
+        lastLocationPathName = pathname;
         yield put(resetForm());
     }
 }
