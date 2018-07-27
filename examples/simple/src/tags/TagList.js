d69c51b54e229d7747ca7e7c54ae138a1b0a612f
Gildas Garcia <gildas.garcia@gmail.com>
2018-07-27 23:30:47 +0200

    Update example

--- examples/simple/src/tags/TagList.js
+++ examples/simple/src/tags/TagList.js
@@ -5,14 +5,13 @@ import {
     List,
     SaveButton,
     ShowButton,
-    TextField,
     TextInput,
 } from 'react-admin';
 import { EditableTree, TreeNodeActions } from 'ra-tree-ui-materialui';
 
 const TagList = props => (
     <List {...props} perPage={1000}>
-        <EditableTree parentSource="parent_id" submitOnEnter={false}>
+        <EditableTree parentSource="parent_id">
             <TextInput source="name" />
             <TreeNodeActions>
                 <SaveButton variant="flat" />
