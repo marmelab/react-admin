49837ed54f69ff119148d81aec7feb7e4b88e1cd
Francois Zaninotto <fzaninotto@gmail.com>
2018-08-09 08:44:01 +0200

    Fix lint error

--- packages/ra-ui-materialui/src/layout/AppBar.js
+++ packages/ra-ui-materialui/src/layout/AppBar.js
@@ -1,4 +1,4 @@
-import React, { cloneElement } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
