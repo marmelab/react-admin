8dd3894841c4594e3b3afa6dba5f96c58a05b515
Gildas Garcia <gildas.garcia@gmail.com>
2018-08-29 16:11:37 +0200

    Switch drag handle and expand icon

--- packages/ra-tree-ui-materialui/src/TreeNode.js
+++ packages/ra-tree-ui-materialui/src/TreeNode.js
@@ -7,6 +7,7 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
     expandIcon: {
         margin: 0,
+        left: -theme.spacing.unit * 6,
     },
     root: {
         alignItems: 'baseline',
@@ -19,14 +20,14 @@ const styles = theme => ({
         display: 'flex',
         padding: 0,
         flexGrow: 1,
-        paddingLeft: theme.spacing.unit * 4,
+        paddingLeft: theme.spacing.unit * 6,
     },
     leaf: {
         display: 'flex',
         flexGrow: 1,
         margin: 0,
         padding: 0,
-        paddingLeft: theme.spacing.unit * 4,
+        paddingLeft: theme.spacing.unit * 6,
         paddingRight: theme.spacing.unit * 4,
         position: 'relative',
     },
@@ -61,8 +62,9 @@ const styles = theme => ({
         },
     },
     handle: {
-        cursor: 'crosshair',
-        marginLeft: theme.spacing.unit * 2,
+        cursor: 'drag',
+        alignItems: 'center',
+        display: 'flex',
         marginRight: theme.spacing.unit * 2,
     },
     draggingOver: {
@@ -181,6 +183,9 @@ class TreeNode extends Component {
                                 isLeaf={true}
                                 cancelDropOnChildren={!!itemType}
                                 onDrop={this.handleDrop}
+                                classes={{
+                                    handle: classes.handle,
+                                }}
                                 {...props}
                             >
                                 {children}
