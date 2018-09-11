56c4a7290f5d9637ee4266348692ae914ca819c2
Francois Zaninotto <fzaninotto@gmail.com>
2018-09-11 17:43:15 +0200

    Add unit tests for Edit and Show

--- packages/ra-core/src/util/index.js
+++ packages/ra-core/src/util/index.js
@@ -7,3 +7,4 @@ export linkToRecord from './linkToRecord';
 export removeEmpty from './removeEmpty';
 export removeKey from './removeKey';
 export resolveRedirectTo from './resolveRedirectTo';
+export TestContext from './TestContext';
---
+++ packages/ra-ui-materialui/src/detail/Edit.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'enzyme';
+import { TestContext } from 'ra-core';
+
+import { Edit } from './Edit';
+
+describe('<Edit />', () => {
+    const defaultEditProps = {
+        basePath: '/',
+        id: '123',
+        resource: 'foo',
+        location: {},
+        match: {},
+    };
+
+    it('should display aside component', () => {
+        const Aside = () => <div id="aside">Hello</div>;
+        const wrapper = render(
+            <TestContext>
+                <Edit {...defaultEditProps} aside={<Aside />}>
+                    <div />
+                </Edit>
+            </TestContext>
+        );
+        const aside = wrapper.find('#aside');
+        expect(aside.text()).toEqual('Hello');
+    });
+});
