2d7f429120492bdfce09d662db57c11f58b2b455
Francois Zaninotto <fzaninotto@gmail.com>
2018-10-12 16:58:09 +0200

    Fix bad commit

--- packages/ra-core/src/reducer/admin/resource/index.js
+++ packages/ra-core/src/reducer/admin/resource/index.js
@@ -1,63 +1,60 @@
-"use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var actions_1 = require("../../../actions");
-var data_1 = __importDefault(require("./data"));
-var list_1 = __importDefault(require("./list"));
-var initialState = {};
-exports.default = (function (previousState, action) {
-    if (previousState === void 0) { previousState = initialState; }
-    var _a;
-    if (action.type === actions_1.REGISTER_RESOURCE) {
-        var resourceState = {
+import { REGISTER_RESOURCE, UNREGISTER_RESOURCE } from '../../../actions';
+
+import data from './data';
+import list from './list';
+
+const initialState = {};
+
+export default (previousState = initialState, action) => {
+    if (action.type === REGISTER_RESOURCE) {
+        const resourceState = {
             props: action.payload,
-            data: data_1.default(undefined, action),
-            list: list_1.default(undefined, action),
+            data: data(undefined, action),
+            list: list(undefined, action),
         };
-        var newState_1 = __assign({}, previousState, (_a = {}, _a[action.payload.name] = resourceState, _a));
-        return newState_1;
+        const newState = {
+            ...previousState,
+            [action.payload.name]: resourceState,
+        };
+
+        return newState;
     }
-    if (action.type === actions_1.UNREGISTER_RESOURCE) {
-        var newState_2 = Object.keys(previousState).reduce(function (acc, key) {
-            var _a;
+
+    if (action.type === UNREGISTER_RESOURCE) {
+        const newState = Object.keys(previousState).reduce((acc, key) => {
             if (key === action.payload) {
                 return acc;
             }
-            return __assign({}, acc, (_a = {}, _a[key] = previousState[key], _a));
+
+            return { ...acc, [key]: previousState[key] };
         }, {});
-        return newState_2;
+        return newState;
     }
+
     if (!action.meta || !action.meta.resource) {
         return previousState;
     }
-    var resources = Object.keys(previousState);
-    var newState = resources.reduce(function (acc, resource) {
-        var _a;
-        return (__assign({}, acc, (_a = {}, _a[resource] = action.meta.resource === resource
-            ? {
-                props: previousState[resource].props,
-                data: data_1.default(previousState[resource].data, action),
-                list: list_1.default(previousState[resource].list, action),
-            }
-            : previousState[resource], _a)));
-    }, {});
+
+    const resources = Object.keys(previousState);
+    const newState = resources.reduce(
+        (acc, resource) => ({
+            ...acc,
+            [resource]:
+                action.meta.resource === resource
+                    ? {
+                          props: previousState[resource].props,
+                          data: data(previousState[resource].data, action),
+                          list: list(previousState[resource].list, action),
+                      }
+                    : previousState[resource],
+        }),
+        {}
+    );
+
     return newState;
-});
-exports.getResources = function (state) {
-    return Object.keys(state).map(function (key) { return state[key].props; });
 };
-exports.getReferenceResource = function (state, props) { return state[props.reference]; };
-//# sourceMappingURL=index.js.map
+
+export const getResources = state =>
+    Object.keys(state).map(key => state[key].props);
+
+export const getReferenceResource = (state, props) => state[props.reference];
