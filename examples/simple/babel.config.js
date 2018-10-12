3393ab961cfe80de8e942c3c23f02b226eacec43
Francois Zaninotto <fzaninotto@gmail.com>
2018-10-12 18:21:37 +0200

    Add typescript support in simple example

--- examples/simple/babel.config.js
+++ examples/simple/babel.config.js
@@ -12,10 +12,12 @@ const presets = [
         },
     ],
     '@babel/preset-react',
+    '@babel/preset-typescript',
 ];
 
 const plugins = [
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
 ];
 
--- examples/simple/webpack.config.js
+++ examples/simple/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(t|j)sx?$/,
                 exclude: /node_modules/,
                 use: { loader: 'babel-loader' },
             },
