6d72b35855466b8b1e49db168a29726803631c15
maruware <me@maruware.com>
2018-10-25 17:09:22 +0900

    Fix ra-data-simple-rest  CREATE doc.

--- packages/ra-data-simple-rest/src/index.js
+++ packages/ra-data-simple-rest/src/index.js
@@ -22,7 +22,7 @@ import {
  * GET_ONE      => GET http://my.api.url/posts/123
  * GET_MANY     => GET http://my.api.url/posts?filter={ids:[123,456,789]}
  * UPDATE       => PUT http://my.api.url/posts/123
- * CREATE       => POST http://my.api.url/posts/123
+ * CREATE       => POST http://my.api.url/posts
  * DELETE       => DELETE http://my.api.url/posts/123
  */
 export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
