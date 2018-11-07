97137c44f51a1afca5d93f808999a94ad9899515
Gildas Garcia <gildas.garcia@pm.me>
2018-11-07 11:50:44 +0100

    [WIP] Migrate ra-core to TypeScript - [x] Fix demo apps - [ ] i18n related code

---
+++ packages/ra-core/src/i18n/defaultI18nProvider.ts
@@ -0,0 +1,6 @@
+import defaultMessages from 'ra-language-english';
+import { I18nProvider } from '../types';
+
+const defaultI18nProvider: I18nProvider = () => defaultMessages;
+
+export default defaultI18nProvider;
