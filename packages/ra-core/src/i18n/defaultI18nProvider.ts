73a719cd6c0733273cf958a953bac1f9e52e1ccf
Gildas Garcia <gildas.garcia@gmail.com>
2018-11-07 15:25:14 +0100

    Finalize i18n

--- packages/ra-core/src/i18n/defaultI18nProvider.ts
+++ packages/ra-core/src/i18n/defaultI18nProvider.ts
@@ -1,6 +1,6 @@
 import defaultMessages from 'ra-language-english';
 import { I18nProvider } from '../types';
 
-const defaultI18nProvider: I18nProvider = () => defaultMessages;
+const defaultI18nProvider: I18nProvider = locale => defaultMessages;
 
 export default defaultI18nProvider;
--- packages/ra-core/src/types.ts
+++ packages/ra-core/src/types.ts
@@ -1,1 +1,9 @@
 export type I18nProvider = (locale: string) => object;
+export type Translate = (id: string, options?: any) => string;
+
+export interface ReduxState {
+    i18n: {
+        locale: string;
+        messages: object;
+    };
+}
