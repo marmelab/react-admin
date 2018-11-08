f4996a9d47a490317a765f279a8b17fb2a623544
Xavier Hans <xavier@marmelab.com>
2018-11-08 17:55:08 +0100

    [WIP] Migrate ra-core actions to Typescript

--- packages/ra-core/src/actions/formActions.ts
+++ packages/ra-core/src/actions/formActions.ts
@@ -2,16 +2,24 @@ export const INITIALIZE_FORM = 'RA/INITIALIZE_FORM';
 export const RESET_FORM = 'RA/RESET_FORM';
 export const BEFORE_LOCATION_CHANGE = 'RA/BEFORE_LOCATION_CHANGE';
 
-export const initializeForm = initialValues => ({
+export const initializeForm = (
+    initialValues: any
+): { type: string; payload: any } => ({
     type: INITIALIZE_FORM,
     payload: initialValues,
 });
 
-export const resetForm = () => ({
+export const resetForm = (): { type: string } => ({
     type: RESET_FORM,
 });
 
-export const beforeLocationChange = ({ payload, meta }) => ({
+export const beforeLocationChange = ({
+    payload,
+    meta,
+}: {
+    payload: any;
+    meta: any;
+}): { type: string; payload: any; meta: any } => ({
     type: BEFORE_LOCATION_CHANGE,
     payload,
     meta,
