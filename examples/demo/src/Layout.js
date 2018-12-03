3663be98f06beb030cbf705b13e2848a6a6b3810
fzaninotto <fzaninotto@gmail.com>
2018-12-03 12:10:39 +0100

    Fix custom login doesn't use theme in demo example
    Refs #2368

--- examples/demo/src/Layout.js
+++ examples/demo/src/Layout.js
@@ -2,28 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Layout, Sidebar } from 'react-admin';
 import AppBar from './AppBar';
+import { darkTheme, lightTheme } from './themes';
 
 const CustomSidebar = props => <Sidebar size={200} {...props} />;
 const CustomLayout = props => (
     <Layout appBar={AppBar} sidebar={CustomSidebar} {...props} />
 );
-
-const darkTheme = {
-    palette: {
-        type: 'dark', // Switching the dark mode on is a single property value change.
-    },
-};
-
-const lightTheme = {
-    palette: {
-        secondary: {
-            light: '#5f5fc4',
-            main: '#283593',
-            dark: '#001064',
-            contrastText: '#fff',
-        },
-    },
-};
 
 export default connect(
     state => ({
