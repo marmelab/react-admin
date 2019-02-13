7dc064bc614c808bce472760cd433d69aeaf4680
Gildas Garcia <gildas.garcia@gmail.com>
2019-02-13 15:37:18 +0100

    Missing index file

--- packages/ra-core/src/controller/field/index.ts
+++ packages/ra-core/src/controller/field/index.ts
@@ -1,6 +1,7 @@
 import ReferenceArrayFieldController from './ReferenceArrayFieldController';
 import ReferenceFieldController from './ReferenceFieldController';
 import ReferenceManyFieldController from './ReferenceManyFieldController';
+
 export {
     ReferenceArrayFieldController,
     ReferenceFieldController,
