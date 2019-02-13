8e2fba31b5661f3813bbf7073dabad753f42d4b3
Gildas Garcia <gildas.garcia@gmail.com>
2019-02-13 10:56:47 +0100

    [RFR] Finalize i18n Migration to TypeScript

--- packages/ra-core/src/i18n/translate.spec.tsx
+++ packages/ra-core/src/i18n/translate.spec.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { SFC } from 'react';
 
 import translate from './translate';
+import { TranslationContextProps } from './TranslationContext';
 
+interface Props extends TranslationContextProps {
+    foo: string;
+}
 describe('translate HOC', () => {
     it('should conserve base component default props', () => {
-        const Component = () => <div />;
+        const Component: SFC<Props> = () => <div />;
         Component.defaultProps = { foo: 'bar' };
 
         const TranslatedComponent = translate(Component);
