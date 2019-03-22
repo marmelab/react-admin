fc1afe8d367082b95b9cacc375b820f07cb304e9
Dimitri KOPRIWA <dimitri.kopriwa@kopaxgroup.com>
2019-03-23 02:41:30 +0700

    fix(Create): allow overriding of props basePath

--- packages/ra-ui-materialui/src/detail/Create.js
+++ packages/ra-ui-materialui/src/detail/Create.js
@@ -72,6 +72,7 @@ export const CreateView = ({
                         basePath,
                         resource,
                         hasList,
+                        ...actions.props,
                     })}
                 </CardContentInner>
             )}
