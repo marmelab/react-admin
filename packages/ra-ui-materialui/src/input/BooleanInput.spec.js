c17ee15f98736f515af42f394f2dcccdab3d4f15
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2019-04-10 16:49:35 +0200

    Display errors on mui BootstrapInput

--- packages/ra-ui-materialui/src/input/BooleanInput.spec.js
+++ packages/ra-ui-materialui/src/input/BooleanInput.spec.js
@@ -6,7 +6,9 @@ import { BooleanInput } from './BooleanInput';
 
 describe('<BooleanInput />', () => {
     it('should render as a mui Toggle', () => {
-        const wrapper = shallow(<BooleanInput source="foo" input={{}} />)
+        const wrapper = shallow(
+            <BooleanInput source="foo" input={{}} meta={{}} />
+        )
             .find('WithStyles(FormControlLabel)')
             .shallow()
             .dive();
@@ -16,7 +18,7 @@ describe('<BooleanInput />', () => {
 
     it('should be checked if the value is true', () => {
         const wrapper = shallow(
-            <BooleanInput source="foo" input={{ value: true }} />
+            <BooleanInput source="foo" input={{ value: true }} meta={{}} />
         )
             .find('WithStyles(FormControlLabel)')
             .shallow()
@@ -26,7 +28,7 @@ describe('<BooleanInput />', () => {
 
     it('should not be checked if the value is false', () => {
         const wrapper = shallow(
-            <BooleanInput source="foo" input={{ value: false }} />
+            <BooleanInput source="foo" input={{ value: false }} meta={{}} />
         )
             .find('WithStyles(FormControlLabel)')
             .shallow()
@@ -35,10 +37,42 @@ describe('<BooleanInput />', () => {
     });
 
     it('should not be checked if the value is undefined', () => {
-        const wrapper = shallow(<BooleanInput source="foo" input={{}} />)
+        const wrapper = shallow(
+            <BooleanInput source="foo" input={{}} meta={{}} />
+        )
             .find('WithStyles(FormControlLabel)')
             .shallow()
             .dive();
         assert.equal(wrapper.find('WithStyles(Switch)').prop('checked'), false);
+    });
+
+    it('should displays errors', () => {
+        const wrapper = shallow(
+            <BooleanInput
+                source="foo"
+                input={{}}
+                meta={{ error: ['foo', 'bar'] }}
+            />
+        )
+            .find('WithStyles(FormGroup)')
+            .shallow()
+            .dive();
+        assert.equal(wrapper.find('WithStyles(FormHelperText)').length, 2);
+        assert.equal(
+            wrapper
+                .find('WithStyles(FormHelperText)')
+                .at(0)
+                .children()
+                .text(),
+            'foo'
+        );
+        assert.equal(
+            wrapper
+                .find('WithStyles(FormHelperText)')
+                .at(1)
+                .children()
+                .text(),
+            'bar'
+        );
     });
 });
