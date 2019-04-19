47ab31914990e9fe22ab602b92ecb6464b9011c0
fzaninotto <fzaninotto@gmail.com>
2019-04-19 11:36:45 +0200

    Fix Query component does not fetch again upon update

--- packages/ra-core/src/util/Query.spec.tsx
+++ packages/ra-core/src/util/Query.spec.tsx
@@ -4,7 +4,7 @@ import {
     cleanup,
     fireEvent,
     // @ts-ignore
-    waitForDomChange
+    waitForDomChange,
 } from 'react-testing-library';
 import expect from 'expect';
 import Query from './Query';
@@ -36,7 +36,11 @@ describe('Query', () => {
                 {({ store }) => {
                     dispatchSpy = jest.spyOn(store, 'dispatch');
                     return (
-                        <Query type="mytype" resource="myresource" payload={myPayload}>
+                        <Query
+                            type="mytype"
+                            resource="myresource"
+                            payload={myPayload}
+                        >
                             {() => <div>Hello</div>}
                         </Query>
                     );
@@ -55,8 +59,16 @@ describe('Query', () => {
         const { getByText } = render(
             <TestContext>
                 {() => (
-                    <Query type="mytype" resource="myresource" payload={myPayload}>
-                        {({ loading }) => <div className={loading ? 'loading' : 'idle'}>Hello</div>}
+                    <Query
+                        type="mytype"
+                        resource="myresource"
+                        payload={myPayload}
+                    >
+                        {({ loading }) => (
+                            <div className={loading ? 'loading' : 'idle'}>
+                                Hello
+                            </div>
+                        )}
                     </Query>
                 )}
             </TestContext>
@@ -66,11 +78,16 @@ describe('Query', () => {
 
     it('should update the data state after a success response', async () => {
         const dataProvider = jest.fn();
-        dataProvider.mockImplementationOnce(() => Promise.resolve({ data: { foo: 'bar' } }));
+        dataProvider.mockImplementationOnce(() =>
+            Promise.resolve({ data: { foo: 'bar' } })
+        );
         const Foo = () => (
             <Query type="mytype" resource="foo">
                 {({ loading, data }) => (
-                    <div data-testid="test" className={loading ? 'loading' : 'idle'}>
+                    <div
+                        data-testid="test"
+                        className={loading ? 'loading' : 'idle'}
+                    >
                         {data ? data.foo : 'no data'}
                     </div>
                 )}
@@ -91,12 +108,17 @@ describe('Query', () => {
 
     it('should return the total prop if available', async () => {
         const dataProvider = jest.fn();
-        dataProvider.mockImplementationOnce(() => Promise.resolve({ data: [{ foo: 'bar' }], total: 42 }));
+        dataProvider.mockImplementationOnce(() =>
+            Promise.resolve({ data: [{ foo: 'bar' }], total: 42 })
+        );
 
         const Foo = () => (
             <Query type="mytype" resource="foo">
                 {({ loading, data, total }) => (
-                    <div data-testid="test" className={loading ? 'loading' : 'idle'}>
+                    <div
+                        data-testid="test"
+                        className={loading ? 'loading' : 'idle'}
+                    >
                         {loading ? 'no data' : total}
                     </div>
                 )}
@@ -120,11 +142,16 @@ describe('Query', () => {
 
     it('should update the error state after an error response', async () => {
         const dataProvider = jest.fn();
-        dataProvider.mockImplementationOnce(() => Promise.reject({ message: 'provider error' }));
+        dataProvider.mockImplementationOnce(() =>
+            Promise.reject({ message: 'provider error' })
+        );
         const Foo = () => (
             <Query type="mytype" resource="foo">
                 {({ loading, error }) => (
-                    <div data-testid="test" className={loading ? 'loading' : 'idle'}>
+                    <div
+                        data-testid="test"
+                        className={loading ? 'loading' : 'idle'}
+                    >
                         {error ? error.message : 'no data'}
                     </div>
                 )}
@@ -141,5 +168,81 @@ describe('Query', () => {
         await waitForDomChange({ container: testElement });
         expect(testElement.textContent).toEqual('provider error');
         expect(testElement.className).toEqual('idle');
+    });
+
+    it('should dispatch a new fetch action when updating', () => {
+        let dispatchSpy;
+        const myPayload = {};
+        const { rerender } = render(
+            <TestContext>
+                {({ store }) => {
+                    dispatchSpy = jest.spyOn(store, 'dispatch');
+                    return (
+                        <Query
+                            type="mytype"
+                            resource="myresource"
+                            payload={myPayload}
+                        >
+                            {() => <div>Hello</div>}
+                        </Query>
+                    );
+                }}
+            </TestContext>
+        );
+        const mySecondPayload = { foo: 1 };
+        rerender(
+            <TestContext>
+                {() => (
+                    <Query
+                        type="mytype"
+                        resource="myresource"
+                        payload={mySecondPayload}
+                    >
+                        {() => <div>Hello</div>}
+                    </Query>
+                )}
+            </TestContext>
+        );
+        expect(dispatchSpy.mock.calls.length).toEqual(2);
+        const action = dispatchSpy.mock.calls[1][0];
+        expect(action.type).toEqual('CUSTOM_FETCH');
+        expect(action.payload).toEqual(mySecondPayload);
+        expect(action.meta.fetch).toEqual('mytype');
+        expect(action.meta.resource).toEqual('myresource');
+    });
+
+    it('should not dispatch a new fetch action when updating with the same query props', () => {
+        let dispatchSpy;
+        const myPayload = {};
+        const { rerender } = render(
+            <TestContext>
+                {({ store }) => {
+                    dispatchSpy = jest.spyOn(store, 'dispatch');
+                    return (
+                        <Query
+                            type="mytype"
+                            resource="myresource"
+                            payload={myPayload}
+                        >
+                            {() => <div>Hello</div>}
+                        </Query>
+                    );
+                }}
+            </TestContext>
+        );
+        rerender(
+            <TestContext>
+                {() => (
+                    <Query
+                        type="mytype"
+                        resource="myresource"
+                        payload={myPayload}
+                    >
+                        {() => <div>Hello</div>}
+                    </Query>
+                )}
+            </TestContext>
+        );
+        expect(dispatchSpy.mock.calls.length).toEqual(1);
     });
 });
