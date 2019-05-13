d1008a2620c5deffd9ab04363fedd63648ec6497
Gildas Garcia <gildas.garcia@gmail.com>
2019-05-13 10:58:36 +0200

    Review

--- packages/ra-core/src/reducer/admin/references/oneToMany.ts
+++ packages/ra-core/src/reducer/admin/references/oneToMany.ts
@@ -23,47 +23,15 @@ const oneToManyReducer: Reducer<State> = (
     action: ActionTypes
 ) => {
     if (action.meta && action.meta.fetch === DELETE && action.meta.optimistic) {
-        const relatedTo = Object.keys(previousState).filter(key =>
-            key.includes(action.meta.resource)
-        );
-
-        return relatedTo.reduce(
-            (acc, key) => ({
-                ...acc,
-                [key]: {
-                    ids: previousState[key].ids.filter(
-                        id => id !== action.payload.id
-                    ),
-                    total: previousState[key].total - 1,
-                },
-            }),
-            previousState
-        );
+        return removeDeletedReference(previousState, action);
     }
+
     if (
         action.meta &&
         action.meta.fetch === DELETE_MANY &&
         action.meta.optimistic
     ) {
-        const relatedTo = Object.keys(previousState).filter(key =>
-            key.includes(action.meta.resource)
-        );
-
-        return relatedTo.reduce((acc, key) => {
-            const idsToRemove = previousState[key].ids.filter(id =>
-                action.payload.ids.includes(id)
-            );
-
-            return {
-                ...acc,
-                [key]: {
-                    ids: previousState[key].ids.filter(
-                        id => !action.payload.ids.includes(id)
-                    ),
-                    total: previousState[key].total - idsToRemove.length,
-                },
-            };
-        }, previousState);
+        return removeDeletedReferences(previousState, action);
     }
     switch (action.type) {
         case CRUD_GET_MANY_REFERENCE_SUCCESS:
@@ -159,6 +127,46 @@ export const getReferencesByIds = (
         }, {});
 
     return Object.keys(references).length > 0 ? references : null;
+};
+
+const getRelatedReferences = (previousState, resource) =>
+    Object.keys(previousState).filter(key => key.includes(resource));
+
+const removeDeletedReference = (previousState, action: ActionTypes) => {
+    const relatedTo = getRelatedReferences(previousState, action.meta.resource);
+
+    return relatedTo.reduce(
+        (acc, key) => ({
+            ...acc,
+            [key]: {
+                ids: previousState[key].ids.filter(
+                    id => id !== action.payload.id
+                ),
+                total: previousState[key].total - 1,
+            },
+        }),
+        previousState
+    );
+};
+
+const removeDeletedReferences = (previousState, action: ActionTypes) => {
+    const relatedTo = getRelatedReferences(previousState, action.meta.resource);
+
+    return relatedTo.reduce((acc, key) => {
+        const idsToRemove = previousState[key].ids.filter(id =>
+            action.payload.ids.includes(id)
+        );
+
+        return {
+            ...acc,
+            [key]: {
+                ids: previousState[key].ids.filter(
+                    id => !action.payload.ids.includes(id)
+                ),
+                total: previousState[key].total - idsToRemove.length,
+            },
+        };
+    }, previousState);
 };
 
 export const nameRelatedTo = (reference, id, resource, target, filter = {}) => {
