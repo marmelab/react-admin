69693e602db22958021b931afbae7b23673691c1
Andrew Cherniavskii <andrew.cherniavskii@gmail.com>
2019-05-14 18:04:50 +0200

    do not block rerendering

--- packages/ra-input-rich-text/src/index.js
+++ packages/ra-input-rich-text/src/index.js
@@ -58,12 +58,8 @@ export class RichTextInput extends Component {
         this.quill.on('text-change', debounce(this.onTextChange, 500));
     }
 
-    shouldComponentUpdate(nextProps) {
-        return nextProps.input.value !== this.lastValueChange;
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.input.value !== this.props.input.value) {
+    componentDidUpdate() {
+        if (this.lastValueChange !== this.props.input.value) {
             const selection = this.quill.getSelection();
             this.quill.setContents(
                 this.quill.clipboard.convert(this.props.input.value)
