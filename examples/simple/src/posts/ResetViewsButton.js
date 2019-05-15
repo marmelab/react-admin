46f7572c909f135ab6f5a837ccae9a6dddc137e3
zyhou <richard.maxime9@gmail.com>
2019-05-15 12:01:17 +0200

    Migrate ResetViewsButton to hooks

--- examples/simple/src/posts/ResetViewsButton.js
+++ examples/simple/src/posts/ResetViewsButton.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import { startUndoable, crudUpdateMany, Button } from 'react-admin';
 
-class ResetViewsAction extends Component {
-    handleClick = () => {
-        const { basePath, startUndoable, resource, selectedIds } = this.props;
-        startUndoable(
-            crudUpdateMany(resource, selectedIds, { views: 0 }, basePath)
+const ResetViewsButton = props => {
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        const { basePath, resource, selectedIds } = props;
+        dispatch(
+            startUndoable(
+                crudUpdateMany(resource, selectedIds, { views: 0 }, basePath),
+            ),
         );
     };
 
-    render() {
-        return (
-            <Button label="simple.action.resetViews" onClick={this.handleClick}>
-                <VisibilityOff />
-            </Button>
-        );
-    }
-}
+    return (
+        <Button label="simple.action.resetViews" onClick={handleClick}>
+            <VisibilityOff />
+        </Button>
+    );
+};
 
-ResetViewsAction.propTypes = {
+ResetViewsButton.propTypes = {
     basePath: PropTypes.string,
     label: PropTypes.string,
     resource: PropTypes.string.isRequired,
     selectedIds: PropTypes.arrayOf(PropTypes.any).isRequired,
-    startUndoable: PropTypes.func.isRequired,
 };
 
-export default connect(
-    undefined,
-    { startUndoable }
-)(ResetViewsAction);
+export default ResetViewsButton;
