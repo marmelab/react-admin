6d74e66e980e20a425e48d1dfa703097b318a9f9
fzaninotto <fzaninotto@gmail.com>
2019-05-21 23:52:34 +0200

    Syntax tweaks

--- packages/ra-core/src/controller/useSelectItems.ts
+++ packages/ra-core/src/controller/useSelectItems.ts
@@ -5,7 +5,7 @@ import { setListSelectedIds, toggleListItem } from '../actions/listActions';
 import { Identifier, ReduxState } from '../types';
 
 /**
- * Get and update the list of selected items for a resource
+ * Get the list of selected items for a resource, and callbacks to change the selection
  *
  * @param resource The resource name, e.g. 'posts'
  *
@@ -13,30 +13,28 @@ import { Identifier, ReduxState } from '../types';
  */
 const useSelectItems = (resource: string) => {
     const dispatch = useDispatch();
-
-    const selectedIds = useSelector(
-        (reduxState: ReduxState) =>
-            reduxState.admin.resources[resource].list.selectedIds,
-        [resource]
-    );
-
-    const select = useCallback(
-        (newIds: Identifier[]) => {
-            dispatch(setListSelectedIds(resource, newIds));
-        },
-        [resource]
-    );
-    const toggle = useCallback(
-        (id: Identifier) => {
-            dispatch(toggleListItem(resource, id));
-        },
-        [resource]
-    );
-    const clearSelection = useCallback(() => {
-        dispatch(setListSelectedIds(resource, []));
-    }, [resource]);
-
-    return { selectedIds, select, toggle, clearSelection };
+    return {
+        selectedIds: useSelector(
+            (reduxState: ReduxState) =>
+                reduxState.admin.resources[resource].list.selectedIds,
+            [resource]
+        ),
+        select: useCallback(
+            (newIds: Identifier[]) => {
+                dispatch(setListSelectedIds(resource, newIds));
+            },
+            [resource]
+        ),
+        toggle: useCallback(
+            (id: Identifier) => {
+                dispatch(toggleListItem(resource, id));
+            },
+            [resource]
+        ),
+        clearSelection: useCallback(() => {
+            dispatch(setListSelectedIds(resource, []));
+        }, [resource]),
+    };
 };
 
 export default useSelectItems;
