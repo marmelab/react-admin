078d04c035b5106f3f1368ef23fb0eb3591ece90
Gildas Garcia <gildas.garcia@gmail.com>
2019-06-05 10:30:51 +0200

    Ensure Prettier Config is complete to avoid user overrides and to apply it to all files

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 120,
+    quoteProps: 'as-needed',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    useTabs: false
+}
