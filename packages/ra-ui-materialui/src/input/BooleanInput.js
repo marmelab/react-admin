4a629487bf01dbcae5d7ca764a4f25cb3eb504a2
Gildas Garcia <gildas.garcia@gmail.com>
2019-06-19 15:36:51 +0200

    Migrated BooleanInput

--- packages/ra-ui-materialui/src/input/BooleanInput.js
+++ packages/ra-ui-materialui/src/input/BooleanInput.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import FormGroup from '@material-ui/core/FormGroup';
 import Switch from '@material-ui/core/Switch';
 import { addField, FieldTitle } from 'ra-core';
 
 import sanitizeRestProps from './sanitizeRestProps';
+import InputHelperText from './InputHelperText';
 
 export class BooleanInput extends Component {
     handleChange = (event, value) => {
@@ -25,38 +27,48 @@ export class BooleanInput extends Component {
             options,
             fullWidth,
             meta,
+            helperText,
             ...rest
         } = this.props;
 
         const { value, ...inputProps } = input;
+        const { touched, error } = meta;
 
         return (
-            <FormGroup className={className} {...sanitizeRestProps(rest)}>
-                <FormControlLabel
-                    htmlFor={id}
-                    control={
-                        <Switch
-                            id={id}
-                            color="primary"
-                            checked={!!value}
-                            onChange={this.handleChange}
-                            {...inputProps}
-                            {...options}
-                        />
-                    }
-                    label={
-                        <FieldTitle
-                            label={label}
-                            source={source}
-                            resource={resource}
-                            isRequired={isRequired}
-                        />
-                    }
-                />
-                {meta.error && (
-                    <FormHelperText error>{meta.error}</FormHelperText>
-                )}
-            </FormGroup>
+            <FormControl className={className} {...sanitizeRestProps(rest)}>
+                <FormGroup>
+                    <FormControlLabel
+                        htmlFor={id}
+                        control={
+                            <Switch
+                                id={id}
+                                color="primary"
+                                checked={!!value}
+                                onChange={this.handleChange}
+                                {...inputProps}
+                                {...options}
+                            />
+                        }
+                        label={
+                            <FieldTitle
+                                label={label}
+                                source={source}
+                                resource={resource}
+                                isRequired={isRequired}
+                            />
+                        }
+                    />
+                    {helperText || (touched && error) ? (
+                        <FormHelperText>
+                            <InputHelperText
+                                touched={touched}
+                                error={error}
+                                helperText={helperText}
+                            />
+                        </FormHelperText>
+                    ) : null}
+                </FormGroup>
+            </FormControl>
         );
     }
 }
