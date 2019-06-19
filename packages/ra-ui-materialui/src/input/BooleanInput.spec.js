055c5054149c9b90a1d5b7962db02b88ad6ba193
Gildas Garcia <gildas.garcia@gmail.com>
2019-06-19 17:42:16 +0200

    Fix BooleanInput tests

--- packages/ra-ui-materialui/src/input/BooleanInput.spec.js
+++ packages/ra-ui-materialui/src/input/BooleanInput.spec.js
@@ -6,14 +6,6 @@ import { BooleanInput } from './BooleanInput';
 describe('<BooleanInput />', () => {
     afterEach(cleanup);
 
-<<<<<<< HEAD
-    const defaultProps = {
-        resource: 'foo',
-        meta: {},
-    };
-
-||||||| merged common ancestors
-=======
     const defaultProps = {
         id: 'bar',
         resource: 'foo',
@@ -22,19 +14,8 @@ describe('<BooleanInput />', () => {
         meta: {},
     };
 
->>>>>>> Migrated BooleanInput
     it('should render as a checkbox', () => {
-<<<<<<< HEAD
-        const { getByLabelText } = render(
-            <BooleanInput {...defaultProps} source="bar" input={{}} />
-        );
-||||||| merged common ancestors
-        const { getByLabelText } = render(
-            <BooleanInput resource="foo" source="bar" input={{}} />
-        );
-=======
         const { getByLabelText } = render(<BooleanInput {...defaultProps} />);
->>>>>>> Migrated BooleanInput
         expect(getByLabelText('resources.foo.fields.bar').type).toBe(
             'checkbox'
         );
@@ -42,51 +23,21 @@ describe('<BooleanInput />', () => {
 
     it('should be checked if the value is true', () => {
         const { getByLabelText } = render(
-<<<<<<< HEAD
-            <BooleanInput
-                {...defaultProps}
-                source="bar"
-                input={{ value: true }}
-            />
-||||||| merged common ancestors
-            <BooleanInput resource="foo" source="bar" input={{ value: true }} />
-=======
             <BooleanInput {...defaultProps} input={{ value: true }} />
->>>>>>> Migrated BooleanInput
         );
         expect(getByLabelText('resources.foo.fields.bar').checked).toBe(true);
     });
 
     it('should not be checked if the value is false', () => {
         const { getByLabelText } = render(
-<<<<<<< HEAD
-            <BooleanInput
-                {...defaultProps}
-                source="bar"
-                input={{ value: false }}
-            />
-||||||| merged common ancestors
-            <BooleanInput
-                resource="foo"
-                source="bar"
-                input={{ value: false }}
-            />
-=======
             <BooleanInput {...defaultProps} input={{ value: false }} />
->>>>>>> Migrated BooleanInput
         );
         expect(getByLabelText('resources.foo.fields.bar').checked).toBe(false);
     });
 
     it('should not be checked if the value is undefined', () => {
         const { getByLabelText } = render(
-<<<<<<< HEAD
-            <BooleanInput {...defaultProps} source="bar" input={{}} />
-||||||| merged common ancestors
-            <BooleanInput resource="foo" source="bar" input={{}} />
-=======
             <BooleanInput {...defaultProps} input={{}} />
->>>>>>> Migrated BooleanInput
         );
         expect(getByLabelText('resources.foo.fields.bar').checked).toBe(false);
     });
@@ -97,7 +48,7 @@ describe('<BooleanInput />', () => {
                 {...defaultProps}
                 source="foo"
                 input={{}}
-                meta={{ error: 'foobar' }}
+                meta={{ touched: true, error: 'foobar' }}
             />
         );
         expect(queryAllByText('foobar')).toHaveLength(1);
