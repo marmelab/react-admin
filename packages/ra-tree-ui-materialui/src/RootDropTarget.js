1b912c490662ebfe9233ce48a453db6cfcdfe6be
Mohammed Faragallah <o0frego0o@hotmail.com>
2019-07-26 22:32:11 +0200

    ra-tree-ui-materialui RootDropTarget/tree

--- packages/ra-tree-ui-materialui/src/RootDropTarget.js
+++ packages/ra-tree-ui-materialui/src/RootDropTarget.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import classNames from 'classnames';
 import { DropTarget } from 'react-dnd';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import ListItem from '@material-ui/core/ListItem';
 import IconGetApp from '@material-ui/icons/GetApp';
@@ -11,7 +11,7 @@ import { translate } from 'ra-core';
 
 import { DROP_TARGET_TYPE } from './constants';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
         paddingLeft: theme.spacing(6),
     },
@@ -23,51 +23,41 @@ const styles = theme => ({
     hover: {
         backgroundColor: theme.palette.action.active,
     },
-});
+}));
 
-class RootDropTarget extends Component {
-    static propTypes = {
-        canDrop: PropTypes.bool,
-        classes: PropTypes.object.isRequired,
-        connectDropTarget: PropTypes.func.isRequired,
-        isOverCurrent: PropTypes.bool,
-        translate: PropTypes.func.isRequired,
-    };
+const RootDropTarget = ({
+    canDrop,
+    connectDropTarget,
+    isOverCurrent,
+    translate,
+}) => {
+    const classes = useStyles();
 
-    shouldComponentUpdate(nextProps) {
-        return (
-            this.props.canDrop !== nextProps.canDrop ||
-            this.props.isOverCurrent !== nextProps.isOverCurrent
-        );
-    }
+    return (
+        <ListItem
+            className={classNames(classes.root, {
+                [classes.hover]: canDrop && isOverCurrent,
+            })}
+            disabled={!canDrop}
+        >
+            <IconGetApp />
+            {connectDropTarget(
+                <div>
+                    <Typography className={classes.text}>
+                        {translate('ra.tree.root_target')}
+                    </Typography>
+                </div>
+            )}
+        </ListItem>
+    );
+};
 
-    render() {
-        const {
-            canDrop,
-            classes,
-            connectDropTarget,
-            isOverCurrent,
-            translate,
-        } = this.props;
-        return (
-            <ListItem
-                className={classNames(classes.root, {
-                    [classes.hover]: canDrop && isOverCurrent,
-                })}
-                disabled={!canDrop}
-            >
-                <IconGetApp />
-                {connectDropTarget(
-                    <div>
-                        <Typography className={classes.text}>
-                            {translate('ra.tree.root_target')}
-                        </Typography>
-                    </div>
-                )}
-            </ListItem>
-        );
-    }
-}
+RootDropTarget.propTypes = {
+    canDrop: PropTypes.bool,
+    connectDropTarget: PropTypes.func.isRequired,
+    isOverCurrent: PropTypes.bool,
+    translate: PropTypes.func.isRequired,
+};
 
 const dropTargetSpecs = {
     drop(props, monitor) {
@@ -93,6 +83,12 @@ const dropTargetConnect = (connect, monitor) => ({
 
 export default compose(
     DropTarget(DROP_TARGET_TYPE, dropTargetSpecs, dropTargetConnect),
-    translate,
-    withStyles(styles)
-)(RootDropTarget);
+    translate
+)(
+    React.memo(RootDropTarget, (prevProps, nextProps) => {
+        return (
+            prevProps.canDrop !== nextProps.canDrop ||
+            prevProps.isOverCurrent !== nextProps.isOverCurrent
+        );
+    })
+);
