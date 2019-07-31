0d8a8640bac79ebc78ea58f117251a3c947318c7
fzaninotto <fzaninotto@gmail.com>
2019-07-31 09:26:32 +0200

    Fix bulk accept and delete in demo

--- examples/demo/src/reviews/BulkAcceptButton.js
+++ examples/demo/src/reviews/BulkAcceptButton.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ThumbUp from '@material-ui/icons/ThumbUp';
-import { Button, useMutation, UPDATE_MANY } from 'react-admin';
-
-const options = {
-    undoable: true,
-    onSuccess: {
-        notification: {
-            body: 'resources.reviews.notification.approved_success',
-            level: 'info',
-        },
-        redirectTo: '/reviews',
-    },
-    onFailure: {
-        notification: {
-            body: 'resources.reviews.notification.approved_error',
-            level: 'warning',
-        },
-    },
-};
+import {
+    Button,
+    useMutation,
+    useNotify,
+    useRedirect,
+    useUnselectAll,
+    UPDATE_MANY,
+} from 'react-admin';
 
 const BulkAcceptButton = ({ selectedIds }) => {
+    const notify = useNotify();
+    const redirect = useRedirect();
+    const unselectAll = useUnselectAll('reviews');
     const [approve, { loading }] = useMutation(
         {
             type: UPDATE_MANY,
             resource: 'reviews',
             payload: { ids: selectedIds, data: { status: 'accepted' } },
         },
-        options
+        {
+            undoable: true,
+            onSuccess: () => {
+                notify(
+                    'resources.reviews.notification.approved_success',
+                    'info',
+                    {},
+                    true
+                );
+                redirect('/reviews');
+                unselectAll();
+            },
+            onFailure: () =>
+                notify(
+                    'resources.reviews.notification.approved_error',
+                    'warning'
+                ),
+        }
     );
 
     return (
