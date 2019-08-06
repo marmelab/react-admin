de08fcd12320a995f0f89c1291566b3724271016
Gildas Garcia <gildas.garcia@gmail.com>
2019-08-06 14:13:49 +0200

    Fix after rebase

--- packages/ra-core/src/form/FormField.spec.tsx
+++ packages/ra-core/src/form/FormField.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form } from 'react-final-form';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup } from '@testing-library/react';
 import FormField from './FormField';
 
 describe('<FormField>', () => {
