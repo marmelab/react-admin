e19d287438cb11d30fae5439412bec9daf64f92f
Gildas Garcia <gildas.garcia@gmail.com>
2019-08-09 15:11:40 +0200

    Export useInput from ra-core

--- packages/ra-core/src/form/index.ts
+++ packages/ra-core/src/form/index.ts
@@ -1,9 +1,10 @@
 import addField from './addField';
 import FormDataConsumer from './FormDataConsumer';
 import FormField from './FormField';
+import useInput from './useInput';
 import ValidationError from './ValidationError';
 
-export { addField, FormDataConsumer, FormField, ValidationError };
+export { addField, FormDataConsumer, FormField, useInput, ValidationError };
 export { isRequired } from './FormField';
 export * from './validate';
 export * from './constants';
