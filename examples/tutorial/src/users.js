d4f5dbcd44c7564937858b63aa5bcd63c9ab1399
Gildas Garcia <gildas.garcia@gmail.com>
2019-08-09 16:30:51 +0200

    Make example code more readable

--- examples/tutorial/src/users.js
+++ examples/tutorial/src/users.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { useMediaQuery } from '@material-ui/core';
 import { SimpleList, List, Datagrid, EmailField, TextField } from 'react-admin';
 
-export const UserList = props => (
-    <List title="All users" {...props}>
-        {useMediaQuery(theme => theme.breakpoints.down('sm')) ? (
-            <SimpleList
-                primaryText={record => record.name}
-                secondaryText={record => record.username}
-                tertiaryText={record => record.email}
-            />
-        ) : (
-            <Datagrid>
-                <TextField source="id" />
-                <TextField source="name" />
-                <TextField source="username" />
-                <EmailField source="email" />
-            </Datagrid>
-        )}
-    </List>
-);
+export const UserList = props => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+
+    return (
+        <List title="All users" {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.name}
+                    secondaryText={record => record.username}
+                    tertiaryText={record => record.email}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    <TextField source="username" />
+                    <EmailField source="email" />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
