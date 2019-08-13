4bce2224725277a0a33f7c32eb4fb13e63b9ed0c
djhi <gildas.garcia@gmail.com>
2019-08-13 11:07:43 +0200

    [RFR] Migrate TextInput and LongTextInput to use useInput

--- packages/ra-ui-materialui/src/input/LongTextInput.js
+++ packages/ra-ui-materialui/src/input/LongTextInput.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import { addField } from 'ra-core';
-import { TextInput } from './TextInput';
+import TextInput from './TextInput';
 
 /**
  * @deprecated use <TextInput multiline /> instead
  */
 export const LongTextInput = props => {
-    console.warn(
-        'The LongTextInput component is deprecated. You should instead use the TextInput component and set its multiline and fullWidth props to true.'
-    );
-
     return <TextInput {...props} />;
 };
 
 LongTextInput.defaultProps = {
     multiline: true,
-};
-
-const EnhancedLongTextInput = addField(LongTextInput);
-
-EnhancedLongTextInput.defaultProps = {
     fullWidth: true,
 };
 
-export default EnhancedLongTextInput;
+export default LongTextInput;
