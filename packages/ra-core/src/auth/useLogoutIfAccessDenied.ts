55f25999314fdf5e9ed4539d428b0fbf60b828f9
fzaninotto <fzaninotto@gmail.com>
2019-09-08 22:17:33 +0200

    Use the new hook in dataProvider

--- packages/ra-core/src/auth/useLogoutIfAccessDenied.ts
+++ packages/ra-core/src/auth/useLogoutIfAccessDenied.ts
@@ -8,7 +8,7 @@ import { useNotify } from '../sideEffect';
 /**
  * Returns a callback used to call the authProvidr with the AUTH_ERROR verb
  * and an error from the dataProvider. If the authProvider rejects the call,
- * the hook logs the user out and shows a logget out notification.
+ * the hook logs the user out and shows a logged out notification.
  *
  * Used in the useDataProvider hook to check for access denied responses
  * (e.g. 401 or 403 responses) and trigger a logout.
@@ -36,19 +36,32 @@ import { useNotify } from '../sideEffect';
  *     // ...
  * }
  */
-const useLogoutIfAccessDenied = () => {
+const useLogoutIfAccessDenied = (): LogoutIfAccessDenied => {
     const authProvider = useAuthProvider();
     const logout = useLogout();
     const notify = useNotify();
     const logoutIfAccessDenied = useCallback(
         (error?: any) =>
-            authProvider(AUTH_ERROR, error).catch(e => {
-                logout();
-                notify('ra.notification.logged_out', 'warning');
-            }),
+            authProvider(AUTH_ERROR, error)
+                .then(() => false)
+                .catch(e => {
+                    logout();
+                    notify('ra.notification.logged_out', 'warning');
+                    return true;
+                }),
         [authProvider, logout, notify]
     );
     return logoutIfAccessDenied;
 };
+
+/**
+ * Call the authProvidr with the AUTH_ERROR verb and the error passed as argument.
+ * If the authProvider rejects the call, logs the user out and shows a logged out notification.
+ *
+ * @param {Error} error An Error object (usually returned by the dataProvider)
+ *
+ * @return {Promise} Resolved to true if there was a logout, false otherwise
+ */
+type LogoutIfAccessDenied = (error?: any) => Promise<boolean>;
 
 export default useLogoutIfAccessDenied;
