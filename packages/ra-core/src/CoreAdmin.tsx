9bdac1cc160634393a78efa51b2256bf4a809d21
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2019-09-12 10:21:09 -0300

    Revert #3661

--- packages/ra-core/src/CoreAdmin.tsx
+++ packages/ra-core/src/CoreAdmin.tsx
@@ -56,7 +56,6 @@ export interface AdminProps {
     menu?: ComponentType;
     theme?: object;
     title?: TitleComponent;
-    devToolsTrace?: boolean;
 }
 
 const CoreAdmin: FunctionComponent<AdminProps> = ({
@@ -80,7 +79,6 @@ const CoreAdmin: FunctionComponent<AdminProps> = ({
     i18nProvider,
     initialState,
     locale,
-    devToolsTrace = false,
 }) => {
     const reduxIsAlreadyInitialized = !!useContext(ReactReduxContext);
 
@@ -176,7 +174,6 @@ React-admin requires a valid dataProvider function to work.`);
                         initialState,
                         locale,
                         history: finalHistory,
-                        devToolsTrace,
                     })}
                 >
                     {renderCore(finalHistory)}
