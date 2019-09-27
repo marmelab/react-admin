7bcdd9e97182d3c822218f9bd69b624bb76a3a15
Clément Escolano <clement.escolano@icloud.com>
2019-09-27 13:48:41 +0200

    Mock fetch Headers to make tests work

--- test-setup.js
+++ test-setup.js
@@ -45,3 +45,13 @@ jest.mock('popper.js', () => {
     ];
     return Popper;
 });
+
+/**
+ * Mock fetch objects Response, Request and Headers
+ */
+
+const { Response, Headers, Request } = require('whatwg-fetch');
+
+global.Response = Response;
+global.Headers = Headers;
+global.Request = Request;
