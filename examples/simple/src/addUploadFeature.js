6a076adf4e823625c5ca5fe52a01c1b14a92e6ae
fzaninotto <fzaninotto@gmail.com>
2019-09-27 10:34:23 +0200

    rewrite DataProvider documentation

--- examples/simple/src/addUploadFeature.js
+++ examples/simple/src/addUploadFeature.js
@@ -1,3 +1,51 @@
+/**
+ * For posts update only, convert uploaded image in base 64 and attach it to
+ * the `picture` sent property, with `src` and `title` attributes.
+ */
+const addUploadCapabilities = dataProvider => ({
+    ...dataProvider,
+    update: (resource, params) => {
+        if (resource !== 'posts' || !params.data.pictures) {
+            // fallback to the default implementation
+            return dataProvider.update(resource, params);
+        }
+        // The posts edition form uses a file upload widget for the pictures field.
+        // Freshly dropped pictures are File objects
+        // and must be converted to base64 strings
+        const newPictures = params.data.pictures.filter(
+            p => p.rawFile instanceof File
+        );
+        const formerPictures = params.data.pictures.filter(
+            p => !(p.rawFile instanceof File)
+        );
+
+        return Promise.all(newPictures.map(convertFileToBase64))
+            .then(base64Pictures =>
+                base64Pictures.map(picture64 => ({
+                    src: picture64,
+                    title: `${params.data.title}`,
+                }))
+            )
+            .then(transformedNewPictures =>
+                dataProvider.update(resource, {
+                    ...params,
+                    data: {
+                        ...params.data,
+                        pictures: [
+                            ...transformedNewPictures,
+                            ...formerPictures,
+                        ],
+                    },
+                })
+            );
+    },
+});
+
+/**
+ * Convert a `File` object returned by the upload input into a base 64 string.
+ * That's not the most optimized way to store images in production, but it's
+ * enough to illustrate the idea of data provider decoration.
+ */
 const convertFileToBase64 = file =>
     new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -5,45 +53,6 @@ const convertFileToBase64 = file =>
 
         reader.onload = () => resolve(reader.result);
         reader.onerror = reject;
-    });
-
-const addUploadCapabilities = dataProvider =>
-    new Proxy(dataProvider, {
-        get: (target, name) => (resource, params) => {
-            if (name === 'update' && resource === 'posts') {
-                if (params.data.pictures && params.data.pictures.length) {
-                    // only freshly dropped pictures are instance of File
-                    const formerPictures = params.data.pictures.filter(
-                        p => !(p.rawFile instanceof File)
-                    );
-                    const newPictures = params.data.pictures.filter(
-                        p => p.rawFile instanceof File
-                    );
-
-                    return Promise.all(newPictures.map(convertFileToBase64))
-                        .then(base64Pictures =>
-                            base64Pictures.map(picture64 => ({
-                                src: picture64,
-                                title: `${params.data.title}`,
-                            }))
-                        )
-                        .then(transformedNewPictures =>
-                            dataProvider.update(resource, {
-                                ...params,
-                                data: {
-                                    ...params.data,
-                                    pictures: [
-                                        ...transformedNewPictures,
-                                        ...formerPictures,
-                                    ],
-                                },
-                            })
-                        );
-                }
-            }
-
-            return dataProvider[name](resource, params);
-        },
     });
 
 export default addUploadCapabilities;
