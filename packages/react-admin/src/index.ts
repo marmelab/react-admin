9d19a236489b46e9b70c9c70a90a81f7caa2ce4d
Gildas Garcia <gildas.garcia@gmail.com>
2019-10-14 12:17:53 +0200

    Fix defaultI18nProvider Export Fixes #3819

--- packages/react-admin/src/index.ts
+++ packages/react-admin/src/index.ts
@@ -1,6 +1,7 @@
 import Admin from './Admin';
 import AdminRouter from './AdminRouter';
+import defaultI18nProvider from './defaultI18nProvider';
 
 export * from 'ra-core';
 export * from 'ra-ui-materialui';
-export { Admin, AdminRouter };
+export { Admin, AdminRouter, defaultI18nProvider };
