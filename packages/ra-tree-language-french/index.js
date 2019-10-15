c2c816bbb34d4e38ae267401681106277ba25910
Gildas Garcia <gildas.garcia@gmail.com>
2019-10-15 21:02:40 +0200

    Update packages/ra-tree-language-french/index.js
    Co-Authored-By: Francois Zaninotto <francois@marmelab.com>

--- packages/ra-tree-language-french/index.js
+++ packages/ra-tree-language-french/index.js
@@ -2,7 +2,7 @@ module.exports = {
     ra: {
         tree: {
             add_child_node: 'Ajouter un enfant',
-            add_node_after: 'Insérer un noeud après',
+            add_node_after: 'Insérer un nœud après',
             add_node_before: 'Insérer un noeud avant',
             close: 'Fermer',
             expand: 'Ouvre',
