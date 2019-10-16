e2f674d8c2df733b49e7966422eaef6af80e2ae2
Gildas Garcia <gildas.garcia@gmail.com>
2019-10-16 14:41:51 +0200

    Refactor nodes reducer

--- packages/ra-tree-core/src/reducers/nodes.ts
+++ packages/ra-tree-core/src/reducers/nodes.ts
@@ -6,125 +6,36 @@ import {
 } from '../actions';
 import { ROOT_NODE_ID } from '../constants';
 
-type IdentifierArray = Identifier[];
-
-export interface IdentifierArrayWithDate extends IdentifierArray {
-    fetchedAt?: Date;
-}
-
 type MapOfIdentifierArray = {
-    [key: string]: IdentifierArrayWithDate | boolean;
+    [key: string]: Identifier[] | boolean;
 };
 
 type State = MapOfIdentifierArray;
 
+/**
+ * This reducer maintains a map of all nodes (using their id) and their children (only their id too).
+ * Root nodes are persisted as the children of a special node which has the identifier ROOT_NODE_ID
+ * @example
+ * {
+ *     ROOT_NODE_ID: [1, 2],
+ *     1: [3, 4],
+ *     2: [5, 6],
+ * }
+ */
 const nodesReducer = (
     previousState: State = { [ROOT_NODE_ID]: [] },
     action
 ) => {
     if (action.meta && action.meta.optimistic) {
         if (action.meta.fetch === MOVE_NODE) {
-            // We need to remove this node from its previous parent as it may have changed
-            const previousParentId =
-                action.payload.previousData[action.meta.parentSource] ||
-                ROOT_NODE_ID;
-
-            const newState = {
-                ...previousState,
-                [previousParentId]: (previousState[
-                    previousParentId
-                ] as IdentifierArray).filter(
-                    id => id !== action.payload.data.id
-                ),
-            };
-
-            const newParentId =
-                action.payload.data[action.meta.parentSource] || ROOT_NODE_ID;
-            // The new parent may not have any child yet
-            const currentParentChildren = newState[newParentId] || [];
-
-            if (action.meta.positionSource) {
-                // Then we need to update the new parent (which may be the same) and the node position
-                newState[newParentId] = [
-                    ...currentParentChildren.slice(
-                        0,
-                        action.payload.data[action.meta.positionSource]
-                    ),
-                    action.payload.data.id,
-                    ...currentParentChildren.slice(
-                        action.payload.data[action.meta.positionSource]
-                    ),
-                ];
-                return newState;
-            }
-
-            newState[newParentId] = [
-                ...currentParentChildren,
-                action.payload.data.id,
-            ];
-
-            return newState;
+            return moveNode(previousState, action);
         }
 
         if (action.meta.fetch === DELETE) {
-            const idToRemove = action.payload.id;
-
-            const newState = Object.keys(previousState).reduce((acc, id) => {
-                if (id === idToRemove || acc[id] === false) {
-                    return acc;
-                }
-
-                const children = acc[id] as IdentifierArray;
-                const index = children.findIndex(
-                    nodeId => nodeId === idToRemove
-                );
-
-                if (index === -1) {
-                    return acc;
-                }
-
-                return {
-                    ...acc,
-                    [id]: [
-                        ...children.slice(0, index),
-                        ...children.slice(index + 1),
-                    ],
-                };
-            }, previousState);
-
-            return newState;
+            return removeNodes(previousState, [action.payload.id]);
         }
         if (action.meta.fetch === DELETE_MANY) {
-            const newState = action.payload.ids.reduce(
-                (acc, idToRemove) =>
-                    Object.keys(acc).reduce((acc2, id) => {
-                        if (id === idToRemove || acc[id] === false) {
-                            return acc2;
-                        }
-
-                        const index = acc[id].findIndex(
-                            nodeId => nodeId === idToRemove
-                        );
-
-                        if (index === -1) {
-                            return {
-                                ...acc2,
-                                [id]: acc[id],
-                            };
-                        }
-
-                        return {
-                            ...acc2,
-                            [id]: [
-                                ...acc[id].slice(0, index),
-                                ...acc[id].slice(index + 1),
-                            ],
-                        };
-                    }, {}),
-                previousState
-            );
-
-            return newState;
+            return removeNodes(previousState, action.payload.ids);
         }
     }
 
@@ -134,16 +45,7 @@ const nodesReducer = (
                 ...previousState,
                 [ROOT_NODE_ID]: action.payload.data.map(({ id }) => id),
                 // Initialize the children for all root nodes if necessary
-                ...action.payload.data.reduce(
-                    (acc, { id }) => ({
-                        ...acc,
-                        [id]:
-                            previousState[id] != undefined // eslint-disable-line eqeqeq
-                                ? previousState[id]
-                                : [],
-                    }),
-                    {}
-                ),
+                ...setupNodesChildren(previousState, action.payload.data),
             };
 
             return newState;
@@ -157,16 +59,7 @@ const nodesReducer = (
                         ? action.payload.data.map(({ id }) => id)
                         : false,
                 // Initialize the children for all the children nodes if necessary
-                ...action.payload.data.reduce(
-                    (acc, { id }) => ({
-                        ...acc,
-                        [id]:
-                            previousState[id] != undefined // eslint-disable-line eqeqeq
-                                ? previousState[id]
-                                : [],
-                    }),
-                    {}
-                ),
+                ...setupNodesChildren(previousState, action.payload.data),
             };
 
             return newState;
@@ -177,3 +70,94 @@ const nodesReducer = (
 };
 
 export default nodesReducer;
+
+const setupNodesChildren = (previousState: State, nodes) =>
+    nodes.reduce(
+        (acc, { id }) => ({
+            ...acc,
+            [id]:
+                previousState[id] != undefined // eslint-disable-line eqeqeq
+                    ? previousState[id]
+                    : [],
+        }),
+        {}
+    );
+
+const moveNode = (previousState: State, action) => {
+    // We need to remove this node from its previous parent as it may have changed
+    const previousParentId =
+        action.payload.previousData[action.meta.parentSource] || ROOT_NODE_ID;
+
+    const updatedPreviousParent = (previousState[
+        previousParentId
+    ] as Identifier[]).filter(id => id !== action.payload.data.id);
+
+    const newState = {
+        ...previousState,
+        [previousParentId]: updatedPreviousParent,
+    };
+
+    // Then we need to update the new parent (which may be the same)
+    const newParentId =
+        action.payload.data[action.meta.parentSource] || ROOT_NODE_ID;
+    // The new parent may not have any child yet
+    const currentParentChildren = newState[newParentId] || [];
+
+    // We may have to update the node position
+    if (action.meta.positionSource) {
+        const childrenBeforeMovedNode = currentParentChildren.slice(
+            0,
+            action.payload.data[action.meta.positionSource]
+        );
+
+        const childrenAfterMovedNode = currentParentChildren.slice(
+            action.payload.data[action.meta.positionSource]
+        );
+
+        newState[newParentId] = [
+            ...childrenBeforeMovedNode,
+            action.payload.data.id,
+            ...childrenAfterMovedNode,
+        ];
+        return newState;
+    }
+
+    // Otherwise, we just append the moved node to its new parent
+    newState[newParentId] = [...currentParentChildren, action.payload.data.id];
+
+    return newState;
+};
+
+const removeNodes = (previousState: State, idsToRemove: Identifier[]) =>
+    idsToRemove.reduce(
+        (newState, idToRemove) =>
+            // Traverse all registered nodes and remove the node from their children
+            Object.keys(newState).reduce((acc, id) => {
+                // If this is a removed node or this node has no children (false indicates we fetched them but found none)
+                if (id === idToRemove || newState[id] === false) {
+                    return acc;
+                }
+
+                const children = newState[id] as Identifier[];
+
+                const index = children.findIndex(
+                    nodeId => nodeId === idToRemove
+                );
+
+                if (index === -1) {
+                    return {
+                        ...acc,
+                        [id]: children,
+                    };
+                }
+
+                return {
+                    ...acc,
+                    [id]: [
+                        ...children.slice(0, index),
+                        ...children.slice(index + 1),
+                    ],
+                };
+            }, {}),
+        previousState
+    );
