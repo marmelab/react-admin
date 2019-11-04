e8e6e3f5cee5530979fc10c35e6d961ecd3ed352
fzaninotto <fzaninotto@gmail.com>
2019-11-04 21:45:23 +0100

    Fix overly aggressive EditButton memoization
    Closes #3910

--- packages/ra-ui-materialui/src/button/EditButton.js
+++ packages/ra-ui-materialui/src/button/EditButton.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import shouldUpdate from 'recompose/shouldUpdate';
 import ContentCreate from '@material-ui/icons/Create';
 import { Link } from 'react-router-dom';
 import { linkToRecord } from 'ra-core';
@@ -37,14 +36,4 @@ EditButton.propTypes = {
     icon: PropTypes.element,
 };
 
-const enhance = shouldUpdate(
-    (props, nextProps) =>
-        props.translate !== nextProps.translate ||
-        (props.record &&
-            nextProps.record &&
-            props.record.id !== nextProps.record.id) ||
-        props.basePath !== nextProps.basePath ||
-        (props.record == null && nextProps.record != null)
-);
-
-export default enhance(EditButton);
+export default EditButton;
