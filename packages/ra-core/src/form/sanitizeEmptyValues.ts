e1a2f9eec75443ca7e187eedaa5b540363646d9e
fzaninotto <fzaninotto@gmail.com>
2019-11-29 08:24:10 +0100

    Fix typos

--- packages/ra-core/src/form/sanitizeEmptyValues.ts
+++ packages/ra-core/src/form/sanitizeEmptyValues.ts
@@ -3,15 +3,15 @@ import merge from 'lodash/merge';
 /**
  * Because final-form removes undefined and empty string values completely
  * (the key for the empty field is removed from the values), we have to check
- * wether this value was initially provided so that it is correctly sent to
+ * whether this value was initially provided so that it is correctly sent to
  * the backend.
- * See https://github.com/final-form/react-final-form/issues/130#issuecomment-493447888
+ * @see https://github.com/final-form/react-final-form/issues/130#issuecomment-493447888
  *
  * @param initialValues The initial values provided to the form
  * @param values The current form values
  */
 const sanitizeEmptyValues = (initialValues: object, values: object) => {
-    // For every field initialy provided, we check wether it value has been removed
+    // For every field initially provided, we check whether it value has been removed
     // and set it explicitly to an empty string
     if (!initialValues) return values;
     const initialValuesWithEmptyFields = Object.keys(initialValues).reduce(
@@ -32,7 +32,7 @@ const sanitizeEmptyValues = (initialValues: object, values: object) => {
         {}
     );
 
-    // Finaly, we merge back the values to not miss any which wasn't initialy provided
+    // Finally, we merge back the values to not miss any which wasn't initially provided
     return merge(initialValuesWithEmptyFields, values);
 };
 
