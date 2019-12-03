bb1b0be82417d865eadedd60b631d47765d4440e
fzaninotto <fzaninotto@gmail.com>
2019-12-03 16:14:13 +0100

    Use context instead of props to get exporter

--- packages/ra-ui-materialui/src/button/ExportButton.tsx
+++ packages/ra-ui-materialui/src/button/ExportButton.tsx
@@ -1,19 +1,17 @@
-import React, { useCallback, FunctionComponent } from 'react';
+import React, { useCallback, useContext, FunctionComponent } from 'react';
 import PropTypes from 'prop-types';
 import DownloadIcon from '@material-ui/icons/GetApp';
 import {
-    downloadCSV,
+    fetchRelatedRecords,
     useDataProvider,
     useNotify,
     Sort,
     DataProvider,
+    ExporterContext,
 } from 'ra-core';
-import jsonExport from 'jsonexport/dist';
-
 import Button, { ButtonProps } from './Button';
 
 const ExportButton: FunctionComponent<ExportButtonProps> = ({
-    exporter,
     sort,
     filter = defaultFilter,
     maxResults = 1000,
@@ -23,6 +21,7 @@ const ExportButton: FunctionComponent<ExportButtonProps> = ({
     icon = defaultIcon,
     ...rest
 }) => {
+    const exporter = useContext(ExporterContext);
     const dataProvider = useDataProvider();
     const notify = useNotify();
     const handleClick = useCallback(
@@ -33,16 +32,15 @@ const ExportButton: FunctionComponent<ExportButtonProps> = ({
                     filter,
                     pagination: { page: 1, perPage: maxResults },
                 })
-                .then(({ data }) =>
-                    exporter
-                        ? exporter(
-                              data,
-                              fetchRelatedRecords(dataProvider),
-                              dataProvider
-                          )
-                        : jsonExport(data, (err, csv) =>
-                              downloadCSV(csv, resource)
-                          )
+                .then(
+                    ({ data }) =>
+                        exporter &&
+                        exporter(
+                            data,
+                            fetchRelatedRecords(dataProvider),
+                            dataProvider,
+                            resource
+                        )
                 )
                 .catch(error => {
                     console.error(error);
@@ -75,68 +73,16 @@ const ExportButton: FunctionComponent<ExportButtonProps> = ({
     );
 };
 
-/**
- * Extracts, aggregates and deduplicates the ids of related records
- *
- * @example
- *     const books = [
- *         { id: 1, author_id: 123, title: 'Pride and Prejudice' },
- *         { id: 2, author_id: 123, title: 'Sense and Sensibility' },
- *         { id: 3, author_id: 456, title: 'War and Peace' },
- *     ];
- *     getRelatedIds(books, 'author_id'); => [123, 456]
- *
- * @example
- *     const books = [
- *         { id: 1, tag_ids: [1, 2], title: 'Pride and Prejudice' },
- *         { id: 2, tag_ids: [2, 3], title: 'Sense and Sensibility' },
- *         { id: 3, tag_ids: [4], title: 'War and Peace' },
- *     ];
- *     getRelatedIds(records, 'tag_ids'); => [1, 2, 3, 4]
- *
- * @param {Object[]} records An array of records
- * @param {string} field the identifier of the record field to use
- */
-export const getRelatedIds = (records, field) =>
-    Array.from(
-        new Set(
-            records
-                .filter(record => record[field] != null)
-                .map(record => record[field])
-                .reduce((ids, value) => ids.concat(value), [])
-        )
-    );
-
-/**
- * Helper function for calling the data provider with GET_MANY
- * via redux and saga, and getting a Promise in return
- *
- * @example
- *     fetchRelatedRecords(records, 'post_id', 'posts').then(posts =>
- *          posts.map(record => ({
- *              ...record,
- *              post_title: posts[record.post_id].title,
- *          }));
- */
-const fetchRelatedRecords = dataProvider => (data, field, resource) =>
-    dataProvider
-        .getMany(resource, { ids: getRelatedIds(data, field) })
-        .then(({ data }) =>
-            data.reduce((acc, post) => {
-                acc[post.id] = post;
-                return acc;
-            }, {})
-        );
-
 const defaultIcon = <DownloadIcon />;
 const defaultFilter = {};
 
 const sanitizeRestProps = ({
     basePath,
+    exporter,
     ...rest
 }: Omit<
     ExportButtonProps,
-    'exporter' | 'sort' | 'filter' | 'maxResults' | 'resource' | 'label'
+    'sort' | 'filter' | 'maxResults' | 'resource' | 'label'
 >) => rest;
 
 interface Props {
