c36177657d89793c8c33e599164bbfbecdff7ae0
fzaninotto <fzaninotto@gmail.com>
2019-12-05 17:27:58 +0100

    Use SimpleForm instead of TabbedForm in VisitorCreate

--- examples/demo/src/visitors/VisitorCreate.js
+++ examples/demo/src/visitors/VisitorCreate.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Create, DateInput, FormTab, TabbedForm, TextInput } from 'react-admin';
+import {
+    Create,
+    DateInput,
+    SimpleForm,
+    TextInput,
+    useTranslate,
+    required,
+} from 'react-admin';
+import { Typography, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 export const styles = {
@@ -23,45 +31,54 @@ const VisitorCreate = props => {
     const classes = useStyles();
     return (
         <Create {...props}>
-            <TabbedForm>
-                <FormTab label="resources.customers.tabs.identity">
-                    <TextInput
-                        autoFocus
-                        source="first_name"
-                        formClassName={classes.first_name}
-                    />
-                    <TextInput
-                        source="last_name"
-                        formClassName={classes.last_name}
-                    />
-                    <TextInput
-                        type="email"
-                        source="email"
-                        validation={{ email: true }}
-                        fullWidth={true}
-                        formClassName={classes.email}
-                    />
-                    <DateInput source="birthday" />
-                </FormTab>
-                <FormTab
-                    label="resources.customers.tabs.address"
-                    path="address"
-                >
-                    <TextInput
-                        source="address"
-                        formClassName={classes.address}
-                        multiline={true}
-                        fullWidth={true}
-                    />
-                    <TextInput
-                        source="zipcode"
-                        formClassName={classes.zipcode}
-                    />
-                    <TextInput source="city" formClassName={classes.city} />
-                </FormTab>
-            </TabbedForm>
+            <SimpleForm>
+                <SectionTitle label="resources.customers.fieldGroups.identity" />
+                <TextInput
+                    autoFocus
+                    source="first_name"
+                    formClassName={classes.first_name}
+                    validate={requiredValidate}
+                />
+                <TextInput
+                    source="last_name"
+                    formClassName={classes.last_name}
+                    validate={requiredValidate}
+                />
+                <TextInput
+                    type="email"
+                    source="email"
+                    validation={{ email: true }}
+                    fullWidth={true}
+                    formClassName={classes.email}
+                    validate={requiredValidate}
+                />
+                <DateInput source="birthday" />
+                <Separator />
+                <SectionTitle label="resources.customers.fieldGroups.address" />
+                <TextInput
+                    source="address"
+                    formClassName={classes.address}
+                    multiline={true}
+                    fullWidth={true}
+                />
+                <TextInput source="zipcode" formClassName={classes.zipcode} />
+                <TextInput source="city" formClassName={classes.city} />
+            </SimpleForm>
         </Create>
     );
 };
+
+const requiredValidate = [required()];
+
+const SectionTitle = ({ label }) => {
+    const translate = useTranslate();
+    return (
+        <Typography variant="h6" gutterBottom>
+            {translate(label)}
+        </Typography>
+    );
+};
+
+const Separator = () => <Box pt="1em" />;
 
 export default VisitorCreate;
