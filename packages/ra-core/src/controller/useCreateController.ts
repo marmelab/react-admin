de36ea449606565f6c64e11e6bb9054836188290
Carson Full <carson_full@tsco.org>
2020-01-03 16:51:41 -0600

    Update useCreateController to use useLocation

--- packages/ra-core/src/controller/useCreateController.ts
+++ packages/ra-core/src/controller/useCreateController.ts
@@ -3,7 +3,7 @@ import { useCallback } from 'react';
 import inflection from 'inflection';
 import { parse } from 'query-string';
 import { Location } from 'history';
-import { match as Match } from 'react-router-dom';
+import { match as Match, useLocation } from 'react-router-dom';
 
 import { useCheckMinimumRequiredProps } from './checkMinimumRequiredProps';
 import { useCreate } from '../dataProvider';
@@ -39,8 +39,8 @@ export interface CreateProps {
     hasEdit?: boolean;
     hasList?: boolean;
     hasShow?: boolean;
-    location: Location;
-    match: Match;
+    location?: Location;
+    match?: Match;
     record?: Partial<Record>;
     resource: string;
     successMessage?: string;
@@ -64,21 +64,17 @@ export interface CreateProps {
  * }
  */
 const useCreateController = (props: CreateProps): CreateControllerProps => {
-    useCheckMinimumRequiredProps(
-        'Create',
-        ['basePath', 'location', 'resource'],
-        props
-    );
+    useCheckMinimumRequiredProps('Create', ['basePath', 'resource'], props);
     const {
         basePath,
         resource,
-        location,
         record = {},
         hasShow,
         hasEdit,
         successMessage,
     } = props;
 
+    const location = useLocation();
     const translate = useTranslate();
     const notify = useNotify();
     const redirect = useRedirect();
