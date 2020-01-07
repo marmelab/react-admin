3727948bd847e8a488c12e7b3d587f078e5f6ea9
thiery <thiery@marmelab.com>
2020-01-07 17:06:56 +0100

    add addRecords and add test on data GET_MANY_REFERENCE and GET_MANY action

--- packages/ra-core/src/reducer/admin/resource/data.spec.ts
+++ packages/ra-core/src/reducer/admin/resource/data.spec.ts
@@ -1,8 +1,15 @@
 import assert from 'assert';
 
-import { DELETE, DELETE_MANY, UPDATE } from '../../../core';
+import {
+    DELETE,
+    DELETE_MANY,
+    UPDATE,
+    GET_MANY,
+    GET_MANY_REFERENCE,
+} from '../../../core';
 import getFetchedAt from '../../../util/getFetchedAt';
 import dataReducer, { replaceRecords, addOneRecord } from './data';
+import { FETCH_END } from '../../../actions';
 
 jest.mock('../../../util/getFetchedAt');
 
@@ -251,6 +258,55 @@ describe('Resources data reducer', () => {
             assert.deepEqual(newState.fetchedAt.record3, before);
 
             assert.notDeepEqual(newState.fetchedAt.record2, before);
+        });
+    });
+
+    describe.each([GET_MANY_REFERENCE, GET_MANY])('%s', actionType => {
+        it('should add new records to the old one', () => {
+            const before = new Date(0);
+            const now = new Date();
+
+            // @ts-ignore
+            getFetchedAt.mockImplementationOnce(() => ({
+                new_record: now,
+                record2: now,
+            }));
+
+            const state = {
+                record1: { id: 'record1', prop: 'value' },
+                record2: { id: 'record2', prop: 'value' },
+                record3: { id: 'record3', prop: 'value' },
+                fetchedAt: {
+                    record1: before,
+                    record2: before,
+                    record3: before,
+                },
+            };
+
+            const newState = dataReducer(state, {
+                type: actionType,
+                payload: {
+                    data: [
+                        { id: 'record2', prop: 'updated value' },
+                        { id: 'new_record', prop: 'new value' },
+                    ],
+                },
+                meta: {
+                    fetchResponse: actionType,
+                    fetchStatus: FETCH_END,
+                },
+            });
+            assert.deepEqual(newState, {
+                record1: { id: 'record1', prop: 'value' },
+                record2: { id: 'record2', prop: 'updated value' },
+                record3: { id: 'record3', prop: 'value' },
+                new_record: { id: 'new_record', prop: 'new value' },
+            });
+            assert.deepEqual(newState.fetchedAt.record1, before);
+            assert.deepEqual(newState.fetchedAt.record3, before);
+
+            assert.notDeepEqual(newState.fetchedAt.record2, before);
+            assert.notDeepEqual(newState.fetchedAt.new_record, before);
         });
     });
 });
