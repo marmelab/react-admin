3d4aafc6497e8954dbb7748761099e2897178175
fzaninotto <fzaninotto@gmail.com>
2020-02-03 15:26:04 +0100

    Explain change

--- packages/ra-ui-materialui/src/input/InputHelperText.tsx
+++ packages/ra-ui-materialui/src/input/InputHelperText.tsx
@@ -19,6 +19,11 @@ const InputHelperText: FunctionComponent<Props> = ({
     ) : helperText ? (
         <>{translate(helperText, { _: helperText })}</>
     ) : (
+        // material-ui's HelperText cannot reserve space unless we pass a single
+        // space as child, which isn't possible when the child is a component.
+        // Therefore, we must reserve the space ourselves by passing the same
+        // markup as material-ui.
+        // @see https://github.com/mui-org/material-ui/blob/62e439b7022d519ab638d65201e204b59b77f8da/packages/material-ui/src/FormHelperText/FormHelperText.js#L85-L90
         // eslint-disable-next-line react/no-danger
         <span dangerouslySetInnerHTML={{ __html: '&#8203;' }} />
     );
