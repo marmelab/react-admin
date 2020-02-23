76dcd8af96c5cb0648e61ac53f31744abab81b32
Joseph Cheung <josephktcheung@gmail.com>
2020-02-23 10:31:22 +0800

    Convert BulkDeleteWithConfirmButton to typescript

--- packages/ra-core/src/dataProvider/useDeleteMany.ts
+++ packages/ra-core/src/dataProvider/useDeleteMany.ts
@@ -27,7 +27,7 @@ import { Identifier } from '../types';
  *     return <button disabled={loading} onClick={deleteMany}>Delete selected posts</button>;
  * };
  */
-const useDeleteMany = (resource: string, ids: [Identifier], options?: any) =>
+const useDeleteMany = (resource: string, ids: Identifier[], options?: any) =>
     useMutation({ type: 'deleteMany', resource, payload: { ids } }, options);
 
 export default useDeleteMany;
