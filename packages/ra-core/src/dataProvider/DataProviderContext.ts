53c5431768b194d33e88d3f371e8d390b6401530
fzaninotto <fzaninotto@gmail.com>
2020-02-25 23:57:52 +0100

    add unit tests for useGetList

--- packages/ra-core/src/dataProvider/DataProviderContext.ts
+++ packages/ra-core/src/dataProvider/DataProviderContext.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 
 import { DataProvider } from '../types';
 
-const DataProviderContext = createContext<DataProvider>(null);
+const DataProviderContext = createContext<Partial<DataProvider>>(null);
 
 DataProviderContext.displayName = 'DataProviderContext';
 
