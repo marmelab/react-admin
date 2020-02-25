fdc246dc24b038da0f8f43ecf6716e244b90fa18
Matheus Wichman <matheushw@outlook.com>
2020-02-25 19:05:42 -0300

    Migrate DataGridCell tests to react-testing-library

--- packages/ra-ui-materialui/src/list/DatagridCell.spec.js
+++ packages/ra-ui-materialui/src/list/DatagridCell.spec.js
@@ -1,12 +1,21 @@
 import assert from 'assert';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { shallow } from 'enzyme';
+import { render, cleanup } from '@testing-library/react';
 
 import DatagridCell from './DatagridCell';
 
+const renderWithTable = element =>
+    render(
+        <table>
+            <tbody>
+                <tr>{element}</tr>
+            </tbody>
+        </table>
+    );
+
 describe('<DatagridCell />', () => {
-    const Field = () => <div />;
+    const Field = ({ basePath }) => <div>{basePath}</div>;
     Field.propTypes = {
         type: PropTypes.string,
         basePath: PropTypes.string,
@@ -16,25 +25,24 @@ describe('<DatagridCell />', () => {
         type: 'foo',
     };
 
-    it('should render as a mui <TableRowColumn /> component', () => {
-        const wrapper = shallow(<DatagridCell field={<Field />} />);
-        const col = wrapper.find('WithStyles(ForwardRef(TableCell))');
-        assert.equal(col.length, 1);
+    it('should render as a mui <TableCell /> component', () => {
+        const { getByRole } = renderWithTable(
+            <DatagridCell field={<Field />} />
+        );
+        assert.equal(getByRole('cell').className, 'MuiTableCell-root');
     });
 
     it('should pass the Datagrid basePath by default', () => {
-        const wrapper = shallow(
+        const { queryByText } = renderWithTable(
             <DatagridCell basePath="default" field={<Field />} />
         );
-        const col = wrapper.find('Field');
-        assert.equal(col.prop('basePath'), 'default');
+        assert.notEqual(queryByText('default'), null);
     });
 
     it('should allow to overwrite the `basePath` field', () => {
-        const wrapper = shallow(
+        const { queryByText } = renderWithTable(
             <DatagridCell basePath="default" field={<Field basePath="new" />} />
         );
-        const col = wrapper.find('Field');
-        assert.equal(col.prop('basePath'), 'new');
+        assert.notEqual(queryByText('new'), null);
     });
 });
