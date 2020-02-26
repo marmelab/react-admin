36635530bed3b6d66f1888d1b19715b64a3a6bf7
Joseph Cheung <josephktcheung@gmail.com>
2020-02-26 23:03:15 +0800

    Import Theme from material-ui

--- packages/ra-ui-materialui/src/layout/AppBar.tsx
+++ packages/ra-ui-materialui/src/layout/AppBar.tsx
@@ -8,7 +8,7 @@ import MuiAppBar, {
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { toggleSidebar } from 'ra-core';
@@ -62,7 +62,9 @@ const AppBar: FC<AppBarProps> = ({
 }) => {
     const classes = useStyles({ classes: classesOverride });
     const dispatch = useDispatch();
-    const isXSmall = useMediaQuery<Theme>(theme => theme.breakpoints.down('xs'));
+    const isXSmall = useMediaQuery<Theme>(theme =>
+        theme.breakpoints.down('xs')
+    );
 
     return (
         <HideOnScroll>
