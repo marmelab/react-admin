5097061043a60a6cafb6b4995a4b032bcc055ed2
Joseph Cheung <josephktcheung@gmail.com>
2020-03-12 00:37:32 +0800

    Convert routes and themeReducer to ts

--- examples/demo/src/themeReducer.ts
+++ examples/demo/src/themeReducer.ts
@@ -1,6 +1,9 @@
 import { CHANGE_THEME } from './configuration/actions';
 
-export default (previousState = 'light', { type, payload }) => {
+export default (
+    previousState = 'light',
+    { type, payload }: { type: string; payload: any }
+) => {
     if (type === CHANGE_THEME) {
         return payload;
     }
