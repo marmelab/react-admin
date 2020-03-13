f38f10b3d1f39c0bdd9d79c04df2e5e139229822
Joseph Cheung <josephktcheung@gmail.com>
2020-03-13 08:03:56 +0800

    Convert NbItemField to ts

---
+++ examples/demo/src/orders/NbItemsField.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import { FunctionField } from 'react-admin';
+import { Order, FieldProps } from '../types';
+
+const render = (record: Order) => record.basket.length;
+
+interface NbItemsFieldProps extends FieldProps {
+    textAlign?: string;
+}
+
+const NbItemsField: FC<NbItemsFieldProps> = props => (
+    <FunctionField {...props} render={render} />
+);
+
+NbItemsField.defaultProps = {
+    label: 'Nb Items',
+    textAlign: 'right',
+};
+
+export default NbItemsField;
