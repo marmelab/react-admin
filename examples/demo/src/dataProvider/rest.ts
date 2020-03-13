91a0f6911ac3c0e656564ba2505698261a847d91
Joseph Cheung <josephktcheung@gmail.com>
2020-03-13 23:15:28 +0800

    Properly type rest.ts

--- examples/demo/src/dataProvider/rest.ts
+++ examples/demo/src/dataProvider/rest.ts
@@ -6,12 +6,12 @@ const delayedDataProvider = new Proxy(restProvider, {
     get: (target, name, self) =>
         name === 'then' // as we await for the dataProvider, JS calls then on it. We must trap that call or else the dataProvider will be called with the then method
             ? self
-            : (resource: any, params: any) =>
+            : (resource: string, params: any) =>
                   new Promise(resolve =>
                       setTimeout(
                           () =>
                               resolve(
-                                  restProvider[name as any](resource, params)
+                                  restProvider[name as string](resource, params)
                               ),
                           500
                       )
