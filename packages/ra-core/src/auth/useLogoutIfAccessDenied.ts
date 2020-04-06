32a725e5a43df77fd63b7f3088747adc94275323
djhi <1122076+djhi@users.noreply.github.com>
2020-04-06 18:11:20 +0200

    Avoid Multiple Notifications on Expired Session

--- packages/ra-core/src/auth/useLogoutIfAccessDenied.ts
+++ packages/ra-core/src/auth/useLogoutIfAccessDenied.ts
@@ -4,6 +4,8 @@ import useAuthProvider from './useAuthProvider';
 import useLogout from './useLogout';
 import { useNotify } from '../sideEffect';
 
+let authCheckPromise;
+
 /**
  * Returns a callback used to call the authProvider.checkError() method
  * and an error from the dataProvider. If the authProvider rejects the call,
@@ -40,21 +42,32 @@ const useLogoutIfAccessDenied = (): LogoutIfAccessDenied => {
     const logout = useLogout();
     const notify = useNotify();
     const logoutIfAccessDenied = useCallback(
-        (error?: any) =>
-            authProvider
-                .checkError(error)
-                .then(() => false)
-                .catch(e => {
-                    const redirectTo =
-                        e && e.redirectTo
-                            ? e.redirectTo
-                            : error && error.redirectTo
-                            ? error.redirectto
-                            : undefined;
-                    logout({}, redirectTo);
-                    notify('ra.notification.logged_out', 'warning');
-                    return true;
-                }),
+        (error?: any) => {
+            // Sometimes, a component might trigger multiple simultaneous
+            // dataProvider calls which all fail and call this function.
+            // To avoid having multiple notifications, we first verify if
+            // a checkError promise is already ongoing
+            if (!authCheckPromise) {
+                authCheckPromise = authProvider
+                    .checkError(error)
+                    .then(() => false)
+                    .catch(async e => {
+                        const redirectTo =
+                            e && e.redirectTo
+                                ? e.redirectTo
+                                : error && error.redirectTo
+                                ? error.redirectto
+                                : undefined;
+                        logout({}, redirectTo);
+                        notify('ra.notification.logged_out', 'warning');
+                        return true;
+                    })
+                    .finally(() => {
+                        authCheckPromise = undefined;
+                    });
+            }
+            return authCheckPromise;
+        },
         [authProvider, logout, notify]
     );
     return authProvider
