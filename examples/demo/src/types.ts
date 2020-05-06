0347b89139c58b0fd7f82d8f9e88dbb2bc06fb7e
kheradmand <kheradmand.v1@gmail.com>
2020-05-06 14:17:36 +0430

    Start to make types for ProductList component

--- examples/demo/src/types.ts
+++ examples/demo/src/types.ts
@@ -1,4 +1,6 @@
-import { ReduxState, Record, Identifier } from 'ra-core';
+import { ReactElement } from 'react';
+import { ReduxState, Record, Identifier, Sort, RecordMap } from 'ra-core';
+import { History, Location } from 'history';
 
 export type ThemeName = 'light' | 'dark';
 
@@ -62,3 +64,54 @@ export interface FieldProps<T extends Record = Record> {
 export interface Review extends Record {
     customer_id: string;
 }
+
+export interface ListProps {
+    permissions?: object;
+    basePath: string;
+    history: History;
+    location: Location;
+    match: {
+        path: string;
+        url: string;
+        isExact: boolean;
+        params: object;
+    };
+    resource: string;
+    options: {};
+    hasList: boolean;
+    hasEdit: boolean;
+    hasShow: boolean;
+    hasCreate: boolean;
+    children: ReactElement;
+}
+
+// interface from logged props
+// export interface ProductFilterProps<T extends Record = Record> {
+//     basePath: string
+//     currentSort: Sort
+//     data: RecordMap<T>
+//     defaultTitle: string
+//     displayedFilters: {[key: string]: boolean}
+//     hasCreate: boolean
+//     hideFilter: ƒ (filterName)
+//     ids: []
+//     loading: boolean
+//     loaded: boolean
+//     onSelect: ƒ (newIds)
+//     onToggleItem: ƒ (id)
+//     onUnselectItems: ƒ ()
+//     page: number
+//     perPage: number
+//     refresh: undefined
+//     resource?: Function
+//     selectedIds: Identifier[]
+//     setFilters: ƒ (filters, displayedFilters)
+//     setPage: (newPage: number) => void
+//     setPerPage: (newPerPage: number) => void
+//     setSort: (newSort: string) => void
+//     showFilter: ƒ (filterName, defaultValue)
+//     total: number
+//     version: number
+//     filterValues: {q: "df", width_gte: 2}
+//     context?: string
+// }
