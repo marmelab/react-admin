faa2011c47254cb8fa3f5ab4a5cff4ed3fa57d39
braco <cezsays+github2@gmail.com>
2020-06-14 15:45:51 -0400

    Check for schema.queryType and schema.mutationType
    Check for top level property before looking deeper into object, was throwing name not found because mutationType doesn't exist in schema object

--- packages/ra-data-graphql/src/introspection.js
+++ packages/ra-data-graphql/src/introspection.js
@@ -42,8 +42,8 @@ export default async (client, options) => {
 
     const queries = schema.types.reduce((acc, type) => {
         if (
-            type.name !== schema.queryType.name &&
-            type.name !== schema.mutationType.name
+            type.name !== (schema.queryType && schema.queryType.name) &&
+            type.name !== (schema.mutationType && schema.mutationType.name)
         )
             return acc;
 
@@ -52,8 +52,8 @@ export default async (client, options) => {
 
     const types = schema.types.filter(
         type =>
-            type.name !== schema.queryType.name &&
-            type.name !== schema.mutationType.name
+            type.name !== (schema.queryType && schema.queryType.name) &&
+            type.name !== (schema.mutationType && schema.mutationType.name)
     );
 
     const isResource = type =>
