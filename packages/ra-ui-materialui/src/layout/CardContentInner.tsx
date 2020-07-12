7750855f3e3f71c282308dab5125e79c05e0ea9e
Pavithran Gridharan <pavithran@schmiede.one>
2020-07-12 12:25:10 +0200

    fix: requested changes in pr

--- packages/ra-ui-materialui/src/layout/CardContentInner.tsx
+++ packages/ra-ui-materialui/src/layout/CardContentInner.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
-import { ClassNameMap } from '@material-ui/styles';
+
+import { ClassesOverride } from '../types';
 
 const useStyles = makeStyles(
     theme => ({
@@ -45,7 +46,7 @@ export declare type CardContentInnerClassKey = 'root';
 
 export interface CardContentInnerProps {
     className?: string;
-    classes?: ClassNameMap<CardContentInnerClassKey>;
+    classes?: ClassesOverride<typeof useStyles>;
 }
 
 CardContentInner.propTypes = {
