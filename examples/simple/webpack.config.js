f36156fcd81865cd083ae6079f09789a5c1a7001
JulienM <julienm@marmelab.com>
2020-07-21 16:39:40 +0200

    Fix CORS issue un simple example codesandbox

--- examples/simple/webpack.config.js
+++ examples/simple/webpack.config.js
@@ -103,6 +103,9 @@ module.exports = {
               }
             : {},
     devServer: {
+        disableHostCheck: true,
+        host: '127.0.0.1',
+        port: 8080,
         stats: {
             children: false,
             chunks: false,
