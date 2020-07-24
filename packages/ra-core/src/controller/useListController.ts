446ed85df6692e868d0f006a75149c04df11be2c
Luwangel <adrien.amoros@gmail.com>
2020-07-24 11:45:14 +0200

    Make the total pages optionnal in TypeScript types

--- packages/ra-core/src/controller/useListController.ts
+++ packages/ra-core/src/controller/useListController.ts
@@ -79,7 +79,7 @@ export interface ListControllerProps<RecordType = Record> {
     setSort: (sort: string, order?: string) => void;
     showFilter: (filterName: string, defaultValue: any) => void;
     total: number;
-    totalPages: number;
+    totalPages?: number;
 }
 
 /**
