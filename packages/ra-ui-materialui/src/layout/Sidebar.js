f03c17d9f2b0e5322e5ac4a95e4b8736ba578f86
djhi <1122076+djhi@users.noreply.github.com>
2020-08-04 16:07:30 +0200

    Move margins from the Sidebar to the Menu

--- packages/ra-ui-materialui/src/layout/Sidebar.js
+++ packages/ra-ui-materialui/src/layout/Sidebar.js
@@ -29,7 +29,6 @@ const useStyles = makeStyles(
                 duration: theme.transitions.duration.leavingScreen,
             }),
             backgroundColor: 'transparent',
-            marginTop: '0.5em',
             borderRight: 'none',
             [theme.breakpoints.only('xs')]: {
                 marginTop: 0,
@@ -39,7 +38,6 @@ const useStyles = makeStyles(
             },
             [theme.breakpoints.up('md')]: {
                 border: 'none',
-                marginTop: '1.5em',
             },
             zIndex: 'inherit',
         },
