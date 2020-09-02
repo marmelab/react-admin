78b7d4791640ab81bbbf641343a996809c915bf7
djhi <1122076+djhi@users.noreply.github.com>
2020-09-02 08:36:33 +0200

    Support function for initialValues

--- packages/ra-core/src/form/getFormInitialValues.ts
+++ packages/ra-core/src/form/getFormInitialValues.ts
@@ -3,28 +3,27 @@ export default function getFormInitialValues(
     defaultValue,
     record
 ) {
-    let finalInitialValues = {
-        ...initialValues,
-        ...record,
-    };
-
     if (typeof defaultValue !== 'undefined') {
         console.warn(
             '"defaultValue" is deprecated, please use "initialValues" instead'
         );
     }
 
-    if (typeof defaultValue === 'object') {
-        finalInitialValues = {
-            ...defaultValue,
-            ...finalInitialValues,
-        };
-    } else if (typeof defaultValue === 'function') {
-        finalInitialValues = {
-            ...defaultValue(record),
-            ...finalInitialValues,
-        };
+    return {
+        ...getValues(defaultValue),
+        ...getValues(initialValues),
+        ...record,
+    };
+}
+
+function getValues(values) {
+    if (typeof values === 'object') {
+        return values;
     }
 
-    return finalInitialValues;
+    if (typeof values === 'function') {
+        return values();
+    }
+
+    return {};
 }
