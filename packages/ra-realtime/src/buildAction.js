30939b0a0225f4eeee092df5f92928f6f0a287ae
gstvg <28798827+gstvg@users.noreply.github.com>
2020-09-18 10:41:23 -0300

    Update buildAction.js

--- packages/ra-realtime/src/buildAction.js
+++ packages/ra-realtime/src/buildAction.js
@@ -1,11 +1,30 @@
-import { FETCH_END } from 'ra-core';
+import {
+  CRUD_GET_LIST,
+  CRUD_GET_ONE,
+  FETCH_END,
+  GET_LIST,
+  GET_ONE
+} from "ra-core";
+
+export const getFetchType = (actionType) => {
+  if (actionType === CRUD_GET_LIST) {
+    return GET_LIST;
+  } else if (actionType === CRUD_GET_ONE) {
+    return GET_ONE;
+  } else {
+    console.error(
+      `unexpected action type: ${actionType}, should be only ${CRUD_GET_LIST} or ${CRUD_GET_ONE}`
+    );
+    return undefined;
+  }
+};
 
 export default (
-    { type, payload: requestPayload, meta: { fetch: restType, ...meta } },
-    payload
+  { type, payload: requestPayload, meta: { fetch: restType, ...meta } },
+  payload
 ) => ({
-    type: `${type}_SUCCESS`,
-    payload,
-    requestPayload,
-    meta: { ...meta, fetchResponse: restType, fetchStatus: FETCH_END },
+  type: `${type}_SUCCESS`,
+  payload,
+  requestPayload,
+  meta: { ...meta, fetchResponse: getFetchType(type), fetchStatus: FETCH_END }
 });
