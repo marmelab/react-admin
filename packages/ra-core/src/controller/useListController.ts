marmelab/react-admin

commit e6357bcc95f53d588a8b44073c1dc22fcbcc9b85

Compute the total pages for a list and pass it to the list context

packages/ra-core/src/controller/useListController.ts
@@ -184,6 +184,10 @@ const useListController = <RecordType = Record>(
 
     const finalIds = typeof total === 'undefined' ? defaultIds : ids;
 
+    const totalPages = useMemo(() => {
+        return Math.ceil(total / query.perPage) || 1;
+    }, [query, total]);
+
     useEffect(() => {
         if (
             query.page <= 0 ||
@@ -237,6 +241,7 @@ const useListController = <RecordType = Record>(
         setSort: queryModifiers.setSort,
         showFilter: queryModifiers.showFilter,
         total: typeof total === 'undefined' ? defaultTotal : total,
+        totalPages,
     };
 };
 
@@ -268,6 +273,7 @@ export const injectedProps = [
     'setSort',
     'showFilter',
     'total',
+    'totalPages',
     'version',
 ];
 

packages/ra-core/src/controller/ListContext.tsx
@@ -14,6 +14,7 @@ import { ListControllerProps } from './useListController';
  * @prop {boolean}  loaded boolean that is false until the data is available
  * @prop {boolean}  loading boolean that is true on mount, and false once the data was fetched
  * @prop {integer}  page the current page. Starts at 1
+ * @prop {integer}  totalPages the total number of pages for the current filters. Corresponds to total / perPage.
  * @prop {Function} setPage a callback to change the page, e.g. setPage(3)
  * @prop {integer}  perPage the number of results per page. Defaults to 25
  * @prop {Function} setPerPage a callback to change the number of results per page, e.g. setPerPage(25)
@@ -78,6 +79,7 @@ const ListContext = createContext<ListControllerProps>({
     setSort: null,
     showFilter: null,
     total: null,
+    totalPages: null,
 });
 
 ListContext.displayName = 'ListContext';

packages/ra-ui-materialui/src/list/Pagination.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect, useMemo, useCallback, FC, ReactElement } from 'react';
+import { useCallback, FC, ReactElement } from 'react';
 import PropTypes from 'prop-types';
 import {
     TablePagination,
@@ -25,23 +25,12 @@ const Pagination: FC<PaginationProps> = props => {
     const {
         loading,
         page,
+        nbPages,
         perPage,
         total,
         setPage,
         setPerPage,
     } = useListContext(props);
-
-    const nbPages = useMemo(() => {
-        return Math.ceil(total / perPage) || 1;
-    }, [perPage, total]);
-
-    useEffect(() => {
-        if (page < 1 || isNaN(page)) {
-            setPage(1);
-        } else if (page > nbPages) {
-            setPage(nbPages);
-        }
-    }, [page, nbPages, total, perPage, setPage]);
 
     const translate = useTranslate();
     const isSmall = useMediaQuery((theme: Theme) =>

commit 8b0f518064ec89197b1eedd3ebdaadc00d3a97f2

Fixes #5168 - Pagination broke after e6357bc

packages/ra-core/src/controller/useListController.ts
@@ -173,18 +173,22 @@ const useListController = <RecordType = Record>(
     // useGetList hook again. While the result of this new call is loading,
     // the ids and total are empty. To avoid rendering an empty list at that
     // moment, we override the ids and total with the latest loaded ones.
-    const defaultIds = useSelector((state: ReduxState): Identifier[] =>
-        get(state.admin.resources, [resource, 'list', 'ids'], [])
+    const defaultIds = useSelector(
+        (state: ReduxState): Identifier[] =>
+            get(state.admin.resources, [resource, 'list', 'ids'], [])
     );
-    const defaultTotal = useSelector((state: ReduxState): number =>
-        get(state.admin.resources, [resource, 'list', 'total'], 0)
+    const defaultTotal = useSelector(
+        (state: ReduxState): number =>
+            get(state.admin.resources, [resource, 'list', 'total'], 0)
     );
 
     const finalIds = typeof total === 'undefined' ? defaultIds : ids;
 
+    const totalOrDefault = typeof total === 'undefined' ? defaultTotal : total;
+
     const totalPages = useMemo(() => {
-        return Math.ceil(total / query.perPage) || 1;
-    }, [query.perPage, total]);
+        return Math.ceil(totalOrDefault / query.perPage) || 1;
+    }, [query.perPage, totalOrDefault]);
 
     useEffect(() => {
         if (
@@ -250,7 +254,7 @@ const useListController = <RecordType = Record>(
         setPerPage: queryModifiers.setPerPage,
         setSort: queryModifiers.setSort,
         showFilter: queryModifiers.showFilter,
-        total: typeof total === 'undefined' ? defaultTotal : total,
+        total: totalOrDefault,
     };
 };
 