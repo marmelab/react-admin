2482ff92178eaa920b1f56041dae84256ce95921
fzaninotto <fzaninotto@gmail.com>
2020-10-08 17:43:03 +0200

    Fix dataProvider fails silently when response has wrong type
    Closes #5228

--- packages/ra-core/src/core/dataFetchActions.ts
+++ packages/ra-core/src/core/dataFetchActions.ts
@@ -8,18 +8,18 @@ export const UPDATE_MANY = 'UPDATE_MANY';
 export const DELETE = 'DELETE';
 export const DELETE_MANY = 'DELETE_MANY';
 
-export const fetchActionsWithRecordResponse = [GET_ONE, CREATE, UPDATE];
+export const fetchActionsWithRecordResponse = ['getOne', 'create', 'update'];
 export const fetchActionsWithArrayOfIdentifiedRecordsResponse = [
-    GET_LIST,
-    GET_MANY,
-    GET_MANY_REFERENCE,
+    'getList',
+    'getMany',
+    'getManyReference',
 ];
 export const fetchActionsWithArrayOfRecordsResponse = [
     ...fetchActionsWithArrayOfIdentifiedRecordsResponse,
-    UPDATE_MANY,
-    DELETE_MANY,
+    'updateMany',
+    'deleteMany',
 ];
-export const fetchActionsWithTotalResponse = [GET_LIST, GET_MANY_REFERENCE];
+export const fetchActionsWithTotalResponse = ['getList', 'getManyReference'];
 
 export const sanitizeFetchType = (fetchType: string) => {
     switch (fetchType) {
