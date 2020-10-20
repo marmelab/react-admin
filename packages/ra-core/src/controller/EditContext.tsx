a4abcc9bb3cc8515bd1123a8665fcb660c11310a
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2020-10-20 11:40:58 +0200

    Fix typo

--- packages/ra-core/src/controller/EditContext.tsx
+++ packages/ra-core/src/controller/EditContext.tsx
@@ -5,7 +5,7 @@ import { EditControllerProps } from './useEditController';
 /**
  * Context to store the result of the useEditController() hook.
  *
- * Use the useEditContext() hook to read the context. That's what the Edit components do in react-admn.
+ * Use the useEditContext() hook to read the context. That's what the Edit components do in react-admin.
  *
  * @example
  *
