62b349232296e95113954521f0e7e2d3b0fc3bea
Valentin Dimitroff <26602880+ValentinnDimitroff@users.noreply.github.com>
2020-11-07 12:54:09 +0200

    Renamed blobType parameter in downloadCSV

--- packages/ra-core/src/export/downloadCSV.ts
+++ packages/ra-core/src/export/downloadCSV.ts
@@ -1,8 +1,8 @@
-export default (csv, filename, outputType) => {
+export default (csv, filename, blobType = 'text/csv') => {
     const fakeLink = document.createElement('a');
     fakeLink.style.display = 'none';
     document.body.appendChild(fakeLink);
-    const blob = new Blob([csv], { type: outputType ? outputType : 'text/csv' });
+    const blob = new Blob([csv], { type: blobType });
     if (window.navigator && window.navigator.msSaveOrOpenBlob) {
         // Manage IE11+ & Edge
         window.navigator.msSaveOrOpenBlob(blob, `${filename}.csv`);
