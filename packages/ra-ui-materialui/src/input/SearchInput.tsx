e071c652a0bf3fc7dfceef36b324be2cc9a2dcb2
Md-khaleelur-rehman <rehman.khaleelur@zemosolabs.com>
2020-12-16 11:39:24 +0530

    Bug fix for autocomplete off

--- packages/ra-ui-materialui/src/input/SearchInput.tsx
+++ packages/ra-ui-materialui/src/input/SearchInput.tsx
@@ -42,6 +42,7 @@ const SearchInput: FunctionComponent<
                         <SearchIcon color="disabled" />
                     </InputAdornment>
                 ),
+                autoComplete: 'off',
             }}
             className={classes.input}
             {...rest}
