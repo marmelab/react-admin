145cf35ba51391df4d0628ce6a6a5c8e65f25fa6
fzaninotto <fzaninotto@gmail.com>
2021-01-03 21:01:00 +0100

    [Doc] Fix ra-data-json-server getMany is documented as multiple getOne calls
    Closes #5680

--- packages/ra-data-json-server/src/index.ts
+++ packages/ra-data-json-server/src/index.ts
@@ -11,7 +11,7 @@ import { fetchUtils, DataProvider } from 'ra-core';
  * getList          => GET http://my.api.url/posts?_sort=title&_order=ASC&_start=0&_end=24
  * getOne           => GET http://my.api.url/posts/123
  * getManyReference => GET http://my.api.url/posts?author_id=345
- * getMany          => GET http://my.api.url/posts/123, GET http://my.api.url/posts/456, GET http://my.api.url/posts/789
+ * getMany          => GET http://my.api.url/posts?id=123&id=456&id=789
  * create           => POST http://my.api.url/posts/123
  * update           => PUT http://my.api.url/posts/123
  * updateMany       => PUT http://my.api.url/posts/123, PUT http://my.api.url/posts/456, PUT http://my.api.url/posts/789
