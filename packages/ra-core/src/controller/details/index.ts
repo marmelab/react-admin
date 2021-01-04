f738a82871da854daae7557781662d44d6524712
asvarcas <asvarcas@etriek.com.uy>
2021-01-04 12:54:41 -0300

    Improve fix

--- packages/ra-core/src/controller/details/index.ts
+++ packages/ra-core/src/controller/details/index.ts
@@ -23,7 +23,7 @@ export * from './useEditContext';
 export * from './useShowContext';
 
 // We don't want to export CreateProps, EditProps and ShowProps as they should
-// not be used outside ra-core, which would conflict with ra-ui-materialui types,
+// not be used outside ra-core, since it would conflict with ra-ui-materialui types,
 // hence the named imports/exports
 export type { CreateControllerProps, EditControllerProps, ShowControllerProps };
 export { useCreateController, useEditController, useShowController };
