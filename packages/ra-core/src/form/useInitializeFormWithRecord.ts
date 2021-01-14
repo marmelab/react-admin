9d0fd2701215d0643b9e3e59275c925b650030c9
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-01-14 16:20:38 +0100

    Fix Warning Avout Unsaved Change

--- packages/ra-core/src/form/useInitializeFormWithRecord.ts
+++ packages/ra-core/src/form/useInitializeFormWithRecord.ts
@@ -15,7 +15,14 @@ const useInitializeFormWithRecord = record => {
 
         const initialValues = form.getState().initialValues;
         const initialValuesMergedWithRecord = merge({}, initialValues, record);
-        form.initialize(initialValuesMergedWithRecord);
+
+        // Disable this option when re-initializing the form because in this case, it should reset the dirty state of all fields
+        // We do need to keep this option for dynamically added inputs though which is why it is kept at the form level
+        form.setConfig('keepDirtyOnReinitialize', false);
+        // Ignored until next version of final-form is released. See https://github.com/final-form/final-form/pull/376
+        // @ts-ignore
+        form.restart(initialValuesMergedWithRecord);
+        form.setConfig('keepDirtyOnReinitialize', true);
     }, [form, JSON.stringify(record)]); // eslint-disable-line react-hooks/exhaustive-deps
 };
 
