31aa45017d3f2455dce93e20b6176896d946758b
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-01-18 12:13:39 +0100

    Linting

--- packages/ra-core/src/form/FormGroupContextProvider.tsx
+++ packages/ra-core/src/form/FormGroupContextProvider.tsx
@@ -57,7 +57,11 @@ export const FormGroupContextProvider = ({
     const formContext = useFormContext();
 
     useEffect(() => {
-        if (!formContext || !formContext.registerGroup || !formContext.unregisterGroup) {
+        if (
+            !formContext ||
+            !formContext.registerGroup ||
+            !formContext.unregisterGroup
+        ) {
             console.warn(
                 `The FormGroupContextProvider can only be used inside a FormContext such as provided by the SimpleForm and TabbedForm components`
             );
