e9f7aa63b9c7b70bdf78e7e8c197fdd5eb39786b
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-01-22 16:33:33 +0100

    Add selector customization test for TranslatableFields

--- packages/ra-ui-materialui/src/field/TranslatableFields.spec.tsx
+++ packages/ra-ui-materialui/src/field/TranslatableFields.spec.tsx
@@ -3,6 +3,7 @@ import expect from 'expect';
 import { fireEvent, render } from '@testing-library/react';
 import { TranslatableFields } from './TranslatableFields';
 import TextField from './TextField';
+import { useTranslatableContext } from 'ra-core';
 
 const record = {
     id: 123,
@@ -59,5 +60,69 @@ describe('<TranslatableFields />', () => {
         expect(
             getByLabelText('ra.languages.fr').getAttribute('hidden')
         ).toBeNull();
+    });
+
+    it('should allow to customize the language selector', () => {
+        const Selector = () => {
+            const {
+                languages,
+                selectLanguage,
+                selectedLanguage,
+            } = useTranslatableContext();
+
+            const handleChange = (event): void => {
+                console.log(event.target.value);
+                selectLanguage(event.target.value);
+            };
+
+            return (
+                <select
+                    aria-label="select language"
+                    onChange={handleChange}
+                    value={selectedLanguage}
+                >
+                    {languages.map(locale => (
+                        <option
+                            key={locale}
+                            value={locale}
+                            id={`translatable-header-${locale}`}
+                        >
+                            {locale}
+                        </option>
+                    ))}
+                </select>
+            );
+        };
+
+        const { queryByText, getByLabelText } = render(
+            <TranslatableFields
+                record={record}
+                resource="products"
+                basePath="/products"
+                languages={['en', 'fr']}
+                selector={<Selector />}
+            >
+                <TextField source="name" />
+                <TextField source="description" />
+                <TextField source="nested.field" />
+            </TranslatableFields>
+        );
+
+        expect(getByLabelText('en').getAttribute('hidden')).toBeNull();
+        expect(getByLabelText('fr').getAttribute('hidden')).toBeDefined();
+
+        expect(queryByText('english name')).not.toBeNull();
+        expect(queryByText('english description')).not.toBeNull();
+        expect(queryByText('english nested field')).not.toBeNull();
+
+        expect(queryByText('french name')).not.toBeNull();
+        expect(queryByText('french description')).not.toBeNull();
+        expect(queryByText('french nested field')).not.toBeNull();
+
+        fireEvent.change(getByLabelText('select language'), {
+            target: { value: 'fr' },
+        });
+        expect(getByLabelText('en').getAttribute('hidden')).toBeDefined();
+        expect(getByLabelText('fr').getAttribute('hidden')).toBeNull();
     });
 });
