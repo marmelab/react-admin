fac67a20e54566f13cbbc84adf84f111e20b6a6e
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-01-26 16:36:10 +0100

    Documentation for useTranslatableContext

--- packages/ra-core/src/i18n/useTranslatableContext.ts
+++ packages/ra-core/src/i18n/useTranslatableContext.ts
@@ -6,6 +6,32 @@ import {
 
 /**
  * Gives access to the current TranslatableContext.
+ *
+ * @example
+ * <TranslatableFields
+ *     selector={<MyLanguageSelector />}
+ *     locales={['en', 'fr']}
+ * >
+ *     <TextField source={getSource('title')} />
+ * <TranslatableFields>
+ *
+ * const MyLanguageSelector = () => {
+ *     const {
+ *         locales,
+ *         selectedLocale,
+ *         selectLocale,
+ *     } = useTranslatableContext();
+ *
+ *     return (
+ *         <select onChange={selectLocale}>
+ *             {locales.map((locale) => (
+ *                 <option selected={locale.locale === selectedLocale}>
+ *                     {locale.name}
+ *                 </option>
+ *             ))}
+ *        </select>
+ *     );
+ * }
  */
 export const useTranslatableContext = (): TranslatableContextValue => {
     const context = useContext(TranslatableContext);
