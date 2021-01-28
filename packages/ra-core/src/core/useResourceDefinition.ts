74a35c7f35a642d9ed79384f0e7afbf7181c6a9f
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-01-28 12:23:13 +0100

    Ensure Resource Definition is Overridable with Props

--- packages/ra-core/src/core/useResourceDefinition.ts
+++ packages/ra-core/src/core/useResourceDefinition.ts
@@ -6,9 +6,34 @@ import { useResourceContext } from './useResourceContext';
 /**
  * Hook which returns the definition of the requested resource
  */
-export const useResourceDefinition = (props): ResourceDefinition => {
+export const useResourceDefinition = (
+    props: UseResourceDefinitionOptions
+): ResourceDefinition => {
     const resource = useResourceContext(props);
     const resources = useSelector(getResources);
 
+    if (
+        props.resource &&
+        (typeof props.hasCreate !== 'undefined' ||
+            typeof props.hasEdit !== 'undefined' ||
+            typeof props.hasList !== 'undefined' ||
+            typeof props.hasShow !== 'undefined')
+    ) {
+        return {
+            ...props,
+            name: props.resource,
+        };
+    }
+
     return resources.find(r => r?.name === resource) || props;
 };
+
+export interface UseResourceDefinitionOptions {
+    readonly resource?: string;
+    readonly options?: any;
+    readonly hasList?: boolean;
+    readonly hasEdit?: boolean;
+    readonly hasShow?: boolean;
+    readonly hasCreate?: boolean;
+    readonly icon?: any;
+}
