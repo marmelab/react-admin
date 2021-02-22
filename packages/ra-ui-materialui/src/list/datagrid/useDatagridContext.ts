63e6db597891f9146b6a47c2b26432f734fe2e6c
asvarcas <asvarcas@etriek.com.uy>
2021-02-22 16:00:12 -0300

    Throw error if no context is found

--- packages/ra-ui-materialui/src/list/datagrid/useDatagridContext.ts
+++ packages/ra-ui-materialui/src/list/datagrid/useDatagridContext.ts
@@ -3,5 +3,12 @@ import DatagridContext, { DatagridContextValue } from './DatagridContext';
 
 export const useDatagridContext = (): DatagridContextValue => {
     const context = useContext(DatagridContext);
+
+    if (!context) {
+        throw new Error(
+            'useDatagridContext must be used inside a DatagridContextProvider'
+        );
+    }
+
     return context;
 };
