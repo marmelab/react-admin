39c32a9edbad416900cfac9799a5399b56a44882
Alan Poulain <contact@alanpoulain.eu>
2021-02-27 12:15:54 +0100

    Do not use useFormState

--- packages/ra-core/src/form/useResetSubmitErrors.ts
+++ packages/ra-core/src/form/useResetSubmitErrors.ts
@@ -1,25 +1,27 @@
-import { useRef } from 'react';
-import { useForm, useFormState } from 'react-final-form';
+import { useEffect, useRef } from 'react';
+import { useForm } from 'react-final-form';
 
 /**
  * Reset the submission error when the corresponding field changes.
  * final-form doesn't do this by default.
  */
 const useResetSubmitErrors = () => {
-    const { mutators } = useForm();
-    const { values } = useFormState({ subscription: { values: true } });
-    const prevValues = useRef(values);
-    useFormState({
-        onChange: ({ values }) => {
-            mutators.resetSubmitErrors({
-                current: values,
-                prev: prevValues.current,
-            });
+    const form = useForm();
+    const prevValues = useRef(form.getState().values);
+    useEffect(() => {
+        const unsubscribe = form.subscribe(
+            ({ values }) => {
+                form.mutators.resetSubmitErrors({
+                    current: values,
+                    prev: prevValues.current,
+                });
 
-            prevValues.current = values;
-        },
-        subscription: { values: true },
-    });
+                prevValues.current = values;
+            },
+            { values: true }
+        );
+        return unsubscribe;
+    }, [form]);
 };
 
 export default useResetSubmitErrors;
