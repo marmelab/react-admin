fe2630b08028a5d23907597d9287d0c337900728
asvarcas <asvarcas@etriek.com.uy>
2021-03-05 15:24:16 -0300

    Export SimpleFormView

--- packages/ra-ui-materialui/src/form/index.tsx
+++ packages/ra-ui-materialui/src/form/index.tsx
@@ -6,17 +6,24 @@ import SimpleFormIterator, {
 import TabbedFormTabs from './TabbedFormTabs';
 import Toolbar, { ToolbarProps } from './Toolbar';
 import getFormInitialValues from './getFormInitialValues';
+import { SimpleFormView, SimpleFormViewProps } from './SimpleFormView';
 export * from './FormTabHeader';
 
 export * from './TabbedForm';
 export * from './FormTab';
 export * from './FormTabHeader';
 
-export type { SimpleFormProps, SimpleFormIteratorProps, ToolbarProps };
+export type {
+    SimpleFormProps,
+    SimpleFormIteratorProps,
+    SimpleFormViewProps,
+    ToolbarProps,
+};
 
 export {
     FormInput,
     SimpleForm,
+    SimpleFormView,
     SimpleFormIterator,
     TabbedFormTabs,
     Toolbar,
