42b1f8ffe68eff2e25bab2e84927a1bd19298f29
asvarcas <asvarcas@etriek.com.uy>
2021-04-19 11:29:25 -0300

    Fix type

--- packages/ra-ui-materialui/src/layout/Notification.tsx
+++ packages/ra-ui-materialui/src/layout/Notification.tsx
@@ -21,7 +21,7 @@ interface Props {
 }
 
 const useStyles = makeStyles(
-    theme => ({
+    (theme: Theme) => ({
         success: {
             backgroundColor: theme.palette.success.main,
             color: theme.palette.success.contrastText,
