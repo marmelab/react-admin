bfa6ad1e40262cc1ac2a21544768058e0ea68d74
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-04-26 11:43:36 +0200

    Add more inferences & export more inference utilities

--- packages/ra-core/src/inference/assertions.ts
+++ packages/ra-core/src/inference/assertions.ts
@@ -4,27 +4,41 @@ export const isNumeric = (value: any) =>
     !isNaN(parseFloat(value)) && isFinite(value);
 export const valuesAreNumeric = (values: any[]) => values.every(isNumeric);
 
-export const isInteger = (value: any) => Number.isInteger(value);
+export const isInteger = (value: any) =>
+    Number.isInteger(value) || !isNaN(parseInt(value));
 export const valuesAreInteger = (values: any[]) => values.every(isInteger);
 
-export const isBoolean = (value: any) => typeof value === 'boolean';
+export const isBoolean = (value: any) =>
+    typeof value === 'boolean' ||
+    ['true', 'false'].includes(value.toLowerCase());
 export const valuesAreBoolean = (values: any[]) => values.every(isBoolean);
 
 export const isString = (value: any) => typeof value === 'string';
 export const valuesAreString = (values: any[]) => values.every(isString);
 
 const HtmlRegexp = /<([A-Z][A-Z0-9]*)\b[^>]*>(.*?)<\/\1>/i;
-export const isHtml = (value: any) => HtmlRegexp.test(value);
+export const isHtml = (value: any) => !value || HtmlRegexp.test(value);
 export const valuesAreHtml = (values: any[]) => values.every(isHtml);
+
+const UrlRegexp = /http(s*):\/\/.*/i;
+export const isUrl = (value: any) => !value || UrlRegexp.test(value);
+export const valuesAreUrl = (values: any[]) => values.every(isUrl);
+
+const ImageUrlRegexp = /.*\.(jpeg|jpg|jfif|pjpeg|pjp|png|svg|gif|webp|apng|bmp|ico|cur|tif|tiff)/i;
+export const isImageUrl = (value: any) => !value || ImageUrlRegexp.test(value);
+export const valuesAreImageUrl = (values: any[]) => values.every(isImageUrl);
+
+export const isEmail = (value: any) => !value || value.indexOf('@') > -1;
+export const valuesAreEmail = (values: any[]) => values.every(isEmail);
 
 export const isArray = (value: any) => Array.isArray(value);
 export const valuesAreArray = (values: any[]) => values.every(isArray);
 
-export const isDate = (value: any) => value instanceof Date;
+export const isDate = (value: any) => !value || value instanceof Date;
 export const valuesAreDate = (values: any[]) => values.every(isDate);
 
 export const isDateString = (value: any) =>
-    typeof value === 'string' && !isNaN(parseDate(value).getDate());
+    !value || (typeof value === 'string' && !isNaN(parseDate(value).getDate()));
 export const valuesAreDateString = (values: any[]) =>
     values.every(isDateString);
 
