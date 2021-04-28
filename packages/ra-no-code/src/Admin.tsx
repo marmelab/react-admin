9762dc326e2a19b06e620948fd1ae9d30fafc0b8
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-04-28 15:12:24 +0200

    Ensure Admin can take props

--- packages/ra-no-code/src/Admin.tsx
+++ packages/ra-no-code/src/Admin.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Admin as RaAdmin, Resource } from 'react-admin';
+import {
+    Admin as RaAdmin,
+    AdminProps as RaAdminProps,
+    Resource,
+} from 'react-admin';
 import localStorageDataProvider from 'ra-data-local-storage';
 import { Create, Edit, List } from './builders';
 import {
@@ -10,17 +14,22 @@ import { Layout, Ready } from './ui';
 
 const dataProvider = localStorageDataProvider();
 
-export const Admin = () => (
+export const Admin = (props: AdminProps) => (
     <ResourceConfigurationProvider dataProvider={dataProvider}>
-        <InnerAdmin />
+        <InnerAdmin {...props} />
     </ResourceConfigurationProvider>
 );
 
-const InnerAdmin = () => {
+const InnerAdmin = (props: AdminProps) => {
     const [resources] = useResourcesConfiguration();
     const hasResources = !!resources && Object.keys(resources).length > 0;
     return (
-        <RaAdmin dataProvider={dataProvider} ready={Ready} layout={Layout}>
+        <RaAdmin
+            dataProvider={dataProvider}
+            ready={Ready}
+            layout={Layout}
+            {...props}
+        >
             {hasResources
                 ? Object.keys(resources).map(resource => (
                       <Resource
@@ -36,3 +45,5 @@ const InnerAdmin = () => {
         </RaAdmin>
     );
 };
+
+interface AdminProps extends Omit<RaAdminProps, 'dataProvider'> {}
