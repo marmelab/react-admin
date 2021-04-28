44158adb3928699f192af8900287085d26825ebc
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-04-28 14:17:02 +0200

    Simplify getFieldDefinitionsFromRecords

--- packages/ra-no-code/src/ResourceConfiguration/getFieldDefinitionsFromRecords.ts
+++ packages/ra-no-code/src/ResourceConfiguration/getFieldDefinitionsFromRecords.ts
@@ -4,25 +4,13 @@ import {
     inferTypeFromValues,
     Record,
 } from 'ra-core';
-import inflection from 'inflection';
 
 export const getFieldDefinitionsFromRecords = (
     records: Record[]
 ): InferredElementDescription[] => {
     const values = getValuesFromRecords(records);
 
-    return Object.keys(values).map(key => {
-        const type = inferTypeFromValues(key, values[key]);
-
-        return {
-            ...type,
-            props: {
-                ...type.props,
-                label: inflection.transform(type.props.source, [
-                    'underscore',
-                    'humanize',
-                ]),
-            },
-        };
-    });
+    return Object.keys(values).map(key =>
+        inferTypeFromValues(key, values[key])
+    );
 };
