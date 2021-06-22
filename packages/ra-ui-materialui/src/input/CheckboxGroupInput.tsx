2da6c8d0de8a35f3945b79de03520367d65664c9
Vikrant <vvshirvankar@gmail.com>
2021-06-23 01:14:54 +0530

    Fixed label prop type

--- packages/ra-ui-materialui/src/input/CheckboxGroupInput.tsx
+++ packages/ra-ui-materialui/src/input/CheckboxGroupInput.tsx
@@ -238,7 +238,7 @@ const useStyles = makeStyles(
 CheckboxGroupInput.propTypes = {
     choices: PropTypes.arrayOf(PropTypes.object),
     className: PropTypes.string,
-    label: PropTypes.string,
+    label: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
     source: PropTypes.string,
     options: PropTypes.object,
     optionText: PropTypes.oneOfType([
