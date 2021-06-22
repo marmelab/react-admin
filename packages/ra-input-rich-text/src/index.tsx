ba168e0cbb5d42e11bb6746c4537b9a4c95a69d5
Vikrant <vvshirvankar@gmail.com>
2021-06-23 01:26:33 +0530

    Fixed label Prop type

--- packages/ra-input-rich-text/src/index.tsx
+++ packages/ra-input-rich-text/src/index.tsx
@@ -137,16 +137,14 @@ const RichTextInput: FunctionComponent<Props> = props => {
             className="ra-rich-text-input"
             margin={margin}
         >
-            {label !== '' && label !== false && (
-                <InputLabel shrink htmlFor={id} className={classes.label}>
-                    <FieldTitle
-                        label={label}
-                        source={source}
-                        resource={resource}
-                        isRequired={isRequired}
-                    />
-                </InputLabel>
-            )}
+            <InputLabel shrink htmlFor={id} className={classes.label}>
+                <FieldTitle
+                    label={label}
+                    source={source}
+                    resource={resource}
+                    isRequired={isRequired}
+                />
+            </InputLabel>
             <div data-testid="quill" ref={divRef} className={variant} />
             <FormHelperText
                 error={!!error}
@@ -163,7 +161,7 @@ const RichTextInput: FunctionComponent<Props> = props => {
 };
 
 RichTextInput.propTypes = {
-    label: PropTypes.string,
+    label: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
     options: PropTypes.object,
     source: PropTypes.string,
     fullWidth: PropTypes.bool,
