ce116c1059500b8b8758281968f2916bac7b116e
fzaninotto <fzaninotto@gmail.com>
2021-07-22 17:37:29 +0200

    Fix linter warning in Listtoolbar

--- packages/ra-ui-materialui/src/list/ListToolbar.tsx
+++ packages/ra-ui-materialui/src/list/ListToolbar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FC, ReactElement, Children } from 'react';
+import { FC, ReactElement } from 'react';
 import PropTypes from 'prop-types';
 import { Toolbar, ToolbarProps } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
