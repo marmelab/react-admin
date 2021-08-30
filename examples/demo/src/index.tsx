ed20f5db20de00a6071df6c7f1b8580c57459767
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2021-08-30 14:28:43 +0200

    Ensure the demo works with ie11

--- examples/demo/src/index.tsx
+++ examples/demo/src/index.tsx
@@ -1,6 +1,10 @@
+/** @jsxRuntime classic */
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import 'proxy-polyfill';
+// IE11 needs "jsxRuntime classic" for this initial file which means that "React" needs to be in scope
+// https://github.com/facebook/create-react-app/issues/9906
+
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
