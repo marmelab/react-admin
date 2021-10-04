0d0640cf6ed19bf2ce79c6f72e8c48f041f7fe30
Travis CI <travis@travis-ci.org>
2021-10-04 12:06:06 +0200

    graphql: Fix Simple Response Parser Modify all Objects

--- packages/ra-data-graphql-simple/src/getResponseParser.test.ts
+++ packages/ra-data-graphql-simple/src/getResponseParser.test.ts
@@ -9,68 +9,78 @@ import {
 } from 'ra-core';
 import getResponseParser from './getResponseParser';
 
-const testListTypes = type => {
-    it('returns the response expected by AOR for GET_LIST', () => {
-        const introspectionResults = {
-            resources: [
-                {
-                    type: {
-                        name: 'User',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            {
-                                name: 'firstName',
-                                type: { kind: TypeKind.SCALAR },
-                            },
-                        ],
-                    },
-                },
-                {
-                    type: {
-                        name: 'Tag',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            { name: 'name', type: { kind: TypeKind.SCALAR } },
-                        ],
-                    },
-                },
-            ],
-            types: [{ name: 'User' }, { name: 'Tag' }],
-        };
-        const response = {
-            data: {
-                items: [
+describe('getResponseParser', () => {
+    it.each([[GET_LIST], [GET_MANY], [GET_MANY_REFERENCE]])(
+        'returns the response expected for %s',
+        type => {
+            const introspectionResults = {
+                resources: [
                     {
-                        _typeName: 'Post',
-                        id: 'post1',
-                        title: 'title1',
-                        author: { id: 'author1', firstName: 'Toto' },
-                        coauthor: null,
-                        tags: [
-                            { id: 'tag1', name: 'tag1 name' },
-                            { id: 'tag2', name: 'tag2 name' },
-                        ],
-                        embeddedJson: { foo: 'bar' },
+                        type: {
+                            name: 'User',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'firstName',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
                     {
-                        _typeName: 'Post',
-                        id: 'post2',
-                        title: 'title2',
-                        author: { id: 'author1', firstName: 'Toto' },
-                        coauthor: null,
-                        tags: [
-                            { id: 'tag1', name: 'tag1 name' },
-                            { id: 'tag3', name: 'tag3 name' },
-                        ],
-                        embeddedJson: { foo: 'bar' },
+                        type: {
+                            name: 'Tag',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'name',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
                 ],
-                total: { count: 100 },
-            },
-        };
+                types: [{ name: 'User' }, { name: 'Tag' }],
+            };
+            const response = {
+                data: {
+                    items: [
+                        {
+                            _typeName: 'Post',
+                            id: 'post1',
+                            title: 'title1',
+                            author: { id: 'author1', firstName: 'Toto' },
+                            coauthor: null,
+                            tags: [
+                                { id: 'tag1', name: 'tag1 name' },
+                                { id: 'tag2', name: 'tag2 name' },
+                            ],
+                            embeddedJson: { foo: 'bar' },
+                        },
+                        {
+                            _typeName: 'Post',
+                            id: 'post2',
+                            title: 'title2',
+                            author: { id: 'author1', firstName: 'Toto' },
+                            coauthor: null,
+                            tags: [
+                                { id: 'tag1', name: 'tag1 name' },
+                                { id: 'tag3', name: 'tag3 name' },
+                            ],
+                            embeddedJson: { foo: 'bar' },
+                        },
+                    ],
+                    total: { count: 100 },
+                },
+            };
 
-        expect(getResponseParser(introspectionResults)(type)(response)).toEqual(
-            {
+            expect(
+                getResponseParser(introspectionResults)(
+                    type,
+                    undefined,
+                    undefined
+                )(response)
+            ).toEqual({
                 data: [
                     {
                         id: 'post1',
@@ -98,57 +108,64 @@ const testListTypes = type => {
                     },
                 ],
                 total: 100,
-            }
-        );
-    });
-};
+            });
+        }
+    );
 
-const testSingleTypes = type => {
-    it('returns the response expected by AOR for GET_LIST', () => {
-        const introspectionResults = {
-            resources: [
-                {
-                    type: {
-                        name: 'User',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            {
-                                name: 'firstName',
-                                type: { kind: TypeKind.SCALAR },
-                            },
-                        ],
+    describe.each([[CREATE], [UPDATE], [DELETE]])('%s', type => {
+        it('returns the response expected for %s', () => {
+            const introspectionResults = {
+                resources: [
+                    {
+                        type: {
+                            name: 'User',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'firstName',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
-                },
-                {
-                    type: {
-                        name: 'Tag',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            { name: 'name', type: { kind: TypeKind.SCALAR } },
-                        ],
+                    {
+                        type: {
+                            name: 'Tag',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'name',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
-                },
-            ],
-            types: [{ name: 'User' }, { name: 'Tag' }],
-        };
-        const response = {
-            data: {
+                ],
+                types: [{ name: 'User' }, { name: 'Tag' }],
+            };
+            const response = {
                 data: {
-                    _typeName: 'Post',
-                    id: 'post1',
-                    title: 'title1',
-                    author: { id: 'author1', firstName: 'Toto' },
-                    coauthor: null,
-                    tags: [
-                        { id: 'tag1', name: 'tag1 name' },
-                        { id: 'tag2', name: 'tag2 name' },
-                    ],
-                    embeddedJson: { foo: 'bar' },
+                    data: {
+                        _typeName: 'Post',
+                        id: 'post1',
+                        title: 'title1',
+                        author: { id: 'author1', firstName: 'Toto' },
+                        coauthor: null,
+                        tags: [
+                            { id: 'tag1', name: 'tag1 name' },
+                            { id: 'tag2', name: 'tag2 name' },
+                        ],
+                        embeddedJson: { foo: 'bar' },
+                    },
                 },
-            },
-        };
-        expect(getResponseParser(introspectionResults)(type)(response)).toEqual(
-            {
+            };
+            expect(
+                getResponseParser(introspectionResults)(
+                    type,
+                    undefined,
+                    undefined
+                )(response)
+            ).toEqual({
                 data: {
                     id: 'post1',
                     title: 'title1',
@@ -161,56 +178,63 @@ const testSingleTypes = type => {
                     tagsIds: ['tag1', 'tag2'],
                     embeddedJson: { foo: 'bar' },
                 },
-            }
-        );
-    });
+            });
+        });
 
-    it('returns the response expected by AOR for GET_LIST', () => {
-        const introspectionResults = {
-            resources: [
-                {
-                    type: {
-                        name: 'User',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            {
-                                name: 'firstName',
-                                type: { kind: TypeKind.SCALAR },
-                            },
-                        ],
+        it('returns the response expected for %s with simple arrays of values', () => {
+            const introspectionResults = {
+                resources: [
+                    {
+                        type: {
+                            name: 'User',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'firstName',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
-                },
-                {
-                    type: {
-                        name: 'Tag',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            { name: 'name', type: { kind: TypeKind.SCALAR } },
-                        ],
+                    {
+                        type: {
+                            name: 'Tag',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'name',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
-                },
-            ],
-            types: [{ name: 'User' }, { name: 'Tag' }],
-        };
-        const response = {
-            data: {
+                ],
+                types: [{ name: 'User' }, { name: 'Tag' }],
+            };
+            const response = {
                 data: {
-                    _typeName: 'Post',
-                    id: 'post1',
-                    title: 'title1',
-                    author: { id: 'author1', firstName: 'Toto' },
-                    coauthor: null,
-                    tags: [
-                        { id: 'tag1', name: 'tag1 name' },
-                        { id: 'tag2', name: 'tag2 name' },
-                    ],
-                    features: ['feature1', 'feature2'],
-                    embeddedJson: { foo: 'bar' },
+                    data: {
+                        _typeName: 'Post',
+                        id: 'post1',
+                        title: 'title1',
+                        author: { id: 'author1', firstName: 'Toto' },
+                        coauthor: null,
+                        tags: [
+                            { id: 'tag1', name: 'tag1 name' },
+                            { id: 'tag2', name: 'tag2 name' },
+                        ],
+                        features: ['feature1', 'feature2'],
+                        embeddedJson: { foo: 'bar' },
+                    },
                 },
-            },
-        };
-        expect(getResponseParser(introspectionResults)(type)(response)).toEqual(
-            {
+            };
+            expect(
+                getResponseParser(introspectionResults)(
+                    type,
+                    undefined,
+                    undefined
+                )(response)
+            ).toEqual({
                 data: {
                     id: 'post1',
                     title: 'title1',
@@ -224,56 +248,63 @@ const testSingleTypes = type => {
                     tagsIds: ['tag1', 'tag2'],
                     embeddedJson: { foo: 'bar' },
                 },
-            }
-        );
-    });
+            });
+        });
 
-    it('returns the response expected by AOR for GET_LIST with aliases', () => {
-        const introspectionResults = {
-            resources: [
-                {
-                    type: {
-                        name: 'User',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            {
-                                name: 'firstName',
-                                type: { kind: TypeKind.SCALAR },
-                            },
-                        ],
+        it('returns the response expected for %s with aliases', () => {
+            const introspectionResults = {
+                resources: [
+                    {
+                        type: {
+                            name: 'User',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'firstName',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
-                },
-                {
-                    type: {
-                        name: 'Tag',
-                        fields: [
-                            { name: 'id', type: { kind: TypeKind.SCALAR } },
-                            { name: 'name', type: { kind: TypeKind.SCALAR } },
-                        ],
+                    {
+                        type: {
+                            name: 'Tag',
+                            fields: [
+                                { name: 'id', type: { kind: TypeKind.SCALAR } },
+                                {
+                                    name: 'name',
+                                    type: { kind: TypeKind.SCALAR },
+                                },
+                            ],
+                        },
                     },
-                },
-            ],
-            types: [{ name: 'User' }, { name: 'Tag' }],
-        };
-        const response = {
-            data: {
+                ],
+                types: [{ name: 'User' }, { name: 'Tag' }],
+            };
+            const response = {
                 data: {
-                    _typeName: 'Post',
-                    id: 'post1',
-                    aliasTitle: 'title1',
-                    author: { id: 'author1', firstName: 'Toto' },
-                    coauthor: null,
-                    tags: [
-                        { id: 'tag1', name: 'tag1 name' },
-                        { id: 'tag2', name: 'tag2 name' },
-                    ],
-                    embeddedJson: { foo: 'bar' },
+                    data: {
+                        _typeName: 'Post',
+                        id: 'post1',
+                        aliasTitle: 'title1',
+                        author: { id: 'author1', firstName: 'Toto' },
+                        coauthor: null,
+                        tags: [
+                            { id: 'tag1', name: 'tag1 name' },
+                            { id: 'tag2', name: 'tag2 name' },
+                        ],
+                        embeddedJson: { foo: 'bar' },
+                    },
                 },
-            },
-        };
+            };
 
-        expect(getResponseParser(introspectionResults)(type)(response)).toEqual(
-            {
+            expect(
+                getResponseParser(introspectionResults)(
+                    type,
+                    undefined,
+                    undefined
+                )(response)
+            ).toEqual({
                 data: {
                     aliasTitle: 'title1',
                     author: { firstName: 'Toto', id: 'author1' },
@@ -288,16 +319,7 @@ const testSingleTypes = type => {
                     ],
                     tagsIds: ['tag1', 'tag2'],
                 },
-            }
-        );
+            });
+        });
     });
-};
-
-describe('getResponseParser', () => {
-    testListTypes(GET_LIST);
-    testListTypes(GET_MANY);
-    testListTypes(GET_MANY_REFERENCE);
-    testSingleTypes(CREATE);
-    testSingleTypes(UPDATE);
-    testSingleTypes(DELETE);
 });
