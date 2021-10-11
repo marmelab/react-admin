7c66b81e81f7e2e3a7e31d1063ce3c4575a1d518
Travis CI <travis@travis-ci.org>
2021-10-11 12:12:39 +0200

    Fix popperjs related tests

--- test-setup.js
+++ test-setup.js
@@ -6,26 +6,6 @@ require('raf/polyfill');
  */
 require('mutationobserver-shim');
 
-/**
- * Mock PopperJS
- *
- * When using mount(), material-ui calls Popper.js, which is not compatible with JSDom
- * And causes UnhandledPromiseRejectionWarning: TypeError: document.createRange is not a function
- *
- * @see https://github.com/FezVrasta/popper.js/issues/478
- */
-jest.mock('@popperjs/core', () => {
-    const PopperJS = jest.requireActual('@popperjs/core');
-    return {
-        placements: PopperJS.placements,
-        destroy: () => {},
-        scheduleUpdate: () => {},
-        render: function (this) {
-            return this.$options._renderChildren;
-        },
-    };
-});
-
 // Ignore warnings about act()
 // See https://github.com/testing-library/react-testing-library/issues/281,
 // https://github.com/facebook/react/issues/14769
