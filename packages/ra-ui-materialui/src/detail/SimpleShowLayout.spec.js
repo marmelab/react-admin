e78c5d5b4f4bffb5180259388077e3948427b076
djhi <1122076+djhi@users.noreply.github.com>
2021-10-13 11:08:53 +0200

    Fix tests

--- packages/ra-ui-materialui/src/detail/SimpleShowLayout.spec.js
+++ packages/ra-ui-materialui/src/detail/SimpleShowLayout.spec.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 
-import SimpleShowLayout from './SimpleShowLayout';
+import { SimpleShowLayout } from './SimpleShowLayout';
 import TextField from '../field/TextField';
 
 describe('<SimpleShowLayout />', () => {
