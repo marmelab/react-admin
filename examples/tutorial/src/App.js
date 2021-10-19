55c0b5378cb849c3cff3b3de627cdc22a3f7bb55
Sweta Sinha <63840245+sweeta123@users.noreply.github.com>
2021-10-19 17:48:06 +0530

    removed unused spaces

--- examples/tutorial/src/App.js
+++ examples/tutorial/src/App.js
@@ -3,7 +3,6 @@ import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-
 import { PostList, PostEdit, PostCreate, PostShow } from './posts';
 import { UserList } from './users';
 import Dashboard from './Dashboard';
