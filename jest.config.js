c432b06bb24e074c44de7c36c667029f9b44878c
Travis CI <travis@travis-ci.org>
2021-11-02 10:33:06 +0100

    Configure jest so that we don't have to rebuild monorepo internal dependencies

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    globalSetup: './test-global-setup.js',
+    setupFilesAfterEnv: ['./test-setup.js'],
+    preset: 'ts-jest/presets/js-with-ts',
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/lib/',
+        '/esm/',
+        '/examples/simple/',
+    ],
+    transformIgnorePatterns: [
+        '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$',
+    ],
+    globals: {
+        'ts-jest': {
+            isolatedModules: true,
+        },
+    },
+    moduleNameMapper: {
+        '^ra-core(.*)$': path.join(__dirname, './packages/ra-core/src$1'),
+        '^ra-ui-materialui(.*)$': path.join(
+            __dirname,
+            './packages/ra-ui-materialui/src$1'
+        ),
+    },
+};
