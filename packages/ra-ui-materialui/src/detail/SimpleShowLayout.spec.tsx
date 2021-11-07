51bfc7de5064ec8bf76fe7dfd5494d3e849019e8
fzaninotto <fzaninotto@gmail.com>
2021-11-07 19:12:31 +0100

    Fix test

--- packages/ra-ui-materialui/src/detail/SimpleShowLayout.spec.tsx
+++ packages/ra-ui-materialui/src/detail/SimpleShowLayout.spec.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-
+import { RecordContextProvider } from 'ra-core';
 import { SimpleShowLayout } from './SimpleShowLayout';
 import { TextField } from '../field';
 
 describe('<SimpleShowLayout />', () => {
-    it('should display children inputs of SimpleShowLayout', () => {
+    it('should display children filelds', () => {
         const { queryByText } = render(
-            <SimpleShowLayout record={{ foo: 'foo', bar: 'bar' }}>
-                <TextField source="foo" />
-                <TextField source="bar" />
-            </SimpleShowLayout>
+            <RecordContextProvider value={{ source1: 'foo', source2: 'bar' }}>
+                <SimpleShowLayout>
+                    <TextField source="source1" />
+                    <TextField source="source2" />
+                </SimpleShowLayout>
+            </RecordContextProvider>
         );
         expect(queryByText('foo')).not.toBeNull();
         expect(queryByText('bar')).not.toBeNull();
