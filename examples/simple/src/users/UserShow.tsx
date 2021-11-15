bdc8f2ce84f7ba99e504a659f5d21a5e6587dd1f
djhi <1122076+djhi@users.noreply.github.com>
2021-11-15 11:25:16 +0100

    Update simple example

--- examples/simple/src/users/UserShow.tsx
+++ examples/simple/src/users/UserShow.tsx
@@ -1,26 +1,35 @@
 /* eslint react/jsx-key: off */
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { Show, Tab, TabbedShowLayout, TextField } from 'react-admin'; // eslint-disable-line import/no-unresolved
+import {
+    Show,
+    Tab,
+    TabbedShowLayout,
+    TextField,
+    usePermissions,
+} from 'react-admin';
 
 import UserTitle from './UserTitle';
 import Aside from './Aside';
 
-const UserShow = ({ permissions, ...props }) => (
-    <Show title={<UserTitle />} aside={<Aside />} {...props}>
-        <TabbedShowLayout>
-            <Tab label="user.form.summary">
-                <TextField source="id" />
-                <TextField source="name" />
-            </Tab>
-            {permissions === 'admin' && (
-                <Tab label="user.form.security" path="security">
-                    <TextField source="role" />
+const UserShow = () => {
+    const { permissions } = usePermissions();
+    return (
+        <Show title={<UserTitle />} aside={<Aside />}>
+            <TabbedShowLayout>
+                <Tab label="user.form.summary">
+                    <TextField source="id" />
+                    <TextField source="name" />
                 </Tab>
-            )}
-        </TabbedShowLayout>
-    </Show>
-);
+                {permissions === 'admin' && (
+                    <Tab label="user.form.security" path="security">
+                        <TextField source="role" />
+                    </Tab>
+                )}
+            </TabbedShowLayout>
+        </Show>
+    );
+};
 
 UserShow.propTypes = {
     location: PropTypes.object.isRequired,
