50d8058392a287a7eed577237e3268e909493d3a
Travis CI <travis@travis-ci.org>
2021-11-24 11:53:50 +0100

    Use CoreAdminContext in RoutesWithLayout tests

--- packages/ra-core/src/core/RoutesWithLayout.spec.tsx
+++ packages/ra-core/src/core/RoutesWithLayout.spec.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Route, MemoryRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { render, screen } from '@testing-library/react';
 
+import CoreAdminContext from './CoreAdminContext';
 import { CustomRoutes } from './CustomRoutes';
 import { RoutesWithLayout } from './RoutesWithLayout';
 
@@ -13,27 +13,22 @@ describe('<RoutesWithLayout>', () => {
     const Resource = ({ name }) => <div>{name}</div>;
     const CatchAll = () => <div>Catch all</div>;
 
-    // the Provider is required because the dashboard is wrapped by <Authenticated>, which is a connected component
-    const store = createStore(() => ({
-        admin: {},
-        router: { location: { pathname: '/' } },
-    }));
-
     it('should show dashboard on / when provided', () => {
         render(
-            <Provider store={store}>
-                <MemoryRouter initialEntries={['/']}>
-                    <RoutesWithLayout
-                        catchAll={CatchAll}
-                        dashboard={Dashboard}
-                        firstResource="default"
-                    >
-                        <Resource name="default" />
-                        <Resource name="another" />
-                        <Resource name="yetanother" />
-                    </RoutesWithLayout>
-                </MemoryRouter>
-            </Provider>
+            <CoreAdminContext
+                dataProvider={() => Promise.resolve()}
+                history={createMemoryHistory({ initialEntries: ['/'] })}
+            >
+                <RoutesWithLayout
+                    catchAll={CatchAll}
+                    dashboard={Dashboard}
+                    firstResource="default"
+                >
+                    <Resource name="default" />
+                    <Resource name="another" />
+                    <Resource name="yetanother" />
+                </RoutesWithLayout>
+            </CoreAdminContext>
         );
 
         expect(screen.queryByText('Dashboard')).not.toBeNull();
@@ -41,16 +36,14 @@ describe('<RoutesWithLayout>', () => {
 
     it('should show the first resource on / when there is only one resource and no dashboard', () => {
         render(
-            <Provider store={store}>
-                <MemoryRouter initialEntries={['/']}>
-                    <RoutesWithLayout
-                        firstResource="default"
-                        catchAll={CatchAll}
-                    >
-                        <Resource name="default" />
-                    </RoutesWithLayout>
-                </MemoryRouter>
-            </Provider>
+            <CoreAdminContext
+                dataProvider={() => Promise.resolve()}
+                history={createMemoryHistory({ initialEntries: ['/'] })}
+            >
+                <RoutesWithLayout firstResource="default" catchAll={CatchAll}>
+                    <Resource name="default" />
+                </RoutesWithLayout>
+            </CoreAdminContext>
         );
 
         expect(screen.queryByText('default')).not.toBeNull();
@@ -58,18 +51,16 @@ describe('<RoutesWithLayout>', () => {
 
     it('should show the first resource on / when there are multiple resource and no dashboard', () => {
         render(
-            <Provider store={store}>
-                <MemoryRouter initialEntries={['/']}>
-                    <RoutesWithLayout
-                        firstResource="default"
-                        catchAll={CatchAll}
-                    >
-                        <Resource name="default" />
-                        <Resource name="another" />
-                        <Resource name="yetanother" />
-                    </RoutesWithLayout>
-                </MemoryRouter>
-            </Provider>
+            <CoreAdminContext
+                dataProvider={() => Promise.resolve()}
+                history={createMemoryHistory({ initialEntries: ['/'] })}
+            >
+                <RoutesWithLayout firstResource="default" catchAll={CatchAll}>
+                    <Resource name="default" />
+                    <Resource name="another" />
+                    <Resource name="yetanother" />
+                </RoutesWithLayout>
+            </CoreAdminContext>
         );
 
         expect(screen.queryByText('default')).not.toBeNull();
@@ -79,21 +70,19 @@ describe('<RoutesWithLayout>', () => {
 
     it('should accept custom routes', () => {
         render(
-            <Provider store={store}>
-                <MemoryRouter initialEntries={['/custom']}>
-                    <RoutesWithLayout
-                        catchAll={CatchAll}
-                        firstResource="default"
-                    >
-                        <Resource name="default" />
-                        <Resource name="another" />
-                        <Resource name="yetanother" />
-                        <CustomRoutes>
-                            <Route path="/custom" element={<Custom />} />
-                        </CustomRoutes>
-                    </RoutesWithLayout>
-                </MemoryRouter>
-            </Provider>
+            <CoreAdminContext
+                dataProvider={() => Promise.resolve()}
+                history={createMemoryHistory({ initialEntries: ['/custom'] })}
+            >
+                <RoutesWithLayout catchAll={CatchAll} firstResource="default">
+                    <Resource name="default" />
+                    <Resource name="another" />
+                    <Resource name="yetanother" />
+                    <CustomRoutes>
+                        <Route path="/custom" element={<Custom />} />
+                    </CustomRoutes>
+                </RoutesWithLayout>
+            </CoreAdminContext>
         );
 
         expect(screen.queryByText('Custom')).not.toBeNull();
