5108895437f61f9edd6c8d4083b04a4f344092fe
Travis CI <travis@travis-ci.org>
2021-11-26 23:12:45 +0100

    Document how to use the fake browser decorator

--- packages/ra-core/src/storybook/FakeBrowser.tsx
+++ packages/ra-core/src/storybook/FakeBrowser.tsx
@@ -8,6 +8,27 @@ import {
     parsePath,
 } from 'history';
 
+/**
+ * This is a storybook decorator that wrap the story inside a fake browser.
+ * It features an editable address bar with back and forward buttons.
+ *
+ * @example Usage in a storybook
+ * export default {
+ *     title: 'ra-core/Admin/CustomRoutes/Authenticated',
+ *     decorators: [FakeBrowserDecorator],
+ *     parameters: {
+ *         // You can pass the react-router history initial entries like this
+ *         initialEntries: ['/authenticated'],
+ *     },
+ * };
+ *
+ * const MyStory = (args, context) => (
+ *     // Don't forget to pass the history to the Admin component so that
+ *     // user changes from the fake browser address bar can impact the application
+ *     <Admin history={context.history}>
+ *     </Admin>
+ * );
+ */
 export const FakeBrowserDecorator = (Story, context) => {
     const history = createMemoryHistory({
         initialEntries: context.parameters.initialEntries,
