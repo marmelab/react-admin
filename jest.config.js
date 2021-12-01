725b7636bb9536013cafac16975c19bb4a53f757
Travis CI <travis@travis-ci.org>
2021-12-01 09:58:53 +0100

    Fix jest Config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const fs = require('fs');
 
+const packages = fs.readdirSync(path.resolve(__dirname, './packages'));
+const moduleNameMapper = packages.reduce((mapper, dirName) => {
+    const package = require(path.resolve(
+        __dirname,
+        './packages',
+        dirName,
+        'package.json'
+    ));
+    mapper[`^${package.name}(.*)$`] = path.join(
+        __dirname,
+        `./packages/${dirName}/src$1`
+    );
+    return mapper;
+}, {});
+
 module.exports = {
     globalSetup: './test-global-setup.js',
     setupFilesAfterEnv: ['./test-setup.js'],
@@ -21,18 +36,3 @@ module.exports = {
     },
     moduleNameMapper,
 };
-
-const packages = fs.readdirSync(path.resolve(__dirname, './packages'));
-const moduleNameMapper = packages.reduce((mapper, dirName) => {
-    const package = require(path.resolve(
-        __dirname,
-        './packages',
-        dirName,
-        'package.json'
-    ));
-    mapper[`^${package.name}(.*)$`] = path.join(
-        __dirname,
-        `./packages/${dirName}/src$1`
-    );
-    return mapper;
-}, {});
