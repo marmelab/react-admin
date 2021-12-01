61d1363f302a70ab74cbd81d7c1a68771a833c82
Travis CI <travis@travis-ci.org>
2021-12-01 09:53:01 +0100

    Ensure all packages are aliased for Vite

--- examples/simple/vite.config.js
+++ examples/simple/vite.config.js
@@ -1,5 +1,23 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import path from 'path';
+import fs from 'fs';
+
+const packages = fs.readdirSync(path.resolve(__dirname, '../../packages'));
+const aliases = packages.map(dirName => {
+    const packageJson = require(path.resolve(
+        __dirname,
+        '../../packages',
+        dirName,
+        'package.json'
+    ));
+    return {
+        find: new RegExp(`^${packageJson.name}$/`),
+        replacement: path.resolve(
+            __dirname,
+            `../../packages/${packageJson.name}/src`
+        ),
+    };
+}, {});
 
 /**
  * https://vitejs.dev/config/
@@ -9,20 +27,7 @@ export default {
     plugins: [reactRefresh()],
     resolve: {
         alias: [
-            {
-                find: /^react-admin$/,
-                replacement: path.resolve(
-                    __dirname,
-                    '../../packages/react-admin/src'
-                ),
-            },
-            {
-                find: /^ra-(.*)$/,
-                replacement: path.resolve(
-                    __dirname,
-                    '../../packages/ra-$1/src'
-                ),
-            },
+            ...aliases,
             {
                 find: /^@mui\/icons-material\/(.*)/,
                 replacement: '@mui/icons-material/esm/$1',
