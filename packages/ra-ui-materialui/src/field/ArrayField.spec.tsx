41d1b25d7ec6d68ae8c9019cb83536a30a7aff46
Luwangel <adrien.amoros@gmail.com>
2021-12-15 15:20:49 +0100

    Test the usage of a SimpleList as child of an ArrayField

--- packages/ra-ui-materialui/src/field/ArrayField.spec.tsx
+++ packages/ra-ui-materialui/src/field/ArrayField.spec.tsx
@@ -6,6 +6,7 @@ import ArrayField from './ArrayField';
 import NumberField from './NumberField';
 import TextField from './TextField';
 import Datagrid from '../list/datagrid/Datagrid';
+import SimpleList from '../list/SimpleList';
 
 describe('<ArrayField />', () => {
     const currentSort = { field: 'id', order: 'ASC' };
@@ -27,7 +28,27 @@ describe('<ArrayField />', () => {
         );
     });
 
-    it('should render the underlying iterator component', () => {
+    it('should render the alternative empty component', () => {
+        const { queryByText } = render(
+            <TestContext>
+                <ArrayField
+                    source="arr"
+                    resource="posts"
+                    record={{
+                        id: 123,
+                        arr: [],
+                    }}
+                >
+                    <Datagrid empty={<div>No posts</div>}>
+                        <NumberField source="id" />
+                    </Datagrid>
+                </ArrayField>
+            </TestContext>
+        );
+        expect(queryByText('No posts')).not.toBeNull();
+    });
+
+    it('should render the <Datagrid> iterator component', () => {
         const { queryByText } = render(
             <TestContext>
                 <ArrayField
@@ -58,7 +79,7 @@ describe('<ArrayField />', () => {
         expect(queryByText('456')).not.toBeNull();
     });
 
-    it('should render the alternative empty component', () => {
+    it('should render the <SimpleList> iterator component', () => {
         const { queryByText } = render(
             <TestContext>
                 <ArrayField
@@ -66,15 +87,25 @@ describe('<ArrayField />', () => {
                     resource="posts"
                     record={{
                         id: 123,
-                        arr: [],
+                        arr: [
+                            { id: 123, foo: 'bar' },
+                            { id: 456, foo: 'baz' },
+                        ],
                     }}
                 >
-                    <Datagrid empty={<div>No posts</div>}>
-                        <NumberField source="id" />
-                    </Datagrid>
+                    <SimpleList
+                        primaryText={record => record.foo}
+                        secondaryText={record => record.id}
+                    />
                 </ArrayField>
             </TestContext>
         );
-        expect(queryByText('No posts')).not.toBeNull();
+
+        // Test the fields values
+        expect(queryByText('bar')).not.toBeNull();
+        expect(queryByText('123')).not.toBeNull();
+
+        expect(queryByText('baz')).not.toBeNull();
+        expect(queryByText('456')).not.toBeNull();
     });
 });
