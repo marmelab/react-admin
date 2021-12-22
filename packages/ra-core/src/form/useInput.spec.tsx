57624f335b4eb4b9962da395f553053b43a5396c
fzaninotto <fzaninotto@gmail.com>
2021-12-22 10:42:45 +0100

    Fix linter warning

--- packages/ra-core/src/form/useInput.spec.tsx
+++ packages/ra-core/src/form/useInput.spec.tsx
@@ -3,7 +3,6 @@ import { FunctionComponent, ReactElement } from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Form, useFormState } from 'react-final-form';
 import { renderWithRedux } from 'ra-test';
-import { RecordContextProvider } from '../controller';
 import FormWithRedirect from './FormWithRedirect';
 import useInput, { InputProps } from './useInput';
 import { required } from './validate';
