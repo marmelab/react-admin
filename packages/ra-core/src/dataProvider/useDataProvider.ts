45f008e196dc41479ef12c2e4ed5887363425aa9
François Zaninotto <fzaninotto@gmail.com>
2022-01-02 09:03:13 +0100

    Fix useDataProvider docs

--- packages/ra-core/src/dataProvider/useDataProvider.ts
+++ packages/ra-core/src/dataProvider/useDataProvider.ts
@@ -10,15 +10,9 @@ import useLogoutIfAccessDenied from '../auth/useLogoutIfAccessDenied';
  * Hook for getting a dataProvider
  *
  * Gets a dataProvider object, which behaves just like the real dataProvider
- * (same methods returning a Promise). But it's actually a Proxy object, which
- * dispatches Redux actions along the process. The benefit is that react-admin
- * tracks the loading state when using this hook, and stores results in the
- * Redux store for future use.
- *
- * In addition to the 2 usual parameters of the dataProvider methods (resource,
- * payload), the Proxy supports a third parameter for every call. It's an
- * object literal which may contain side effects, or make the action optimistic
- * (with mutationMode: optimistic) or undoable (with mutationMode: undoable).
+ * (same methods returning a Promise). But it's actually a Proxy object,
+ * which validates the response format, and logs the user out upon error
+ * if authProvider.checkError() rejects.
  *
  * @return dataProvider
  *
@@ -76,24 +70,6 @@ import useLogoutIfAccessDenied from '../auth/useLogoutIfAccessDenied';
  *         </ul>
  *     )
  * }
- *
- * @example Action customization
- *
- * dataProvider.getOne('users', { id: 123 });
- * // will dispatch the following actions:
- * // - CUSTOM_FETCH
- * // - CUSTOM_FETCH_LOADING
- * // - FETCH_START
- * // - CUSTOM_FETCH_SUCCESS
- * // - FETCH_END
- *
- * dataProvider.getOne('users', { id: 123 }, { action: CRUD_GET_ONE });
- * // will dispatch the following actions:
- * // - CRUD_GET_ONE
- * // - CRUD_GET_ONE_LOADING
- * // - FETCH_START
- * // - CRUD_GET_ONE_SUCCESS
- * // - FETCH_END
  */
 export const useDataProvider = <
     TDataProvider extends DataProvider = DataProvider
