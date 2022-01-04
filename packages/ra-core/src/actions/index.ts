854d830d38b4dad541e0a183ec86b92e807a8393
François Zaninotto <fzaninotto@gmail.com>
2022-01-04 07:48:56 +0100

    Remove unused reducer

--- packages/ra-core/src/actions/index.ts
+++ packages/ra-core/src/actions/index.ts
@@ -1,7 +1,6 @@
 export * from './clearActions';
 export * from './filterActions';
 export * from './listActions';
-export * from './localeActions';
 export * from './notificationActions';
 export * from './uiActions';
 export * from './undoActions';
