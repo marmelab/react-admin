dd0d2c737264f499474eb6a69c407f6dbf6e7b81
Travis CI <travis@travis-ci.org>
2022-01-11 22:54:07 +0100

    Extract Form Groups Management

--- packages/ra-core/src/types.ts
+++ packages/ra-core/src/types.ts
@@ -360,22 +360,6 @@ export type SetOnSave = (
     onSave?: (values: object, redirect: any) => void
 ) => void;
 
-export type FormGroupSubscriber = () => void;
-export type FormContextValue = {
-    setOnSave?: SetOnSave;
-    registerGroup: (name: string) => void;
-    unregisterGroup: (name: string) => void;
-    registerField: (source: string, group?: string) => void;
-    unregisterField: (source: string, group?: string) => void;
-    getGroupFields: (name: string) => string[];
-    /**
-     * Subscribe to any changes of the group content (fields added or removed).
-     * Subscribers can get the current fields of the group by calling getGroupFields.
-     * Returns a function to unsubscribe.
-     */
-    subscribe: (name: string, subscriber: FormGroupSubscriber) => () => void;
-};
-
 export type FormFunctions = {
     setOnSave?: SetOnSave;
 };
