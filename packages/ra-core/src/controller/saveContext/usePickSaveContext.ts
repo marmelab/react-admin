6845d7d1f757f4ec6d525fd5c847712082d9b077
djhi <1122076+djhi@users.noreply.github.com>
2022-01-13 09:37:10 +0100

    Add comments on usePickSaveContext

--- packages/ra-core/src/controller/saveContext/usePickSaveContext.ts
+++ packages/ra-core/src/controller/saveContext/usePickSaveContext.ts
@@ -2,6 +2,9 @@ import { useMemo } from 'react';
 import pick from 'lodash/pick';
 import { SaveContextValue } from './SaveContext';
 
+/**
+ * This hook extracts the `save` and `saving` properties from either the `CreateContext` or `EditContext`. This ensures the `SaveContext` doesn't rerender when those two contexts have other properties changes.
+ */
 export const usePickSaveContext = <
     ContextType extends SaveContextValue = SaveContextValue
 >(
