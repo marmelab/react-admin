d8f14674e5cd6dbf014582b75a86745aefe56c23
djhi <1122076+djhi@users.noreply.github.com>
2022-01-17 15:42:24 +0100

    Fix simple example Comment with posts reference

--- examples/simple/src/comments/PostQuickCreate.tsx
+++ examples/simple/src/comments/PostQuickCreate.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
+import {
+    Button,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+} from '@mui/material';
 import { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -9,8 +16,10 @@ import {
     Toolbar,
     required,
     useCreate,
+    useCreateSuggestionContext,
     useNotify,
     useLoading,
+    useTranslate,
 } from 'react-admin'; // eslint-disable-line import/no-unresolved
 
 import CancelButton from './PostQuickCreateCancelButton';
@@ -25,25 +34,12 @@ const StyledSimpleForm = styled(SimpleForm)({
     [`& .${classes.form}`]: { padding: 0 },
 });
 
-// We need a custom toolbar to add our custom buttons
-// The CancelButton allows to close the modal without submitting anything
-const PostQuickCreateToolbar = ({ submitting, onCancel, ...props }) => (
-    <Toolbar {...props} disableGutters>
-        <SaveButton />
-        <CancelButton onClick={onCancel} />
-    </Toolbar>
-);
-
-PostQuickCreateToolbar.propTypes = {
-    submitting: PropTypes.bool,
-    onCancel: PropTypes.func.isRequired,
-};
-
-const PostQuickCreate = ({ onCancel, onSave, ...props }) => {
+const PostQuickCreate = props => {
     const [create] = useCreate();
     const notify = useNotify();
     const submitting = useLoading();
 
+    const { onCancel, onCreate } = useCreateSuggestionContext();
     const handleSave = useCallback(
         values => {
             create(
@@ -51,44 +47,56 @@ const PostQuickCreate = ({ onCancel, onSave, ...props }) => {
                 { data: values },
                 {
                     onSuccess: data => {
-                        onSave(data);
+                        onCreate(data);
                     },
                     onError: (error: Error) => {
-                        notify(error.message, 'error');
+                        notify(error.message, { type: 'error' });
                     },
                 }
             );
         },
-        [create, notify, onSave]
+        [create, notify, onCreate]
     );
+
+    const translate = useTranslate();
 
     return (
-        <StyledSimpleForm
-            onSubmit={handleSave}
-            saving={submitting}
-            toolbar={
-                <PostQuickCreateToolbar
-                    onCancel={onCancel}
-                    submitting={submitting}
-                />
-            }
-            classes={{ form: classes.form }}
-            {...props}
+        <Dialog
+            data-testid="dialog-add-post"
+            open
+            fullWidth
+            onClose={onCancel}
+            aria-label={translate('simple.create-post')}
         >
-            <TextInput source="title" validate={required()} />
-            <TextInput
-                source="teaser"
-                validate={required()}
-                fullWidth={true}
-                multiline={true}
-            />
-        </StyledSimpleForm>
+            <DialogTitle>{translate('simple.create-post')}</DialogTitle>
+            <DialogContent>
+                <StyledSimpleForm
+                    id="post-quick-create"
+                    onSubmit={handleSave}
+                    saving={submitting}
+                    toolbar={null}
+                    {...props}
+                >
+                    <TextInput
+                        defaultValue=""
+                        source="title"
+                        validate={required()}
+                    />
+                    <TextInput
+                        defaultValue=""
+                        source="teaser"
+                        validate={required()}
+                        fullWidth={true}
+                        multiline={true}
+                    />
+                </StyledSimpleForm>
+            </DialogContent>
+            <DialogActions>
+                <SaveButton form="post-quick-create" submitOnEnter />
+                <CancelButton onClick={onCancel} />
+            </DialogActions>
+        </Dialog>
     );
-};
-
-PostQuickCreate.propTypes = {
-    onCancel: PropTypes.func.isRequired,
-    onSave: PropTypes.func.isRequired,
 };
 
 export default PostQuickCreate;
