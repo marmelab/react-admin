5d19fd0743728209430592608f55d753178c60e5
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2022-01-17 11:14:09 -0300

    Add Notification JsDocs

--- packages/ra-ui-materialui/src/layout/Notification.tsx
+++ packages/ra-ui-materialui/src/layout/Notification.tsx
@@ -16,6 +16,18 @@ const defaultAnchorOrigin: SnackbarOrigin = {
     horizontal: 'center',
 };
 
+/**
+ * Provides a way to show a notification.
+ * @see useNotify
+ *
+ * @example <caption>Basic usage</caption>
+ * <Notification />
+ *
+ * @param props The component props
+ * @param {string} props.type The notification type. Defaults to 'info'.
+ * @param {number} props.autoHideDuration Duration in milliseconds to wait until hiding a given notification. Defaults to 4000.
+ * @param {boolean} props.multiLine Set it to `true` if the notification message should be shown in more than one line.
+ */
 export const Notification = (props: NotificationProps) => {
     const {
         classes: classesOverride,
