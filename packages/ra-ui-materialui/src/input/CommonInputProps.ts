a69a9a9ad4de4634dee0f4f4376123d3a2853835
djhi <1122076+djhi@users.noreply.github.com>
2022-01-17 17:42:05 +0100

    Better CommonInputProps

--- packages/ra-ui-materialui/src/input/CommonInputProps.ts
+++ packages/ra-ui-materialui/src/input/CommonInputProps.ts
@@ -2,4 +2,6 @@ import { InputProps } from 'ra-core';
 
 export type CommonInputProps = InputProps & {
     fullWidth?: boolean;
+    margin?: 'none' | 'dense' | 'normal';
+    variant?: 'standard' | 'outlined' | 'filled';
 };
