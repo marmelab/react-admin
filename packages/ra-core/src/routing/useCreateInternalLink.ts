1f221af955d6c2a9e266f630e73b7939aaf4f6d7
François Zaninotto <fzaninotto@gmail.com>
2022-01-18 12:49:58 +0100

    add jsDoc

--- packages/ra-core/src/routing/useCreateInternalLink.ts
+++ packages/ra-core/src/routing/useCreateInternalLink.ts
@@ -3,6 +3,40 @@ import { useCallback } from 'react';
 import { Identifier } from '../types';
 import { useBasename } from './useBasename';
 
+/**
+ * Get a callback to create a link to a given page in the admin app.
+ *
+ * The callback expects an object as parameter, containing the following properties:
+ *   - type: 'list', 'edit', 'show' or 'create'
+ *   - resource
+ *   - id (optional), for 'edit' or 'show' pages
+ *
+ * This is used internally by react-admin to allow default components to work
+ * in applications that are mounted on a sub path, e.g. '/admin'. If your app
+ * is mounted in the root path, you don't need it, and you can create links by
+ * hand, e.g. '/articles/1/show'.
+ *
+ * @example
+ * import { useCreateInternalLink, useRecordContext } from 'react-admin';
+ * import { useNavigate } from 'react-router-dom';
+ *
+ * const PostEditButton = () => {
+ *     const createLink = useCreateInternalLink();
+ *     const record = useRecordContext();
+ *     const navigate = useNavigate();
+ *
+ *     const handleClick = () => {
+ *         const link = createLink({
+ *            type: 'edit',
+ *            resource: 'posts',
+ *            id: record.id
+ *         });
+ *         navigate(link);
+ *     };
+ *
+ *    return <button onClick={handleClick}>Edit Post</button>;
+ * };
+ */
 export const useCreateInternalLink = () => {
     const basename = useBasename();
     return useCallback(
