e73070d659d80680b7279c3d6709f81e83e980e9
Travis CI <travis@travis-ci.org>
2022-01-21 23:44:34 +0100

    Fix input common props

--- packages/ra-ui-materialui/src/input/CommonInputProps.ts
+++ packages/ra-ui-materialui/src/input/CommonInputProps.ts
@@ -1,7 +1,10 @@
 import { InputProps } from 'ra-core';
 
 export type CommonInputProps = InputProps & {
+    cellClassName?: string;
+    formClassName?: string;
     fullWidth?: boolean;
+    headerCellClassName?: string;
     margin?: 'none' | 'dense' | 'normal';
     variant?: 'standard' | 'outlined' | 'filled';
 };
