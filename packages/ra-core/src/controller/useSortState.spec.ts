6a9d6c08699a2fd4051451b32668b8ef747802de
François Zaninotto <fzaninotto@gmail.com>
2022-01-27 13:25:23 +0100

    remove usage of renderHook

--- packages/ra-core/src/controller/useSortState.spec.ts
+++ packages/ra-core/src/controller/useSortState.spec.ts
@@ -1,34 +1,34 @@
-import { renderHook } from 'ra-test';
+import { act, renderHook } from '@testing-library/react-hooks';
+
 import useSortState, { defaultSort } from './useSortState';
-import { act } from '@testing-library/react';
 
 describe('useSortState', () => {
     it('should initialize sortState with default sort', () => {
-        const { hookValue } = renderHook(() => useSortState());
+        const { result } = renderHook(() => useSortState());
 
-        expect(hookValue.sort).toEqual(defaultSort);
+        expect(result.current.sort).toEqual(defaultSort);
     });
 
     it('should initialize sortState with given sort', () => {
-        const { hookValue } = renderHook(() =>
+        const { result } = renderHook(() =>
             useSortState({
                 field: 'name',
                 order: 'ASC',
             })
         );
 
-        expect(hookValue.sort).toEqual({ field: 'name', order: 'ASC' });
+        expect(result.current.sort).toEqual({ field: 'name', order: 'ASC' });
     });
 
     it('should provide setSort method to change the whole sort', () => {
-        const { hookValue, childrenMock } = renderHook(() =>
+        const { result } = renderHook(() =>
             useSortState({ field: 'id', order: 'DESC' })
         );
 
-        expect(hookValue.sort).toEqual({ field: 'id', order: 'DESC' });
+        expect(result.current.sort).toEqual({ field: 'id', order: 'DESC' });
 
-        act(() => hookValue.setSort({ field: 'name', order: 'ASC' }));
-        expect(childrenMock.mock.calls[1][0].sort).toEqual({
+        act(() => result.current.setSort({ field: 'name', order: 'ASC' }));
+        expect(result.current.sort).toEqual({
             field: 'name',
             order: 'ASC',
         });
@@ -36,33 +36,33 @@ describe('useSortState', () => {
 
     describe('setSortField in return value', () => {
         it('should just change the order if receiving the current field', () => {
-            const { hookValue, childrenMock } = renderHook(() =>
+            const { result } = renderHook(() =>
                 useSortState({ field: 'id', order: 'DESC' })
             );
 
-            expect(hookValue.sort).toEqual({ field: 'id', order: 'DESC' });
+            expect(result.current.sort).toEqual({ field: 'id', order: 'DESC' });
 
-            act(() => hookValue.setSortField('id'));
-            expect(childrenMock.mock.calls[1][0].sort).toEqual({
+            act(() => result.current.setSortField('id'));
+            expect(result.current.sort).toEqual({
                 field: 'id',
                 order: 'ASC',
             });
         });
 
         it('should change the field and set the order to ASC if receiving another field', () => {
-            const { hookValue, childrenMock } = renderHook(() =>
+            const { result } = renderHook(() =>
                 useSortState({ field: 'id', order: 'ASC' })
             );
 
-            expect(hookValue.sort).toEqual({ field: 'id', order: 'ASC' });
+            expect(result.current.sort).toEqual({ field: 'id', order: 'ASC' });
 
-            act(() => hookValue.setSortField('name'));
-            expect(childrenMock.mock.calls[1][0].sort).toEqual({
+            act(() => result.current.setSortField('name'));
+            expect(result.current.sort).toEqual({
                 field: 'name',
                 order: 'ASC',
             });
-            act(() => hookValue.setSortField('id'));
-            expect(childrenMock.mock.calls[2][0].sort).toEqual({
+            act(() => result.current.setSortField('id'));
+            expect(result.current.sort).toEqual({
                 field: 'id',
                 order: 'ASC',
             });
