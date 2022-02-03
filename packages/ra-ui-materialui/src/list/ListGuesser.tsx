3852852c94d60c44b1938b819ac51a02caa9b186
djhi <1122076+djhi@users.noreply.github.com>
2022-02-03 18:00:06 +0100

    Fix unit tests

--- packages/ra-ui-materialui/src/list/ListGuesser.tsx
+++ packages/ra-ui-materialui/src/list/ListGuesser.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import inflection from 'inflection';
 import {
+    ComponentPropType,
     useListController,
     getElementsFromRecords,
     InferredElement,
@@ -11,6 +13,7 @@ import {
     RaRecord,
 } from 'ra-core';
 
+import { TitlePropType } from '../layout/Title';
 import { ListView, ListViewProps } from './ListView';
 import { listFieldTypes } from './listFieldTypes';
 
@@ -79,4 +82,49 @@ ${inferredChild.getRepresentation()}
     return <ListView {...props}>{inferredChild}</ListView>;
 };
 
-ListViewGuesser.propTypes = ListView.propTypes;
+ListViewGuesser.propTypes = {
+    // @ts-ignore-line
+    actions: PropTypes.oneOfType([PropTypes.bool, PropTypes.element]),
+    aside: PropTypes.element,
+    children: PropTypes.element,
+    className: PropTypes.string,
+    component: ComponentPropType,
+    // @ts-ignore-line
+    sort: PropTypes.shape({
+        field: PropTypes.string.isRequired,
+        order: PropTypes.string.isRequired,
+    }),
+    data: PropTypes.any,
+    defaultTitle: PropTypes.string,
+    displayedFilters: PropTypes.object,
+    emptyWhileLoading: PropTypes.bool,
+    // @ts-ignore-line
+    exporter: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
+    filterDefaultValues: PropTypes.object,
+    filters: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.arrayOf(PropTypes.element),
+    ]),
+    filterValues: PropTypes.object,
+    hasCreate: PropTypes.bool,
+    hideFilter: PropTypes.func,
+    ids: PropTypes.array,
+    loading: PropTypes.bool,
+    onSelect: PropTypes.func,
+    onToggleItem: PropTypes.func,
+    onUnselectItems: PropTypes.func,
+    page: PropTypes.number,
+    // @ts-ignore-line
+    pagination: PropTypes.oneOfType([PropTypes.element, PropTypes.bool]),
+    perPage: PropTypes.number,
+    refresh: PropTypes.func,
+    resource: PropTypes.string,
+    selectedIds: PropTypes.array,
+    setFilters: PropTypes.func,
+    setPage: PropTypes.func,
+    setPerPage: PropTypes.func,
+    setSort: PropTypes.func,
+    showFilter: PropTypes.func,
+    title: TitlePropType,
+    total: PropTypes.number,
+};
