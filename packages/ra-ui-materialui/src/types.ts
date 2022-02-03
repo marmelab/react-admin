091c34ff42a505c521a7a0a5a7eea0b1d8fd7b90
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2022-02-03 11:34:10 -0300

    Remove ClassesOverride type

--- packages/ra-ui-materialui/src/types.ts
+++ packages/ra-ui-materialui/src/types.ts
@@ -66,34 +66,3 @@ export interface BulkActionProps {
     resource?: string;
     selectedIds?: Identifier[];
 }
-
-/**
- * Generic type for the classes prop allowing to override material-ui styles
- *
- * @see https://github.com/mui-org/material-ui/issues/17973#issuecomment-639281445
- *
- * @example
- *
- * const useStyles = makeStyles({
- *     root: {
- *         ...
- *     }
- * })
- *
- * const DummyComponent: FC<DummyComponentProps> = (props) => {
- *     const classes = useStyles();
- *     // ...
- *     return (
- *         <div className={classes.root}>
- *             // ...
- *         </div>
- *     );
- * }
- *
- * interface DummyComponentProps {
- *	    classes?: ClassesOverride<typeof useStyles>;
- * }
- */
-export type ClassesOverride<
-    UseStyles extends (props: any) => Record<string, string>
-> = Partial<Record<keyof ReturnType<UseStyles>, string>>;
