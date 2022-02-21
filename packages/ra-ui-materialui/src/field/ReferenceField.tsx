8294f12d6642a754035b3dff69503f39ddccb629
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2022-02-21 11:14:52 -0300

    Fix link color

--- packages/ra-ui-materialui/src/field/ReferenceField.tsx
+++ packages/ra-ui-materialui/src/field/ReferenceField.tsx
@@ -255,5 +255,9 @@ const Root = styled('span', {
     name: PREFIX,
     overridesResolver: (props, styles) => styles.root,
 })(({ theme }) => ({
-    [`& .${ReferenceFieldClasses.link}`]: {},
+    [`& .${ReferenceFieldClasses.link}`]: {
+        '& :first-child': {
+            color: theme.palette.primary.main,
+        },
+    },
 }));
