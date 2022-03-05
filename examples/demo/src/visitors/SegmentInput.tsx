b05d8c1c8719872fd02c41e4561f947aa9008bab
François Zaninotto <fzaninotto@gmail.com>
2022-03-05 08:29:00 +0100

    [Demo] Make visitors commponent canonical

--- examples/demo/src/visitors/SegmentInput.tsx
+++ examples/demo/src/visitors/SegmentInput.tsx
@@ -1,33 +1,15 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import { useTranslate, SelectInput, InputProps } from 'react-admin';
+import { SelectInput, SelectInputProps } from 'react-admin';
 
 import segments from '../segments/data';
 
-const StyledSelectInput = styled(SelectInput)({
-    '&': { width: 150 },
-});
-
-interface Props extends Omit<InputProps, 'source'> {
-    source?: string;
-}
-
-const SegmentInput = (props: Props) => {
-    const translate = useTranslate();
-
-    return (
-        <StyledSelectInput
-            {...props}
-            choices={segments.map(segment => ({
-                id: segment.id,
-                name: translate(segment.name),
-            }))}
-        />
-    );
-};
-
-SegmentInput.defaultProps = {
-    source: 'groups',
-};
+const SegmentInput = (props: SelectInputProps) => (
+    <SelectInput
+        {...props}
+        source="groups"
+        translateChoice
+        choices={segments}
+    />
+);
 
 export default SegmentInput;
