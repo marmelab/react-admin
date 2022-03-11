fb4b7175ab086101bfcb7932911065f8f3b7df63
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2022-03-11 11:58:45 -0300

    Update packages/ra-ui-materialui/src/form/Toolbar.tsx
    Co-authored-by: Gildas Garcia <1122076+djhi@users.noreply.github.com>

--- packages/ra-ui-materialui/src/form/Toolbar.tsx
+++ packages/ra-ui-materialui/src/form/Toolbar.tsx
@@ -66,7 +66,7 @@ export const Toolbar = <
         children,
         className,
         resource,
-        saving: saveingProp,
+        saving: savingProp,
         submitOnEnter: submitOnEnterProp,
         mutationMode: mutationModeProp,
         ...rest
