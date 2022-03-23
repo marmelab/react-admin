e83e65a26833b3f8fdf13700228af909085747fc
asvarcas <asvarcas@etriek.com.uy>
2022-03-23 16:11:56 -0300

    Remove unnecessary record prop from Avatar

--- examples/crm/src/contacts/ContactShow.tsx
+++ examples/crm/src/contacts/ContactShow.tsx
@@ -29,7 +29,7 @@ const ContactShowContent = () => {
                 <Card>
                     <CardContent>
                         <Box display="flex">
-                            <Avatar record={record} />
+                            <Avatar />
                             <Box ml={2} flex="1">
                                 <Typography variant="h5">
                                     {record.first_name} {record.last_name}
