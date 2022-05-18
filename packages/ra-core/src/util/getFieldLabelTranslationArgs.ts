d1a7869ef5c3e00291dd4bc4b9cd7cf2caeae52b
François Zaninotto <fzaninotto@gmail.com>
2022-05-18 23:07:44 +0200

    Fix code scanning alert

--- packages/ra-core/src/util/getFieldLabelTranslationArgs.ts
+++ packages/ra-core/src/util/getFieldLabelTranslationArgs.ts
@@ -54,12 +54,40 @@ export default (options?: Args): TranslationArguments => {
     ];
 };
 
-// source is like 'pictures.0.url'
+/**
+ * Uses the source string to guess a translation message and a default label.
+ *
+ * @example
+ * getSourceParts('pictures') // { sourceWithoutDigits: 'pictures', sourceSuffix: 'pictures' }
+ * getSourceParts('pictures.url') // { sourceWithoutDigits: 'pictures.url', sourceSuffix: 'pictures.url' }
+ * getSourceParts('pictures.0.url') // { sourceWithoutDigits: 'pictures.url', sourceSuffix: 'url' }
+ * getSourceParts('pictures.12.urls.5.protocol') // { sourceWithoutDigits: 'pictures.urls.protocol', sourceSuffix: 'protocol' }
+ */
 const getSourceParts = (source: string) => {
     // remove digits, e.g. 'book.authors.2.categories.3.identifier.name' => 'book.authors.categories.identifier.name'
     const sourceWithoutDigits = source.replace(/\.\d+\./g, '.');
     // get final part, e.g. 'book.authors.2.categories.3.identifier.name' => 'identifier.name'
-    const matches = source.match(/^.*\.\d+\.(.*)$/);
-    const sourceSuffix = matches ? matches[1] : source;
+    // we're not using a regexp here to avoid code sacnning alert "Polynomial regular expression used on uncontrolled data"
+    const parts = source.split('.');
+    let lastPartWithDigits;
+    parts.forEach((part, index) => {
+        if (onlyDigits(part)) {
+            lastPartWithDigits = index;
+        }
+    });
+    const sourceSuffix =
+        lastPartWithDigits != null
+            ? parts.slice(lastPartWithDigits + 1).join('.')
+            : source;
+
     return { sourceWithoutDigits, sourceSuffix };
 };
+
+// 48 and 57 are the char codes for "0" and "9", respectively
+function onlyDigits(s) {
+    for (let i = s.length - 1; i >= 0; i--) {
+        const d = s.charCodeAt(i);
+        if (d < 48 || d > 57) return false;
+    }
+    return true;
+}
