66786008c230fec3e29619d41166b72d93d28bad
Antoine Fricker <antoine@marmelab.com>
2022-06-17 09:32:09 +0200

    Remove unused elements and imports

--- packages/ra-ui-materialui/src/auth/Logout.spec.tsx
+++ packages/ra-ui-materialui/src/auth/Logout.spec.tsx
@@ -1,18 +1,6 @@
 import * as React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import { AdminContext } from '../AdminContext';
-import { Logout } from './Logout';
 import { UserAuthenticated, UserUnauthenticated } from './Logout.stories';
-
-export default { title: 'ra-ui-materialui/input/AutocompleteArrayInput' };
-
-const MinimalAdmin = authProvider => {
-    return (
-        <AdminContext authProvider={authProvider}>
-            <Logout />
-        </AdminContext>
-    );
-};
 
 it('should display logout button if the auth succeeds', async () => {
     render(<UserAuthenticated />);
