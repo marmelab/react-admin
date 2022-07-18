e5fdb48532bf3cefd78e35128a089855c5b779a9
Antoine Fricker <antoine@marmelab.com>
2022-07-18 17:15:17 +0200

    Add unit test

--- packages/ra-ui-materialui/src/layout/Theme/useTheme.spec.tsx
+++ packages/ra-ui-materialui/src/layout/Theme/useTheme.spec.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { CoreAdminContext, useStore } from 'ra-core';
 import expect from 'expect';
 import { render, screen } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import { useTheme } from './useTheme';
-import { CoreAdminContext } from 'ra-core';
+import { SimplePaletteColorOptions } from '@mui/material';
 
 const authProvider = {
     login: jest.fn().mockResolvedValueOnce(''),
@@ -25,5 +27,17 @@ describe('useTheme', () => {
             </CoreAdminContext>
         );
         expect(screen.getByLabelText('has-theme')).not.toBeNull();
+    });
+
+    it('should return theme from settings when available', () => {
+        const { result: storeResult } = renderHook(() => useStore('theme'));
+        storeResult.current[1]({ palette: { primary: { main: 'red' } } });
+
+        const { result: themeResult } = renderHook(() => useTheme());
+
+        expect(storeResult.current[0].palette.primary.main).toEqual(
+            (themeResult.current[0].palette
+                ?.primary as SimplePaletteColorOptions).main
+        );
     });
 });
