c734e3c72bbcc96e7581cff38c297cc750aad4fa
fzaninotto <fzaninotto@gmail.com>
2022-07-21 21:51:13 +0200

    Add second input in story to show real use case

--- packages/ra-ui-materialui/src/input/ReferenceInput.stories.tsx
+++ packages/ra-ui-materialui/src/input/ReferenceInput.stories.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Form, testDataProvider } from 'ra-core';
-import { AdminContext } from '../AdminContext';
-
-import { SelectInput } from '../input';
-import { ReferenceInput } from './ReferenceInput';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import englishMessages from 'ra-language-english';
+import { Stack } from '@mui/material';
+
+import { AdminContext } from '../AdminContext';
+import { SelectInput, TextInput } from '../input';
+import { ReferenceInput } from './ReferenceInput';
 
 export default { title: 'ra-ui-materialui/input/ReferenceInput' };
 
@@ -40,9 +41,16 @@ const i18nProvider = polyglotI18nProvider(() => englishMessages);
 export const Loading = () => (
     <AdminContext dataProvider={dataProvider} i18nProvider={i18nProvider}>
         <Form onSubmit={() => {}} defaultValues={{ tag_ids: [5] }}>
-            <ReferenceInput reference="tags" resource="posts" source="tag_ids">
-                <SelectInput optionText="name" />
-            </ReferenceInput>
+            <Stack sx={{ width: 200 }}>
+                <ReferenceInput
+                    reference="tags"
+                    resource="posts"
+                    source="tag_ids"
+                >
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+                <TextInput source="foo" />
+            </Stack>
         </Form>
     </AdminContext>
 );
