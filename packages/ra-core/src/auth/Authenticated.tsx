24d57f67cf691eb9d086045de1e3f71ff354b22c
shaoyu <shaoyu>
2022-07-21 04:54:09 +0000

    update imports

--- packages/ra-core/src/auth/Authenticated.tsx
+++ packages/ra-core/src/auth/Authenticated.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ReactNode } from 'react';
 
-import { useAuthState } from './useAuthState';
+import useAuthState from './useAuthState';
 
 /**
  * Restrict access to children to authenticated users.
@@ -9,6 +9,10 @@ import { useAuthState } from './useAuthState';
  *
  * Use it to decorate your custom page components to require
  * authentication.
+ *
+ * By default this component is optimistic: it does not block
+ * rendering children when checking authentication, but this mode
+ * can be turned off by setting `pessimistic` to true.
  *
  * You can set additional `authParams` at will if your authProvider
  * requires it.
@@ -40,13 +44,12 @@ export const Authenticated = (props: AuthenticatedProps) => {
     // this hook will log out if the authProvider doesn't validate that the user is authenticated
     const { isLoading, authenticated } = useAuthState(authParams, true);
 
-    // to prevent flash of UI, we don't render the children
-    // in pessimistic mode until the authProvider has validated the user
-    if (!authenticated || (pessimistic && isLoading)) {
+    // in pessimistic mode don't render the children until authenticated
+    if ((pessimistic && isLoading) || !authenticated) {
         return null;
     }
 
-    // render the children when authenticated or when in optimistic mode (default)
+    // render the children in optimistic rendering or after authenticated
     return <>{children}</>;
 };
 
