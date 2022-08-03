6308d06c8b076b511360649775e7aa79e986360a
fzaninotto <fzaninotto@gmail.com>
2022-08-03 11:31:59 +0200

    Fix demo removes component names

--- examples/demo/build.js
+++ examples/demo/build.js
@@ -2,6 +2,7 @@
  * Rewrite Webpack config without ejecting CRA thanks to rewire
  * @link https://stackoverflow.com/questions/55165466/how-to-build-a-production-version-of-react-without-minification
  */
+const { exit } = require('process');
 const rewire = require('rewire');
 const defaults = rewire('react-scripts/scripts/build.js');
 const config = defaults.__get__('config');
@@ -10,8 +11,8 @@ const config = defaults.__get__('config');
  * Do not mangle component names in production, for better learning experience
  * @link https://kentcdodds.com/blog/profile-a-react-app-for-performance#disable-function-name-mangling
  */
-config.optimization.minimizer[0].options.keep_classnames = true;
-config.optimization.minimizer[0].options.keep_fnames = true;
+config.optimization.minimizer[0].options.minimizer.options.keep_classnames = true;
+config.optimization.minimizer[0].options.minimizer.options.keep_fnames = true;
 
 /**
  * Do not disable component profiling in production, for better learning experience
