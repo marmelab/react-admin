eeece47b496b1ba6f3da08c38bf70d35947b0739
oguhpereira <oguhpereira@protonmail.com>
2022-08-29 16:16:58 -0300

    test: add new test for filter button

--- packages/ra-ui-materialui/src/list/filter/FilterButton.spec.tsx
+++ packages/ra-ui-materialui/src/list/filter/FilterButton.spec.tsx
@@ -124,5 +124,27 @@ describe('<FilterButton />', () => {
                 ).toBeNull();
             });
         });
+
+        it('should not display save query in filter button', () => {
+            const { queryByText } = render(
+                <AdminContext theme={theme}>
+                    <FilterButton
+                        {...defaultProps}
+                        filterValues={{ title: 'foo' }}
+                        filters={[
+                            <TextInput source="Returned" label="Returned" />,
+                        ]}
+                        disableSaveQuery
+                    />
+                </AdminContext>
+            );
+            expect(
+                screen.queryByLabelText('ra.action.add_filter')
+            ).not.toBeNull();
+
+            fireEvent.click(screen.getByLabelText('ra.action.add_filter'));
+
+            expect(queryByText('ra.saved_queries.new_label')).toBeNull();
+        });
     });
 });
