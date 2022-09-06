514d6a7e6cd4e91c95e5ab13f177a9a79cc75446
fzaninotto <fzaninotto@gmail.com>
2022-09-06 13:10:30 +0200

    more complete story

--- packages/ra-ui-materialui/src/configurable/Configurable.stories.tsx
+++ packages/ra-ui-materialui/src/configurable/Configurable.stories.tsx
@@ -5,6 +5,9 @@ import {
     useStore,
     I18nContextProvider,
 } from 'ra-core';
+import { Box, Typography } from '@mui/material';
+import TimelineIcon from '@mui/icons-material/Timeline';
+
 import { InspectorButton } from './InspectorButton';
 import { Inspector } from './Inspector';
 import { Configurable } from './Configurable';
@@ -13,40 +16,110 @@ export default {
     title: 'ra-ui-materialui/customizable/Configurable',
 };
 
-const Dummy = React.forwardRef<HTMLDivElement>((props, ref) => {
-    const [value] = useStore('dummy', '1767');
+const TextBlock = React.forwardRef<HTMLDivElement>((props, ref) => {
+    const [color] = useStore('textBlock.color', '#ffffff');
     return (
-        <div
-            style={{
-                border: 'solid 1px lightgrey',
-                borderRadius: 3,
-                padding: '0 10px',
-                margin: 10,
-                width: 200,
-            }}
+        <Box
+            border="solid 1px lightgrey"
+            borderRadius={3}
+            p={1}
+            margin={1}
+            width={200}
+            bgcolor={color}
             ref={ref}
         >
-            <h5>Captain's log</h5>
-            <p>stardate {value}</p>
-        </div>
+            <Typography variant="h6">Lorem ipsum</Typography>
+            <Typography>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui,
+                quia rem? Nulla asperiores ea beatae iure, dignissimos ut
+                perferendis nemo reiciendis reprehenderit, consequuntur debitis
+                maiores! Quaerat dolor unde dolorum qui.
+            </Typography>
+        </Box>
     );
 });
 
-const DummyInspector = () => {
-    const [value, setValue] = useStore('dummy', '1767');
-    useSetInspectorTitle('ra.inspector.dummy.title', { _: 'Dummy inspector' });
-    return <input value={value} onChange={e => setValue(e.target.value)} />;
+const TextBlockInspector = () => {
+    const [color, setColor] = useStore('textBlock.color', '#ffffff');
+    useSetInspectorTitle('ra.inspector.textBlock', { _: 'Text block' });
+    return (
+        <>
+            <label htmlFor="color">Background color</label>
+            {/* uncontrolled component */}
+            <input
+                defaultValue={color}
+                onBlur={e => setColor(e.target.value)}
+                id="color"
+            />
+        </>
+    );
 };
 
-const ConfigurableDummy = () => {
+const ConfigurableTextBlock = () => {
     const ref = React.useRef<HTMLDivElement>(null);
     return (
         <Configurable
             elementRef={ref}
-            editor={<DummyInspector />}
+            editor={<TextBlockInspector />}
             openButtonLabel="edit dummy"
         >
-            <Dummy ref={ref} />
+            <TextBlock ref={ref} />
+        </Configurable>
+    );
+};
+
+const SalesBlock = React.forwardRef<HTMLDivElement>((props, ref) => {
+    const [showDate] = useStore('salesBlock.showDate', true);
+    return (
+        <Box
+            display="flex"
+            border="solid 1px lightgrey"
+            borderRadius={3}
+            p={1}
+            margin={1}
+            width={200}
+            ref={ref}
+        >
+            <Box flex="1" mr={1}>
+                <Typography variant="h6">Sales</Typography>
+                {showDate && <Typography variant="caption">Today</Typography>}
+                <Typography variant="h4" textAlign="right" mt={2}>
+                    $4,452
+                </Typography>
+            </Box>
+            <Box bgcolor="lightgrey" display="flex" alignItems="center" p={1}>
+                <TimelineIcon />
+            </Box>
+        </Box>
+    );
+});
+
+const SalesBlockInspector = () => {
+    const [showDate, setShowDate] = useStore('salesBlock.showDate', true);
+    useSetInspectorTitle('ra.inspector.salesBlock', { _: 'Sales block' });
+    return (
+        <>
+            <label htmlFor="showDate">Show date</label>
+
+            <input
+                type="checkbox"
+                checked={showDate}
+                onClick={e => setShowDate(showDatz => !showDate)}
+                id="showDate"
+            />
+        </>
+    );
+};
+
+const ConfigurableSalesBlock = () => {
+    const ref = React.useRef<HTMLDivElement>(null);
+    return (
+        <Configurable
+            elementRef={ref}
+            editor={<SalesBlockInspector />}
+            openButtonLabel="edit dummy"
+        >
+            <SalesBlock ref={ref} />
         </Configurable>
     );
 };
@@ -56,7 +129,10 @@ export const Basic = () => (
         <Inspector />
         <InspectorButton />
         <hr />
-        <ConfigurableDummy />
+        <Box display="flex" alignItems="flex-start">
+            <ConfigurableTextBlock />
+            <ConfigurableSalesBlock />
+        </Box>
     </PreferencesEditorContextProvider>
 );
 
