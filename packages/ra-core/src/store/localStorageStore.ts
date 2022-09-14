d595ab5e4c4622988a570e2b8915c378499505ea
fzaninotto <fzaninotto@gmail.com>
2022-09-14 12:18:01 +0200

    Fix localStorageStore.reset() does not remove all items

--- packages/ra-core/src/store/localStorageStore.ts
+++ packages/ra-core/src/store/localStorageStore.ts
@@ -116,15 +116,11 @@ export const localStorageStore = (
         },
         reset(): void {
             const storage = getStorage();
-            for (let i = 0; i < storage.length; i++) {
-                if (storage.key(i)?.substring(0, prefixLength) === prefix) {
-                    const key = storage.key(i)?.substring(prefixLength + 1);
-                    if (!key || !storage.key(i)) return;
-                    // @ts-ignore
-                    storage.removeItem(storage.key(i));
-                    publish(key, undefined);
-                }
-            }
+            Object.keys(storage).forEach(key => {
+                storage.removeItem(key);
+                const publishKey = key.substring(prefixLength + 1);
+                publish(publishKey, undefined);
+            });
         },
         subscribe: (key: string, callback: (value: string) => void) => {
             const id = Math.random().toString();
