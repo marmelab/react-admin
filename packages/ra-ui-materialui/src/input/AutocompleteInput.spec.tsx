a5ca08a43110c526da98769cbb9e4fa757a1d847
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2022-09-20 11:05:06 -0300

    Add test

--- packages/ra-ui-materialui/src/input/AutocompleteInput.spec.tsx
+++ packages/ra-ui-materialui/src/input/AutocompleteInput.spec.tsx
@@ -57,6 +57,33 @@ describe('<AutocompleteInput />', () => {
         expect(screen.queryByDisplayValue('foo')).not.toBeNull();
     });
 
+    describe('emptyText', () => {
+        it('should allow to override the empty menu option text by passing a string', () => {
+            const emptyText = 'Default';
+
+            render(
+                <AdminContext dataProvider={testDataProvider()}>
+                    <SimpleForm onSubmit={jest.fn()}>
+                        <AutocompleteInput
+                            emptyText={emptyText}
+                            {...defaultProps}
+                            choices={[{ id: 2, name: 'foo' }]}
+                        />
+                    </SimpleForm>
+                </AdminContext>
+            );
+            fireEvent.mouseDown(
+                screen.getByLabelText('resources.users.fields.role')
+            );
+
+            expect(screen.queryAllByRole('option').length).toEqual(1);
+
+            const input = screen.getByRole('textbox') as HTMLInputElement;
+
+            expect(input.value).toEqual('Default');
+        });
+    });
+
     describe('optionValue', () => {
         it('should use optionValue as value identifier', async () => {
             render(
