73154888060168aef9126652bf6c8dd3b92134e1
fzaninotto <fzaninotto@gmail.com>
2022-09-28 14:10:32 +0200

    export substituteTokens

--- packages/ra-core/src/i18n/I18nContext.ts
+++ packages/ra-core/src/i18n/I18nContext.ts
@@ -1,30 +1,8 @@
 import { createContext } from 'react';
 import { I18nProvider } from '../types';
+import { substituteTokens } from './substituteTokens';
 
 export type I18nContextProps = I18nProvider;
-
-// tokens are like 'Hello, %{name}'
-const defaultTokenRegex = /%\{(.*?)\}/g;
-
-/**
- * Replace tokens by their value in the given string
- *
- * @param {string} template The template with interpolation tokens, e.g. 'Hello, %{name}'
- * @param {object} data The data to interpolate, e.g. { name: 'John' }
- * @returns {string} The interpolated string, e.g. 'Hello, John'
- */
-const substituteTokens = (template, data) =>
-    template && data
-        ? String.prototype.replace.call(template, defaultTokenRegex, function (
-              expression,
-              argument
-          ) {
-              if (!data.hasOwnProperty(argument) || data[argument] == null) {
-                  return expression;
-              }
-              return data[argument];
-          })
-        : template;
 
 const defaultI18nProvider = {
     translate: (key, options) =>
