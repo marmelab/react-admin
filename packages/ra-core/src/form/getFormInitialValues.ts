88783d03a04cfb2160a798e31d95ae3e8013cee8
fzaninotto <fzaninotto@gmail.com>
2022-09-30 11:55:10 +0200

    Fix typo

--- packages/ra-core/src/form/getFormInitialValues.ts
+++ packages/ra-core/src/form/getFormInitialValues.ts
@@ -10,7 +10,7 @@ export default function getFormInitialValues(
         getValues(defaultValues, record),
         record
     );
-    // replace null values by empty string to avoid controllezd/ uncontrolled input warning
+    // replace null values by empty string to avoid controlled/ uncontrolled input warning
     Object.keys(finalInitialValues).forEach(key => {
         if (finalInitialValues[key] === null) {
             finalInitialValues[key] = '';
