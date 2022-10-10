e2520f0f3b717fa8e4297b49cb5908d0b56950bb
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2022-10-10 09:58:33 -0300

    Fix test

--- packages/ra-ui-materialui/src/input/AutocompleteInput.spec.tsx
+++ packages/ra-ui-materialui/src/input/AutocompleteInput.spec.tsx
@@ -58,34 +58,40 @@ describe('<AutocompleteInput />', () => {
         expect(screen.queryByDisplayValue('foo')).not.toBeNull();
     });
 
-    it('should not match selection when selected choice id equals the emptyValue while changing the input', async () => {
+    it('should allow filter to match the selected choice while removing characters in the input', async () => {
         render(
             <AdminContext dataProvider={testDataProvider()}>
                 <SimpleForm>
                     <AutocompleteInput
                         {...defaultProps}
                         choices={[
-                            { id: 2, foobar: 'foo' },
-                            { id: 3, foobar: 'bar' },
+                            { id: 1, name: 'foo' },
+                            { id: 2, name: 'bar' },
                         ]}
                     />
                 </SimpleForm>
             </AdminContext>
         );
+
         const input = screen.getByLabelText(
             'resources.users.fields.role'
         ) as HTMLInputElement;
 
-        fireEvent.focus(input);
-
-        userEvent.type(input, 'f');
+        fireEvent.mouseDown(input);
         await waitFor(() => {
-            expect(input.value).toEqual('f');
+            expect(screen.getByText('foo')).not.toBe(null);
         });
-
+        fireEvent.click(screen.getByText('foo'));
+        await waitFor(() => {
+            expect(input.value).toEqual('foo');
+        });
+        fireEvent.focus(input);
+        userEvent.type(input, '{end}');
+        userEvent.type(input, '2');
+        expect(input.value).toEqual('foo2');
         userEvent.type(input, '{backspace}');
         await waitFor(() => {
-            expect(input.value).toEqual('');
+            expect(input.value).toEqual('foo');
         });
     });
 
