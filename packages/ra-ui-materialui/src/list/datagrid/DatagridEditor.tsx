9608a4fac861dd358bc582370d49847ce205cbf6
fzaninotto <fzaninotto@gmail.com>
2022-10-18 09:56:02 +0200

    Add hide all / show all buttons

--- packages/ra-ui-materialui/src/list/datagrid/DatagridEditor.tsx
+++ packages/ra-ui-materialui/src/list/datagrid/DatagridEditor.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { usePreference, useSetInspectorTitle } from 'ra-core';
+import { usePreference, useSetInspectorTitle, useTranslate } from 'ra-core';
+import { Box, Button } from '@mui/material';
 
 import { FieldEditor } from './FieldEditor';
 
@@ -7,6 +8,7 @@ export const DatagridEditor = (props: {
     children: React.ReactNode;
     omit?: string[];
 }) => {
+    const translate = useTranslate();
     useSetInspectorTitle('ra.inspector.datagrid', { _: 'Datagrid' });
     const [columns, setColumns] = usePreference(
         'colums',
@@ -16,12 +18,22 @@ export const DatagridEditor = (props: {
             .filter(name => name != null)
             .filter(name => !props.omit?.includes(name))
     );
-    const onToggle = event => {
+    const handleToggle = event => {
         if (event.target.checked) {
             setColumns([...columns, event.target.name]);
         } else {
             setColumns(columns.filter(name => name !== event.target.name));
         }
+    };
+    const handleHideAll = () => {
+        setColumns([]);
+    };
+    const handleShowAll = () => {
+        setColumns(
+            React.Children.map(props.children, child =>
+                React.isValidElement(child) ? child.props.source : null
+            ).filter(name => name != null)
+        );
     };
     return (
         <div>
@@ -31,10 +43,22 @@ export const DatagridEditor = (props: {
                         source={child.props.source}
                         label={child.props.label}
                         selected={columns.includes(child.props.source)}
-                        onToggle={onToggle}
+                        onToggle={handleToggle}
                     />
                 ) : null
             )}
+            <Box display="flex" justifyContent="space-between" mx={-0.5} mt={1}>
+                <Button size="small" onClick={handleHideAll}>
+                    {translate('ra.inspector.datagrid.hideAll', {
+                        _: 'Hide All',
+                    })}
+                </Button>
+                <Button size="small" onClick={handleShowAll}>
+                    {translate('ra.inspector.datagrid.showAll', {
+                        _: 'Show All',
+                    })}
+                </Button>
+            </Box>
         </div>
     );
 };
