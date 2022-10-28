7cccc4abfc9a837cb59eef5825a06e03f24280ea
Aníbal Svarcas <WiXSL@users.noreply.github.com>
2022-10-28 08:10:01 -0300

    Revert changes in PaginationLimit

--- packages/ra-ui-materialui/src/list/pagination/PaginationLimit.tsx
+++ packages/ra-ui-materialui/src/list/pagination/PaginationLimit.tsx
@@ -4,18 +4,13 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { useTranslate } from 'ra-core';
 
-export const PaginationLimit = memo(
-    ({ message = 'ra.navigation.no_results' }: PaginationLimitProps) => {
-        const translate = useTranslate();
-
-        return (
-            <CardContent>
-                <Typography variant="body2">{translate(message)}</Typography>
-            </CardContent>
-        );
-    }
-);
-
-export interface PaginationLimitProps {
-    message?: string;
-}
+export const PaginationLimit = memo(() => {
+    const translate = useTranslate();
+    return (
+        <CardContent>
+            <Typography variant="body2">
+                {translate('ra.navigation.no_results')}
+            </Typography>
+        </CardContent>
+    );
+});
