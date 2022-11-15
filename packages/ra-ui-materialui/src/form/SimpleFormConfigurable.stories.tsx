d399127c067e675ff4697d3e450d4c1f358d2c66
fzaninotto <fzaninotto@gmail.com>
2022-11-15 18:15:52 +0100

    Add i18n story

--- packages/ra-ui-materialui/src/form/SimpleFormConfigurable.stories.tsx
+++ packages/ra-ui-materialui/src/form/SimpleFormConfigurable.stories.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { PreferencesEditorContextProvider } from 'ra-core';
-import { ThemeProvider, createTheme, Box } from '@mui/material';
+import { PreferencesEditorContextProvider, I18nContextProvider } from 'ra-core';
+import { ThemeProvider, createTheme, Box, Paper } from '@mui/material';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import en from 'ra-language-english';
 
 import { Inspector, InspectorButton } from '../preferences';
 import { NumberInput, TextInput } from '../input';
 import { SimpleFormConfigurable } from './SimpleFormConfigurable';
+import { defaultTheme } from '../defaultTheme';
 
 export default { title: 'ra-ui-materialui/forms/SimpleFormConfigurable' };
 
@@ -19,14 +22,14 @@ const data = {
 
 const Wrapper = ({ children }) => (
     <QueryClientProvider client={new QueryClient()}>
-        <ThemeProvider theme={createTheme()}>
+        <ThemeProvider theme={createTheme(defaultTheme)}>
             <PreferencesEditorContextProvider>
                 <MemoryRouter>
                     <Inspector />
                     <Box display="flex" justifyContent="flex-end">
                         <InspectorButton />
                     </Box>
-                    <Box p={2}>{children}</Box>
+                    <Paper sx={{ width: 600, m: 2 }}>{children}</Paper>
                 </MemoryRouter>
             </PreferencesEditorContextProvider>
         </ThemeProvider>
@@ -78,4 +81,19 @@ export const PreferenceKey = () => (
             <NumberInput source="year" />
         </SimpleFormConfigurable>
     </Wrapper>
+);
+
+const translations = { en };
+const i18nProvider = polyglotI18nProvider(locale => translations[locale], 'en');
+
+export const I18N = () => (
+    <I18nContextProvider value={i18nProvider}>
+        <Wrapper>
+            <SimpleFormConfigurable record={data} resource="books">
+                <TextInput source="title" fullWidth />
+                <TextInput source="author" />
+                <NumberInput source="year" />
+            </SimpleFormConfigurable>
+        </Wrapper>
+    </I18nContextProvider>
 );
