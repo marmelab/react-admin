4b063480b01b8221c2fb8f88a27b82f02c1d7671
Antoine Fricker <antoine@marmelab.com>
2022-12-14 17:40:37 +0100

    Fix JS error / Scroll to section in navigation when coming from documentation summary

---
+++ docs/js/ra-doc-exec.js
@@ -0,0 +1,150 @@
+var allMenus, navLinks, versionsLinks;
+
+// eslint-disable-next-line no-unused-vars
+function slugify(text) {
+    return text
+        .toString()
+        .toLowerCase()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(/[^\w-]+/g, '') // Remove all non-word chars
+        .replace(/--+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, ''); // Trim - from end of text
+}
+
+function navigationFitScroll() {
+    var scrollIntoView = window.sessionStorage.getItem('scrollIntoView');
+    if (true || scrollIntoView !== 'false') {
+        var activeMenu = document.querySelector('.sidenav li.active');
+        if (activeMenu) activeMenu.parentNode.scrollIntoView();
+    }
+    window.sessionStorage.removeItem('scrollIntoView');
+}
+
+function buildPageToC() {
+    var M = window.M;
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    // Initialize version selector
+    M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'));
+
+    var Prism = window.Prism;
+    Prism.highlightAll();
+
+    /* Generate table of contents */
+    if (document.querySelector('.toc') != null) {
+        var tocbot = window.tocbot;
+        tocbot.init({
+            // Where to render the table of contents
+            tocSelector: '.toc',
+            positionFixedSelector: '.toc',
+            // Where to grab the headings to build the table of contents
+            contentSelector: '.toc-content',
+            // More options
+            headingSelector: 'h2, h3, h4',
+            includeHtml: true,
+            collapseDepth: 2,
+            hasInnerContainers: true,
+        });
+    }
+}
+
+function replaceContent(text) {
+    var tocContainer = document.querySelector('.toc-container');
+    tocContainer.className =
+        text.trim() !== ''
+            ? 'toc-container col hide-on-small-only m3'
+            : 'toc-container';
+
+    var tmpElement = document.createElement('div');
+    tmpElement.innerHTML = text;
+
+    var content = document.querySelector('.DocSearch-content');
+    document.querySelector('.docBlocks').style.display = 'none';
+    content.innerHTML = tmpElement.querySelector(
+        '.DocSearch-content'
+    ).innerHTML;
+
+    window.scrollTo(0, 0);
+
+    buildPageToC();
+
+    navigationFitScroll();
+}
+
+function changeSelectedMenu() {
+    console.log('changeSelectedMenu');
+    var activeMenu = document.querySelector(`.sidenav li.active`);
+    activeMenu && activeMenu.classList.remove('active');
+    allMenus
+        .find(menuEl => menuEl.href === window.location.href)
+        .parentNode.classList.add('active');
+}
+
+// Replace full page reloads by a fill of the content area
+// so that the side navigation keeps its state
+// use a global event listener to also catch links inside the content area
+document.addEventListener('click', event => {
+    console.log('catchedNavigation click');
+    var link = event.target.closest('a');
+    if (!link) {
+        return; // click not on a link
+    }
+    var location = document.location.href.split('#')[0];
+    var href = link.href;
+    if (href.indexOf(`${location}#`) === 0) {
+        return; // click on an anchor in the current page
+    }
+    if (!navLinks.includes(href)) {
+        return; // not a navigation link
+    }
+    window.sessionStorage.setItem(
+        'scrollIntoView',
+        link.closest('.sidenav') ? 'false' : 'true'
+    );
+    // now we're sure it's an internal navigation link
+    // transform it to an AJAX call
+    event.preventDefault();
+    // update versions links
+    var currentPage = href.split('/').pop();
+    versionsLinks.forEach(link => {
+        link.href =
+            link.href.substr(0, link.href.lastIndexOf('/') + 1) + currentPage;
+    });
+    // fetch the new content
+    fetch(href)
+        .then(res => res.text())
+        .then(replaceContent);
+    // change the URL
+    window.history.pushState(null, null, href);
+    changeSelectedMenu();
+});
+
+// make back button work again
+window.addEventListener('popstate', event => {
+    if (document.location.href.indexOf('#') !== -1) {
+        // popstate triggered by a click on an anchor, not back button
+        return;
+    }
+    if (window.location.pathname === '/documentation.html') {
+        document.querySelector('.DocSearch-content').innerHTML = '';
+        document.querySelector('.docBlocks').style.display = 'grid';
+    } else {
+        // fetch the new content
+        fetch(window.location.pathname)
+            .then(res => res.text())
+            .then(replaceContent);
+    }
+    changeSelectedMenu();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    allMenus = Array.from(document.querySelectorAll(`.sidenav a.nav-link`));
+    navLinks = allMenus
+        .filter(link => !link.classList.contains('external'))
+        .map(link => link.href);
+    versionsLinks = Array.from(document.querySelectorAll('#versions > li > a'));
+
+    buildPageToC();
+
+    navigationFitScroll();
+});
