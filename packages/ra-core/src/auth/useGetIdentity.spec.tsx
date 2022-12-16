fdd6c1d837305914c12db6a24bcbc644d3fd20a4
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-12-16 10:21:23 +0100

    Fix useGetIdentity throws an error when no getIdentity is provided

--- packages/ra-core/src/auth/useGetIdentity.spec.tsx
+++ packages/ra-core/src/auth/useGetIdentity.spec.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 import { Basic, ErrorCase, ResetIdentity } from './useGetIdentity.stories';
+import useGetIdentity from './useGetIdentity';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import AuthContext from './AuthContext';
 
 describe('useGetIdentity', () => {
     it('should return the identity', async () => {
@@ -21,5 +24,36 @@ describe('useGetIdentity', () => {
         fireEvent.click(screen.getByText('Save'));
         await screen.findByText('Jane Doe');
         expect(screen.queryByText('John Doe')).toBeNull();
+    });
+    it('should not throw errors when there is no authProvider.getIdentity', async () => {
+        const authProvider = {
+            login: () => Promise.resolve(),
+            logout: () => Promise.resolve(),
+            checkAuth: () => Promise.resolve(),
+            checkError: () => Promise.resolve(),
+            getPermissions: () => Promise.resolve(),
+        };
+        const Identity = () => {
+            const { data, error, isLoading } = useGetIdentity({ retry: false });
+            return isLoading ? (
+                <>Loading</>
+            ) : error ? (
+                <>{`Error: ${error.message}`}</>
+            ) : (
+                <>{String(data)}</>
+            );
+        };
+        render(
+            <QueryClientProvider client={new QueryClient()}>
+                <AuthContext.Provider value={authProvider}>
+                    <Identity />
+                </AuthContext.Provider>
+            </QueryClientProvider>
+        );
+        await waitFor(() => {
+            expect(screen.queryByText('Loading')).toBeNull();
+        });
+        expect(screen.queryByText(/Error/)).toBeNull();
+        expect(screen.queryByText('undefined')).not.toBeNull();
     });
 });
