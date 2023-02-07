e2aea6c744f8bf6f696d40aae3e495944f91bf2a
Robert Clochard <986531+pamuche@users.noreply.github.com>
2023-02-07 11:26:11 +0100

    remove spaces (prettier ;))

--- packages/ra-ui-materialui/src/input/NumberInput.tsx
+++ packages/ra-ui-materialui/src/input/NumberInput.tsx
@@ -192,11 +192,11 @@ const convertStringToNumber = value => {
     if (value == null || value === '') {
         return null;
     }
-    
+
     if (value === '-0') {
         return value;
     }
-    
+
     const float = parseFloat(value);
 
     return isNaN(float) ? 0 : float;
