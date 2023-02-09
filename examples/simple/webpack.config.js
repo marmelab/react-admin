ba2cf6c552c088117fd1361d4c39082da01efbfd
fzaninotto <fzaninotto@gmail.com>
2023-02-09 09:50:59 +0100

    Make codesandbox happy

--- examples/simple/webpack.config.js
+++ examples/simple/webpack.config.js
@@ -40,8 +40,7 @@ module.exports = {
         }),
     ],
     devServer: {
-        host: '127.0.0.1',
-        port: 8080,
+        allowedHosts: 'all',
     },
     stats: {
         children: true,
