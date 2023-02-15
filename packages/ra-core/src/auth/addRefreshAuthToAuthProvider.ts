7c6d62c6378afc83f9f3a20606f5a89044b07cb3
Jean-Baptiste Kaiser <jb@marmelab.com>
2023-02-15 12:26:31 +0100

    code review changes

--- packages/ra-core/src/auth/addRefreshAuthToAuthProvider.ts
+++ packages/ra-core/src/auth/addRefreshAuthToAuthProvider.ts
@@ -1,39 +1,34 @@
-import { AuthProvider, DataProvider } from '../types';
+import { AuthProvider } from '../types';
 
 /**
- * A higher-order function which wraps a dataProvider or authProvider to handle refreshing authentication.
+ * A higher-order function which wraps an authProvider to handle refreshing authentication.
  * This is useful when the authentication service supports a refresh token mechanism.
- * The wrapped provider will call the refreshAuth function before calling any dataProvider methods and before
+ * The wrapped provider will call the refreshAuth function before
  * calling the authProvider checkAuth, getIdentity and getPermissions methods.
  *
  * The refreshAuth function should return a Promise that resolves when the authentication token has been refreshed.
  * It might throw an error if the refresh failed. In this case, react-admin will handle the error as usual.
  *
- * @param provider Either a dataProvider or an authProvider
+ * @param provider An authProvider
  * @param refreshAuth A function that refreshes the authentication token if needed and returns a Promise.
- * @returns A wrapped dataProvider or authProvider.
+ * @returns A wrapped authProvider.
  *
  * @example
- * import { withRefreshAuth } from 'react-admin';
- * import { jsonServerProvider } from 'ra-data-json-server';
+ * import { addRefreshAuthToAuthProvider } from 'react-admin';
  * import { authProvider } from './authProvider';
  * import { refreshAuth } from './refreshAuth';
  *
- * const dataProvider = withRefreshAuth(jsonServerProvider('http://localhost:3000'), refreshAuth);
- * const authProvider = withRefreshAuth(authProvider, refreshAuth);
+ * const authProvider = addRefreshAuthToAuthProvider(authProvider, refreshAuth);
  */
-export const withRefreshAuth = <
-    ProviderType extends AuthProvider | DataProvider
->(
-    provider: ProviderType,
+export const addRefreshAuthToAuthProvider = (
+    provider: AuthProvider,
     refreshAuth: () => Promise<void>
-): ProviderType => {
+): AuthProvider => {
     const proxy = new Proxy(provider, {
         get(_, name) {
-            const isDataProvider = provider.hasOwnProperty('getList');
-            const shouldIntercept =
-                isDataProvider ||
-                AuthProviderInterceptedMethods.includes(name.toString());
+            const shouldIntercept = AuthProviderInterceptedMethods.includes(
+                name.toString()
+            );
 
             if (shouldIntercept) {
                 return async (...args: any[]) => {
