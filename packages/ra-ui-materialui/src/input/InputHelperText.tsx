480011256c8c8d9be0239b6a50227d640c1cfecb
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-03-03 16:05:23 +0100

    Avoid double validation message translations

--- packages/ra-ui-materialui/src/input/InputHelperText.tsx
+++ packages/ra-ui-materialui/src/input/InputHelperText.tsx
@@ -1,26 +1,41 @@
 import * as React from 'react';
 import { isValidElement, ReactElement } from 'react';
-import { useTranslate, ValidationError, ValidationErrorMessage } from 'ra-core';
+import {
+    useTranslate,
+    ValidationError,
+    ValidationErrorMessage,
+    ValidationErrorMessageWithArgs,
+} from 'ra-core';
 
 export const InputHelperText = (props: InputHelperTextProps) => {
     const { helperText, touched, error } = props;
     const translate = useTranslate();
 
-    return touched && error ? (
-        <ValidationError error={error} />
-    ) : isValidElement(helperText) ? (
-        helperText
-    ) : typeof helperText === 'string' ? (
-        <>{translate(helperText, { _: helperText })}</>
-    ) : helperText !== false ? (
-        // MUI's HelperText cannot reserve space unless we pass a single
-        // space as child, which isn't possible when the child is a component.
-        // Therefore, we must reserve the space ourselves by passing the same
-        // markup as MUI.
-        // @see https://github.com/mui-org/material-ui/blob/62e439b7022d519ab638d65201e204b59b77f8da/packages/material-ui/src/FormHelperText/FormHelperText.js#L85-L90
-        // eslint-disable-next-line react/no-danger
-        <span dangerouslySetInnerHTML={defaultInnerHTML} />
-    ) : null;
+    if (touched && error) {
+        if ((error as ValidationErrorMessageWithArgs).message) {
+            return <ValidationError error={error} />;
+        }
+        return error;
+    }
+
+    if (helperText === false) {
+        return null;
+    }
+
+    if (isValidElement(helperText)) {
+        return helperText;
+    }
+
+    if (typeof helperText === 'string') {
+        return <>{translate(helperText, { _: helperText })}</>;
+    }
+
+    // MUI's HelperText cannot reserve space unless we pass a single
+    // space as child, which isn't possible when the child is a component.
+    // Therefore, we must reserve the space ourselves by passing the same
+    // markup as MUI.
+    // @see https://github.com/mui-org/material-ui/blob/62e439b7022d519ab638d65201e204b59b77f8da/packages/material-ui/src/FormHelperText/FormHelperText.js#L85-L90
+    return <span dangerouslySetInnerHTML={defaultInnerHTML} />;
 };
 
 const defaultInnerHTML = { __html: '&#8203;' };
