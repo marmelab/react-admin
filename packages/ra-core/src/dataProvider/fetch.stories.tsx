182b1a72ebc682a3cc40fd6ad033337d0e7ef83d
Jean-Baptiste Kaiser <jb@marmelab.com>
2023-03-07 11:23:00 +0100

    improve story

--- packages/ra-core/src/dataProvider/fetch.stories.tsx
+++ packages/ra-core/src/dataProvider/fetch.stories.tsx
@@ -5,21 +5,27 @@ export default {
     title: 'ra-core/dataProvider/fetch',
 };
 
-export const WithAuthenticatedUser = () => {
+export const FetchJson = () => {
     const [token, setToken] = React.useState('secret');
     const [record, setRecord] = React.useState('');
     const user = { token: `Bearer ${token}`, authenticated: !!token };
     const doGet = () => {
         fetchJson('https://jsonplaceholder.typicode.com/posts/1', {
             user,
-        }).then(({ body }) => setRecord(body));
+        }).then(({ status, headers, body, json }) => {
+            console.log('GET result', { status, headers, body, json });
+            setRecord(body);
+        });
     };
     const doPut = () => {
         fetchJson('https://jsonplaceholder.typicode.com/posts/1', {
             method: 'PUT',
             body: record,
             user,
-        }).then(({ body }) => setRecord(body));
+        }).then(({ status, headers, body, json }) => {
+            console.log('PUT result', { status, headers, body, json });
+            setRecord(body);
+        });
     };
     return (
         <div
@@ -28,26 +34,38 @@ export const WithAuthenticatedUser = () => {
                 flexDirection: 'column',
                 width: 500,
                 padding: 20,
+                gap: 10,
             }}
         >
+            <p style={{ backgroundColor: '#ffb', textAlign: 'center' }}>
+                <b>Tip:</b> Open the DevTools network tab to see the HTTP
+                Headers
+                <br />
+                <b>Tip:</b> Open the DevTools console tab to see the returned
+                values
+            </p>
             <div style={{ display: 'flex' }}>
-                <span style={{ marginRight: 10 }}>Token:</span>
+                <label htmlFor="token" style={{ marginRight: 10 }}>
+                    Token:
+                </label>
                 <input
+                    id="token"
                     type="text"
                     value={token}
                     onChange={e => setToken(e.target.value)}
                     style={{ flexGrow: 1 }}
+                    title="Clear this field to simulate an unauthenticated user"
                 />
             </div>
-            <button onClick={doGet}>GET</button>
-            <p>Open the DevTools console to see the computed HTTP Headers</p>
+            <button onClick={doGet}>Send GET request</button>
             <textarea
                 value={record}
                 rows={10}
                 onChange={e => setRecord(e.target.value)}
+                placeholder="body"
             />
             <button onClick={doPut} disabled={!record}>
-                PUT
+                Send PUT request
             </button>
         </div>
     );
