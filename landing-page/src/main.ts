75ef8e50c17ea4160cf88a517fa2800796eb4ac8
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-03-23 10:01:26 +0100

    Remove debug

--- landing-page/src/main.ts
+++ landing-page/src/main.ts
@@ -28,7 +28,6 @@ window.addEventListener(
   () => {
     const scroll =
       window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-      console.log({ scroll })
     document.body.style.setProperty("--scroll", `${scroll}`);
   },
   false
