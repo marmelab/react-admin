10dc27abaed8615e9d43e4d69b35083174179fe4
Jean-Baptiste Kaiser <jb@marmelab.com>
2023-04-20 15:12:33 +0200

    Fix AppBar Title disappears on locale change

--- packages/ra-ui-materialui/src/button/LocalesMenuButton.spec.tsx
+++ packages/ra-ui-materialui/src/button/LocalesMenuButton.spec.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { Basic } from './LocalesMenuButton.stories';
+import {
+    fireEvent,
+    render,
+    screen,
+    waitFor,
+    within,
+} from '@testing-library/react';
+import { Basic, FullApp } from './LocalesMenuButton.stories';
 
 describe('LocalesMenuButton', () => {
     it('should allow to change language', async () => {
@@ -16,5 +22,24 @@ describe('LocalesMenuButton', () => {
         await waitFor(() => {
             expect(screen.queryByText('Tableau de bord')).not.toBeNull();
         });
+    });
+
+    it('should not make the title disappear', async () => {
+        const { container } = render(<FullApp />);
+
+        await screen.findByText('War and Peace');
+        let title = container.querySelector(
+            '#react-admin-title'
+        ) as HTMLElement;
+        expect(within(title).queryByText('Books')).not.toBeNull();
+
+        fireEvent.click(screen.getAllByText('English')[0]);
+        expect(screen.queryAllByText('English').length).toEqual(2);
+        expect(screen.queryByText('Français')).not.toBeNull();
+        fireEvent.click(screen.getByText('Français'));
+
+        await screen.findAllByText('Livres');
+        title = container.querySelector('#react-admin-title') as HTMLElement;
+        expect(within(title).queryByText('Livres')).not.toBeNull();
     });
 });
