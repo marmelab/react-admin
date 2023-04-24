c3c1ccbcb6b3aef4c1a3073d6b3d9a1bea6470f3
motion <shaoyu>
2023-04-24 11:49:46 +0800

    make sure the stories no longer have typescript issue

--- packages/ra-ui-materialui/src/layout/AppBar.stories.tsx
+++ packages/ra-ui-materialui/src/layout/AppBar.stories.tsx
@@ -8,6 +8,7 @@ import {
     ListItemText,
     TextField,
     Skeleton,
+    MenuItemProps,
 } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { QueryClientProvider, QueryClient } from 'react-query';
@@ -198,23 +199,25 @@ export const UserMenuCustom = () => (
 );
 
 // It's important to pass the ref to allow MUI to manage the keyboard navigation
-const SettingsMenuItem = React.forwardRef((props, ref) => {
-    // We are not using MenuItemLink so we retrieve the onClose function from the UserContext
-    const { onClose } = useUserMenu();
-    return (
-        <MenuItem
-            onClick={onClose}
-            ref={ref}
-            // It's important to pass the props to allow MUI to manage the keyboard navigation
-            {...props}
-        >
-            <ListItemIcon>
-                <SettingsIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText>Customize</ListItemText>
-        </MenuItem>
-    );
-});
+const SettingsMenuItem: React.FC<MenuItemProps> = React.forwardRef(
+    (props, ref) => {
+        // We are not using MenuItemLink so we retrieve the onClose function from the UserContext
+        const { onClose } = useUserMenu();
+        return (
+            <MenuItem
+                onClick={onClose}
+                ref={ref}
+                // It's important to pass the props to allow MUI to manage the keyboard navigation
+                {...props}
+            >
+                <ListItemIcon>
+                    <SettingsIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Customize</ListItemText>
+            </MenuItem>
+        );
+    }
+);
 
 export const UserMenuElements = () => (
     <Wrapper>
