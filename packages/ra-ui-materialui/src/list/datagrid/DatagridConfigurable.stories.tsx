b424cfae9d7815c585c66f3fa0c3841129ca4b6c
fzaninotto <fzaninotto@gmail.com>
2023-05-19 17:07:30 +0200

    Add test

--- packages/ra-ui-materialui/src/list/datagrid/DatagridConfigurable.stories.tsx
+++ packages/ra-ui-materialui/src/list/datagrid/DatagridConfigurable.stories.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { PreferencesEditorContextProvider } from 'ra-core';
 import { Box } from '@mui/material';
+import { memoryStore, StoreContextProvider } from 'ra-core';
 
 import { DatagridConfigurable } from './DatagridConfigurable';
 import { Inspector, InspectorButton } from '../../preferences';
@@ -43,118 +44,100 @@ AuthorField.defaultProps = { label: 'Author' };
 
 const theme = createTheme();
 
+const Wrapper = ({ children }) => (
+    <StoreContextProvider value={memoryStore()}>
+        <ThemeProvider theme={theme}>
+            <PreferencesEditorContextProvider>
+                <MemoryRouter>
+                    <Inspector />
+                    <Box display="flex" justifyContent="flex-end">
+                        <InspectorButton />
+                    </Box>
+                    <Box p={2}>{children}</Box>
+                </MemoryRouter>
+            </PreferencesEditorContextProvider>
+        </ThemeProvider>
+    </StoreContextProvider>
+);
+
 export const Basic = () => (
-    <ThemeProvider theme={theme}>
-        <PreferencesEditorContextProvider>
-            <MemoryRouter>
-                <Inspector />
-                <Box display="flex" justifyContent="flex-end">
-                    <InspectorButton />
-                </Box>
-                <Box p={2}>
-                    <DatagridConfigurable
-                        resource="books1"
-                        data={data}
-                        sort={{ field: 'title', order: 'ASC' }}
-                        bulkActionButtons={false}
-                    >
-                        <TextField source="id" />
-                        <TextField source="title" label="Original title" />
-                        <TextField source="author" />
-                        <TextField source="year" />
-                        <EditButton />
-                    </DatagridConfigurable>
-                </Box>
-            </MemoryRouter>
-        </PreferencesEditorContextProvider>
-    </ThemeProvider>
+    <Wrapper>
+        <DatagridConfigurable
+            resource="books1"
+            data={data}
+            sort={{ field: 'title', order: 'ASC' }}
+            bulkActionButtons={false}
+        >
+            <TextField source="id" />
+            <TextField source="title" label="Original title" />
+            <TextField source="author" />
+            <TextField source="year" />
+            <EditButton />
+        </DatagridConfigurable>
+    </Wrapper>
 );
 
 export const Omit = () => (
-    <PreferencesEditorContextProvider>
-        <MemoryRouter>
-            <Inspector />
-            <Box display="flex" justifyContent="flex-end">
-                <InspectorButton />
-            </Box>
-            <Box p={2}>
-                <DatagridConfigurable
-                    resource="books2"
-                    data={data}
-                    sort={{ field: 'title', order: 'ASC' }}
-                    bulkActionButtons={false}
-                    omit={['title']}
-                >
-                    <TextField source="id" />
-                    <TextField source="title" label="Original title" />
-                    <AuthorField />
-                    <TextField source="year" />
-                </DatagridConfigurable>
-            </Box>
-        </MemoryRouter>
-    </PreferencesEditorContextProvider>
+    <Wrapper>
+        <DatagridConfigurable
+            resource="books2"
+            data={data}
+            sort={{ field: 'title', order: 'ASC' }}
+            bulkActionButtons={false}
+            omit={['title']}
+        >
+            <TextField source="id" />
+            <TextField source="title" label="Original title" />
+            <AuthorField />
+            <TextField source="year" />
+        </DatagridConfigurable>
+    </Wrapper>
 );
 
 export const PreferenceKey = () => (
-    <PreferencesEditorContextProvider>
-        <MemoryRouter>
-            <Inspector />
-            <Box display="flex" justifyContent="flex-end">
-                <InspectorButton />
-            </Box>
-            <Box p={2} display="flex" justifyContent="space-between">
-                <DatagridConfigurable
-                    resource="books3"
-                    data={data}
-                    sort={{ field: 'title', order: 'ASC' }}
-                    bulkActionButtons={false}
-                    preferenceKey="pref1"
-                >
-                    <TextField source="id" />
-                    <TextField source="title" label="Original title" />
-                    <TextField source="author" />
-                    <TextField source="year" />
-                </DatagridConfigurable>
-                <DatagridConfigurable
-                    resource="books3"
-                    data={data}
-                    sort={{ field: 'title', order: 'ASC' }}
-                    bulkActionButtons={false}
-                    preferenceKey="pref2"
-                >
-                    <TextField source="id" />
-                    <TextField source="title" label="Original title" />
-                    <TextField source="author" />
-                    <TextField source="year" />
-                </DatagridConfigurable>
-            </Box>
-        </MemoryRouter>
-    </PreferencesEditorContextProvider>
+    <Wrapper>
+        <Box p={2} display="flex" justifyContent="space-between">
+            <DatagridConfigurable
+                resource="books3"
+                data={data}
+                sort={{ field: 'title', order: 'ASC' }}
+                bulkActionButtons={false}
+                preferenceKey="pref1"
+            >
+                <TextField source="id" />
+                <TextField source="title" label="Original title" />
+                <TextField source="author" />
+                <TextField source="year" />
+            </DatagridConfigurable>
+            <DatagridConfigurable
+                resource="books3"
+                data={data}
+                sort={{ field: 'title', order: 'ASC' }}
+                bulkActionButtons={false}
+                preferenceKey="pref2"
+            >
+                <TextField source="id" />
+                <TextField source="title" label="Original title" />
+                <TextField source="author" />
+                <TextField source="year" />
+            </DatagridConfigurable>
+        </Box>
+    </Wrapper>
 );
 
 export const LabelElement = () => (
-    <ThemeProvider theme={theme}>
-        <PreferencesEditorContextProvider>
-            <MemoryRouter>
-                <Inspector />
-                <Box display="flex" justifyContent="flex-end">
-                    <InspectorButton />
-                </Box>
-                <Box p={2}>
-                    <DatagridConfigurable
-                        resource="books1"
-                        data={data}
-                        sort={{ field: 'title', order: 'ASC' }}
-                        bulkActionButtons={false}
-                    >
-                        <TextField source="id" />
-                        <TextField source="title" label={<>Original title</>} />
-                        <TextField source="author" />
-                        <TextField source="year" />
-                        <EditButton />
-                    </DatagridConfigurable>
-                </Box>
-            </MemoryRouter>
-        </PreferencesEditorContextProvider>
-    </ThemeProvider>
+    <Wrapper>
+        <DatagridConfigurable
+            resource="books1"
+            data={data}
+            sort={{ field: 'title', order: 'ASC' }}
+            bulkActionButtons={false}
+        >
+            <TextField source="id" />
+            <TextField source="title" label={<>Original title</>} />
+            <TextField source="author" />
+            <TextField source="year" />
+            <EditButton />
+        </DatagridConfigurable>
+    </Wrapper>
 );
