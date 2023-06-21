cebd00780816097c2f7892b837d58bdc46317183
adguernier <adrien@marmelab.com>
2023-06-21 15:47:25 +0200

    prevent render FormHelperText in SelectArrayInput

--- packages/ra-ui-materialui/src/input/SelectArrayInput.tsx
+++ packages/ra-ui-materialui/src/input/SelectArrayInput.tsx
@@ -274,6 +274,8 @@ export const SelectArrayInput = (props: SelectArrayInputProps) => {
                   ),
               }
             : {};
+    const renderHelperText =
+        helperText !== false || ((isTouched || isSubmitted) && invalid);
 
     return (
         <>
@@ -337,13 +339,17 @@ export const SelectArrayInput = (props: SelectArrayInputProps) => {
                 >
                     {finalChoices.map(renderMenuItem)}
                 </Select>
-                <FormHelperText error={fetchError || (isTouched && !!error)}>
-                    <InputHelperText
-                        touched={isTouched || isSubmitted || fetchError}
-                        error={error?.message || fetchError?.message}
-                        helperText={helperText}
-                    />
-                </FormHelperText>
+                {renderHelperText ? (
+                    <FormHelperText
+                        error={fetchError || (isTouched && !!error)}
+                    >
+                        <InputHelperText
+                            touched={isTouched || isSubmitted || fetchError}
+                            error={error?.message || fetchError?.message}
+                            helperText={helperText}
+                        />
+                    </FormHelperText>
+                ) : null}
             </StyledFormControl>
             {createElement}
         </>
