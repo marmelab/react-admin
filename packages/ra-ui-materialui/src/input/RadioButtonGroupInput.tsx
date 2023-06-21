d69e9ec2f9ba4febd982fdfe562f6817056f963e
adguernier <adrien@marmelab.com>
2023-06-21 15:44:13 +0200

    prevent FormHelperText in RadioButtonGroupInput

--- packages/ra-ui-materialui/src/input/RadioButtonGroupInput.tsx
+++ packages/ra-ui-materialui/src/input/RadioButtonGroupInput.tsx
@@ -158,6 +158,10 @@ export const RadioButtonGroupInput = (props: RadioButtonGroupInputProps) => {
             </Labeled>
         );
     }
+
+    const renderHelperText =
+        helperText !== false || ((isTouched || isSubmitted) && invalid);
+
     return (
         <StyledFormControl
             component="fieldset"
@@ -196,13 +200,15 @@ export const RadioButtonGroupInput = (props: RadioButtonGroupInputProps) => {
                     />
                 ))}
             </RadioGroup>
-            <FormHelperText>
-                <InputHelperText
-                    touched={isTouched || isSubmitted || fetchError}
-                    error={error?.message || fetchError?.message}
-                    helperText={helperText}
-                />
-            </FormHelperText>
+            {renderHelperText ? (
+                <FormHelperText>
+                    <InputHelperText
+                        touched={isTouched || isSubmitted || fetchError}
+                        error={error?.message || fetchError?.message}
+                        helperText={helperText}
+                    />
+                </FormHelperText>
+            ) : null}
         </StyledFormControl>
     );
 };
