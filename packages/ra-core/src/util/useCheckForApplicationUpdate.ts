9b25ceead9ab1071e54c44bc8226c9543bce4210
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-07-03 15:30:57 +0200

    Disable update check on localhost

--- packages/ra-core/src/util/useCheckForApplicationUpdate.ts
+++ packages/ra-core/src/util/useCheckForApplicationUpdate.ts
@@ -14,13 +14,17 @@ import { useEvent } from './useEvent';
 export const useCheckForApplicationUpdate = (
     options: UseCheckForApplicationUpdateOptions
 ) => {
+    const shouldDisable =
+        process.env.NODE_ENV !== 'production' ||
+        // Some automation tools may trigger security warnings when the app tries to download itself
+        // so we disable the feature when running on localhost to still allow production builds to be tested locally
+        window.location.hostname === 'localhost';
+
     const {
         url = window.location.href,
         checkInterval = ONE_HOUR,
         onNewVersionAvailable,
-        updateMode = process.env.NODE_ENV === 'production'
-            ? 'manual'
-            : 'disabled',
+        updateMode = shouldDisable ? 'disabled' : 'manual',
     } = options;
     const currentHash = useRef<string>();
     const onCodeHasChanged = useEvent(onNewVersionAvailable);
