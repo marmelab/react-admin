154a579ee54bf125ff7d7b67b8e7e9097d27af07
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-07-12 09:35:01 +0200

    Apply suggestions from code review
    Co-authored-by: Jean-Baptiste Kaiser <jb@marmelab.com>

--- packages/ra-core/src/controller/record/RecordRepresentation.spec.tsx
+++ packages/ra-core/src/controller/record/RecordRepresentation.spec.tsx
@@ -12,17 +12,17 @@ describe('RecordRepresentation', () => {
         render(<NoRecordRepresentation />);
         await screen.findByText('#1');
     });
-    it('should render the record id when provided as a field name on its parent <Resource>', async () => {
+    it('should render the record representation when provided as a field name on its parent <Resource>', async () => {
         render(<StringRecordRepresentation />);
         await screen.findByText("The Hitchhiker's Guide to the Galaxy");
     });
-    it('should render the record id when provided as a function on its parent <Resource>', async () => {
+    it('should render the record representation when provided as a function on its parent <Resource>', async () => {
         render(<FunctionRecordRepresentation />);
         await screen.findByText(
             "The Hitchhiker's Guide to the Galaxy by Douglas Adams"
         );
     });
-    it('should render the record id when provided as a component on its parent <Resource>', async () => {
+    it('should render the record representation when provided as a component on its parent <Resource>', async () => {
         render(<ComponentRecordRepresentation />);
         await screen.findByText(
             (content, element) => {
