4c5867c9bdff9b194957ff4fec26d9cd160d655a
adrien guernier <adrien@marmelab.com>
2023-08-01 15:37:35 +0200

    add CustomSelector story for TranslatableFields

--- packages/ra-ui-materialui/src/field/TranslatableFIelds.stories.tsx
+++ packages/ra-ui-materialui/src/field/TranslatableFIelds.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { TranslatableFields } from './TranslatableFields';
 import { TextField } from './TextField';
-import { RecordContextProvider } from 'ra-core';
+import { RecordContextProvider, useTranslatableContext } from 'ra-core';
 
 export default { title: 'ra-ui-materialui/fields/TranslatableFields' };
 
@@ -26,6 +26,47 @@ const record = {
 export const Basic = () => (
     <RecordContextProvider value={record}>
         <TranslatableFields locales={['en', 'fr']}>
+            <TextField source="name" />
+            <TextField source="description" />
+            <TextField source="nested.field" />
+        </TranslatableFields>
+    </RecordContextProvider>
+);
+
+const Selector = () => {
+    const { locales, selectLocale, selectedLocale } = useTranslatableContext();
+
+    const handleChange = event => {
+        selectLocale(event.target.value);
+    };
+
+    return (
+        <select
+            aria-label="Select the locale"
+            onChange={handleChange}
+            value={selectedLocale}
+        >
+            {locales.map(locale => (
+                <option
+                    key={locale}
+                    value={locale}
+                    id={`translatable-header-${locale}`}
+                >
+                    {locale}
+                </option>
+            ))}
+        </select>
+    );
+};
+
+export const CustomSelector = () => (
+    <RecordContextProvider value={record}>
+        <TranslatableFields
+            record={record}
+            resource="products"
+            locales={['en', 'fr']}
+            selector={<Selector />}
+        >
             <TextField source="name" />
             <TextField source="description" />
             <TextField source="nested.field" />
