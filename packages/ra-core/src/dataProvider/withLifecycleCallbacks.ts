24ff67261f4cf693120e03449d7ca6c71fb9f0cf
Manuel Stahl <37705355+awesome-manuel@users.noreply.github.com>
2023-08-02 11:08:34 +0200

    Try to fix linting errors

--- packages/ra-core/src/dataProvider/withLifecycleCallbacks.ts
+++ packages/ra-core/src/dataProvider/withLifecycleCallbacks.ts
@@ -574,7 +574,10 @@ export type ResourceCallbacks<T extends RaRecord = any> = {
      * Note: This callback doesn't modify the record itself, but the data argument
      * (which may be a diff, especially when called with updateMany).
      */
-    beforeSave?: (data: Partial<T>, dataProvider: DataProvider) => Promise<Partial<T>>;
+    beforeSave?<R extends <Partial<T>>: (
+        data: R,
+        dataProvider: DataProvider
+    ) => Promise<R>;
     /**
      * Update a record after it has been read from the dataProvider
      *
