438ab2c16b1918b10726a4516e271bf92d157d46
adrien guernier <adrien@marmelab.com>
2023-08-03 16:59:40 +0200

    [no-ci][Doc] add FullApp to TranslatableFields stories

--- packages/ra-ui-materialui/src/field/TranslatableFields.stories.tsx
+++ packages/ra-ui-materialui/src/field/TranslatableFields.stories.tsx
@@ -1,21 +1,56 @@
-import * as React from 'react';
+import {
+    RecordContextProvider,
+    Resource,
+    useTranslatableContext,
+} from 'ra-core';
+import fakeRestDataProvider from 'ra-data-fakerest';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import englishMessages from 'ra-language-english';
-
+import * as React from 'react';
 import { AdminContext } from '../AdminContext';
-import { SimpleShowLayout } from '../detail';
-import { TranslatableFields } from './TranslatableFields';
+import { AdminUI } from '../AdminUI';
+import { Show, SimpleShowLayout } from '../detail';
 import { TextField } from './TextField';
-import { RecordContextProvider, useTranslatableContext } from 'ra-core';
+import { TranslatableFields } from './TranslatableFields';
+import { ListGuesser } from '../list';
 
 export default { title: 'ra-ui-materialui/fields/TranslatableFields' };
+
+const defaultData = [
+    {
+        id: 123,
+        title: {
+            en: 'Doctors Without Borders',
+            fr: 'Médecins sans frontières',
+        },
+        description: {
+            en:
+                'International humanitarian medical non-governmental organisation of French origin',
+            fr:
+                "Organisation non gouvernementale (ONG) médicale humanitaire internationale d'origine française fondée en 1971 à Paris",
+        },
+        /*internal_organizations: {
+            OCB: {
+                en: 'Brussels operational center',
+                fr: 'Centre opérationnel de Bruxelles',
+            },
+            OCP: {
+                en: 'Paris operational center',
+                fr: 'Centre opérationnel de Paris',
+            },
+        },*/
+    },
+];
+
+const commonTextFields = [
+    <TextField source="title" />,
+    <TextField source="description" />,
+];
 
 export const Basic = () => (
     <Wrapper>
         <TranslatableFields locales={['en', 'fr']}>
-            <TextField source="title" />
-            <TextField source="description" />
-            <TextField source="internal_organizations.OCP" label="OCP" />
+            {commonTextFields}
         </TranslatableFields>
     </Wrapper>
 );
@@ -72,32 +107,30 @@ const i18nProvider = polyglotI18nProvider(() => englishMessages);
 
 const Wrapper = ({ children }) => (
     <AdminContext i18nProvider={i18nProvider}>
-        <RecordContextProvider
-            value={{
-                id: 123,
-                title: {
-                    en: 'Doctors Without Borders',
-                    fr: 'Médecins sans frontières',
-                },
-                description: {
-                    en:
-                        'International humanitarian medical non-governmental organisation of French origin',
-                    fr:
-                        "Organisation non gouvernementale (ONG) médicale humanitaire internationale d'origine française fondée en 1971 à Paris",
-                },
-                internal_organizations: {
-                    OCB: {
-                        en: 'Brussels operational center',
-                        fr: 'Centre opérationnel de Bruxelles',
-                    },
-                    OCP: {
-                        en: 'Paris operational center',
-                        fr: 'Centre opérationnel de Paris',
-                    },
-                },
-            }}
-        >
+        <RecordContextProvider value={defaultData[0]}>
             <SimpleShowLayout>{children}</SimpleShowLayout>
         </RecordContextProvider>
+    </AdminContext>
+);
+
+const dataProvider = fakeRestDataProvider({
+    ngos: defaultData,
+});
+
+const ShowNgo = () => (
+    <Show>
+        <SimpleShowLayout>
+            <TranslatableFields locales={['en', 'fr']}>
+                {commonTextFields}
+            </TranslatableFields>
+        </SimpleShowLayout>
+    </Show>
+);
+
+export const FullApp = () => (
+    <AdminContext dataProvider={dataProvider} i18nProvider={i18nProvider}>
+        <AdminUI>
+            <Resource name="ngos" list={ListGuesser} show={ShowNgo} />
+        </AdminUI>
     </AdminContext>
 );
