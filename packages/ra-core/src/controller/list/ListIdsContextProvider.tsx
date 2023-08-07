1650dfbe6da95d484ae15928c7acc19cb34fed41
adrien guernier <adrien@marmelab.com>
2023-08-07 14:54:56 +0200

    get list with filters from resource context and keep only the ids to store it in the context

--- packages/ra-core/src/controller/list/ListIdsContextProvider.tsx
+++ packages/ra-core/src/controller/list/ListIdsContextProvider.tsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
+import { useResourceContext } from '../../core';
+import { useGetList } from '../../dataProvider';
 import { ListIdsContext } from './ListIdsContext';
+import { useListParams } from './useListParams';
 
 export const ListIdsContextProvider = ({ children }) => {
+    const resource = useResourceContext();
+
+    const [query] = useListParams({
+        resource,
+    });
+
+    const list = useGetList(resource, {
+        pagination: { page: query.page, perPage: query.perPage },
+        sort: { field: query.sort, order: query.order },
+        filter: { ...query.filter },
+    });
+
+    if (list.isLoading) {
+        return null;
+    }
+
+    const ids = list ? list.data.map(record => record.id) : [];
+
     return (
-        <ListIdsContext.Provider value={[]}>{children}</ListIdsContext.Provider>
+        <ListIdsContext.Provider value={ids}>
+            {children}
+        </ListIdsContext.Provider>
     );
 };
