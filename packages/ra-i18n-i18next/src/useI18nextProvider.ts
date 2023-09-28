12b03dfddad019c7f66adcdfb539fa09ae7bc224
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-09-28 15:23:26 +0200

    Add more documentation

--- packages/ra-i18n-i18next/src/useI18nextProvider.ts
+++ packages/ra-i18n-i18next/src/useI18nextProvider.ts
@@ -4,10 +4,56 @@ import { initReactI18next } from 'react-i18next';
 import { I18nProvider, Locale } from 'ra-core';
 
 /**
- * Build a i18next-based i18nProvider.
+ * A hook that returns an i18nProvider for react-admin applications, based on i18next.
+ * You can provide your own i18next instance but don't initialize it, the hook will do it for you with the options you may provide.
+ * Besides, this hook already adds the `initReactI18next` plugin to i18next.
  *
- * @example
- * TODO
+ * @example Basic usage
+ * import { Admin } from 'react-admin';
+ * import { useI18nextProvider } from 'ra-i18n-i18next';
+ *
+ * const App = () => {
+ *     const i18nProvider = useI18nextProvider();
+ *     if (!i18nProvider) return (<div>Loading...</div>);
+ *
+ *     return (
+ *         <Admin i18nProvider={i18nProvider}>
+ *            ...
+ *         </Admin>
+ *     );
+ * };
+ *
+ * @example With a custom i18next instance and options
+ * import { Admin } from 'react-admin';
+ * import { useI18nextProvider } from 'ra-i18n-i18next';
+ * import i18n from 'i18next';
+ * import Backend from 'i18next-http-backend';
+ * import LanguageDetector from 'i18next-browser-languagedetector';
+ *
+ * const App = () => {
+ *     const i18nextInstance = i18n
+ *         .use(Backend)
+ *         .use(LanguageDetector);
+ *
+ *     const i18nProvider = useI18nextProvider({
+ *         i18nInstance: i18nextInstance,
+ *         options: {
+ *             fallbackLng: 'en',
+ *             debug: true,
+ *             interpolation: {
+ *                 escapeValue: false, // not needed for react!!
+ *             },
+ *         }
+ *     });
+ *
+ *     if (!i18nProvider) return (<div>Loading...</div>);
+ *
+ *     return (
+ *         <Admin i18nProvider={i18nProvider}>
+ *            ...
+ *         </Admin>
+ *     );
+ * };
  */
 export const useI18nextProvider = ({
     i18nInstance = createInstance(),
