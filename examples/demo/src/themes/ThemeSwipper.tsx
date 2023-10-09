74e8c04c9e6527803a28219c9f21de75b39d3f2e
fzaninotto <fzaninotto@gmail.com>
2023-10-09 17:08:49 +0200

    Change ThemeSwapper to an icon button

--- examples/demo/src/themes/ThemeSwipper.tsx
+++ examples/demo/src/themes/ThemeSwipper.tsx
@@ -1,51 +1,60 @@
-import {
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
-    SelectChangeEvent,
-    Stack,
-} from '@mui/material';
-import { useContext } from 'react';
+import { IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+import { useContext, useState } from 'react';
 import { ToggleThemeButton, useStore, useTranslate } from 'react-admin';
+
 import { ThemeContext, ThemeType, themes } from './themeContext';
 
 export const ThemeSwipper = () => {
-    const [_, setThemeStore] = useStore<ThemeType>('theme');
-    const translate = useTranslate();
-
-    const { theme, changeTheme } = useContext(ThemeContext);
-
-    const handleChange = (event: SelectChangeEvent) => {
-        setThemeStore(event.target.value as ThemeType);
-        changeTheme(
-            themes.find(
-                theme => theme.name === (event.target.value as ThemeType)
-            ) || themes[0]
-        );
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
+    const [_, setThemeStore] = useStore<ThemeType>('theme');
+    const { theme: themeFromContext, changeTheme } = useContext(ThemeContext);
+    const handleChange = (_: React.MouseEvent<HTMLElement>, index: number) => {
+        console.log(index);
+        const newTheme = themes[index];
+        setThemeStore(newTheme.name);
+        changeTheme(newTheme);
+        setAnchorEl(null);
+    };
+
+    const translate = useTranslate();
+    const toggleThemeTitle = translate('pos.action.change_theme', {
+        _: 'Change Theme',
+    });
+
     return (
-        <Stack direction={'row'} alignItems={'center'}>
-            <FormControl fullWidth>
-                <InputLabel id="theme-select-label">
-                    {translate('pos.theme.name')}
-                </InputLabel>
-                <Select
-                    labelId="theme-select-label"
-                    id="theme-select"
-                    value={theme.name}
-                    label="Theme"
-                    onChange={handleChange}
+        <>
+            <Tooltip title={toggleThemeTitle} enterDelay={300}>
+                <IconButton
+                    onClick={handleClick}
+                    color="inherit"
+                    aria-label={toggleThemeTitle}
                 >
-                    {themes.map(theme => (
-                        <MenuItem value={theme.name} key={theme.name}>
-                            {theme.name}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            {theme.dark ? <ToggleThemeButton /> : null}
-        </Stack>
+                    <ColorLensIcon />
+                </IconButton>
+            </Tooltip>
+            <Menu open={open} onClose={handleClose} anchorEl={anchorEl}>
+                {themes.map((theme, index) => (
+                    <MenuItem
+                        onClick={event => handleChange(event, index)}
+                        value={theme.name}
+                        key={theme.name}
+                        selected={theme.name === themeFromContext.name}
+                    >
+                        {theme.name}
+                    </MenuItem>
+                ))}
+            </Menu>
+
+            {themeFromContext.dark ? <ToggleThemeButton /> : null}
+        </>
     );
 };
