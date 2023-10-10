2cf47f566e5da1efd8b8afb7facad0d3bb777b9a
François Zaninotto <fzaninotto@gmail.com>
2023-10-10 19:15:01 +0200

    refactor houseTheme

--- examples/demo/src/themes/houseTheme.ts
+++ examples/demo/src/themes/houseTheme.ts
@@ -5,74 +5,20 @@ import {
     Theme,
     PaletteOptions,
 } from '@mui/material';
-import { RaThemeOptions, defaultTheme } from 'react-admin';
+import { RaThemeOptions } from 'react-admin';
 
-const alert = {
-    error: {
-        main: '#DB488B',
-    },
-    warning: {
-        main: '#8C701B',
-    },
-    info: {
-        main: '#3ED0EB',
-    },
-    success: {
-        main: '#0FBF9F',
-    },
-};
+/**
+ * House: A young and joyful theme.
+ *
+ * Uses rounded corners, blurry backdrop, large padding, and a bright color palette.
+ */
 
-const darkPalette: PaletteOptions = {
-    primary: {
-        main: '#ec7a77',
-        light: '#fbcf33',
-    },
-    background: {
-        default: '#363D40',
-        paper: '#2B3033',
-    },
-    ...alert,
-    mode: 'dark' as 'dark',
-};
-
-const lightPalette: PaletteOptions = {
-    primary: {
-        main: '#344767',
-        light: '#7928ca',
-    },
-    secondary: {
-        main: '#f90283',
-    },
-
-    background: {
-        default: '#f7f8f9',
-        paper: '#ffffff',
-    },
-    ...alert,
-    mode: 'light' as 'light',
-};
-
-const typography = {
-    fontFamily: `'Open Sans', sans-serif`,
-};
-
-const shape = {
-    borderRadius: 20,
-};
-
-const sidebar = {
-    width: 250,
-};
-
-const spacing = 9;
-
-const Backdrop = () => ({
+const componentsOverrides = (theme: Theme) => ({
     MuiBackdrop: {
         styleOverrides: {
             root: {
                 backgroundColor: alpha(darken('#000C57', 0.4), 0.2),
                 backdropFilter: 'blur(2px)',
-
                 '&.MuiBackdrop-invisible': {
                     backgroundColor: 'transparent',
                     backdropFilter: 'blur(2px)',
@@ -80,27 +26,94 @@ const Backdrop = () => ({
             },
         },
     },
-});
-
-const FormControl = () => ({
-    MuiFormControl: {
-        defaultProps: {
-            padding: 20,
-        },
-    },
-});
-
-const Input = () => ({
     MuiOutlinedInput: {
         styleOverrides: {
             input: {
-                padding: 16,
+                padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
             },
         },
     },
-});
+    MuiTab: {
+        styleOverrides: {
+            root: {
+                padding: 0,
+                height: 38,
+                minHeight: 38,
+                borderRadius: 6,
+                transition: 'color .2s',
 
-const ItemLink = (theme: Theme) => ({
+                '&.MuiButtonBase-root': {
+                    minWidth: 'auto',
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                    marginRight: 4,
+                },
+                '&.Mui-selected, &.Mui-selected:hover': {
+                    color: theme.palette.primary.contrastText,
+                    zIndex: 5,
+                },
+                '&:hover': {
+                    color: theme.palette.primary.main,
+                },
+            },
+        },
+    },
+    MuiTableRow: {
+        styleOverrides: {
+            root: {
+                '&:last-child td': { border: 0 },
+            },
+        },
+    },
+    MuiTableCell: {
+        styleOverrides: {
+            root: {
+                padding: theme.spacing(2),
+                '&.MuiTableCell-sizeSmall': {
+                    padding: theme.spacing(1.5),
+                },
+                '&.MuiTableCell-paddingNone': {
+                    padding: theme.spacing(0.5),
+                },
+            },
+        },
+    },
+    MuiTabs: {
+        styleOverrides: {
+            root: {
+                height: 38,
+                minHeight: 38,
+                overflow: 'visible',
+            },
+            indicator: {
+                height: 38,
+                minHeight: 38,
+                borderRadius: 6,
+                border: `1px solid ${theme.palette.primary.light}`,
+                boxShadow: theme.shadows[1],
+            },
+            scrollableX: {
+                overflow: 'visible !important',
+            },
+        },
+    },
+    MuiTextField: {
+        defaultProps: {
+            variant: 'outlined' as const,
+        },
+    },
+    RaAppBar: {
+        styleOverrides: {
+            root: {
+                color: theme.palette.text.primary,
+                '& .RaAppBar-toolbar': {
+                    backgroundColor: theme.palette.primary.main,
+                    color: theme.palette.background.default,
+                    backgroundImage: `linear-gradient(310deg, ${theme.palette.primary.light}, ${theme.palette.secondary.main})`,
+                },
+            },
+        },
+    },
     RaMenuItemLink: {
         styleOverrides: {
             root: {
@@ -132,150 +145,40 @@ const ItemLink = (theme: Theme) => ({
     },
 });
 
-const Sidebar = () => ({
-    RaSidebar: {
-        styleOverrides: {
-            root: {
-                marginLeft: 20,
-            },
-        },
-    },
-});
+const alert = {
+    error: { main: '#DB488B' },
+    warning: { main: '#8C701B' },
+    info: { main: '#3ED0EB' },
+    success: { main: '#0FBF9F' },
+};
 
-const Tab = (theme: Theme) => ({
-    MuiTabs: {
-        styleOverrides: {
-            root: {
-                height: 38,
-                minHeight: 38,
-                overflow: 'visible',
-            },
-            indicator: {
-                height: 38,
-                minHeight: 38,
-                borderRadius: 6,
-                border: `1px solid ${theme.palette.primary.light}`,
-                boxShadow: theme.shadows[1],
-            },
-            scrollableX: {
-                overflow: 'visible !important',
-            },
-        },
-    },
-    MuiTab: {
-        styleOverrides: {
-            root: {
-                padding: 0,
-                height: 38,
-                minHeight: 38,
-                borderRadius: 6,
-                transition: 'color .2s',
+const darkPalette: PaletteOptions = {
+    primary: { main: '#ec7a77', light: '#fbcf33' },
+    background: { default: '#363D40', paper: '#2B3033' },
+    ...alert,
+    mode: 'dark' as 'dark',
+};
 
-                '&.MuiButtonBase-root': {
-                    minWidth: 'auto',
-                    paddingLeft: 20,
-                    paddingRight: 20,
-                    marginRight: 4,
-                },
-                '&.Mui-selected, &.Mui-selected:hover': {
-                    color: theme.palette.primary.contrastText,
-                    zIndex: 5,
-                },
-                '&:hover': {
-                    color: theme.palette.primary.main,
-                },
-            },
-        },
-    },
-});
+const lightPalette: PaletteOptions = {
+    primary: { main: '#344767', light: '#7928ca' },
+    secondary: { main: '#f90283' },
+    background: { default: '#f7f8f9', paper: '#ffffff' },
+    ...alert,
+    mode: 'light' as 'light',
+};
 
-const Table = () => ({
-    MuiTableRow: {
-        styleOverrides: {
-            root: {
-                '&:last-child td': { border: 0 },
-            },
-        },
-    },
-    MuiTableCell: {
-        styleOverrides: {
-            root: {
-                padding: 16,
-            },
-        },
-    },
-});
-
-const TextField = () => ({
-    MuiTextField: {
-        defaultProps: {
-            variant: 'outlined' as const,
-        },
-    },
-});
-
-const Toolbar = (theme: Theme) => ({
-    MuiToolbar: {
-        styleOverrides: {
-            root: {
-                backgroundColor: theme.palette.background.default,
-                color: theme.palette.text.primary,
-            },
-            regular: {
-                backgroundColor: theme.palette.background.paper,
-            },
-        },
-    },
-    RaAppBar: {
-        styleOverrides: {
-            root: {
-                color: theme.palette.text.primary,
-                '& .RaAppBar-toolbar': {
-                    backgroundColor: theme.palette.primary.main,
-                    color: theme.palette.background.default,
-                    backgroundImage: `linear-gradient(310deg, ${theme.palette.primary.light}, ${theme.palette.secondary.main})`,
-                },
-            },
-        },
-    },
-});
-
-const componentsOverrides = (theme: Theme) => ({
-    ...Backdrop(),
-    ...FormControl(),
-    ...Input(),
-    ...ItemLink(theme),
-    ...Sidebar(),
-    ...Tab(theme),
-    ...Table(),
-    ...TextField(),
-    ...Toolbar(theme),
-});
-
-const createHouseTheme = (themeOptions: RaThemeOptions) => {
-    const theme = createTheme(themeOptions);
-    theme.components = {
-        ...defaultTheme.components,
-        ...componentsOverrides(theme),
+const createHouseTheme = (palette: RaThemeOptions['palette']) => {
+    const themeOptions = {
+        palette,
+        shape: { borderRadius: 20 },
+        sidebar: { width: 250 },
+        spacing: 9,
+        typography: { fontFamily: `'Open Sans', sans-serif` },
     };
-
+    const theme = createTheme(themeOptions);
+    theme.components = componentsOverrides(theme);
     return theme;
 };
 
-export const houseLightTheme = createHouseTheme({
-    ...defaultTheme,
-    palette: lightPalette,
-    typography,
-    shape,
-    sidebar,
-    spacing,
-});
-
-export const houseDarkTheme = createHouseTheme({
-    ...defaultTheme,
-    palette: darkPalette,
-    typography,
-    shape,
-    sidebar,
-    spacing,
-});
+export const houseLightTheme = createHouseTheme(lightPalette);
+export const houseDarkTheme = createHouseTheme(darkPalette);
