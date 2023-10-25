b8af91f90308ab93fdd3523763190be3741fa3ba
erwanMarmelab <erwan@marmelab.com>
2023-10-25 10:51:22 +0200

    apply François feedbacks

--- packages/ra-ui-materialui/src/field/SelectField.stories.tsx
+++ packages/ra-ui-materialui/src/field/SelectField.stories.tsx
@@ -1,29 +1,42 @@
-import dompurify from 'dompurify';
-import { RecordContextProvider, useTimeout } from 'ra-core';
+import { RecordContextProvider } from 'ra-core';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import englishMessages from 'ra-language-english';
+import frenchMessages from 'ra-language-french';
 import * as React from 'react';
 
+import { AdminContext } from '../AdminContext';
+import { Labeled } from '../Labeled';
 import { SelectField } from './SelectField';
 
 export default {
     title: 'ra-ui-materialui/fields/SelectField',
 };
 
+const i18nProvider = polyglotI18nProvider(
+    locale => (locale === 'fr' ? frenchMessages : englishMessages),
+    'en',
+    [
+        { locale: 'en', name: 'English' },
+        { locale: 'fr', name: 'Français' },
+    ]
+);
+
 const record = {
     id: 1,
     gender: 'M',
     language: 'ar',
     country: 'Albania',
+    data: 'no results',
 };
+
+const genderChoices = [
+    { id: 'M', name: 'Male' },
+    { id: 'F', name: 'Female' },
+];
 
 export const Basic = () => (
     <RecordContextProvider value={record}>
-        <SelectField
-            source="gender"
-            choices={[
-                { id: 'M', name: 'Male' },
-                { id: 'F', name: 'Female' },
-            ]}
-        />
+        <SelectField source="gender" choices={genderChoices} />
     </RecordContextProvider>
 );
 
@@ -50,7 +63,7 @@ export const OptionValue = () => (
 
 const optionRenderer = choice => `${choice.first_name} ${choice.last_name}`;
 const authors = [{ id: 1, first_name: 'John', last_name: 'Doe' }];
-export const Record = () => (
+export const OptionTextFunction = () => (
     <RecordContextProvider value={record}>
         <SelectField
             source="id"
@@ -60,15 +73,32 @@ export const Record = () => (
     </RecordContextProvider>
 );
 
+const translateChoice = [
+    { id: 'no results', name: 'ra.navigation.no_results' },
+];
 export const TranslateChoice = () => (
-    <RecordContextProvider value={record}>
-        <SelectField
-            source="gender"
-            choices={[
-                { id: 'M', name: 'myroot.gender.male' },
-                { id: 'F', name: 'myroot.gender.female' },
-            ]}
-            translateChoice={false}
-        />
-    </RecordContextProvider>
+    <AdminContext i18nProvider={i18nProvider}>
+        <RecordContextProvider value={record}>
+            <Labeled
+                label="translateChoice={true}"
+                sx={{ border: '1px solid', margin: '1rem', padding: '1rem' }}
+            >
+                <SelectField
+                    source="data"
+                    choices={translateChoice}
+                    // translateChoice={true} is set by default
+                />
+            </Labeled>
+            <Labeled
+                label="translateChoice={false}"
+                sx={{ border: '1px solid', margin: '1rem', padding: '1rem' }}
+            >
+                <SelectField
+                    source="data"
+                    choices={translateChoice}
+                    translateChoice={false}
+                />
+            </Labeled>
+        </RecordContextProvider>
+    </AdminContext>
 );
