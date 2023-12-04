017ea11aa7843a865ded472417659e881d246c49
adrien guernier <adrien@marmelab.com>
2023-12-04 10:27:15 +0100

    update AdminContext spec

--- packages/ra-ui-materialui/src/AdminContext.spec.tsx
+++ packages/ra-ui-materialui/src/AdminContext.spec.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Typography, ThemeOptions } from '@mui/material';
+import { Typography } from '@mui/material';
 import expect from 'expect';
 import { memoryStore } from 'ra-core';
 
 import { AdminContext } from './AdminContext';
 import { ThemeTestWrapper } from './layout/ThemeTestWrapper';
-
-const lightTheme: ThemeOptions = {};
-const darkTheme: ThemeOptions = { palette: { mode: 'dark' } };
 
 const LIGHT_MODE_TEXT_COLOR = 'rgb(25, 118, 210)'; // text is dark blue in light mode
 const DARK_MODE_TEXT_COLOR = 'rgb(144, 202, 249)'; // text is light blue in dark mode
@@ -16,7 +13,7 @@ const DARK_MODE_TEXT_COLOR = 'rgb(144, 202, 249)'; // text is light blue in dark
 describe('AdminContext', () => {
     it('should default to light theme', () => {
         render(
-            <AdminContext theme={lightTheme} darkTheme={darkTheme}>
+            <AdminContext>
                 <Typography color="primary">Test</Typography>
             </AdminContext>
         );
@@ -26,7 +23,7 @@ describe('AdminContext', () => {
     it('should default to dark theme when the browser detects a dark mode preference', () => {
         render(
             <ThemeTestWrapper mode="dark">
-                <AdminContext theme={lightTheme} darkTheme={darkTheme}>
+                <AdminContext>
                     <Typography color="primary">Test</Typography>
                 </AdminContext>
             </ThemeTestWrapper>
@@ -37,7 +34,7 @@ describe('AdminContext', () => {
     it('should default to light theme when the browser detects a dark mode preference', () => {
         render(
             <ThemeTestWrapper mode="light">
-                <AdminContext theme={lightTheme} darkTheme={darkTheme}>
+                <AdminContext>
                     <Typography color="primary">Test</Typography>
                 </AdminContext>
             </ThemeTestWrapper>
@@ -47,11 +44,7 @@ describe('AdminContext', () => {
     });
     it('should default to dark theme when user preference is dark', () => {
         render(
-            <AdminContext
-                theme={lightTheme}
-                darkTheme={darkTheme}
-                store={memoryStore({ theme: 'dark' })}
-            >
+            <AdminContext store={memoryStore({ theme: 'dark' })}>
                 <Typography color="primary">Test</Typography>
             </AdminContext>
         );
@@ -61,11 +54,7 @@ describe('AdminContext', () => {
     it('should default to light theme when user preference is light', () => {
         render(
             <ThemeTestWrapper mode="dark">
-                <AdminContext
-                    theme={lightTheme}
-                    darkTheme={darkTheme}
-                    store={memoryStore({ theme: 'light' })}
-                >
+                <AdminContext store={memoryStore({ theme: 'light' })}>
                     <Typography color="primary">Test</Typography>
                 </AdminContext>
             </ThemeTestWrapper>
