d804c149ead2c026cb3757e0544220da0e58c338
François Zaninotto <fzaninotto@gmail.com>
2023-12-08 12:39:37 +0100

    Fix crm app

--- examples/crm/src/index.tsx
+++ examples/crm/src/index.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.createElement('root');
-const root = createRoot(container);
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
 root.render(
     <React.StrictMode>
