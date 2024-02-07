27a64832cadce90ecac96e6bc58bc04e6d2742e8
adrien guernier <adrien@marmelab.com>
2024-02-07 11:03:59 +0100

    Revert "Fix: no old LabelPrefix concatenation"
    This reverts commit 73be01fdba3ea9633cd460217f05bd012729f855.

--- packages/ra-core/src/util/LabelPrefixContextProvider.tsx
+++ packages/ra-core/src/util/LabelPrefixContextProvider.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import { LabelPrefixContext } from './LabelPrefixContext';
+import { useLabelPrefix } from './useLabelPrefix';
 
-export const LabelPrefixContextProvider = ({ prefix, children }) => {
+export const LabelPrefixContextProvider = ({
+    prefix,
+    concatenate = true,
+    children,
+}) => {
+    const oldPrefix = useLabelPrefix();
+    const newPrefix =
+        oldPrefix && concatenate ? `${oldPrefix}.${prefix}` : prefix;
     return (
-        <LabelPrefixContext.Provider value={prefix}>
+        <LabelPrefixContext.Provider value={newPrefix}>
             {children}
         </LabelPrefixContext.Provider>
     );
