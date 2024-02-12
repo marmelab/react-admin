dfe7932e428f3cb94ce72dce18c676a3d1947e39
erwanMarmelab <erwan@marmelab.com>
2024-02-12 12:16:02 +0100

    fix CommonInpuProps

--- packages/ra-ui-materialui/src/input/CommonInputProps.ts
+++ packages/ra-ui-materialui/src/input/CommonInputProps.ts
@@ -6,8 +6,8 @@ export type CommonInputProps = InputProps & {
      * @deprecated this property is not used anymore
      */
     formClassName?: string;
-    disabled?: string;
-    readOnly?: string;
+    disabled?: boolean;
+    readOnly?: boolean;
     fullWidth?: boolean;
     headerCellClassName?: string;
     margin?: 'none' | 'dense' | 'normal';
