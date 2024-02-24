cecb441b1e17524814728a0efef9a3201c2b41e0
Sam Bull <git@sambull.org>
2024-02-24 16:02:27 +0000

    Update BulkUpdateWithConfirmButton.spec.tsx

--- packages/ra-ui-materialui/src/button/BulkUpdateWithConfirmButton.spec.tsx
+++ packages/ra-ui-materialui/src/button/BulkUpdateWithConfirmButton.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { screen, render, waitFor, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
 import expect from 'expect';
 import {
     CoreAdminContext,
