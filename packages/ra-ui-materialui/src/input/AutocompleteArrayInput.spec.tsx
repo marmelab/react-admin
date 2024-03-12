29cd5dd7fc643b65b1c49d9b648ac4004c06b7e1
fzaninotto <fzaninotto@gmail.com>
2024-03-12 18:26:57 +0100

    Fix test

--- packages/ra-ui-materialui/src/input/AutocompleteArrayInput.spec.tsx
+++ packages/ra-ui-materialui/src/input/AutocompleteArrayInput.spec.tsx
@@ -809,7 +809,7 @@ describe('<AutocompleteArrayInput />', () => {
                 selector: 'input',
             })
         );
-        expect(screen.queryAllByRole('option')).toHaveLength(2);
+        expect(screen.queryAllByRole('option')).toHaveLength(3);
         expect(screen.getByText('Technical')).not.toBeNull();
         expect(screen.getByText('Programming')).not.toBeNull();
     });
