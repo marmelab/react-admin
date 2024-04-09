83b92f2a49647b8f330d1fa8f79795a43d066bb7
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-04-09 17:57:00 +0200

    Suppress cypress error

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,1 +1,11 @@
 import 'cypress-plugin-tab';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    if (
+        err.message.includes(
+            'ResizeObserver loop completed with undelivered notifications'
+        )
+    ) {
+        return false;
+    }
+});
