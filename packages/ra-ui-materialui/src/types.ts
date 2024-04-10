b950109880959b19fcbdf0ef23e16655f8fc8ae4
hiaselhans <simon.klemenc@gmail.com>
2024-04-10 19:55:07 +0200

    run linter

--- packages/ra-ui-materialui/src/types.ts
+++ packages/ra-ui-materialui/src/types.ts
@@ -31,7 +31,11 @@ export interface EditProps<
     redirect?: RedirectionSideEffect;
     resource?: string;
     transform?: TransformData;
-    title?: string | ReactElement | false | ((arg0: RecordType | undefined) => string);
+    title?:
+        | string
+        | ReactElement
+        | false
+        | ((arg0: RecordType | undefined) => string);
     sx?: SxProps;
 }
 
@@ -70,7 +74,11 @@ export interface ShowProps<RecordType extends RaRecord = any> {
     id?: Identifier;
     queryOptions?: UseQueryOptions<RecordType> & { meta?: any };
     resource?: string;
-    title?: string | ReactElement | false | ((arg0: RecordType | undefined) => string);
+    title?:
+        | string
+        | ReactElement
+        | false
+        | ((arg0: RecordType | undefined) => string);
     sx?: SxProps;
 }
 
