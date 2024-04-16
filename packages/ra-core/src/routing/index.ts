2466c7deee27ae586e10f1587478205804882056
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-04-16 09:36:25 +0200

    Export RestoreScrollPosition

--- packages/ra-core/src/routing/index.ts
+++ packages/ra-core/src/routing/index.ts
@@ -2,6 +2,7 @@ export * from './AdminRouter';
 export * from './BasenameContextProvider';
 export * from './linkToRecord';
 export * from './resolveRedirectTo';
+export * from './RestoreScrollPosition';
 export * from './useBasename';
 export * from './useCreatePath';
 export * from './useRedirect';
