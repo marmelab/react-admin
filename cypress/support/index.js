c37fd9ea88bd140c02ad91df6dcdb992e74f7c4b
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-04-18 17:40:14 +0200

    Add comment for cypress issue

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,11 @@
 import 'cypress-plugin-tab';
 
-Cypress.on('uncaught:exception', (err, runnable) => {
+/*
+ * This is a workaround for the ResizeObserver loop error that occurs in Cypress.
+ * See https://github.com/cypress-io/cypress/issues/20341
+ * See https://github.com/cypress-io/cypress/issues/29277
+ */
+Cypress.on('uncaught:exception', err => {
     if (
         err.message.includes(
             'ResizeObserver loop completed with undelivered notifications'
