f7421b92cf51a0213135704aa8b346f3aa2ee8de
François Zaninotto <fzaninotto@gmail.com>
2024-05-03 22:47:12 +0200

    Fix Datagrid uses wrong element for "Select All" label

--- packages/ra-ui-materialui/src/list/datagrid/DatagridHeader.tsx
+++ packages/ra-ui-materialui/src/list/datagrid/DatagridHeader.tsx
@@ -111,9 +111,14 @@ export const DatagridHeader = (props: DatagridHeaderProps) => {
                         className={DatagridClasses.headerCell}
                     >
                         <Checkbox
-                            aria-label={translate('ra.action.select_all', {
-                                _: 'Select all',
-                            })}
+                            inputProps={{
+                                'aria-label': translate(
+                                    'ra.action.select_all',
+                                    {
+                                        _: 'Select all',
+                                    }
+                                ),
+                            }}
                             className="select-all"
                             color="primary"
                             checked={
