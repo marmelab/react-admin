a4c29e020c7c2e453c6bb74eb1a6bbaf9653779b
erwanMarmelab <erwan@marmelab.com>
2024-06-05 17:31:06 +0200

    fix height

--- packages/ra-ui-materialui/src/AdminUI.tsx
+++ packages/ra-ui-materialui/src/AdminUI.tsx
@@ -22,7 +22,7 @@ export const AdminUI = ({
     error = Error,
     ...props
 }: AdminUIProps) => (
-    <ScopedCssBaseline enableColorScheme>
+    <ScopedCssBaseline enableColorScheme sx={{ height: '100vh' }}>
         <CoreAdminUI
             layout={layout}
             catchAll={catchAll}
