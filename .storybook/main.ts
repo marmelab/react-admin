4869dbded3f415c4652b5fcab5c957ca01af3bbd
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-06-17 11:51:28 +0200

    Try babel for storysource

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,37 @@
 import { StorybookConfig } from '@storybook/react-webpack5';
 import fs from 'fs';
-import path from 'path';
+import path, { dirname, join } from 'path';
 
 const packages = fs.readdirSync(path.resolve(__dirname, '../packages'));
 
 const config: StorybookConfig = {
-    stories: [`../packages/${process.env.ONLY || '**'}/**/*.stories.@(tsx)`],
+    stories: [
+        path.resolve(
+            __dirname,
+            `../packages/${process.env.ONLY || '**'}/**/*.stories.@(tsx)`
+        ),
+    ],
     addons: [
+        '@storybook/addon-webpack5-compiler-babel',
         {
             name: '@storybook/addon-storysource',
             options: {
+                rule: {
+                    include: [
+                        path.resolve(
+                            __dirname,
+                            `../packages/${process.env.ONLY || '**'}/**/*.stories.@(tsx)`
+                        ),
+                    ],
+                },
                 loaderOptions: {
                     parser: 'typescript',
+                    injectStoryParameters: false,
                 },
             },
         },
         '@storybook/addon-actions',
         '@storybook/addon-controls',
-        '@storybook/addon-webpack5-compiler-swc',
     ],
     typescript: {
         check: false,
@@ -73,10 +87,15 @@ const config: StorybookConfig = {
             },
         };
     },
-    framework: { name: '@storybook/react-webpack5', options: {} },
-    docs: {
-        autodocs: false,
+    framework: {
+        name: getAbsolutePath('@storybook/react-webpack5'),
+        options: {},
     },
+    docs: {},
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, 'package.json')));
+}
