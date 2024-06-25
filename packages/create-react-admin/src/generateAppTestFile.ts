b3eae966a579eb395c6fa268f75fc3ce3983c334
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-06-25 15:59:07 +0200

    Fix create-react-admin tests

--- packages/create-react-admin/src/generateAppTestFile.ts
+++ packages/create-react-admin/src/generateAppTestFile.ts
@@ -20,6 +20,9 @@ test("should pass", async () => {
             ? `
 
 	// Sign in
+  	// Ensure the form is fully loaded before interacting with it
+  	await new Promise((resolve) => setTimeout(resolve, 1000));
+
 	fireEvent.change(await screen.findByLabelText("Username *"), {
 		target: { value: "janedoe" },
 	});
@@ -33,6 +36,9 @@ test("should pass", async () => {
 	// Open the first post
 	fireEvent.click(await screen.findByText("Post 1"));
 	fireEvent.click(await screen.findByText("Edit"));
+	await screen.findByDisplayValue("Post 1");
+	// Ensure the form is fully loaded before interacting with it
+	await new Promise((resolve) => setTimeout(resolve, 1000));
 	// Update its title
 	fireEvent.change(await screen.findByDisplayValue("Post 1"), {
 		target: { value: "Post 1 edited" },
@@ -45,6 +51,9 @@ test("should pass", async () => {
 	// Open the first comment
 	fireEvent.click(await screen.findByText("Comment 1"));
 	fireEvent.click(await screen.findByText("Edit"));
+	await screen.findByDisplayValue("Post 1 edited");
+	// Ensure the form is fully loaded before interacting with it
+	await new Promise((resolve) => setTimeout(resolve, 1000));
 	// Edit the comment selected post
 	fireEvent.click(await screen.findByDisplayValue("Post 1 edited"));
 	fireEvent.click(await screen.findByText("Post 11"));
