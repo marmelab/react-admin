f5d5a7807f1c780a4b2c4bc9e6727f34b7c457df
Anthony Rimet <anthony.rimet03@gmail.com>
2024-07-14 22:58:04 +0200

    Fix(crm): Update test to use jest instead of vite

--- examples/crm/src/misc/getContactAvatar.spec.ts
+++ examples/crm/src/misc/getContactAvatar.spec.ts
@@ -1,8 +1,11 @@
+/**
+ * @jest-environment node
+ */
+
 import { Contact } from '../types';
 import { getAvatarUrl, hash } from './getContactAvatar';
-import { test, expect } from 'vitest';
 
-test('should return gravatar URL for anthony@marmelab.com', async () => {
+it('should return gravatar URL for anthony@marmelab.com', async () => {
     const email = 'anthony@marmelab.com';
     const record: Partial<Contact> = { email };
 
@@ -13,15 +16,15 @@ test('should return gravatar URL for anthony@marmelab.com', async () => {
     );
 });
 
-test('should return favicon URL if gravatar does not exist', async () => {
-    const email = 'no-gravatar@wikipedia.com';
+it('should return favicon URL if gravatar does not exist', async () => {
+    const email = 'no-gravatar@gravatar.com';
     const record: Partial<Contact> = { email };
 
     const avatarUrl = await getAvatarUrl(record);
-    expect(avatarUrl).toBe('https://wikipedia.com/favicon.ico');
+    expect(avatarUrl).toBe('https://gravatar.com/favicon.ico');
 });
 
-test('should not return favicon URL if not domain not allowed', async () => {
+it('should not return favicon URL if not domain not allowed', async () => {
     const email = 'no-gravatar@google.com';
     const record: Partial<Contact> = { email };
 
@@ -29,14 +32,14 @@ test('should not return favicon URL if not domain not allowed', async () => {
     expect(avatarUrl).toBeNull();
 });
 
-test('should return null if no email is provided', async () => {
+it('should return null if no email is provided', async () => {
     const record: Partial<Contact> = {};
 
     const avatarUrl = await getAvatarUrl(record);
     expect(avatarUrl).toBeNull();
 });
 
-test('should return null if email has no gravatar or validate domain', async () => {
+it('should return null if email has no gravatar or validate domain', async () => {
     const email = 'anthony@fake-domain-marmelab.com';
     const record: Partial<Contact> = { email };
 
