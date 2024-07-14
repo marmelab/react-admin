11284df76ecbea613c1f10a51a929f31b2af16ab
Anthony Rimet <anthony.rimet03@gmail.com>
2024-07-14 14:23:33 +0200

    Feat(crm): Add empty companies page

--- examples/crm/src/companies/CompanyList.tsx
+++ examples/crm/src/companies/CompanyList.tsx
@@ -1,31 +1,52 @@
 import * as React from 'react';
 import {
-    List,
     TopToolbar,
     ExportButton,
     CreateButton,
     Pagination,
     useGetIdentity,
+    ListBase,
+    Title,
+    ListToolbar,
+    useListContext,
 } from 'react-admin';
 
 import { ImageList } from './GridList';
 import { CompanyListFilter } from './CompanyListFilter';
+import { Card, LinearProgress, Stack } from '@mui/material';
+import { CompanyEmpty } from './CompanyEmpty';
 
 export const CompanyList = () => {
     const { identity } = useGetIdentity();
     if (!identity) return null;
     return (
-        <List
-            actions={<CompanyListActions />}
-            aside={<CompanyListFilter />}
-            filterDefaultValues={{ sales_id: identity?.id }}
-            pagination={<Pagination rowsPerPageOptions={[15, 25, 50, 100]} />}
+        <ListBase
             perPage={25}
+            filterDefaultValues={{ sales_id: identity?.id }}
             sort={{ field: 'name', order: 'ASC' }}
-            component="div"
         >
-            <ImageList />
-        </List>
+            <CompanyListLayout />
+        </ListBase>
+    );
+};
+
+const CompanyListLayout = () => {
+    const { data, isPending } = useListContext();
+    if (isPending) return <LinearProgress />;
+    if (!data?.length) return <CompanyEmpty />;
+
+    return (
+        <Stack direction="row" component="div">
+            <CompanyListFilter />
+            <Stack sx={{ width: '100%' }}>
+                <Title title={'Companies'} />
+                <ListToolbar actions={<CompanyListActions />} />
+                <Card>
+                    <ImageList />
+                </Card>
+                <Pagination rowsPerPageOptions={[10, 25, 50, 100]} />
+            </Stack>
+        </Stack>
     );
 };
 
