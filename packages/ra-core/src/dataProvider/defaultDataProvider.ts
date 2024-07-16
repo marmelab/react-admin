284fd8ab77e027bfedcac2d2572317c40c9b8dba
Ahmed Ayoub <great.nile@gmail.com>
2024-07-16 18:55:14 +0200

    fix types and remove typo

--- packages/ra-core/src/dataProvider/defaultDataProvider.ts
+++ packages/ra-core/src/dataProvider/defaultDataProvider.ts
@@ -1,14 +1,20 @@
-import { DataProvider } from '../types';
+import {
+    CreateResult,
+    DataProvider,
+    DeleteResult,
+    GetOneResult,
+    UpdateResult,
+} from '../types';
 
 // avoids adding a context in tests
 export const defaultDataProvider: DataProvider = {
-    create: () => Promise.resolve({ data: null }),
-    delete: () => Promise.resolve({ data: null }),
+    create: () => Promise.resolve<CreateResult>({ data: null }),
+    delete: () => Promise.resolve<DeleteResult>({ data: null }),
     deleteMany: () => Promise.resolve({ data: [] }),
     getList: () => Promise.resolve({ data: [], total: 0 }),
     getMany: () => Promise.resolve({ data: [] }),
     getManyReference: () => Promise.resolve({ data: [], total: 0 }),
-    getOne: () => Promise.resolve({ data: { id: 'o' } }),
-    update: () => Promise.resolve({ data: null }),
+    getOne: () => Promise.resolve<GetOneResult>({ data: null }),
+    update: () => Promise.resolve<UpdateResult>({ data: null }),
     updateMany: () => Promise.resolve({ data: [] }),
-} as DataProvider;
+};
