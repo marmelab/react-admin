20dfcfb2399682f2e51124cb8117338fab91eba1
Jonathan ARNAULT <jonathan@marmelab.com>
2024-07-23 16:36:42 +0200

    Feat(crm): Add ability to get back to show from deal edit and on save

--- examples/crm/src/deals/DealEdit.tsx
+++ examples/crm/src/deals/DealEdit.tsx
@@ -1,6 +1,22 @@
-import * as React from 'react';
-import { SimpleForm, useRedirect, Edit } from 'react-admin';
-import { Dialog } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import CloseIcon from '@mui/icons-material/Close';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import {
+    DeleteWithConfirmButton,
+    EditBase,
+    Form,
+    SaveButton,
+    useRecordContext,
+    useRedirect,
+} from 'react-admin';
+import { Link } from 'react-router-dom';
+import { Deal } from '../types';
 import { DealForm } from './DealForm';
 
 export const DealEdit = ({ open, id }: { open: boolean; id?: string }) => {
@@ -10,19 +26,80 @@ export const DealEdit = ({ open, id }: { open: boolean; id?: string }) => {
         redirect('/deals');
     };
 
+    if (id == null) {
+        return null;
+    }
+
     return (
-        <Dialog open={open} onClose={handleClose} fullWidth maxWidth="lg">
-            {!!id ? (
-                <Edit
-                    id={id}
-                    redirect="list"
-                    sx={{ '& .RaCreate-main': { mt: 0 } }}
+        <EditBase id={id} redirect="show">
+            <Form>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    fullWidth
+                    maxWidth="lg"
+                    scroll="paper"
+                    sx={{
+                        '& form': {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            flex: 1,
+                            flexBasis: '100%',
+                        },
+                    }}
+                    disablePortal
                 >
-                    <SimpleForm>
+                    <EditHeader onClose={handleClose} />
+                    <DialogContent
+                        sx={{
+                            scrollbarGutter: 'stable',
+                            paddingInlineEnd: 1.5,
+                        }}
+                    >
                         <DealForm />
-                    </SimpleForm>
-                </Edit>
-            ) : null}
-        </Dialog>
+                    </DialogContent>
+                    <DialogActions
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            paddingBlock: 1,
+                            paddingInline: 3,
+                        }}
+                    >
+                        <SaveButton />
+                        <DeleteWithConfirmButton />
+                    </DialogActions>
+                </Dialog>
+            </Form>
+        </EditBase>
     );
 };
+
+function EditHeader({ onClose }: { onClose: () => void }) {
+    const deal = useRecordContext<Deal>();
+    if (!deal) {
+        return null;
+    }
+
+    return (
+        <DialogTitle>
+            <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                spacing={1}
+            >
+                <Button
+                    component={Link}
+                    to={`/deals/${deal.id}/show`}
+                    startIcon={<ArrowBackIcon />}
+                >
+                    Back to {deal.name}
+                </Button>
+                <IconButton aria-label="Close" onClick={onClose} size="small">
+                    <CloseIcon />
+                </IconButton>
+            </Stack>
+        </DialogTitle>
+    );
+}
