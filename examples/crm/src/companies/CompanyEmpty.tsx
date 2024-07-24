1634fc8a7253e57acbbb0065b219532b75b897d6
Anthony Rimet <anthony.rimet03@gmail.com>
2024-07-24 10:32:03 +0200

    Fix(crm): Improve spacing for empty components

--- examples/crm/src/companies/CompanyEmpty.tsx
+++ examples/crm/src/companies/CompanyEmpty.tsx
@@ -8,23 +8,25 @@ export const CompanyEmpty = () => {
         <Stack
             justifyContent="center"
             alignItems="center"
-            gap={1}
+            gap={3}
             sx={{
                 height: `calc(100dvh - ${appbarHeight}px)`,
             }}
         >
             <img src="./img/empty.svg" alt="No contacts found" />
-            <Typography variant="h6" fontWeight="bold">
-                No companies found
-            </Typography>
-            <Typography
-                variant="body2"
-                align="center"
-                color="text.secondary"
-                gutterBottom
-            >
-                It looks like your company list is empty.
-            </Typography>
+            <Stack gap={0} alignItems="center">
+                <Typography variant="h6" fontWeight="bold">
+                    No companies found
+                </Typography>
+                <Typography
+                    variant="body2"
+                    align="center"
+                    color="text.secondary"
+                    gutterBottom
+                >
+                    It looks like your company list is empty.
+                </Typography>
+            </Stack>
             <Stack spacing={2} direction="row">
                 <CreateButton variant="contained" label="Create Company" />
             </Stack>
--- examples/crm/src/contacts/ContactEmpty.tsx
+++ examples/crm/src/contacts/ContactEmpty.tsx
@@ -9,23 +9,25 @@ export const ContactEmpty = () => {
         <Stack
             justifyContent="center"
             alignItems="center"
-            gap={1}
+            gap={3}
             sx={{
                 height: `calc(100dvh - ${appbarHeight}px)`,
             }}
         >
             <img src="./img/empty.svg" alt="No contacts found" />
-            <Typography variant="h6" fontWeight="bold">
-                No contacts found
-            </Typography>
-            <Typography
-                variant="body2"
-                align="center"
-                color="text.secondary"
-                gutterBottom
-            >
-                It looks like your contact list is empty.
-            </Typography>
+            <Stack gap={0} alignItems="center">
+                <Typography variant="h6" fontWeight="bold">
+                    No contacts found
+                </Typography>
+                <Typography
+                    variant="body2"
+                    align="center"
+                    color="text.secondary"
+                    gutterBottom
+                >
+                    It looks like your contact list is empty.
+                </Typography>
+            </Stack>
             <Stack spacing={2} direction="row">
                 <CreateButton variant="contained" label="New Contact" />
                 <ContactImportButton />
