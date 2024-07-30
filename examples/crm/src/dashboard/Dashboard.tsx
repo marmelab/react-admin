ea952d2bebe21cf32910d3dedcb2580566eb0cf1
Anthony Rimet <anthony.rimet03@gmail.com>
2024-07-31 00:03:13 +0200

    Feat(crm): Add onboarding for empty project

--- examples/crm/src/dashboard/Dashboard.tsx
+++ examples/crm/src/dashboard/Dashboard.tsx
@@ -4,24 +4,64 @@ import { DealsChart } from './DealsChart';
 import { HotContacts } from './HotContacts';
 import { TasksList } from './TasksList';
 import { Welcome } from './Welcome';
+import { useGetList } from 'react-admin';
+import { Contact, ContactNote } from '../types';
+import { DashboardStepper } from './DashboardStepper';
 
-export const Dashboard = () => (
-    <Grid container spacing={2} mt={1} rowGap={4}>
-        <Grid item xs={12} md={3}>
-            <Stack gap={4}>
-                <Welcome />
-                <HotContacts />
-            </Stack>
-        </Grid>
-        <Grid item xs={12} md={6}>
-            <Stack gap={4}>
-                <DealsChart />
-                <DashboardActivityLog />
-            </Stack>
-        </Grid>
+export const Dashboard = () => {
+    const {
+        data: dataContact,
+        total: totalContact,
+        isPending: isPendingContact,
+    } = useGetList<Contact>('contacts', {
+        pagination: { page: 1, perPage: 1 },
+    });
 
-        <Grid item xs={12} md={3}>
-            <TasksList />
+    const { total: totalContactNotes, isPending: isPendingContactNotes } =
+        useGetList<ContactNote>('contactNotes', {
+            pagination: { page: 1, perPage: 1 },
+        });
+
+    const { total: totalDeal, isPending: isPendingDeal } = useGetList<Contact>(
+        'deals',
+        {
+            pagination: { page: 1, perPage: 1 },
+        }
+    );
+
+    const isPending =
+        isPendingContact || isPendingContactNotes || isPendingDeal;
+
+    if (isPending) {
+        return null;
+    }
+
+    if (!totalContact) {
+        return <DashboardStepper step={1} />;
+    }
+
+    if (!totalContactNotes) {
+        return <DashboardStepper step={2} contactId={dataContact?.[0].id} />;
+    }
+
+    return (
+        <Grid container spacing={2} mt={1} rowGap={4}>
+            <Grid item xs={12} md={3}>
+                <Stack gap={4}>
+                    <Welcome />
+                    <HotContacts />
+                </Stack>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Stack gap={4}>
+                    {totalDeal ? <DealsChart /> : null}
+                    <DashboardActivityLog />
+                </Stack>
+            </Grid>
+
+            <Grid item xs={12} md={3}>
+                <TasksList />
+            </Grid>
         </Grid>
-    </Grid>
-);
+    );
+};
