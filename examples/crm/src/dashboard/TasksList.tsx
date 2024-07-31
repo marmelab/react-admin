01a5c79b47d18c96d61b506c47844e067dc9b7bd
Anthony Rimet <anthony.rimet03@gmail.com>
2024-07-31 11:38:18 +0200

    Feat(crm): Display due date for Task dashboard

--- examples/crm/src/dashboard/TasksList.tsx
+++ examples/crm/src/dashboard/TasksList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Card, Box, Button } from '@mui/material';
+import { Card, Box, Typography, Stack } from '@mui/material';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import {
     useGetList,
@@ -13,11 +13,17 @@ import { TasksIterator } from '../tasks/TasksIterator';
 
 import { Contact } from '../types';
 import { AddTask } from '../tasks/AddTask';
+import { startOfToday, endOfToday, addDays } from 'date-fns';
+
+const today = new Date();
+const startOfTodayDateISO = startOfToday().toISOString();
+const endOfTodayDateISO = endOfToday().toISOString();
+const startOfWeekDateISO = addDays(today, 1).toISOString();
+const endOfWeekDateISO = addDays(today, 7).toISOString();
 
 export const TasksList = () => {
     const { identity } = useGetIdentity();
 
-    // get all the contacts for this sales
     const { data: contacts, isPending: contactsLoading } = useGetList<Contact>(
         'contacts',
         {
@@ -27,29 +33,115 @@ export const TasksList = () => {
         { enabled: !!identity }
     );
 
-    // get the first 100 upcoming tasks for these contacts
-    const { data: tasks, isPending: tasksLoading } = useGetList(
+    const {
+        data: overdueTasks,
+        total: overdueTotal,
+        isPending: overdueTasksLoading,
+    } = useGetList(
         'tasks',
         {
             pagination: { page: 1, perPage: 100 },
             sort: { field: 'due_date', order: 'ASC' },
             filter: {
                 done_date: undefined,
+                due_date_lt: startOfTodayDateISO,
                 contact_id: contacts?.map(contact => contact.id),
             },
         },
         { enabled: !!contacts }
     );
 
-    const isPending = tasksLoading || contactsLoading;
+    const {
+        data: todayTasks,
+        total: todayTotal,
+        isPending: todayTasksLoading,
+    } = useGetList(
+        'tasks',
+        {
+            pagination: { page: 1, perPage: 100 },
+            sort: { field: 'due_date', order: 'ASC' },
+            filter: {
+                done_date: undefined,
+                due_date_gte: startOfTodayDateISO,
+                due_date_lte: endOfTodayDateISO,
+                contact_id: contacts?.map(contact => contact.id),
+            },
+        },
+        { enabled: !!contacts }
+    );
 
-    // limit to 10 tasks and provide the list context
-    const listContext = useList({
-        data: tasks,
+    const {
+        data: thisWeekTasks,
+        total: thisWeekTotal,
+        isPending: thisWeekTasksLoading,
+    } = useGetList(
+        'tasks',
+        {
+            pagination: { page: 1, perPage: 100 },
+            sort: { field: 'due_date', order: 'ASC' },
+            filter: {
+                done_date: undefined,
+                due_date_gte: startOfWeekDateISO,
+                due_date_lte: endOfWeekDateISO,
+                contact_id: contacts?.map(contact => contact.id),
+            },
+        },
+        { enabled: !!contacts }
+    );
+
+    const {
+        data: laterTasks,
+        total: laterTotal,
+        isPending: laterTasksLoading,
+    } = useGetList(
+        'tasks',
+        {
+            pagination: { page: 1, perPage: 100 },
+            sort: { field: 'due_date', order: 'ASC' },
+            filter: {
+                done_date: undefined,
+                due_date_gt: endOfWeekDateISO,
+                contact_id: contacts?.map(contact => contact.id),
+            },
+        },
+        { enabled: !!contacts }
+    );
+
+    const isPending =
+        overdueTasksLoading ||
+        todayTasksLoading ||
+        thisWeekTasksLoading ||
+        laterTasksLoading ||
+        contactsLoading;
+
+    const listOverdueContext = useList({
+        data: overdueTasks,
         isPending,
         resource: 'tasks',
-        perPage: 10,
+        perPage: 5,
     });
+
+    const listTodayContext = useList({
+        data: todayTasks,
+        isPending,
+        resource: 'tasks',
+        perPage: 5,
+    });
+
+    const listThisWeekContext = useList({
+        data: thisWeekTasks,
+        isPending,
+        resource: 'tasks',
+        perPage: 5,
+    });
+
+    const listLaterContext = useList({
+        data: laterTasks,
+        isPending,
+        resource: 'tasks',
+        perPage: 5,
+    });
+
     return (
         <>
             <Box display="flex" alignItems="center" marginBottom="1em">
@@ -67,21 +159,134 @@ export const TasksList = () => {
             </Box>
             <Card sx={{ px: 2, mb: '2em' }}>
                 <AddTask selectContact />
-                <ResourceContextProvider value="tasks">
-                    <ListContextProvider value={listContext}>
-                        <TasksIterator showContact />
-                    </ListContextProvider>
-                </ResourceContextProvider>
-                {!isPending && (
-                    <Button
-                        onClick={() =>
-                            listContext.setPerPage(listContext.perPage + 10)
-                        }
-                        fullWidth
-                    >
-                        Load more
-                    </Button>
-                )}
+                <Stack gap={3} mt={2}>
+                    <Stack>
+                        <Typography variant="body1" fontWeight="bold">
+                            Overdue
+                        </Typography>
+                        <ResourceContextProvider value="tasks">
+                            <ListContextProvider value={listOverdueContext}>
+                                <TasksIterator showContact />
+                            </ListContextProvider>
+                        </ResourceContextProvider>
+                        {!isPending &&
+                            overdueTotal &&
+                            overdueTotal > listOverdueContext.perPage && (
+                                <Stack
+                                    justifyContent="flex-end"
+                                    direction="row"
+                                >
+                                    <Typography
+                                        onClick={() =>
+                                            listOverdueContext.setPerPage(
+                                                listOverdueContext.perPage + 10
+                                            )
+                                        }
+                                        variant="caption"
+                                        sx={{ cursor: 'pointer' }}
+                                        color="text.primary"
+                                    >
+                                        5 more
+                                    </Typography>
+                                </Stack>
+                            )}
+                    </Stack>
+
+                    <Stack>
+                        <Typography variant="body1" fontWeight="bold">
+                            Today
+                        </Typography>
+                        <ResourceContextProvider value="tasks">
+                            <ListContextProvider value={listTodayContext}>
+                                <TasksIterator showContact />
+                            </ListContextProvider>
+                        </ResourceContextProvider>
+                        {!isPending &&
+                            todayTotal &&
+                            todayTotal > listTodayContext.perPage && (
+                                <Stack
+                                    justifyContent="flex-end"
+                                    direction="row"
+                                >
+                                    <Typography
+                                        onClick={() =>
+                                            listTodayContext.setPerPage(
+                                                listTodayContext.perPage + 10
+                                            )
+                                        }
+                                        variant="caption"
+                                        sx={{ cursor: 'pointer' }}
+                                        color="text.primary"
+                                    >
+                                        5 more
+                                    </Typography>
+                                </Stack>
+                            )}
+                    </Stack>
+                    <Stack>
+                        <Typography variant="body1" fontWeight="bold">
+                            This week
+                        </Typography>
+                        <ResourceContextProvider value="tasks">
+                            <ListContextProvider value={listThisWeekContext}>
+                                <TasksIterator showContact />
+                            </ListContextProvider>
+                        </ResourceContextProvider>
+                        {!isPending &&
+                            thisWeekTotal &&
+                            thisWeekTotal > listThisWeekContext.perPage && (
+                                <Stack
+                                    justifyContent="flex-end"
+                                    direction="row"
+                                >
+                                    <Typography
+                                        onClick={() =>
+                                            listThisWeekContext.setPerPage(
+                                                listThisWeekContext.perPage + 10
+                                            )
+                                        }
+                                        variant="caption"
+                                        sx={{ cursor: 'pointer' }}
+                                        color="text.primary"
+                                    >
+                                        5 more
+                                    </Typography>
+                                </Stack>
+                            )}
+                    </Stack>
+
+                    <Stack>
+                        <Typography variant="body1" fontWeight="bold">
+                            Later
+                        </Typography>
+                        <ResourceContextProvider value="tasks">
+                            <ListContextProvider value={listLaterContext}>
+                                <TasksIterator showContact />
+                            </ListContextProvider>
+                        </ResourceContextProvider>
+                        {!isPending &&
+                            laterTotal &&
+                            laterTotal > listLaterContext.perPage && (
+                                <Stack
+                                    justifyContent="flex-end"
+                                    direction="row"
+                                >
+                                    <Typography
+                                        onClick={() =>
+                                            listLaterContext.setPerPage(
+                                                listLaterContext.perPage + 10
+                                            )
+                                        }
+                                        variant="caption"
+                                        color="text.primary"
+                                        sx={{ cursor: 'pointer' }}
+                                    >
+                                        5 more
+                                    </Typography>
+                                </Stack>
+                            )}
+                    </Stack>
+                </Stack>
             </Card>
         </>
     );
