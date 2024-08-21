6472e3af0a8b06e3168a19f3b30115b8b4c6f7f2
François Zaninotto <fzaninotto@gmail.com>
2024-08-21 13:58:33 +0200

    [no ci] [CRM] Improve welcome card

--- examples/crm/src/dashboard/Welcome.tsx
+++ examples/crm/src/dashboard/Welcome.tsx
@@ -1,65 +1,33 @@
-import CodeIcon from '@mui/icons-material/Code';
-import HomeIcon from '@mui/icons-material/Home';
-import {
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    Typography,
-} from '@mui/material';
+import { Card, CardContent, Link, Typography } from '@mui/material';
 
 export const Welcome = () => (
     <Card
         sx={{
             background: `#c5dedd`,
             color: 'rgba(0, 0, 0, 0.87)',
-            padding: '1em',
-            [`& .MuiCardActions-root`]: {
-                p: 2,
-                mt: -2,
-                mb: -1,
-                flexDirection: 'column',
-                '& a': {
-                    mb: 1,
-                    color: 'rgba(0, 0, 0, 0.87)',
-                    backgroundColor: 'white',
-                    marginLeft: '0 !important',
-                },
-            },
         }}
     >
         <CardContent>
-            <Typography variant="h5" gutterBottom>
-                CRM Demo
+            <Typography variant="h6" gutterBottom>
+                Your CRM Starter Kit
             </Typography>
             <Typography variant="body2" gutterBottom>
-                This app runs in the browser, and relies on a mock REST API.
-                Feel free to explore and modify the data - it's local to your
-                computer, and will reset each time you reload.
+                This demo is a template you can use to kickstart your own CRM.
             </Typography>
             <Typography variant="body2" gutterBottom>
-                It was built using react-admin, an open-source framework. The
-                code for this demo is also open-source. Reading it is a great
-                way to learn about react-admin!
+                Feel free to explore and modify the data—it’s stored locally and
+                resets on reload. While this demo uses a mock API, the full
+                version uses Supabase for the backend.
+            </Typography>
+            <Typography variant="body2">
+                Built with{' '}
+                <Link href="https://marmelab.com/react-admin">react-admin</Link>
+                , Atomic CRM is fully open-source. Checkout the code at{' '}
+                <Link href="https://github.com/marmelab/atomic-crm">
+                    marmelab/atomic-crm
+                </Link>
+                .
             </Typography>
         </CardContent>
-        <CardActions>
-            <Button
-                variant="contained"
-                fullWidth
-                href="https://marmelab.com/react-admin"
-                startIcon={<HomeIcon />}
-            >
-                React-admin site
-            </Button>
-            <Button
-                variant="contained"
-                fullWidth
-                href="https://github.com/marmelab/react-admin/tree/master/examples/crm"
-                startIcon={<CodeIcon />}
-            >
-                Source of this demo
-            </Button>
-        </CardActions>
     </Card>
 );
