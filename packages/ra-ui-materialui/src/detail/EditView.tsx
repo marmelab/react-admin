161c8594452f55e5ad8d997b60b1bf2e725faa1f
thiery <thiery@marmelab.com>
2024-08-29 10:02:35 +0200

    update EditView

--- packages/ra-ui-materialui/src/detail/EditView.tsx
+++ packages/ra-ui-materialui/src/detail/EditView.tsx
@@ -1,13 +1,72 @@
 import * as React from 'react';
 import { ReactElement, ElementType } from 'react';
-import { Card, CardContent, styled, SxProps } from '@mui/material';
+import { Box, Button, Card, CardContent, styled, SxProps } from '@mui/material';
 import clsx from 'clsx';
-import { useEditContext, useResourceDefinition } from 'ra-core';
+import {
+    useEditContext,
+    useResourceDefinition,
+    useCanAccess,
+    useTranslate,
+} from 'ra-core';
+import LockIcon from '@mui/icons-material/Lock';
 
 import { EditActions } from './EditActions';
 import { Title } from '../layout';
+import { History } from '@mui/icons-material';
 
 const defaultActions = <EditActions />;
+
+function goBack() {
+    window.history.go(-1);
+}
+
+const DefaultUnauthorizedView = () => {
+    const translate = useTranslate();
+    return (
+        <Box
+            sx={theme => ({
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                [theme.breakpoints.down('md')]: {
+                    padding: '1em',
+                },
+                fontFamily: 'Roboto, sans-serif',
+                opacity: 0.5,
+                '& h1': {
+                    display: 'flex',
+                    alignItems: 'center',
+                },
+            })}
+        >
+            <h1 role="alert">
+                <LockIcon
+                    sx={{
+                        width: '2em',
+                        height: '2em',
+                        marginRight: '0.5em',
+                    }}
+                />
+                {translate('ra-rbac.page.unauthorized')}
+            </h1>
+            <div>{translate('ra-rbac.message.unauthorized')}</div>
+            <Box
+                sx={{
+                    marginTop: '2em',
+                }}
+            >
+                <Button
+                    variant="contained"
+                    startIcon={<History />}
+                    onClick={goBack}
+                >
+                    {translate('ra.action.back')}
+                </Button>
+            </Box>
+        </Box>
+    );
+};
 
 export const EditView = (props: EditViewProps) => {
     const {
@@ -17,16 +76,26 @@ export const EditView = (props: EditViewProps) => {
         className,
         component: Content = Card,
         title,
+        unauthorizedView = <DefaultUnauthorizedView />,
         ...rest
     } = props;
 
     const { hasShow } = useResourceDefinition();
     const { resource, defaultTitle, record } = useEditContext();
+    const { isAccessible, isPending } = useCanAccess({
+        resource,
+        action: 'edit',
+        record,
+    });
 
     const finalActions =
         typeof actions === 'undefined' && hasShow ? defaultActions : actions;
-    if (!children) {
+    if (!children || isPending) {
         return null;
+    }
+
+    if (!isAccessible) {
+        return unauthorizedView;
     }
 
     return (
@@ -60,6 +129,7 @@ export interface EditViewProps
     component?: ElementType;
     title?: string | ReactElement | false;
     sx?: SxProps;
+    unauthorizedView?: ReactElement;
 }
 
 const PREFIX = 'RaEdit';
