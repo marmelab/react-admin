9c0f8a3e20969b1564833bc0d3177610889813e8
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-09-03 16:25:45 +0200

    Stories and tests for useExporter

--- packages/ra-core/src/export/useExporter.spec.tsx
+++ packages/ra-core/src/export/useExporter.spec.tsx
@@ -1,62 +1,19 @@
 import * as React from 'react';
 import expect from 'expect';
-import { waitFor, render } from '@testing-library/react';
-import { CoreAdminContext } from '../core/CoreAdminContext';
+import { waitFor, render, fireEvent, screen } from '@testing-library/react';
 
-import useExporter, { getAllKeys } from './useExporter';
-import { Exporter } from '../types';
-
-const UseExporter = ({
-    customExporter,
-    records,
-    resource,
-}: {
-    customExporter: Exporter;
-    records: Record<string, unknown>[];
-    resource: string;
-}) => {
-    const exporter = useExporter({ exporter: customExporter });
-    React.useEffect(() => {
-        exporter(records, null, null, resource);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return null;
-};
+import { getAllKeys } from './useExporter';
+import {
+    AccessControl,
+    AccessControlWithoutAuthProvider,
+    AccessControlWithoutAuthProviderCanAccess,
+} from './useExporter.stories';
 
 describe('useExporter', () => {
-    it('should call given customExporter with keys deemed accesible by authProvider.canAccess', async () => {
+    it('should call given customExporter with keys deemed accessible by authProvider.canAccess', async () => {
         const customExporter = jest.fn();
-        const canAccess = jest.fn().mockImplementation(
-            async ({ resource }) => resource !== 'posts.id' // omitting id
-        );
-        const authProvider = {
-            login: () => Promise.reject('bad method'),
-            logout: () => Promise.reject('bad method'),
-            checkAuth: () => Promise.reject('bad method'),
-            checkError: () => Promise.reject('bad method'),
-            getPermissions: () => Promise.reject('bad method'),
-            canAccess,
-        };
-        render(
-            <CoreAdminContext authProvider={authProvider}>
-                <UseExporter
-                    customExporter={customExporter}
-                    records={[
-                        {
-                            id: 1,
-                            title: 'How to implement access control',
-                        },
-                        {
-                            id: 2,
-                            title: 'How to test access control',
-                            author: 'John Doe',
-                        },
-                    ]}
-                    resource="posts"
-                />
-            </CoreAdminContext>
-        );
+        render(<AccessControl customExporter={customExporter} />);
+        fireEvent.click(await screen.findByText('Export'));
 
         await waitFor(() => {
             expect(customExporter).toBeCalledTimes(1);
@@ -76,43 +33,35 @@ describe('useExporter', () => {
             'posts'
         );
 
-        expect(canAccess).toBeCalledTimes(3);
-        expect(canAccess).toBeCalledWith({
-            action: 'read',
-            resource: 'posts.id',
+        fireEvent.click(await screen.findByLabelText('posts.id access'));
+        fireEvent.click(await screen.findByText('Export'));
+        await waitFor(() => {
+            expect(customExporter).toBeCalledTimes(2);
         });
-        expect(canAccess).toBeCalledWith({
-            action: 'read',
-            resource: 'posts.title',
-        });
-        expect(canAccess).toBeCalledWith({
-            action: 'read',
-            resource: 'posts.author',
-        });
+        expect(customExporter).toHaveBeenCalledWith(
+            [
+                {
+                    id: 1,
+                    title: 'How to implement access control',
+                },
+                {
+                    id: 2,
+                    title: 'How to test access control',
+                    author: 'John Doe',
+                },
+            ],
+            null,
+            null,
+            'posts'
+        );
     });
 
     it('should call given customExporter with all keys when no authProvider', async () => {
         const customExporter = jest.fn();
         render(
-            <CoreAdminContext>
-                <UseExporter
-                    customExporter={customExporter}
-                    records={[
-                        {
-                            id: 1,
-                            title: 'How to implement access control',
-                        },
-                        {
-                            id: 2,
-                            title: 'How to test access control',
-                            author: 'John Doe',
-                        },
-                    ]}
-                    resource="posts"
-                />
-            </CoreAdminContext>
+            <AccessControlWithoutAuthProvider customExporter={customExporter} />
         );
-
+        fireEvent.click(await screen.findByText('Export'));
         await waitFor(() => {
             expect(customExporter).toBeCalledTimes(1);
         });
@@ -135,33 +84,12 @@ describe('useExporter', () => {
     });
     it('should call given customExporter with all keys when no authProvider.canAccess', async () => {
         const customExporter = jest.fn();
-        const authProvider = {
-            login: () => Promise.reject('bad method'),
-            logout: () => Promise.reject('bad method'),
-            checkAuth: () => Promise.reject('bad method'),
-            checkError: () => Promise.reject('bad method'),
-            getPermissions: () => Promise.reject('bad method'),
-        };
         render(
-            <CoreAdminContext authProvider={authProvider}>
-                <UseExporter
-                    customExporter={customExporter}
-                    records={[
-                        {
-                            id: 1,
-                            title: 'How to implement access control',
-                        },
-                        {
-                            id: 2,
-                            title: 'How to test access control',
-                            author: 'John Doe',
-                        },
-                    ]}
-                    resource="posts"
-                />
-            </CoreAdminContext>
+            <AccessControlWithoutAuthProviderCanAccess
+                customExporter={customExporter}
+            />
         );
-
+        fireEvent.click(await screen.findByText('Export'));
         await waitFor(() => {
             expect(customExporter).toBeCalledTimes(1);
         });
---
+++ packages/ra-core/src/export/useExporter.stories.tsx
@@ -0,0 +1,267 @@
+import React from 'react';
+import { QueryClient } from '@tanstack/react-query';
+import { AuthProvider, Exporter } from '../types';
+import { CoreAdminContext, defaultExporter, TestMemoryRouter } from '..';
+import useExporter from './useExporter';
+
+export default {
+    title: 'ra-core/export/useExporter',
+};
+
+const AccessControlUI = ({
+    children,
+    setAuthorizedResources,
+    authorizedResources,
+    queryClient,
+}: {
+    children: React.ReactNode;
+    setAuthorizedResources: Function;
+    authorizedResources: {
+        posts: boolean;
+        'posts.id': boolean;
+        'posts.title': boolean;
+        'posts.author': boolean;
+    };
+    queryClient: QueryClient;
+}) => {
+    return (
+        <div>
+            <div>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={authorizedResources.posts}
+                        onChange={() => {
+                            setAuthorizedResources(state => ({
+                                ...state,
+                                posts: !authorizedResources.posts,
+                            }));
+                            queryClient.clear();
+                        }}
+                    />
+                    posts access
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={authorizedResources['posts.id']}
+                        onChange={() => {
+                            setAuthorizedResources(state => ({
+                                ...state,
+                                'posts.id': !authorizedResources['posts.id'],
+                            }));
+
+                            queryClient.clear();
+                        }}
+                    />
+                    posts.id access
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={authorizedResources['posts.title']}
+                        onChange={() => {
+                            setAuthorizedResources(state => ({
+                                ...state,
+                                'posts.title':
+                                    !authorizedResources['posts.title'],
+                            }));
+
+                            queryClient.clear();
+                        }}
+                    />
+                    posts.title access
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={authorizedResources['posts.author']}
+                        onChange={() => {
+                            setAuthorizedResources(state => ({
+                                ...state,
+                                'posts.author':
+                                    !authorizedResources['posts.author'],
+                            }));
+
+                            queryClient.clear();
+                        }}
+                    />
+                    posts.author access
+                </label>
+            </div>
+            <div>{children}</div>
+        </div>
+    );
+};
+
+export const AccessControl = ({
+    customExporter = defaultExporter,
+    initialAuthorizedResources = {
+        posts: true,
+        'posts.id': false,
+        'posts.title': true,
+        'posts.author': true,
+    },
+}: {
+    customExporter?: Exporter;
+    initialAuthorizedResources?: {
+        posts: boolean;
+        'posts.id': boolean;
+        'posts.title': boolean;
+        'posts.author': boolean;
+    };
+}) => {
+    const queryClient = new QueryClient();
+
+    return (
+        <TestMemoryRouter initialEntries={['/books']}>
+            <AdminWithAccessControl
+                customExporter={customExporter}
+                queryClient={queryClient}
+                initialAuthorizedResources={initialAuthorizedResources}
+            />
+        </TestMemoryRouter>
+    );
+};
+
+export const AccessControlWithoutAuthProvider = ({
+    customExporter,
+}: {
+    customExporter: Exporter;
+}) => (
+    <CoreAdminContext>
+        <UseExporter
+            customExporter={customExporter}
+            records={[
+                {
+                    id: 1,
+                    title: 'How to implement access control',
+                },
+                {
+                    id: 2,
+                    title: 'How to test access control',
+                    author: 'John Doe',
+                },
+            ]}
+            resource="posts"
+        />
+    </CoreAdminContext>
+);
+
+export const AccessControlWithoutAuthProviderCanAccess = ({
+    customExporter,
+}: {
+    customExporter: Exporter;
+}) => (
+    <CoreAdminContext
+        authProvider={{
+            checkAuth: () => Promise.resolve(),
+            login: () => Promise.resolve(),
+            logout: () => Promise.resolve(),
+            checkError: () => Promise.resolve(),
+            getPermissions: () => Promise.resolve(),
+        }}
+    >
+        <UseExporter
+            customExporter={customExporter}
+            records={[
+                {
+                    id: 1,
+                    title: 'How to implement access control',
+                },
+                {
+                    id: 2,
+                    title: 'How to test access control',
+                    author: 'John Doe',
+                },
+            ]}
+            resource="posts"
+        />
+    </CoreAdminContext>
+);
+
+const AdminWithAccessControl = ({
+    customExporter,
+    queryClient,
+    initialAuthorizedResources,
+}: {
+    customExporter: Exporter;
+    queryClient: QueryClient;
+    initialAuthorizedResources: {
+        posts: boolean;
+        'posts.id': boolean;
+        'posts.title': boolean;
+        'posts.author': boolean;
+    };
+}) => {
+    const [authorizedResources, setAuthorizedResources] = React.useState(
+        initialAuthorizedResources
+    );
+
+    const authProvider: AuthProvider = {
+        canAccess: async ({ resource }) => {
+            return new Promise(resolve =>
+                setTimeout(resolve, 100, authorizedResources[resource])
+            );
+        },
+        logout: () => Promise.reject(new Error('Not implemented')),
+        checkError: () => Promise.reject(new Error('Not implemented')),
+        checkAuth: () => Promise.resolve(),
+        getPermissions: () => Promise.reject(new Error('Not implemented')),
+        login: () => Promise.reject(new Error('Not implemented')),
+    };
+    return (
+        <CoreAdminContext queryClient={queryClient} authProvider={authProvider}>
+            <AccessControlUI
+                authorizedResources={authorizedResources}
+                setAuthorizedResources={setAuthorizedResources}
+                queryClient={queryClient}
+            >
+                <UseExporter
+                    customExporter={customExporter}
+                    records={[
+                        {
+                            id: 1,
+                            title: 'How to implement access control',
+                        },
+                        {
+                            id: 2,
+                            title: 'How to test access control',
+                            author: 'John Doe',
+                        },
+                    ]}
+                    resource="posts"
+                />
+            </AccessControlUI>
+        </CoreAdminContext>
+    );
+};
+
+const UseExporter = ({
+    customExporter,
+    records,
+    resource,
+}: {
+    customExporter: Exporter;
+    records: Record<string, unknown>[];
+    resource: string;
+}) => {
+    const exporter = useExporter({ exporter: customExporter });
+
+    return (
+        <button
+            onClick={() => {
+                if (!exporter) {
+                    return;
+                }
+                exporter(records, null, null, resource);
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+            }}
+        >
+            Export
+        </button>
+    );
+};
