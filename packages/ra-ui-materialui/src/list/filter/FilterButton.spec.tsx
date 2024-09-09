e7be9cc0ee1c9e836968f0c14f912af576487e68
erwanMarmelab <erwan@marmelab.com>
2024-09-09 11:19:24 +0200

    remove useless import

--- packages/ra-ui-materialui/src/list/filter/FilterButton.spec.tsx
+++ packages/ra-ui-materialui/src/list/filter/FilterButton.spec.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import expect from 'expect';
-import {
-    render,
-    fireEvent,
-    screen,
-    waitFor,
-    within,
-} from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createTheme } from '@mui/material/styles';
 import {
