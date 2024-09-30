fb57816ca31b3d2a0534ffe6f9c51fb6cd051be4
fzaninotto <fzaninotto@gmail.com>
2024-09-30 11:50:01 +0200

    Fix List empty component wrongly appears when using partial pagination
    Closes #10240

--- packages/ra-ui-materialui/src/list/ListView.tsx
+++ packages/ra-ui-materialui/src/list/ListView.tsx
@@ -67,13 +67,16 @@ export const ListView = <RecordType extends RaRecord = any>(
         empty !== false && <div className={ListClasses.noResults}>{empty}</div>;
 
     const shouldRenderEmptyPage =
+        !error &&
         // the list is not loading data for the first time
         !isPending &&
         // the API returned no data (using either normal or partial pagination)
         (total === 0 ||
             (total == null &&
                 hasPreviousPage === false &&
-                hasNextPage === false)) &&
+                hasNextPage === false &&
+                // @ts-ignore FIXME total my be undefined when using partial pagination
+                data.length === 0)) &&
         // the user didn't set any filters
         !Object.keys(filterValues).length &&
         // there is an empty page component
