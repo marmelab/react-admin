61b14c86ca37669cd61d1054e06d25bbcbce145d
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2024-10-03 12:34:00 +0200

    Export NavigateToFirstResource

--- packages/ra-core/src/core/index.ts
+++ packages/ra-core/src/core/index.ts
@@ -5,6 +5,7 @@ export * from './CoreAdminUI';
 export * from './CustomRoutes';
 export * from './DefaultTitleContext';
 export * from './HasDashboardContext';
+export * from './NavigateToFirstResource';
 export * from './OptionalResourceContextProvider';
 export * from './Resource';
 export * from './ResourceContext';
