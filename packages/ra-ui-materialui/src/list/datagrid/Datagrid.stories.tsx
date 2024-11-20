4cd44ea2eff9da83c623cb6c2da9df6b3309519f
erwanMarmelab <erwan@marmelab.com>
2024-11-20 22:40:41 +0100

    add more data in the story to be able to test the "SELECT ALL" button

--- packages/ra-ui-materialui/src/list/datagrid/Datagrid.stories.tsx
+++ packages/ra-ui-materialui/src/list/datagrid/Datagrid.stories.tsx
@@ -58,6 +58,54 @@ const data = [
         author: 'Antoine de Saint-Exupéry',
         year: 1943,
     },
+    {
+        id: 5,
+        title: 'Les Misérables',
+        author: 'Victor Hugo',
+        year: 1862,
+    },
+    {
+        id: 6,
+        title: 'Brave New World',
+        author: 'Aldous Huxley',
+        year: 1932,
+    },
+    {
+        id: 7,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        year: 1925,
+    },
+    {
+        id: 8,
+        title: 'One Hundred Years of Solitude',
+        author: 'Gabriel García Márquez',
+        year: 1967,
+    },
+    {
+        id: 9,
+        title: "Harry Potter and the Philosopher's Stone",
+        author: 'J.K. Rowling',
+        year: 1997,
+    },
+    {
+        id: 10,
+        title: 'The Catcher in the Rye',
+        author: 'J.D. Salinger',
+        year: 1951,
+    },
+    {
+        id: 11,
+        title: 'The Lord of the Rings',
+        author: 'J.R.R. Tolkien',
+        year: 1954,
+    },
+    {
+        id: 12,
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        year: 1937,
+    },
 ];
 
 const theme = createTheme();
