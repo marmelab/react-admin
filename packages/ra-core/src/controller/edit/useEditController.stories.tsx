db4823ba46dcd7d419e2bc075e6fbec78eb776f5
Gildas <1122076+djhi@users.noreply.github.com>
2024-12-12 15:23:52 +0100

    Add story for edition

--- packages/ra-core/src/controller/edit/useEditController.stories.tsx
+++ packages/ra-core/src/controller/edit/useEditController.stories.tsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 import { Route, Routes, useLocation } from 'react-router';
 import {
     CoreAdminContext,
+    EditBase,
     EditController,
+    Form,
+    InputProps,
     testDataProvider,
     TestMemoryRouter,
+    useInput,
 } from '../..';
 
 export default {
@@ -75,11 +79,93 @@ export const EncodedIdWithPercentage = ({
     );
 };
 
-const LocationInspector = () => {
+export const OverrideRecordWithLocation = ({
+    url = '/posts/1',
+    dataProvider = testDataProvider({
+        // @ts-expect-error
+        getOne: () =>
+            Promise.resolve({
+                data: { id: 1, title: 'hello', value: 'a value' },
+            }),
+    }),
+}) => {
+    return (
+        <TestMemoryRouter key={url} initialEntries={[url]}>
+            <CoreAdminContext dataProvider={dataProvider}>
+                <Routes>
+                    <Route
+                        path="/posts/:id"
+                        element={
+                            <EditBase resource="posts">
+                                <div
+                                    style={{
+                                        padding: '1rem',
+                                    }}
+                                >
+                                    <LocationInspector deep />
+                                    <Form>
+                                        <div
+                                            style={{
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                                gap: '1rem',
+                                            }}
+                                        >
+                                            <TextInput source="title" />
+                                            <TextInput source="value" />
+                                        </div>
+                                    </Form>
+                                </div>
+                            </EditBase>
+                        }
+                    />
+                </Routes>
+            </CoreAdminContext>
+        </TestMemoryRouter>
+    );
+};
+
+OverrideRecordWithLocation.argTypes = {
+    url: {
+        options: [
+            'unmodified',
+            'modified with location state',
+            'modified with location search',
+        ],
+        mapping: {
+            unmodified: '/posts/1',
+            'modified with location state': {
+                pathname: '/posts/1',
+                state: { record: { value: 'from-state' } },
+            },
+            'modified with location search': `/posts/1?source=${encodeURIComponent(JSON.stringify({ value: 'from-search' }))}`,
+        },
+        control: { type: 'select' },
+    },
+};
+
+const LocationInspector = ({ deep }: { deep?: boolean }) => {
     const location = useLocation();
     return (
         <p>
-            Location: <code>{location.pathname}</code>
+            Location:{' '}
+            <code>{deep ? JSON.stringify(location) : location.pathname}</code>
         </p>
+    );
+};
+
+const TextInput = (props: InputProps) => {
+    const input = useInput(props);
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <label htmlFor={input.id}>{props.source}</label>
+            <input id={input.id} {...input.field} />
+        </div>
     );
 };
