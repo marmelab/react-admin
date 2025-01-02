0a2399a5fdfc5395d27c8ff968cc3a4b27c0648a
erwanMarmelab <erwan@marmelab.com>
2025-01-02 15:56:45 +0100

    wrap tests in some `describe`

--- packages/ra-ui-materialui/src/button/SelectAllButton.spec.tsx
+++ packages/ra-ui-materialui/src/button/SelectAllButton.spec.tsx
@@ -32,41 +32,49 @@ describe('<SelectAllButton />', () => {
         await screen.findByText('17 items selected');
     });
 
-    it('should render a button with a custom label', async () => {
-        render(<Label />);
-        await screen.findByText('War and Peace');
-        fireEvent.click(screen.getAllByRole('checkbox')[0]);
-        await screen.findByText('10 items selected');
-        await screen.findByRole('button', { name: 'Select all books' });
+    describe('label', () => {
+        it('should allow to customize the label', async () => {
+            render(<Label />);
+            await screen.findByText('War and Peace');
+            fireEvent.click(screen.getAllByRole('checkbox')[0]);
+            await screen.findByText('10 items selected');
+            await screen.findByRole('button', { name: 'Select all books' });
+        });
     });
 
-    it('should not select more records than the limit', async () => {
-        render(<Limit />);
-        await screen.findByText('War and Peace');
-        fireEvent.click(screen.getAllByRole('checkbox')[0]);
-        await screen.findByText('10 items selected');
-        fireEvent.click(
-            screen.getByRole('button', { name: 'Select all books (max 15)' })
-        );
-        await screen.findByText(
-            'There are too many elements to select them all. Only the first 15 elements were selected.'
-        );
-        await screen.findByText('15 items selected');
-    });
+    describe('limit', () => {
+        it('should not select more records than the limit', async () => {
+            render(<Limit />);
+            await screen.findByText('War and Peace');
+            fireEvent.click(screen.getAllByRole('checkbox')[0]);
+            await screen.findByText('10 items selected');
+            fireEvent.click(
+                screen.getByRole('button', {
+                    name: 'Select all books (max 15)',
+                })
+            );
+            await screen.findByText(
+                'There are too many elements to select them all. Only the first 15 elements were selected.'
+            );
+            await screen.findByText('15 items selected');
+        });
 
-    it('should not render if we select more items than the limit', async () => {
-        render(<Limit />);
-        await screen.findByText('War and Peace');
-        fireEvent.click(screen.getAllByRole('checkbox')[0]);
-        await screen.findByText('10 items selected');
-        fireEvent.click(
-            screen.getByRole('button', { name: 'Go to next page' })
-        );
-        await screen.findByText('Ulysses');
-        fireEvent.click(screen.getAllByRole('checkbox')[0]);
-        await screen.findByText('17 items selected');
-        expect(
-            screen.queryByRole('button', { name: 'Select all books (max 15)' })
-        ).toBeNull();
+        it('should not render if we select more items than the limit', async () => {
+            render(<Limit />);
+            await screen.findByText('War and Peace');
+            fireEvent.click(screen.getAllByRole('checkbox')[0]);
+            await screen.findByText('10 items selected');
+            fireEvent.click(
+                screen.getByRole('button', { name: 'Go to next page' })
+            );
+            await screen.findByText('Ulysses');
+            fireEvent.click(screen.getAllByRole('checkbox')[0]);
+            await screen.findByText('17 items selected');
+            expect(
+                screen.queryByRole('button', {
+                    name: 'Select all books (max 15)',
+                })
+            ).toBeNull();
+        });
     });
 });
