3727b5990423fa02a09672b38efd448abc54ce50
Gildas <1122076+djhi@users.noreply.github.com>
2025-01-10 12:09:54 +0100

    Fix output

--- scripts/check-documentation-videos-format.sh
+++ scripts/check-documentation-videos-format.sh
@@ -1,9 +1,11 @@
 for file in ./docs/img/**; do
   codec=$(mediainfo --Inform="Video;%CodecID%" "$file")
   if [ "$codec" = "hvc1" ]; then
+    # Construct the output file name
+    output_file="${file%.*}_avc1.${file##*.}"
     echo "Invalid codec for $file"
     echo "Convert it to avc1 with:"
-    echo "ffmpeg -i $file -c:v libx264 -c:a copy ${file%.*}_avc1.${file##*.}"
+    echo "ffmpeg -i $file -c:v libx264 -c:a copy ${output_file}"
     echo "rm $file"
     echo "mv $output_file $file"
     exit 1;
