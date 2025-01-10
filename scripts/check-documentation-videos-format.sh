446a42d7ff00747891963b0ea0db9557aba9721f
Gildas <1122076+djhi@users.noreply.github.com>
2025-01-10 11:14:38 +0100

    Only output the warning

--- scripts/check-documentation-videos-format.sh
+++ scripts/check-documentation-videos-format.sh
@@ -1,12 +1,10 @@
-for file in ./docs/img/*; do
+for file in ./docs/img/**; do
   codec=$(mediainfo --Inform="Video;%CodecID%" "$file")
   if [ "$codec" = "hvc1" ]; then
-    # Construct the output file name
-    output_file="${file%.*}_avc1.${file##*.}"
-    # Convert the file to avc1 (H.264)
-    ffmpeg -i "$file" -c:v libx264 -c:a copy "$output_file"
-    rm $file
-    mv $output_file $file
-    echo "Fixed codec for $file"
+    echo "Invalid codec for $file"
+    echo "Convert it to avc1 with:"
+    echo "ffmpeg -i $file -c:v libx264 -c:a copy ${file%.*}_avc1.${file##*.}"
+    echo "rm $file"
+    echo "mv $output_file $file"
   fi
 done
