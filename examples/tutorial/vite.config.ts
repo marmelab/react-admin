9b02936be04ba88c768b60a943fa5c257aa6d8d3
Gildas <1122076+djhi@users.noreply.github.com>
2025-01-23 10:47:01 +0100

    Fix tutorial has multiple conflicting MUI packages

--- examples/tutorial/vite.config.ts
+++ examples/tutorial/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        preserveSymlinks: true,
+        alias: [
+            // The 2 next aliases are needed to avoid having multiple MUI instances
+            {
+                find: '@mui/material',
+                replacement: path.resolve(
+                    __dirname,
+                    'node_modules/@mui/material'
+                ),
+            },
+            {
+                find: '@mui/icons-material',
+                replacement: path.resolve(
+                    __dirname,
+                    'node_modules/@mui/icons-material'
+                ),
+            },
+        ],
+    },
 });
