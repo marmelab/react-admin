606ec184c1423f982d457130fc8030b6ab1206da
erwanMarmelab <erwan@marmelab.com>
2025-02-17 17:15:54 +0100

    Check videos "Chroma Subsampling" on CI check and explain how to fix a bad encoding

--- scripts/check-documentation-videos-format.sh
+++ scripts/check-documentation-videos-format.sh
@@ -1,5 +1,6 @@
 for file in ./docs/img/**; do
   codec=$(mediainfo --Inform="Video;%CodecID%" "$file")
+
   if [ "$codec" = "hvc1" ]; then
     # Construct the output file name
     output_file="${file%.*}_avc1.${file##*.}"
@@ -8,6 +9,21 @@ for file in ./docs/img/**; do
     echo "ffmpeg -i $file -c:v libx264 -c:a copy ${output_file}"
     echo "rm $file"
     echo "mv $output_file $file"
-    exit 1;
+    exit 1
   fi
+
+  imageencode=$(mediainfo --Inform="Video;%ChromaSubsampling%" "$file")
+
+  if [ "$imageencode" != "4:2:0" ]; then
+    # Construct the output file name
+    output_file="${file%.*}_420.${file##*.}"
+    echo "Invalid ChromaSubsampling for $file"
+    echo "Convert it to yuv420p with:"
+    echo "ffmpeg -i ${file} -c:v libx264 -pix_fmt yuv420p -c:a copy ${output_file}"
+    echo "rm $file"
+    echo "mv $output_file $file"
+    exit 1
+  fi
+
+
 done
