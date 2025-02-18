032a4f337807924ae77653b72eba343a6affa920
erwanMarmelab <erwan@marmelab.com>
2025-02-18 14:12:28 +0100

    finalize checkFormat script

--- scripts/check-documentation-videos-format.sh
+++ scripts/check-documentation-videos-format.sh
@@ -1,29 +1,42 @@
-for file in ./docs/img/**; do
-  codec=$(mediainfo --Inform="Video;%CodecID%" "$file")
+checkCodecId() {
+  codec=$(mediainfo --Inform="Video;%CodecID%" "$1")
 
   if [ "$codec" = "hvc1" ]; then
     # Construct the output file name
-    output_file="${file%.*}_avc1.${file##*.}"
-    echo "Invalid codec for $file"
+    output_file="${1%.*}_avc1.${1##*.}"
+    echo "Invalid codec for $1"
     echo "Convert it to avc1 with:"
-    echo "ffmpeg -i $file -c:v libx264 -c:a copy ${output_file}"
-    echo "rm $file"
-    echo "mv $output_file $file"
+    echo "ffmpeg -i $1 -c:v libx264 -c:a copy ${output_file}"
+    echo "rm $1"
+    echo "mv $output_file $1"
     exit 1
   fi
 
-  imageencode=$(mediainfo --Inform="Video;%ChromaSubsampling%" "$file")
+}
+
+checkChromaSubsampling() {
+  imageencode=$(mediainfo --Inform="Video;%ChromaSubsampling%" "$1")
 
   if [ "$imageencode" != "4:2:0" ]; then
     # Construct the output file name
-    output_file="${file%.*}_420.${file##*.}"
-    echo "Invalid ChromaSubsampling for $file"
+    output_file="${1%.*}_420.${1##*.}"
+    echo "Invalid ChromaSubsampling for $1"
     echo "Convert it to yuv420p with:"
-    echo "ffmpeg -i ${file} -c:v libx264 -pix_fmt yuv420p -c:a copy ${output_file}"
-    echo "rm $file"
-    echo "mv $output_file $file"
+    echo "ffmpeg -i ${1} -c:v libx264 -pix_fmt yuv420p -c:a copy ${output_file}"
+    echo "rm $1"
+    echo "mv $output_file $1"
     exit 1
   fi
 
+}
+
+for file in ./docs/img/**; do
+  checkCodecId $file
+
+  case "$file" in
+  *.mp4) checkChromaSubsampling $file ;;
+  *.webm) checkChromaSubsampling $file ;;
+  *.gif) checkChromaSubsampling $file ;;
+  esac
 
 done
