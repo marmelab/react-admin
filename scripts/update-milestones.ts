d4a5d831bdaf8b36df8a4f40cb7772a685fcf75a
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-02-18 13:58:52 +0100

    [no ci] add update-milestones script

---
+++ scripts/update-milestones.ts
@@ -0,0 +1,111 @@
+import 'dotenv/config';
+import { Octokit } from '@octokit/core';
+import { addWeeks, addMonths, formatISO } from 'date-fns';
+
+const main = async () => {
+    if (!process.env.GITHUB_ACCESS_TOKEN) {
+        console.error(
+            'Please provide the GITHUB_ACCESS_TOKEN variable in the .env file'
+        );
+        process.exit(1);
+    }
+
+    const current_version = process.argv[2];
+
+    if (
+        !current_version ||
+        !current_version.match(/^\d{1,2}\.\d{1,2}\.\d{1,2}$/)
+    ) {
+        console.error(`Invalid version provided: ${current_version}`);
+        console.error('Usage: yarn run update-milestones <current_version>');
+        process.exit(1);
+    }
+
+    const current_version_array = current_version.split('.').map(Number);
+    const next_patch_version = [
+        current_version_array[0],
+        current_version_array[1],
+        current_version_array[2] + 1,
+    ].join('.');
+    const next_minor_version = [
+        current_version_array[0],
+        current_version_array[1] + 1,
+        0,
+    ].join('.');
+
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_ACCESS_TOKEN,
+    });
+
+    console.log(`Fetching currently open milestones`);
+    const open_milestones = await octokit.request(
+        'GET /repos/{owner}/{repo}/milestones',
+        {
+            owner: 'marmelab',
+            repo: 'react-admin',
+            state: 'open',
+        }
+    );
+
+    const current_milestone = open_milestones.data.find(
+        milestone => milestone.title === current_version
+    );
+    if (!current_milestone) {
+        console.log(`No open milestone for current version ${current_version}`);
+    } else {
+        console.log(`Closing milestone for current version ${current_version}`);
+        await octokit.request(
+            'PATCH /repos/{owner}/{repo}/milestones/{milestone_number}',
+            {
+                owner: 'marmelab',
+                repo: 'react-admin',
+                milestone_number: current_milestone.number,
+                state: 'closed',
+            }
+        );
+    }
+
+    const next_patch_milestone = open_milestones.data.find(
+        milestone => milestone.title === next_patch_version
+    );
+    if (next_patch_milestone) {
+        console.log(
+            `Milestone for next patch version ${next_patch_version} already exists`
+        );
+    } else {
+        console.log(
+            `Creating milestone for next patch version ${next_patch_version}`
+        );
+        await octokit.request('POST /repos/{owner}/{repo}/milestones', {
+            owner: 'marmelab',
+            repo: 'react-admin',
+            title: next_patch_version,
+            state: 'open',
+            due_on: formatISO(addWeeks(new Date(), 1)),
+            description: 'this is a test milestone, do not use',
+        });
+    }
+
+    const next_minor_milestone = open_milestones.data.find(
+        milestone => milestone.title === next_minor_version
+    );
+    if (next_minor_milestone) {
+        console.log(
+            `Milestone for next minor version ${next_minor_version} already exists`
+        );
+    } else {
+        console.log(
+            `Creating milestone for next minor version ${next_minor_version}`
+        );
+        await octokit.request('POST /repos/{owner}/{repo}/milestones', {
+            owner: 'marmelab',
+            repo: 'react-admin',
+            title: next_minor_version,
+            state: 'open',
+            due_on: formatISO(addMonths(new Date(), 1)),
+            description: 'this is a test milestone, do not use',
+        });
+    }
+};
+
+main();
