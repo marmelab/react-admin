8ebc4fc4197296b15d219a589eeff6e12cc7a07f
Gildas <1122076+djhi@users.noreply.github.com>
2025-02-18 17:25:31 +0100

    Remove unnecessary RouterProvider in generated app

--- packages/create-react-admin/templates/common/src/index.tsx
+++ packages/create-react-admin/templates/common/src/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router';
-import { createHashRouter } from 'react-router-dom';
 import { App } from './App';
-
-const router = createHashRouter([
-    {
-        path: '*',
-        element: <App />
-    }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 );
