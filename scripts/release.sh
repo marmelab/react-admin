bfda85420381ecd4fb37a1493022d367fac87877
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-02-21 11:53:18 +0100

    [no ci] rewrite release script without lifecycle scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,6 +11,10 @@ step() {
     info "$1"
 }
 
+if [ ! -z "$RELEASE_DRY_RUN" ]; then
+    echo "Dry run mode is enabled"
+fi
+
 info "Starting the release process"
 
 step "make install"
@@ -35,10 +39,49 @@ echo "[Minor version only] Update the dependencies to RA packages in the create-
 echo "Press Enter when this is done"
 read
 
+step "lerna version"
+./node_modules/.bin/lerna version --force-publish --no-push
+
+# Get the version from package.json
+npm_package_version=$(jq -r '.version' ./packages/react-admin/package.json)
+
+step "update-changelog"
+yarn run update-changelog ${npm_package_version}
+echo "Please review the ./CHANGELOG.md file and update it if needed."
+echo "Press Enter when this is done"
+read
 if [ -z "$RELEASE_DRY_RUN" ]; then
-    step "lerna publish"
-    ./node_modules/.bin/lerna publish --force-publish
+    echo "Committing the changelog"
+    git add CHANGELOG.md
+    git commit -m "Update changelog for version ${npm_package_version}"
+fi
+
+step "git push"
+if [ -z "$RELEASE_DRY_RUN" ]; then
+    echo "Pushing commits and tags to git"
+    git push origin HEAD
+    git push origin --tags
+else
+    echo "dry mode -- skipping git push"
+fi
+
+step "lerna publish"
+if [ -z "$RELEASE_DRY_RUN" ]; then
+    # explicitly publish packages where the latest version is not present in the registry
+    ./node_modules/.bin/lerna publish from-package
 else
     echo "dry mode -- skipping lerna publish"
 fi
 
+step "update-milestones"
+yarn run update-milestones ${npm_package_version}
+
+step "create-github-release"
+yarn run create-github-release ${npm_package_version}
+
+step "manual step: Update the documentation"
+echo "You can use the 'copy-ra-oss-docs.sh' script if you have it"
+echo "Press Enter when this is done"
+read
+
+step "The release is done! 🎉"
