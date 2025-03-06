93089d6468cef2fe4277afd1f0055a76ef24e48f
Gildas <1122076+djhi@users.noreply.github.com>
2025-03-06 16:59:38 +0100

    Add a story

--- packages/ra-core/src/routing/useRedirect.stories.tsx
+++ packages/ra-core/src/routing/useRedirect.stories.tsx
@@ -28,6 +28,11 @@ const Home = () => {
                     </button>
                 </li>
                 <li>
+                    <button onClick={() => redirect(() => '/dashboard')}>
+                        Relative url from a function
+                    </button>
+                </li>
+                <li>
                     <button onClick={() => redirect('list', 'posts')}>
                         View name
                     </button>
