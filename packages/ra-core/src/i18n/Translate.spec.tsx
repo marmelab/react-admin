d75648ec40cbcedf671d2b8891b8160a29181d8b
erwanMarmelab <erwan@marmelab.com>
2025-03-18 10:36:49 +0100

    fix tests without component

--- packages/ra-core/src/i18n/Translate.spec.tsx
+++ packages/ra-core/src/i18n/Translate.spec.tsx
@@ -14,7 +14,7 @@ import { Translate } from './Translate';
 describe('<Translate />', () => {
     it('should render the translation', () => {
         const { container } = render(<Basic />);
-        expect(container.innerHTML).toBe('<span>My Translated Key</span>');
+        expect(container.innerHTML).toBe('My Translated Key');
     });
 
     it('should render the translation event if children is set', () => {
@@ -29,26 +29,26 @@ describe('<Translate />', () => {
                 <Translate i18nKey="custom.myKey" />
             </TestTranslationProvider>
         );
-        expect(container.innerHTML).toBe('<span>My Translated Key</span>');
+        expect(container.innerHTML).toBe('My Translated Key');
     });
 
     it('should render the empty prop if no translation available', () => {
         const { container } = render(<NoTranslation />);
-        expect(container.innerHTML).toBe('<span>no translation</span>');
+        expect(container.innerHTML).toBe('no translation');
     });
 
     it('should render the children if no translation available', () => {
         const { container } = render(<NoTranslationWithChildren />);
-        expect(container.innerHTML).toBe('<span>My Key</span>');
+        expect(container.innerHTML).toBe('My Key');
     });
 
     it('should render the empty prop if no translation available', () => {
         const { container } = render(<NoTranslationWithEmpty />);
-        expect(container.innerHTML).toBe('<span>translation failed</span>');
+        expect(container.innerHTML).toBe('translation failed');
     });
 
     it('should render the translation with args', () => {
         const { container } = render(<Args />);
-        expect(container.innerHTML).toBe('<span>It cost 6.00 $</span>');
+        expect(container.innerHTML).toBe('It cost 6.00 $');
     });
 });
