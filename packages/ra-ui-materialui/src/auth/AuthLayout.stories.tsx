27420d4c071a549d3c043d28b5869ff1df532da6
Gildas <1122076+djhi@users.noreply.github.com>
2025-04-04 17:02:26 +0200

    Improve SX story

--- packages/ra-ui-materialui/src/auth/AuthLayout.stories.tsx
+++ packages/ra-ui-materialui/src/auth/AuthLayout.stories.tsx
@@ -6,7 +6,7 @@ import polyglotI18nProvider from 'ra-i18n-polyglot';
 import englishMessages from 'ra-language-english';
 import { TextInput } from '../input';
 import { AdminContext } from '../AdminContext';
-import { AuthLayout, AuthLayoutClasses } from './AuthLayout';
+import { AuthLayout } from './AuthLayout';
 import { Stack } from '@mui/system';
 
 export default { title: 'ra-ui-materialui/auth/AuthLayout' };
@@ -51,41 +51,36 @@ export const Sx = () => (
     <AdminContext i18nProvider={i18nProvider}>
         <AuthLayout
             sx={{
-                [`& .${AuthLayoutClasses.card}`]: {
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                },
+                backgroundImage:
+                    'radial-gradient(circle at 50% 14em, #bb44f0 0%, #9614d0 60%, #660094 100%)',
             }}
         >
-            <Avatar
-                sx={{
-                    margin: '1em',
-                }}
-            >
-                <LockIcon color="inherit" />
-            </Avatar>
             <CardContent>
-                <Typography gutterBottom>
-                    Please enter your username to reset your password
-                </Typography>
-                <Form>
-                    <TextInput
-                        autoFocus
-                        source="username"
-                        label="ra.auth.username"
-                        autoComplete="username"
-                        validate={required()}
-                    />
-                    <Button
-                        variant="contained"
-                        type="submit"
-                        color="primary"
-                        fullWidth
-                    >
-                        Reset my password
-                    </Button>
-                </Form>
+                <Stack alignItems="center" gap={2}>
+                    <Avatar>
+                        <LockIcon color="inherit" />
+                    </Avatar>
+                    <Typography>
+                        Please enter your username to reset your password
+                    </Typography>
+                    <Form>
+                        <TextInput
+                            autoFocus
+                            source="username"
+                            label="ra.auth.username"
+                            autoComplete="username"
+                            validate={required()}
+                        />
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            color="primary"
+                            fullWidth
+                        >
+                            Reset my password
+                        </Button>
+                    </Form>
+                </Stack>
             </CardContent>
         </AuthLayout>
     </AdminContext>
