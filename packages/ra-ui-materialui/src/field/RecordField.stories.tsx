cce833d9cd2d3c5e0c3faa04ea29dcc89e9cfb05
fzaninotto <fzaninotto@gmail.com>
2025-05-16 19:05:39 +0200

    Add RecordField component

---
+++ packages/ra-ui-materialui/src/field/RecordField.stories.tsx
@@ -0,0 +1,220 @@
+import * as React from 'react';
+import { Stack, Typography } from '@mui/material';
+import {
+    I18nContextProvider,
+    RecordContextProvider,
+    ResourceContext,
+} from 'ra-core';
+import { NumberField, TextField } from '.';
+import { RecordField } from './RecordField';
+
+export default { title: 'ra-ui-materialui/fields/RecordField' };
+
+const record = {
+    id: 1,
+    title: 'War and Peace',
+    author: 'Leo Tolstoy',
+    summary:
+        "War and Peace broadly focuses on Napoleon's invasion of Russia, and the impact it had on Tsarist society. The book explores themes such as revolution, revolution and empire, the growth and decline of various states and the impact it had on their economies, culture, and society.",
+    year: 1869,
+};
+
+export const Basic = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack gap={1} sx={{ py: 1, px: 2 }}>
+                <RecordField source="id" />
+                <RecordField source="title" />
+                <RecordField source="author" />
+                <RecordField source="summary" />
+                <RecordField source="year" field={NumberField} />
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const Field = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack>
+                <RecordField source="year" field={NumberField} />
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const Render = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack>
+                <RecordField
+                    source="author"
+                    render={record => <span>{record.author}</span>}
+                />
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const Children = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack>
+                <RecordField source="title">
+                    <TextField source="title" variant="body1" />
+                </RecordField>
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const Label = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack gap={1} sx={{ py: 1, px: 2 }}>
+                <RecordField label="Identifier" source="id" />
+                <RecordField source="title" />
+                <RecordField label="Author name" source="author" />
+                <RecordField label={false} source="summary" />
+                <RecordField source="year" field={NumberField} />
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const DirectionRow = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack gap={1} sx={{ py: 1, px: 2 }} direction="row">
+                <RecordField source="id" />
+                <RecordField source="title" />
+                <RecordField source="author" />
+                <RecordField source="summary" />
+                <RecordField source="year" field={NumberField} />
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const Variant = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Typography gutterBottom>Default label width</Typography>
+            <Stack>
+                <RecordField
+                    variant="inline"
+                    source="id"
+                    TypographyProps={{ sx: { width: 200 } }}
+                />
+                <RecordField variant="inline" source="title" />
+                <RecordField variant="inline" source="author" />
+                <RecordField variant="inline" source="summary" />
+                <RecordField
+                    variant="inline"
+                    source="year"
+                    field={NumberField}
+                />
+            </Stack>
+            <Typography gutterBottom>Custom label width</Typography>
+            <Stack sx={{ '& .RaRecordField-label': { width: 200 } }}>
+                <RecordField variant="inline" source="id" />
+                <RecordField variant="inline" source="title" />
+                <RecordField variant="inline" source="author" />
+                <RecordField variant="inline" source="summary" />
+                <RecordField
+                    variant="inline"
+                    source="year"
+                    field={NumberField}
+                />
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const SX = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack gap={1} sx={{ py: 1, px: 2 }}>
+                <RecordField source="id" sx={{ opacity: 0.5 }} />
+                <RecordField source="title" sx={{ color: 'info.main' }} />
+                <RecordField
+                    source="author"
+                    sx={{ borderLeft: 'solid 2px green', pl: 1 }}
+                />
+                <RecordField source="summary">
+                    <TextField source="summary" sx={{ fontStyle: 'italic' }} />
+                </RecordField>
+                <RecordField
+                    source="year"
+                    field={NumberField}
+                    sx={{ '& .RaRecordField-label': { color: 'red' } }}
+                />
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
+
+export const I18nKey = () => (
+    <I18nContextProvider
+        value={{
+            getLocale: () => 'en',
+            translate: m => m,
+            changeLocale: async () => {},
+        }}
+    >
+        <ResourceContext.Provider value="books">
+            <RecordContextProvider value={record}>
+                <Stack gap={1} sx={{ py: 1, px: 2 }}>
+                    <RecordField source="id" />
+                    <RecordField source="title" />
+                    <RecordField source="author" />
+                    <RecordField source="summary" />
+                    <RecordField source="year" field={NumberField} />
+                </Stack>
+            </RecordContextProvider>
+        </ResourceContext.Provider>
+    </I18nContextProvider>
+);
+
+type Book = {
+    id: string;
+    title: string;
+    author: string;
+    summary: string;
+    year: number;
+};
+
+export const Generic = () => {
+    const Field = RecordField<Book>;
+    return (
+        <ResourceContext.Provider value="books">
+            <RecordContextProvider value={record}>
+                <Stack gap={1} sx={{ py: 1, px: 2 }}>
+                    <Field source="id" />
+                    <Field source="title" />
+                    <Field source="author" />
+                    <Field source="summary" />
+                    <Field source="year" field={NumberField} />
+                </Stack>
+            </RecordContextProvider>
+        </ResourceContext.Provider>
+    );
+};
+
+export const Nested = () => (
+    <ResourceContext.Provider value="books">
+        <RecordContextProvider value={record}>
+            <Stack gap={1} sx={{ py: 1, px: 2 }}>
+                <RecordField source="title" />
+                <RecordField source="summary" />
+                <RecordField label="Others">
+                    <Stack direction="row" sx={{ padding: 0 }}>
+                        <RecordField source="id" sx={{ width: 50 }} />
+                        <RecordField source="author" sx={{ width: 150 }} />
+                        <RecordField source="year" field={NumberField} />
+                    </Stack>
+                </RecordField>
+            </Stack>
+        </RecordContextProvider>
+    </ResourceContext.Provider>
+);
---
+++ packages/ra-ui-materialui/src/field/RecordField.tsx
@@ -0,0 +1,130 @@
+import * as React from 'react';
+import { type ReactNode, type ElementType } from 'react';
+import { styled } from '@mui/material/styles';
+import {
+    Stack,
+    Typography,
+    type StackProps,
+    type SxProps,
+    type TypographyProps,
+} from '@mui/material';
+import {
+    FieldTitle,
+    useRecordContext,
+    useResourceContext,
+    type ExtractRecordPaths,
+    type HintedString,
+    type RaRecord,
+} from 'ra-core';
+import clsx from 'clsx';
+
+import { TextField } from './TextField';
+
+export const RecordField = <
+    RecordType extends Record<string, any> = Record<string, any>,
+>({
+    children,
+    className,
+    field,
+    label,
+    render,
+    source,
+    sx,
+    TypographyProps,
+    variant,
+    ...rest
+}: RecordFieldProps<RecordType>) => {
+    const resource = useResourceContext();
+    const record = useRecordContext<RecordType>();
+    if (!source && !label) return null;
+    return (
+        <Root
+            sx={sx}
+            className={clsx(className, {
+                [RecordFieldClasses.inline]: variant === 'inline',
+            })}
+            {...rest}
+        >
+            {label !== '' && label !== false ? (
+                <Typography
+                    className={RecordFieldClasses.label}
+                    {...TypographyProps}
+                >
+                    <FieldTitle
+                        label={label}
+                        source={source}
+                        resource={resource}
+                        isRequired={false}
+                    />
+                </Typography>
+            ) : null}
+            {children ??
+                (render ? (
+                    record && (
+                        <Typography
+                            component="span"
+                            variant="body2"
+                            className={RecordFieldClasses.value}
+                        >
+                            {render(record)}
+                        </Typography>
+                    )
+                ) : field ? (
+                    React.createElement(field, {
+                        source,
+                        className: RecordFieldClasses.value,
+                    })
+                ) : source ? (
+                    <TextField
+                        source={source}
+                        className={RecordFieldClasses.value}
+                    />
+                ) : null)}
+        </Root>
+    );
+};
+
+export interface RecordFieldProps<
+    RecordType extends Record<string, any> = Record<string, any>,
+> extends StackProps {
+    children?: ReactNode;
+    className?: string;
+    field?: ElementType;
+    label?: ReactNode;
+    render?: (record: RecordType) => React.ReactNode;
+    source?: NoInfer<HintedString<ExtractRecordPaths<RecordType>>>;
+    record?: RaRecord;
+    sx?: SxProps;
+    TypographyProps?: TypographyProps;
+    variant?: 'default' | 'inline';
+}
+
+const PREFIX = 'RaRecordField';
+
+export const RecordFieldClasses = {
+    label: `${PREFIX}-label`,
+    value: `${PREFIX}-value`,
+    inline: `${PREFIX}-inline`,
+};
+
+const Root = styled(Stack, {
+    name: PREFIX,
+    overridesResolver: (props, styles) => styles.root,
+})(({ theme }) => ({
+    [`&.${RecordFieldClasses.inline}`]: {
+        flexDirection: 'row',
+    },
+    [`& .${RecordFieldClasses.label}`]: {
+        fontSize: '0.75em',
+        marginBottom: '0.2em',
+        color: (theme.vars || theme).palette.text.secondary,
+    },
+    [`&.${RecordFieldClasses.inline} .${RecordFieldClasses.label}`]: {
+        fontSize: '0.875rem',
+        display: 'block',
+        minWidth: 150,
+    },
+    [`& .${RecordFieldClasses.value}`]: {
+        flex: 1,
+    },
+}));
