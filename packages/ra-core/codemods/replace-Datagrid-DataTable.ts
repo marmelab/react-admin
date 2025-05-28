0b7d2c591f8a670ad032f53d735e7be5b0a52ec3
erwanMarmelab <erwan@marmelab.com>
2025-05-28 18:28:26 +0200

    codemod -> wrap every fields with a `<DataTable.Col source="xxx">`

--- packages/ra-core/codemods/replace-Datagrid-DataTable.ts
+++ packages/ra-core/codemods/replace-Datagrid-DataTable.ts
@@ -9,8 +9,13 @@ module.exports = (file, api: j.API) => {
         return root.toSource();
     }
 
-    const continueAfterComponent = replaceComponent(root, j);
-    if (!continueAfterComponent) {
+    const continueAfterReplaceDatagrid = replaceDatagrid(root, j);
+    if (!continueAfterReplaceDatagrid) {
+        return root.toSource();
+    }
+
+    const continueAfterWrap = wrapChildren(root, j);
+    if (!continueAfterWrap) {
         return root.toSource();
     }
 
@@ -58,7 +63,7 @@ const replaceImport = (root, j) => {
     return true;
 };
 
-const replaceComponent = (root, j) => {
+const replaceDatagrid = (root, j) => {
     // Find all instances of Datagrid
     const datagridComponents = root.find(j.JSXElement, {
         openingElement: {
@@ -87,4 +92,68 @@ const replaceComponent = (root, j) => {
     });
 
     return true;
+};
+
+const wrapChildren = (root, j) => {
+    // Find all instances of Datagrid
+    const datagridComponents = root.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                type: 'JSXIdentifier',
+                name: 'DataTable',
+            },
+        },
+    });
+    if (!datagridComponents.length) {
+        return false;
+    }
+
+    // For each DataTable component, wrap its children in DataTable.Col
+    datagridComponents.forEach(dataTableComponent => {
+        const children = dataTableComponent.value.children.filter(child =>
+            j.JSXElement.check(child)
+        );
+        children.forEach(child => {
+            wrapChild(root, j, child);
+        });
+    });
+};
+
+const wrapChild = (root, j, child) => {
+    // Wrap the child in a DataTable.Col component
+    const wrappedChild = j.jsxElement(
+        j.jsxOpeningElement(
+            j.jsxIdentifier('DataTable.Col'),
+            [
+                j.jsxAttribute(
+                    j.jsxIdentifier('source'),
+                    j.stringLiteral(
+                        child.openingElement.attributes.find(
+                            attr =>
+                                j.JSXAttribute.check(attr) &&
+                                attr.name.name === 'source'
+                        )?.value?.value || ''
+                    )
+                ),
+            ],
+            false
+        ),
+        j.jsxClosingElement(j.jsxIdentifier('DataTable.Col')),
+        [j.jsxText('\n'), child, j.jsxText('\n')]
+    );
+
+    // Replace the original child with the wrapped child
+    root.find(j.JSXElement, {
+        openingElement: {
+            name: {
+                type: 'JSXIdentifier',
+                name: 'DataTable',
+            },
+        },
+    }).forEach(dataTableComponent => {
+        dataTableComponent.value.children =
+            dataTableComponent.value.children.map(c =>
+                c === child ? wrappedChild : c
+            );
+    });
 };
