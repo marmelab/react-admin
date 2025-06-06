4ddc8f9ae4e85bac4ffa2b1d6427f057196e659e
Matthieu Hochlander <matthieu@marmelab.com>
2025-06-06 15:44:29 +0200

    Customizable text field and add basic story to test it.

---
+++ packages/ra-ui-materialui/src/field/TextField.stories.tsx
@@ -0,0 +1,43 @@
+import { Paper, Stack } from '@mui/material';
+import * as React from 'react';
+
+import { TextField, TextFieldProps } from './TextField';
+import { AdminContext } from '../AdminContext';
+
+export default { title: 'ra-ui-materialui/fields/TextField' };
+
+export const Basic = ({
+    value,
+    theme,
+    ...props
+}: Partial<TextFieldProps> & { value?: string; theme?: 'light' | 'dark' }) => {
+    return (
+        <AdminContext defaultTheme={theme}>
+            <Paper sx={{ p: 2 }}>
+                <Stack direction="row">
+                    <TextField record={{ value }} source="value" {...props} />
+                </Stack>
+            </Paper>
+        </AdminContext>
+    );
+};
+
+Basic.argTypes = {
+    value: {
+        options: ['filled', 'empty', 'undefined'],
+        mapping: {
+            filled: 'test',
+            empty: '',
+            undefined: undefined,
+        },
+        control: { type: 'select' },
+    },
+    theme: {
+        options: ['light', 'dark'],
+        control: { type: 'select' },
+    },
+};
+Basic.args = {
+    theme: 'light',
+    value: 'filled',
+};
