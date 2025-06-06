3a4240d0301f8907b7c27630c61a6a3576fa7444
Matthieu Hochlander <matthieu@marmelab.com>
2025-06-06 15:39:07 +0200

    Customizable select field and improve stories to test it.

--- packages/ra-ui-materialui/src/field/SelectField.stories.tsx
+++ packages/ra-ui-materialui/src/field/SelectField.stories.tsx
@@ -35,9 +35,9 @@ const genderChoices = [
 ];
 
 export const Basic = () => (
-    <RecordContextProvider value={record}>
+    <Wrapper record={record}>
         <SelectField source="gender" choices={genderChoices} />
-    </RecordContextProvider>
+    </Wrapper>
 );
 
 const languages = [
@@ -45,60 +45,67 @@ const languages = [
     { id: 'ar', name: 'Arabic', nativeName: 'العربية' },
 ];
 export const OptionText = () => (
-    <RecordContextProvider value={record}>
+    <Wrapper record={record}>
         <SelectField
             source="language"
             choices={languages}
             optionText="nativeName"
         />
-    </RecordContextProvider>
+    </Wrapper>
 );
 
 const countries = [{ name: 'Arabic', code: 'ar' }];
 export const OptionValue = () => (
-    <RecordContextProvider value={record}>
+    <Wrapper record={record}>
         <SelectField source="language" choices={countries} optionValue="code" />
-    </RecordContextProvider>
+    </Wrapper>
 );
 
 const optionRenderer = choice => `${choice.first_name} ${choice.last_name}`;
 const authors = [{ id: 1, first_name: 'John', last_name: 'Doe' }];
 export const OptionTextFunction = () => (
-    <RecordContextProvider value={{ id: 1 }}>
+    <Wrapper record={{ id: 1 }}>
         <SelectField
             source="id"
             choices={authors}
             optionText={optionRenderer}
         />
-    </RecordContextProvider>
+    </Wrapper>
 );
 
 const translateChoice = [
     { id: 'no results', name: 'ra.navigation.no_results' },
 ];
 export const TranslateChoice = () => (
-    <AdminContext i18nProvider={i18nProvider}>
-        <RecordContextProvider value={record}>
-            <Labeled
-                label="translateChoice={true}"
-                sx={{ border: '1px solid', margin: '1rem', padding: '1rem' }}
-            >
-                <SelectField
-                    source="data"
-                    choices={translateChoice}
-                    // translateChoice={true} is set by default
-                />
-            </Labeled>
-            <Labeled
-                label="translateChoice={false}"
-                sx={{ border: '1px solid', margin: '1rem', padding: '1rem' }}
-            >
-                <SelectField
-                    source="data"
-                    choices={translateChoice}
-                    translateChoice={false}
-                />
-            </Labeled>
-        </RecordContextProvider>
+    <Wrapper record={record}>
+        <Labeled
+            label="translateChoice={true}"
+            sx={{ border: '1px solid', margin: '1rem', padding: '1rem' }}
+        >
+            <SelectField
+                source="data"
+                choices={translateChoice}
+                // translateChoice={true} is set by default
+            />
+        </Labeled>
+        <Labeled
+            label="translateChoice={false}"
+            sx={{ border: '1px solid', margin: '1rem', padding: '1rem' }}
+        >
+            <SelectField
+                source="data"
+                choices={translateChoice}
+                translateChoice={false}
+            />
+        </Labeled>
+    </Wrapper>
+);
+
+const Wrapper = ({ children, record, defaultTheme = 'light' }) => (
+    <AdminContext
+        i18nProvider={i18nProvider}
+        defaultTheme={defaultTheme as any}
+    >
+        <RecordContextProvider value={record}>{children}</RecordContextProvider>
     </AdminContext>
 );
