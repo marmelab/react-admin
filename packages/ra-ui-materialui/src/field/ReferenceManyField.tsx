8046b0e6931848904f382006c94cc66065565b9f
ThieryMichel <thiery@marmelab.com>
2025-07-17 18:25:24 +0200

    move pagination on ReferenceManyField side

--- packages/ra-ui-materialui/src/field/ReferenceManyField.tsx
+++ packages/ra-ui-materialui/src/field/ReferenceManyField.tsx
@@ -74,7 +74,10 @@ export const ReferenceManyField = <
                     props.empty
                 )
             }
-        />
+        >
+            {props.children}
+            {!props.render && props.pagination}
+        </ReferenceManyFieldBase>
     );
 };
 
