b2af17a86c1598c92453380214a93af066618bf1
erwanMarmelab <erwan@marmelab.com>
2025-07-21 15:18:26 +0200

    pass the `crm` example to MSW (with a fake server)

--- examples/crm/src/providers/fakerest/fakeServer.ts
+++ examples/crm/src/providers/fakerest/fakeServer.ts
@@ -1,11 +1,11 @@
-import data from './data';
 import { http } from 'msw';
 import { setupWorker } from 'msw/browser';
-import { getMswHandler } from 'fakerest';
+import { type CollectionItem, getMswHandler } from 'fakerest';
+import generateData from './dataGenerator';
 
 const handler = getMswHandler({
     baseUrl: 'http://localhost:4000',
-    data,
+    data: generateData() as CollectionItem,
 });
 export const worker = setupWorker(http.all(/http:\/\/localhost:4000/, handler));
 
