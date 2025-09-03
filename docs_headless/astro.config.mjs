270dfd36f5c452b3f994611b381172d1f1a9ca35
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-09-03 15:46:47 +0200

    fix missing trailing slash on internal links

--- docs_headless/astro.config.mjs
+++ docs_headless/astro.config.mjs
@@ -132,6 +132,7 @@ export default defineConfig({
                 {
                     base: '/ra-core/',
                     collectionBase: false,
+                    trailingSlash: 'always',
                 },
             ],
         ],
