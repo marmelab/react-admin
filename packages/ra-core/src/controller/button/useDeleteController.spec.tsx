59c7de1a6f73b75dbd6ab36a6bc4016613efb7c0
Gildas <1122076+djhi@users.noreply.github.com>
2025-09-25 17:41:19 +0200

    Add more tests

--- packages/ra-core/src/controller/button/useDeleteController.spec.tsx
+++ packages/ra-core/src/controller/button/useDeleteController.spec.tsx
@@ -52,4 +52,49 @@ describe('useDeleteController', () => {
             })
         );
     });
+    it('should allow to override the record and the resource from closest context providers', async () => {
+        const dataProvider = testDataProvider({
+            delete: jest.fn((ressource, params) => {
+                return Promise.resolve({ data: params?.previousData });
+            }),
+        });
+
+        const MockComponent = () => {
+            const { handleDelete } = useDeleteController({
+                resource: 'comments',
+                record: { id: 2 },
+                mutationMode: 'pessimistic',
+            });
+            return <button onClick={handleDelete}>Delete</button>;
+        };
+
+        render(
+            <TestMemoryRouter>
+                <CoreAdminContext dataProvider={dataProvider}>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <ResourceContextProvider value="posts">
+                                    <RecordContextProvider value={{ id: 1 }}>
+                                        <MockComponent />
+                                    </RecordContextProvider>
+                                </ResourceContextProvider>
+                            }
+                        />
+                    </Routes>
+                </CoreAdminContext>
+            </TestMemoryRouter>
+        );
+
+        const button = await screen.findByText('Delete');
+        fireEvent.click(button);
+
+        await waitFor(() =>
+            expect(dataProvider.delete).toHaveBeenCalledWith('comments', {
+                id: 2,
+                previousData: { id: 2 },
+            })
+        );
+    });
 });
