c33c6f68aa0a28f271c45522d7c37ad39425350b
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-10-29 13:45:24 +0100

    story and test

--- packages/ra-core/src/dataProvider/useUpdate.spec.tsx
+++ packages/ra-core/src/dataProvider/useUpdate.spec.tsx
@@ -31,7 +31,12 @@ import {
     WithMiddlewaresSuccess as WithMiddlewaresSuccessUndoable,
     WithMiddlewaresError as WithMiddlewaresErrorUndoable,
 } from './useUpdate.undoable.stories';
-import { Middleware, MutationMode, Params } from './useUpdate.stories';
+import {
+    Middleware,
+    MutationMode,
+    Params,
+    InvalidateList,
+} from './useUpdate.stories';
 
 describe('useUpdate', () => {
     describe('mutate', () => {
@@ -653,6 +658,18 @@ describe('useUpdate', () => {
                 });
             });
         });
+
+        it('invalidates getList query dataProvider resolves in undoable mode', async () => {
+            render(<InvalidateList mutationMode="undoable" />);
+            fireEvent.change(await screen.findByDisplayValue('Hello'), {
+                target: { value: 'Hello changed' },
+            });
+            fireEvent.click(screen.getByText('Save'));
+            await screen.findByText('resources.posts.notifications.updated');
+            fireEvent.click(screen.getByText('Close'));
+            await screen.findByText(/Hello changed/);
+        });
+
         describe('pessimistic mutation mode', () => {
             it('updates getOne query cache when dataProvider promise resolves', async () => {
                 const queryClient = new QueryClient();
