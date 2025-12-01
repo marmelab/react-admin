f51fb1af5b7a9555cec2eb0bc8dc71d1afa7e92a
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-12-01 18:34:38 +0100

    useSupportCreateSuggestion doc

--- docs_headless/astro.config.mjs
+++ docs_headless/astro.config.mjs
@@ -215,6 +215,7 @@ export default defineConfig({
                         'usechoicescontext',
                         'useinput',
                         'usesourcecontext',
+                        'usesupportcreatesuggestion',
                     ],
                 },
                 {
