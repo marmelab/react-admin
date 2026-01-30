96474ae648d0b2a519f00a7140f676c711c6c730
Debugger-ops <vivek9to5@gmail.com>
2026-01-30 21:05:38 +0530

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    arrowParens: 'avoid',
-    bracketSpacing: true,
-    bracketSameLine: false,
-    jsxSingleQuote: false,
-    printWidth: 80,
-    quoteProps: 'as-needed',
+    arrowParens: 'avoid',        // Avoid parentheses for single-arg arrow functions
+    bracketSpacing: true,        // { foo: bar } instead of {foo: bar}
+    bracketSameLine: false,      // Put > of multi-line JSX tags on its own line
+    jsxSingleQuote: false,       // Use double quotes in JSX
+    printWidth: 80,              // Wrap lines at 80 characters
+    quoteProps: 'as-needed',     // Only quote object properties when required
     rangeStart: 0,
     rangeEnd: Infinity,
-    semi: true,
-    singleQuote: true,
-    tabWidth: 4,
-    trailingComma: 'es5',
-    useTabs: false
-}
+    semi: true,                  // Add semicolons
+    singleQuote: true,           // Use single quotes in JS
+    tabWidth: 4,                 // Indent with 4 spaces
+    trailingComma: 'es5',        // Add trailing commas where valid in ES5
+    useTabs: false               // Use spaces instead of tabs
+};
