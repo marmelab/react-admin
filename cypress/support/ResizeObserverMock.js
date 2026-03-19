b68342f600de49fa73e71d6cc9e764e6d5534328
aarish mansur <aarishmansur@gmail.com>
2026-03-19 15:42:14 +0530

    improve ResizeObserver mock and update tsconfig

--- cypress/support/ResizeObserverMock.js
+++ cypress/support/ResizeObserverMock.js
@@ -3,21 +3,47 @@ class ResizeObserverMock {
         this.callback = callback;
         this.elements = new Set();
         this.handleResize = this.handleResize.bind(this);
+        this._window = null;
+        this._listening = false;
     }
 
     observe(element) {
+        if (!this._window && element && element.ownerDocument) {
+            this._window = element.ownerDocument.defaultView || null;
+        }
+
+        const win =
+            this._window || (typeof window !== 'undefined' ? window : null);
+
+        if (!this.elements) return;
         this.elements.add(element);
+
+        if (!this._listening && win) {
+            win.addEventListener('resize', this.handleResize);
+            this._listening = true;
+        }
+
         this.trigger();
-        window.addEventListener('resize', this.handleResize);
     }
 
     unobserve(element) {
         this.elements.delete(element);
+        const win =
+            this._window || (typeof window !== 'undefined' ? window : null);
+        if (this.elements.size === 0 && this._listening && win) {
+            win.removeEventListener('resize', this.handleResize);
+            this._listening = false;
+        }
     }
 
     disconnect() {
         this.elements.clear();
-        window.removeEventListener('resize', this.handleResize);
+        const win =
+            this._window || (typeof window !== 'undefined' ? window : null);
+        if (this._listening && win) {
+            win.removeEventListener('resize', this.handleResize);
+            this._listening = false;
+        }
     }
 
     handleResize() {
@@ -32,7 +58,16 @@ class ResizeObserverMock {
             contentRect: e.getBoundingClientRect(),
         }));
 
-        requestAnimationFrame(() => {
+        const win =
+            this._window || (typeof window !== 'undefined' ? window : null);
+        const raf =
+            win && typeof win.requestAnimationFrame === 'function'
+                ? win.requestAnimationFrame.bind(win)
+                : typeof requestAnimationFrame === 'function'
+                  ? requestAnimationFrame
+                  : cb => setTimeout(cb, 0);
+
+        raf(() => {
             this.callback(entries, this);
         });
     }
