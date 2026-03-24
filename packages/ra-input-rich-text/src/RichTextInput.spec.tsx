1e7c6156bd8dd4654f5e163e4cb91d8da7e83dca
ThieryMichel <thiery@marmelab.com>
2026-03-24 16:17:54 +0100

    update test

--- packages/ra-input-rich-text/src/RichTextInput.spec.tsx
+++ packages/ra-input-rich-text/src/RichTextInput.spec.tsx
@@ -4,13 +4,13 @@ import { render, waitFor } from '@testing-library/react';
 import { Basic } from './RichTextInput.stories';
 
 describe('<RichTextInput />', () => {
-    it('should update its content when fields value changes', async () => {
+    it('should update its content when fields value changes and add a trailing break to it', async () => {
         const record = { id: 123, body: '<h1>Hello world!</h1>' };
         const { container, rerender } = render(<Basic record={record} />);
 
         await waitFor(() => {
             expect(container.querySelector('.ProseMirror')?.innerHTML).toEqual(
-                '<h1>Hello world!</h1>'
+                '<h1>Hello world!</h1><p><br class="ProseMirror-trailingBreak"></p>'
             );
         });
 
@@ -19,7 +19,7 @@ describe('<RichTextInput />', () => {
 
         await waitFor(() => {
             expect(container.querySelector('.ProseMirror')?.innerHTML).toEqual(
-                '<h1>Goodbye world!</h1>'
+                '<h1>Goodbye world!</h1><p><br class="ProseMirror-trailingBreak"></p>'
             );
         });
     });
