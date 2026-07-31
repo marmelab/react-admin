75258f8c0f74e29dc6780f46ca83bb9b474345ea
erwanMarmelab <erwan@marmelab.com>
2026-07-31 16:30:01 +0200

    fix graphQl demo

--- packages/ra-data-graphql-simple/src/getGqlType.test.ts
+++ packages/ra-data-graphql-simple/src/getGqlType.test.ts
@@ -1,4 +1,10 @@
-import { TypeKind, print } from 'graphql';
+import {
+    buildSchema,
+    getIntrospectionQuery,
+    graphqlSync,
+    print,
+    TypeKind,
+} from 'graphql';
 import { getGqlType } from './getGqlType';
 
 describe('getGqlType', () => {
@@ -105,5 +111,77 @@ describe('getGqlType', () => {
                 })
             )
         ).toEqual('[[ID!]]!');
+    });
+
+    it('returns the arg type for a named type carrying a null ofType', () => {
+        expect(
+            print(
+                getGqlType({
+                    kind: TypeKind.SCALAR,
+                    name: 'foo',
+                    ofType: null,
+                })
+            )
+        ).toEqual('foo');
+    });
+
+    it('returns the arg type for wrapped types carrying a null ofType', () => {
+        expect(
+            print(
+                getGqlType({
+                    kind: TypeKind.NON_NULL,
+                    name: null,
+                    ofType: {
+                        kind: TypeKind.LIST,
+                        name: null,
+                        ofType: {
+                            kind: TypeKind.NON_NULL,
+                            name: null,
+                            ofType: {
+                                kind: TypeKind.SCALAR,
+                                name: 'ID',
+                                ofType: null,
+                            },
+                        },
+                    },
+                })
+            )
+        ).toEqual('[ID!]!');
+    });
+
+    it('prints every argument type of a real introspection result', () => {
+        const schema = buildSchema(`
+            input CommandInput { id: ID }
+            type Command { id: ID! }
+            type Query {
+                command(
+                    id: ID!
+                    tags: [String]
+                    requiredTags: [String!]!
+                    nested: [[Int!]]!
+                    input: CommandInput
+                ): Command
+            }
+        `);
+        const introspection = graphqlSync({
+            schema,
+            source: getIntrospectionQuery(),
+        });
+        const types = (introspection.data as any).__schema.types;
+        const args = types
+            .find(type => type.name === 'Query')
+            .fields.find(field => field.name === 'command').args;
+
+        expect(
+            Object.fromEntries(
+                args.map(arg => [arg.name, print(getGqlType(arg.type))])
+            )
+        ).toEqual({
+            id: 'ID!',
+            tags: '[String]',
+            requiredTags: '[String!]!',
+            nested: '[[Int!]]!',
+            input: 'CommandInput',
+        });
     });
 });
--- packages/ra-data-graphql-simple/src/getGqlType.ts
+++ packages/ra-data-graphql-simple/src/getGqlType.ts
@@ -6,10 +6,15 @@ import {
 } from 'graphql';
 import * as gqlTypes from 'graphql-ast-types-browser';
 
+const isWrappingTypeRef = (
+    type: IntrospectionType | IntrospectionTypeRef
+): type is Extract<IntrospectionTypeRef, { ofType: unknown }> =>
+    type.kind === TypeKind.LIST || type.kind === TypeKind.NON_NULL;
+
 export const getGqlType = (
     type: IntrospectionType | IntrospectionTypeRef
 ): TypeNode => {
-    if ('ofType' in type) {
+    if (isWrappingTypeRef(type)) {
         return type.kind === TypeKind.LIST
             ? gqlTypes.listType(getGqlType(type.ofType))
             : gqlTypes.nonNullType(getGqlType(type.ofType));
