3eaa6747997a96824b7cc9af9cbf4b2890aea6ba
dawn <93917549+dawNotPoi@users.noreply.github.com>
2026-09-01 17:09:18 +0800

    test: use real MUI notification transitions

--- packages/ra-ui-materialui/src/layout/Notification.spec.tsx
+++ packages/ra-ui-materialui/src/layout/Notification.spec.tsx
@@ -1,55 +1,6 @@
 import * as React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-
-let mockMuiMajor = 5;
-
-jest.mock('@mui/material', () => {
-    const actual = jest.requireActual('@mui/material');
-    const ReactModule = jest.requireActual('react') as typeof React;
-    const Mui6Snackbar = ReactModule.forwardRef<any, any>((props, ref) => {
-        const wasOpen = ReactModule.useRef(props.open);
-
-        ReactModule.useEffect(() => {
-            if (wasOpen.current && !props.open) {
-                props.slotProps?.transition?.onExited?.(null);
-                props.TransitionProps?.onExited?.(null);
-            }
-            wasOpen.current = props.open;
-        }, [props.open, props.slotProps, props.TransitionProps]);
-
-        ReactModule.useEffect(() => {
-            if (!props.open) return;
-
-            document.addEventListener('click', props.onClose);
-            return () => document.removeEventListener('click', props.onClose);
-        }, [props.open, props.onClose]);
-
-        if (!props.open) return null;
-
-        return ReactModule.createElement(
-            'div',
-            { ref },
-            props.children ?? props.message,
-            props.action,
-            ReactModule.createElement(
-                'button',
-                { onClick: props.onClose },
-                'Close notification'
-            )
-        );
-    });
-    const Snackbar = ReactModule.forwardRef<any, any>((props, ref) =>
-        mockMuiMajor >= 6
-            ? ReactModule.createElement(Mui6Snackbar, { ...props, ref })
-            : ReactModule.createElement(actual.Snackbar, { ...props, ref })
-    );
-
-    return {
-        ...actual,
-        major: { valueOf: () => mockMuiMajor },
-        Snackbar,
-    };
-});
+import { major as muiMajor } from '@mui/material';
 
 import {
     ConsecutiveNotifications,
@@ -58,31 +9,26 @@ import {
 } from './Notification.stories';
 
 describe('<Notification />', () => {
-    afterEach(() => {
-        mockMuiMajor = 5;
-    });
-
-    it.each([6, 7, 9])(
-        'should confirm an undoable mutation once when the notification exits with MUI %s',
-        async muiMajor => {
-            mockMuiMajor = muiMajor;
+    (muiMajor >= 6 ? it : it.skip)(
+        'should confirm an undoable mutation only once when its notification exits with MUI 6+',
+        async () => {
             const deleteOne = jest
                 .fn()
                 .mockImplementation((_resource, { id }) =>
                     Promise.resolve({ data: { id } })
                 );
             const dataProvider = { delete: deleteOne } as any;
-            render(<ConsecutiveUndoable dataProvider={dataProvider} />);
+            const { container } = render(
+                <ConsecutiveUndoable dataProvider={dataProvider} />
+            );
 
             (await screen.findByText('Delete post 1')).click();
             await screen.findByText('Post 1 deleted');
-
-            fireEvent.click(
-                screen.getByRole('button', { name: 'Close notification' })
-            );
+            fireEvent.click(container);
 
             await waitFor(() => expect(deleteOne).toHaveBeenCalled());
             expect(deleteOne).toHaveBeenCalledTimes(1);
+            expect(deleteOne).toHaveBeenCalledWith('posts', { id: 1 });
         }
     );
 
